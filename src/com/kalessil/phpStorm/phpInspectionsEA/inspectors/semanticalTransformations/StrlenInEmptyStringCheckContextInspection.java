0d671e8d10d141169cc80936c385b3c86f826c51
Vladimir Reznichenko <kalessil@gmail.com>
2014-12-14 16:35:05 +0100

    imports optimized

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalTransformations/StrlenInEmptyStringCheckContextInspection.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalTransformations/StrlenInEmptyStringCheckContextInspection.java
@@ -5,10 +5,11 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.parser.PhpElementTypes;
-import com.jetbrains.php.lang.psi.elements.*;
+import com.jetbrains.php.lang.psi.elements.BinaryExpression;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.PhpExpression;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
-import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class StrlenInEmptyStringCheckContextInspection extends BasePhpInspection {

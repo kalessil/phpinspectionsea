9bfb6cf829e7dffbb67989293935c560bdb226ab
Vladimir Reznichenko <kalessil@gmail.com>
2014-12-24 10:05:24 +0100

    ForeachSourceInspector - worflow changed

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/ForeachSourceInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/ForeachSourceInspector.java
@@ -13,12 +13,15 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 public class ForeachSourceInspector extends BasePhpInspection {
-    private static final String strProblemResolvingClassSlotType = "Could not resolve this source type, method annotations needed";
-    private static final String strProblemResolvingParameterType = "Could not resolve this source type, parameter annotations needed";
-    private static final String strProblemResolvingClassSlot = "Could not resolve this source type, annotations needed";
-    private static final String strProblemDescription = "This loop does not loop";
+    private static final String strProblemResolvingMixed = "Could not resolve this source type, specify possible types instead of mixed";
+    private static final String strProblemResolvingArrayItemType = "Could not resolve this source type, array item type annotation needed";
+    private static final String strProblemResolvingClassSlotType = "Could not resolve this source type, method or property type annotation needed";
+    private static final String strProblemResolvingParameterType = "Could not resolve this source type, parameter type annotation needed";
+    private static final String strProblemResolvingClassSlot = "Could not resolve this source type, expression  type annotation needed";
+    private static final String strProblemDescription = "This source type can not be iterated: given ";
 
     @NotNull
     public String getDisplayName() {
@@ -44,156 +47,156 @@ public class ForeachSourceInspector extends BasePhpInspection {
                     return;
                 }
 
-                String[] arrTypes = null;
+                LinkedList<String> listSignatureTypes = new LinkedList<>();
                 if (objSource instanceof Variable) {
-                    arrTypes = this.lookupType(((Variable) objSource).getSignature(), objSource);
+                    this.lookupType(((Variable) objSource).getSignature(), objSource, listSignatureTypes);
                 }
                 if (objSource instanceof FieldReference) {
-                    arrTypes = this.lookupType(((FieldReference) objSource).getSignature(), objSource);
+                    this.lookupType(((FieldReference) objSource).getSignature(), objSource, listSignatureTypes);
                 }
                 if (objSource instanceof MethodReference) {
-                    arrTypes = this.lookupType(((MethodReference) objSource).getSignature(), objSource);
+                    this.lookupType(((MethodReference) objSource).getSignature(), objSource, listSignatureTypes);
                 }
                 if (objSource instanceof FunctionReference && !(objSource instanceof MethodReference)) {
-                    arrTypes = this.lookupType(((FunctionReference) objSource).getSignature(), objSource);
+                    this.lookupType(((FunctionReference) objSource).getSignature(), objSource, listSignatureTypes);
                 }
 
-                if (null != arrTypes) {
+                this.analyseTypesProvided(objSource, listSignatureTypes);
+
+                final boolean isProcessed = this.analyseTypesProvided(objSource, listSignatureTypes);
+                if (!isProcessed) {
                     /** debug warnings */
                     String strTypes = "";
                     String strGlue = "";
-                    for (String strType : arrTypes) {
-                        if (strType.equals("")) {
-                            continue;
-                        }
-
+                    for (String strType : listSignatureTypes) {
                         strTypes += strGlue + strType;
                         strGlue = "|";
                     }
 
-                    /** fits for top-level analysis, except it's still strings manipulation */
-                    if (strTypes.equals("array")) {
-                        return;
-                    }
-
                     holder.registerProblem(objSource, "types extracted: " + strTypes, ProblemHighlightType.WEAK_WARNING);
                 }
+                listSignatureTypes.clear();
             }
 
-            @Nullable
-            private String[] lookupType (String strSignature, PsiElement objTargetExpression) {
+
+            private boolean analyseTypesProvided(PsiElement objTargetExpression, LinkedList<String> listSignatureTypes) {
+                if (listSignatureTypes.size() == 0) {
+                    return false;
+                }
+
+                if (listSignatureTypes.size() == 1) {
+                    String strType = listSignatureTypes.get(0);
+                    if (strType.equals("\\array")) {
+                        return true;
+                    }
+
+                    if (strType.equals("\\string") || strType.equals("\\null") || strType.equals("\\bool")) {
+                        holder.registerProblem(objTargetExpression, strProblemDescription + strType, ProblemHighlightType.ERROR);
+                        return true;
+                    }
+
+                    if (strType.equals("\\mixed")) {
+                        holder.registerProblem(objTargetExpression, strProblemResolvingMixed, ProblemHighlightType.WEAK_WARNING);
+                        return true;
+                    }
+                }
+
+                /** poly - variant analysis here*/
+                return false;
+            }
+
+            private void lookupType (String strSignature, PsiElement objTargetExpression, LinkedList<String> listSignatureTypes) {
+                /** re-dispatch or terminate lookup */
                 if (null == strSignature || strSignature.equals("")) {
-                    return null;
+                    return;
+                }
+                if (strSignature.contains("|")) {
+                    for (String strSignaturePart : strSignature.split("\\|")) {
+                        this.lookupType(strSignaturePart, objTargetExpression, listSignatureTypes);
+                    }
+                    return;
                 }
 
+                /** === early returns=== **/
                 /** skip looking up into variable assignment / function */
-                if (
-                    strSignature.startsWith("#V") ||
-                    strSignature.startsWith("#F")
-                ) {
+                if (strSignature.startsWith("#V") || strSignature.startsWith("#F")) {
                     /** TODO: lookup assignments for types extraction, pipe check */
-                    return null;
+                    return;
                 }
-
                 /** problem referenced to arguments */
                 if (strSignature.startsWith("#A")) {
-                    holder.registerProblem(objTargetExpression, strProblemResolvingParameterType, ProblemHighlightType.ERROR);
-                    return null;
+                    holder.registerProblem(objTargetExpression, strProblemResolvingParameterType, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                    return;
                 }
-
-                /** lookup base types */
+                /** problem referenced to array item */
+                if (strSignature.startsWith("#E")) {
+                    holder.registerProblem(objTargetExpression, strProblemResolvingArrayItemType, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                    return;
+                }
+                /** lookup core types and classes */
                 if (strSignature.startsWith("#C")) {
-                    if (strSignature.contains("|")) {
-                        /** TODO: remove error logging */
-                        holder.registerProblem(objTargetExpression, "pipe in type: " + strSignature, ProblemHighlightType.ERROR);
-                        return null;
-                    }
-                    /** classes, simple types */
-                    if (!strSignature.matches(".+\\\\(array|\\w+\\[\\])")) {
-                        return new String[] { strSignature.replace("#C\\", "") };
-                    }
-
-                    return new String[] { "array" };
+                    String strTypeExtracted = strSignature.replace("#C", "");
+                    listSignatureTypes.add(strTypeExtracted);
+                    return;
+                }
+                /** problem referenced to calls resolving */
+                if (strSignature.contains("?")) {
+                    holder.registerProblem(objTargetExpression, strProblemResolvingClassSlotType, ProblemHighlightType.ERROR);
+                    return;
                 }
 
+
+                /** === more complex analysis - classes are here === */
                 /** lookup for property or method */
                 final boolean isProperty = strSignature.startsWith("#P");
                 final boolean isMethod = strSignature.startsWith("#M");
                 if (isProperty || isMethod) {
-                    if (strSignature.contains("|")) {
-                        /** TODO: remove error logging */
-                        holder.registerProblem(objTargetExpression, "pipe in slot: " + strSignature, ProblemHighlightType.ERROR);
-                        return null;
-                    }
-
-                    String[] arrParts = strSignature.split("#C\\\\");
+                    /** check signature structure */
+                    String[] arrParts = strSignature.split("#C");
                     if (arrParts.length != 2) {
-                        /** lookup failed - eg reflection */
+                        /** lookup failed - eg not recognizable behind reflection */
                         holder.registerProblem(objTargetExpression, strProblemResolvingClassSlot, ProblemHighlightType.ERROR);
-                        return null;
+                        return;
                     }
-
                     arrParts = arrParts[1].split("\\.");
                     if (arrParts.length != 2) {
                         /** TODO: resolve chain type */
                         //holder.registerProblem(objTargetExpression, "2+ call chain: " + strSignature, ProblemHighlightType.ERROR);
-                        return null;
+                        return;
                     }
 
                     String strClassName = arrParts[0];
                     String strSlotName  = arrParts[1];
+
                     String strAllTypes = "";
-
                     Collection<PhpClass> objClasses = PhpIndex.getInstance(holder.getProject()).getClassesByName(strClassName);
-                    /** class is not resolved */
-                    if (objClasses.size() == 0) {
-                        return null;
-                    }
-
-                    boolean isSlotResolved = false;
-                    /** resolve the slot */
-                    for (PhpClass objClass : objClasses) {
-                        if (isMethod) {
-                            for (Method objMethod : objClass.getMethods()) {
-                                if (objMethod.getName().equals(strSlotName)) {
-                                    isSlotResolved = true;
-                                    strAllTypes += "|" + objMethod.getType().toStringRelativized("\\");
+                    /** resolve the slot in known classes */
+                    if (objClasses.size() > 0) {
+                        for (PhpClass objClass : objClasses) {
+                            if (isMethod) {
+                                for (Method objMethod : objClass.getMethods()) {
+                                    if (objMethod.getName().equals(strSlotName)) {
+                                        strAllTypes += "|" + objMethod.getType().toString();
+                                    }
                                 }
                             }
-                        }
 
-                        if (isProperty) {
-                            for (Field objField : objClass.getFields()) {
-                                if (objField.getName().equals(strSlotName)) {
-                                    isSlotResolved = true;
-                                    strAllTypes += "|" + objField.getType().toStringRelativized("\\");
+                            if (isProperty) {
+                                for (Field objField : objClass.getFields()) {
+                                    if (objField.getName().equals(strSlotName)) {
+                                        strAllTypes += "|" + objField.getType().toString();
+                                    }
                                 }
                             }
                         }
                     }
-                    /** slot is not resolved */
-                    if (!isSlotResolved) {
-                        return null;
-                    }
 
-                    if(strAllTypes.contains("?")) {
-                        holder.registerProblem(objTargetExpression, strProblemResolvingClassSlotType, ProblemHighlightType.ERROR);
-                        return null;
-                    }
-
-                    /** typeOrClass[] => array */
-                    String[] arrSlotTypes = strAllTypes.split("\\|");
-                    for (int i = 0; i < arrSlotTypes.length; ++i) {
-                        if (arrSlotTypes[i].contains("[]")) {
-                            arrSlotTypes[i] = "array";
-                        }
-                    }
-                    return arrSlotTypes;
+                    this.lookupType(strAllTypes, objTargetExpression, listSignatureTypes);
+                    return;
                 }
 
                 /** TODO: remove error logging */
                 holder.registerProblem(objTargetExpression, "not handled: " + strSignature, ProblemHighlightType.ERROR);
-                return null;
             }
         };
     }

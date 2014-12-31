bff9f12f5390d5bd66a186627c90de7174f886a2
Vladimir Reznichenko <kalessil@gmail.com>
2014-12-31 17:08:07 +0100

    added todos

--- src/com/kalessil/phpStorm/phpInspectionsEA/PhpInspectionsEAProvider.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/PhpInspectionsEAProvider.java
@@ -21,9 +21,14 @@ import com.kalessil.phpStorm.phpInspectionsEA.inspectors.strictInterfaces.ArrayT
 ===TODO===:
 
 NotOptimalIfConditions
-        - try resolving if a variable is a binary operation, then estimate costs there as well =>
-                - stick to binary operations as well for order lookup
         - try resolving binary operations with constants and lookup types of both arguments
+        - method/function references with bool result should be explicitly compared with bool
+        - null comparison && instanceof: null comparison not needed (check if instanceof, take argument,
+                find if used with !is_null, !== null - no sense or logical error)
+
+isset inspection: review
+        - isset($...[<concatenation here>])
+        - do not spam when it's array access (se la vie)
 
 ===Cases of interest===
 
@@ -32,8 +37,6 @@ condition: test if null and test type(s)/instance(s)of or test several types|ins
 Chain calls, where one of cell can return not an object, producing php fatal
 
 A extends B,C, B extends C - interface contracts or lack of structure knowledge?
-
-Binary comparison operations: boolean vs expression returning boolean
 
 Migration to Reflection API (ReflectionClass):
         constant, is_a, method_exists, is_subclass_of are from PHP 4 world

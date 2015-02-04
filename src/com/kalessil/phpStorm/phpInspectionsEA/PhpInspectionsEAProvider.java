35d4343b37a5c1f55600186b178de8db35f0be18
Vladimir Reznichenko <kalessil@gmail.com>
2015-02-04 21:16:58 +0100

    updated todo

--- src/com/kalessil/phpStorm/phpInspectionsEA/PhpInspectionsEAProvider.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/PhpInspectionsEAProvider.java
@@ -6,14 +6,14 @@ import com.intellij.codeInspection.InspectionToolProvider;
 
 ===TODO===:
 
+array_search in in_array context:
+        binary operation with false
+
+Method/Property definition:
+        always with access modifier
+
 $this->staticMemberOrFunction call
         shall be static::staticMemberOrFunction
-
-AlterInForeachInspector (inspect assignment expressions):
-        left is array access
-        wrapped within foreach
-            - index is foreach key
-            - container is foreach source
 
 AmbiguousMethodsCallsInArrayMappingInspector:
         foreach ($content['multifiles'] as $multifile) {
@@ -29,12 +29,6 @@ SlowArrayOperationsInLoopInspector:
 ===POOL===
 
 Confusing construct: BO ? bool|BO : BO|bool
-
-Confusing construct: Unnecessary 'return/continue' statement
-
-Confusing construct: IfReturnReturnSimplificationInspector:
-        ifReturnElseReturn
-        ifReturn[,ElseIfReturn]{1,}ElseReturn - possibly some branches can be merged
 
 PHP 5 migration: reflection API usage (ReflectionClass):
         constant, is_a, method_exists, property_exists, is_subclass_of are from PHP 4 world

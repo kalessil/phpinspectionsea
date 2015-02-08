941c656e29d6ac1bd7ca9ed823ac8d8d7ac2ff31
Vladimir Reznichenko <kalessil@gmail.com>
2015-02-08 15:26:40 +0100

    StaticInvocationViaThisInspector - added comments

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/StaticInvocationViaThisInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/StaticInvocationViaThisInspector.java
@@ -12,22 +12,24 @@ import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import org.jetbrains.annotations.NotNull;
 
 public class StaticInvocationViaThisInspector extends BasePhpInspection {
-    private static final String strProblemDescription = "'static::...' or 'self::...' shall be used instead";
+    private static final String strProblemDescription = "'static::...' shall be used instead";
 
     @Override
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
+            /** Static fields will be simply not resolved properly, so we can not do checking for them */
 
-            /** TODO: properties */
             public void visitPhpMethodReference(MethodReference reference) {
                 PsiReference objReference = reference.getReference();
                 if (null != objReference) {
                     PsiElement objResolvedRef = objReference.resolve();
+                    /** resolved method is static but called with $ this*/
                     if (
-                            objResolvedRef instanceof Method &&
-                            ((Method) objResolvedRef).isStatic() &&
-                            reference.getFirstChild().getText().equals("$this")
+                        objResolvedRef instanceof Method &&
+                        ((Method) objResolvedRef).isStatic() &&
+                        reference.getFirstChild().getText().equals("$this")
                     ) {
+                        /** report an issue */
                         holder.registerProblem(reference.getFirstChild(), strProblemDescription, ProblemHighlightType.ERROR);
                     }
                 }

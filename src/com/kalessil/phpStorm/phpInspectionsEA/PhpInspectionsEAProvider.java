01a4ecbc55798dd75290ddea690f46c4f89f365c
Vladimir Reznichenko <reznichenko@beontra.com>
2015-02-09 09:33:04 +0100

    added cases of interest

--- src/com/kalessil/phpStorm/phpInspectionsEA/PhpInspectionsEAProvider.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/PhpInspectionsEAProvider.java
@@ -9,12 +9,21 @@ import com.intellij.codeInspection.InspectionToolProvider;
 AdditionOperationOnArraysInspection:
     - re-implement to check any of binary/mathematical operations has been applied on an array
 
-===POOL===
+Throwing if \Exception
+    shall not happen - there are more specific SPL exceptions - weak warning level (confusing constructs)
 
-Confusing construct: BO ? bool|BO : BO|bool
+'$... (+=|-=) 1', '$... = $... (+|-) 1'
+    shall be refactored as (--|++)$... - weak warning level (code style)
+
+===POOL===
 
 'For' loops:
     use foreach instead
+
+Magic numbers:
+    needs additional research here
+
+Confusing construct: BO ? bool|BO : BO|bool
 
 PHP 5 migration: reflection API usage (ReflectionClass):
         constant, is_a, method_exists, property_exists, is_subclass_of are from PHP 4 world

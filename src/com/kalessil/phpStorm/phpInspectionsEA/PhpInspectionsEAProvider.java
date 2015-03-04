9895399c02a0cc5a794d164870d2ac0c4afb3558
Vladimir Reznichenko <kalessil@gmail.com>
2015-03-04 18:22:27 +0100

    ForeachOnArrayComponentsInspector: added

--- src/com/kalessil/phpStorm/phpInspectionsEA/PhpInspectionsEAProvider.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/PhpInspectionsEAProvider.java
@@ -6,10 +6,6 @@ import com.intellij.codeInspection.InspectionToolProvider;
 
 ===TODO===:
 
-foreach (array_keys|array_values(...) as $keyWithoutValue)
-    - Performance: array_keys, array_values used as foreach array
-    - Probably classic foreach construction shall be used (can trigger unused variable warning)
-
 AdditionOperationOnArraysInspection:
     - re-implement to check any of binary/mathematical operations has been applied on an array
 

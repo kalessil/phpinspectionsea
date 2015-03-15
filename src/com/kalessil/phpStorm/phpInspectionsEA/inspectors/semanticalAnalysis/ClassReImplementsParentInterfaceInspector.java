0a14136fc6e2fb566d970b75e3fcd09c3ada4e7a
Vladimir Reznichenko <kalessil@gmail.com>
2015-03-15 08:32:43 +0100

    ClassReImplementsParentInterfaceInspector: specify which interface and where was announced

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/ClassReImplementsParentInterfaceInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/ClassReImplementsParentInterfaceInspector.java
@@ -12,7 +12,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class ClassReImplementsParentInterfaceInspector extends BasePhpInspection {
-    private static final String strProblemDescription = "Interface %s% is already defined in a parent class.";
+    private static final String strProblemDescription = "%i% is already announced in %c%.";
 
     @NotNull
     public String getShortName() {
@@ -25,30 +25,41 @@ public class ClassReImplementsParentInterfaceInspector extends BasePhpInspection
             public void visitPhpClass(PhpClass clazz) {
                 /** check if parent class and own implements are available */
                 List<ClassReference> listImplements = clazz.getImplementsList().getReferenceElements();
-                if (null == listImplements || listImplements.size() == 0) {
-                    return;
-                }
-                PhpClass objParentClass = clazz.getSuperClass();
-                if (null == objParentClass || null == clazz.getNameIdentifier()) {
+                if (null == listImplements || listImplements.size() == 0 || null == clazz.getNameIdentifier()) {
                     return;
                 }
 
-                for (PhpClass parentInterface : objParentClass.getImplementedInterfaces()) {
-                    String parentInterfaceFQN = parentInterface.getFQN();
-                    if (null == parentInterfaceFQN) {
+                for (PhpClass objParentClass : clazz.getSupers()) {
+                    /* ensure not interface, implements some interfaces and discoverable properly */
+                    List<ClassReference> listImplementsParents = objParentClass.getImplementsList().getReferenceElements();
+                    String strParentFQN = objParentClass.getFQN();
+                    if (
+                        null == strParentFQN ||
+                        null == listImplementsParents || listImplementsParents.size() == 0 ||
+                        objParentClass.isInterface()
+                    ) {
                         continue;
                     }
 
-                    for (ClassReference ownInterface : listImplements) {
-                        String ownInterfaceFQN = ownInterface.getFQN();
-                        if (null == ownInterfaceFQN) {
+                    /* check interfaces of a parent class */
+                    for (ClassReference parentInterfaceRef : listImplementsParents) {
+                        String parentInterfaceFQN = parentInterfaceRef.getFQN();
+                        String parentInterfaceName = parentInterfaceRef.getName();
+                        if (null == parentInterfaceFQN || null == parentInterfaceName) {
                             continue;
                         }
 
-                        if (ownInterfaceFQN.equals(parentInterfaceFQN)) {
-                            String strWarning = strProblemDescription.replace("%s%", ownInterfaceFQN);
-                            holder.registerProblem(clazz.getNameIdentifier(), strWarning, ProblemHighlightType.WEAK_WARNING);
-                            break;
+                        /* match parents interfaces against class we checking */
+                        for (ClassReference ownInterface : listImplements) {
+                            /* ensure FQNs matches */
+                            String ownInterfaceFQN = ownInterface.getFQN();
+                            if (null != ownInterfaceFQN && ownInterfaceFQN.equals(parentInterfaceFQN)) {
+                                String strWarning = strProblemDescription
+                                        .replace("%i%", parentInterfaceName)
+                                        .replace("%c%", strParentFQN);
+                                holder.registerProblem(clazz.getNameIdentifier(), strWarning, ProblemHighlightType.WEAK_WARNING);
+                                break;
+                            }
                         }
                     }
                 }

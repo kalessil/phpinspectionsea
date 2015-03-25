22f9e29b51d358e958dbdf9e1dbae58d2d8c1a68
Vladimir Reznichenko <kalessil@gmail.com>
2015-03-25 20:14:44 +0100

    TypeUnsafeArraySearchInspection: when 1st parameter is string show different message (fixed #29)

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/TypeUnsafeArraySearchInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/TypeUnsafeArraySearchInspector.java
@@ -5,6 +5,7 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import org.jetbrains.annotations.NotNull;
@@ -12,7 +13,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashSet;
 
 public class TypeUnsafeArraySearchInspector extends BasePhpInspection {
-    private static final String strProblemDescription = "Third parameter shall be provided to clarify if types safety important in this context";
+    private static final String strProblemDescription      = "Third parameter shall be provided to clarify if types safety important in this context";
+    private static final String strProblemSafeToMakeStrict = "Safely place true as 3rd parameter (strict mode)";
 
     @NotNull
     public String getShortName() {
@@ -42,7 +44,12 @@ public class TypeUnsafeArraySearchInspector extends BasePhpInspection {
                 }
 
                 if (getFunctionsSet().contains(strFunction)) {
-                    holder.registerProblem(reference, strProblemDescription, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                    String strMessage = strProblemDescription;
+                    if (reference.getParameters()[0] instanceof StringLiteralExpression) {
+                        strMessage = strProblemSafeToMakeStrict;
+                    }
+
+                    holder.registerProblem(reference, strMessage, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
                 }
             }
         };

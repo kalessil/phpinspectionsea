da03bf37ee6f8153d702f1d55036ec44f7f4f361
Vladimir Reznichenko <kalessil@gmail.com>
2015-03-27 19:50:53 +0100

    DefaultValueInElseBranchInspector: fixed false positives on array element addition (fixed #35), warning level lowered to weak warning

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalTransformations/DefaultValueInElseBranchInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalTransformations/DefaultValueInElseBranchInspector.java
@@ -28,14 +28,14 @@ public class DefaultValueInElseBranchInspector extends BasePhpInspection {
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
             public void visitPhpIf(If ifStatement) {
-                /** skip ifs without else */
+                /* skip ifs without else */
                 Else objElseStatement = ifStatement.getElseBranch();
                 if (null == objElseStatement) {
                     return;
                 }
 
 
-                /** collect all group statement for further analysis */
+                /* collect all group statement for further analysis */
                 LinkedList<GroupStatement> objGroupStatementsList = new LinkedList<GroupStatement>();
                 objGroupStatementsList.add(ExpressionSemanticUtil.getGroupStatement(ifStatement));
                 for (ControlStatement objElseIf : ifStatement.getElseIfBranches()) {
@@ -44,21 +44,26 @@ public class DefaultValueInElseBranchInspector extends BasePhpInspection {
                 objGroupStatementsList.add(ExpressionSemanticUtil.getGroupStatement(objElseStatement));
 
 
-                /** collect assignments or stop inspecting when structure expectations are not met */
+                /* collect assignments or stop inspecting when structure expectations are not met */
                 LinkedList<AssignmentExpression> objAssignmentsList = new LinkedList<AssignmentExpression>();
                 for (GroupStatement objGroup : objGroupStatementsList) {
-                    if (null == objGroup || ExpressionSemanticUtil.countExpressionsInGroup(objGroup) != 1) {
+                    /* only one expression in group statement */
+                    if (null == objGroup || 1 != ExpressionSemanticUtil.countExpressionsInGroup(objGroup)) {
                         objGroupStatementsList.clear();
                         return;
                     }
 
+                    /* find assignments, take comments in account */
                     AssignmentExpression objAssignmentExpression = null;
                     for (PsiElement objIfChild : objGroup.getChildren()) {
-                        if (
-                            objIfChild instanceof Statement &&
-                            objIfChild.getFirstChild() instanceof AssignmentExpression
-                        ) {
-                            objAssignmentExpression = (AssignmentExpression) objIfChild.getFirstChild();
+                        /* assignment expression check */
+                        if (objIfChild instanceof Statement && objIfChild.getFirstChild() instanceof AssignmentExpression) {
+                            AssignmentExpression branchAssignment  = (AssignmentExpression) objIfChild.getFirstChild();
+                            PhpPsiElement branchAssignmentVariable = branchAssignment.getVariable();
+                            /* target value is variable/property */
+                            if (branchAssignmentVariable instanceof Variable || branchAssignmentVariable instanceof FieldReference) {
+                                objAssignmentExpression = branchAssignment;
+                            }
                             break;
                         }
                     }
@@ -72,32 +77,34 @@ public class DefaultValueInElseBranchInspector extends BasePhpInspection {
                 objGroupStatementsList.clear();
 
 
-                /** ensure all assignments has one subject */
+                /* ensure all assignments has one subject, define the one to compare with */
                 PhpPsiElement objSubjectToCompareWith = objAssignmentsList.peekFirst().getVariable();
                 if (null == objSubjectToCompareWith) {
                     objAssignmentsList.clear();
                     return;
                 }
-
+                /* now check all assignments against subject identity */
                 PhpPsiElement objSubjectFromExpression;
                 for (AssignmentExpression objSubjectAssignmentExpression : objAssignmentsList) {
+                    /* ensure target variable is discoverable */
                     objSubjectFromExpression = objSubjectAssignmentExpression.getVariable();
                     if (null == objSubjectFromExpression) {
                         objAssignmentsList.clear();
                         return;
                     }
 
-                    /** check assignment type */
+                    /* check assignment type, work with siblings due to lack of API */
                     PsiElement objOperation = objSubjectFromExpression.getNextSibling();
                     if (objOperation instanceof PsiWhiteSpace) {
                         objOperation = objOperation.getNextSibling();
                     }
-                    if (objOperation.getNode().getElementType() != PhpTokenTypes.opASGN) {
+                    /* assignment operator check */
+                    if (PhpTokenTypes.opASGN != objOperation.getNode().getElementType()) {
                         objAssignmentsList.clear();
                         return;
                     }
 
-                    /** ensure subjects matched */
+                    /** ensure target variables matches */
                     if (!PsiEquivalenceUtil.areElementsEquivalent(objSubjectToCompareWith, objSubjectFromExpression)) {
                         objAssignmentsList.clear();
                         return;
@@ -105,21 +112,17 @@ public class DefaultValueInElseBranchInspector extends BasePhpInspection {
                 }
 
 
-                /** verify candidate value: array/string/number/constant */
+                /* verify candidate value: array/string/number/constant */
                 PhpPsiElement objCandidate = objAssignmentsList.getLast().getValue();
                 objAssignmentsList.clear();
                 if (this.isDefaultValueCandidateFits(objCandidate)) {
-                    /** point the problem out */
-                    holder.registerProblem(objElseStatement.getFirstChild(), strProblemDescription, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                    /* point the problem out */
+                    holder.registerProblem(objElseStatement.getFirstChild(), strProblemDescription, ProblemHighlightType.WEAK_WARNING);
                 }
             }
 
-            /**
-             * @param objCandidate to check
-             * @return boolean
-             */
             private boolean isDefaultValueCandidateFits(PhpPsiElement objCandidate) {
-                /** quick check on expression type basis*/
+                /* quick check on expression type basis */
                 if (
                     objCandidate instanceof StringLiteralExpression ||
                     objCandidate instanceof ArrayCreationExpression ||
@@ -129,12 +132,9 @@ public class DefaultValueInElseBranchInspector extends BasePhpInspection {
                     return true;
                 }
 
-                /** numbers check needs more detailed inspection */
+                /* numbers check needs additional checks */
                 //noinspection RedundantIfStatement
-                if (
-                    objCandidate instanceof PhpExpression &&
-                    objCandidate.getNode().getElementType() == PhpElementTypes.NUMBER
-                ) {
+                if (objCandidate instanceof PhpExpression && PhpElementTypes.NUMBER == objCandidate.getNode().getElementType()) {
                     return true;
                 }
 

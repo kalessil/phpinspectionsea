ec6b488e42717454bccbd037fe20c51c22572808
Vladimir Reznichenko <kalessil@gmail.com>
2015-04-06 20:25:23 +0200

    AlterInForeachInspector: use-cases updated

--- php/alter-in-foreach.php
+++ php/alter-in-foreach.php
@@ -6,5 +6,9 @@
         foreach ($source as & $id => $element) {
             $source[$id] = $element + 1;
         }
+
+        foreach ($source as & $element) {
+            ++$element;
+        }
     }
 

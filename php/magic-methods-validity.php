90e254f6b1d49227931660a7532ad55fa6a5530a
Vladimir Reznichenko <kalessil@gmail.com>
2015-04-10 18:40:25 +0200

    PrototypeValidityInspector: updating use-cases

--- php/magic-methods-validity.php
+++ php/magic-methods-validity.php
@@ -5,18 +5,22 @@
         static function __destruct(&$arg1, &$arg2, &$arg3);
     }
 
-    class classWithMagicMethods {
+    /** @noinspection SingletonFactoryPatternViolationInspection */
+    class classWithMagicMethods implements classContract {
+        /** @noinspection PhpHierarchyChecksInspection */
         /* name (1) and return (1) are highlighted */
         static protected function __construct(&$arg1, &$arg2, &$arg3)
         {
             return '';
         }
 
+        /** @noinspection PhpHierarchyChecksInspection */
         /* name (2) and return (1) are highlighted */
         static protected function __clone(&$arg1, &$arg2, &$arg3)
         {
             return '';
         }
+        /** @noinspection PhpHierarchyChecksInspection */
         /* name (2) and return (1) are highlighted */
         static protected function __destruct(&$arg1, &$arg2, &$arg3)
         {

cc4e2b4aba930069758edf4943e9846c2c7cc798
Vladimir Reznichenko <kalessil@gmail.com>
2015-05-25 20:40:42 +0200

    NotOptimaRegularExpressionsInspector: WIP, finally can continue

---
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regualrExpressions/NotOptimaRegularExpressionsInspector.java
@@ -0,0 +1,121 @@
+package com.kalessil.phpStorm.phpInspectionsEA.inspectors.regualrExpressions;
+
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.regualrExpressions.modifiersStrategy.AllowedModifierCheckStrategy;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.regualrExpressions.modifiersStrategy.DeprecatedModifiersCheckStrategy;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NotOptimaRegularExpressionsInspector extends BasePhpInspection {
+
+    @NotNull
+    public String getShortName() {
+        return "NotOptimaRegularExpressionsInspection";
+    }
+
+    private static HashSet<String> functions = null;
+    private static HashSet<String> getFunctions() {
+        if (null == functions) {
+            functions = new HashSet<String>();
+
+            functions.add("preg_filter");
+            functions.add("preg_grep");
+            functions.add("preg_match_all");
+            functions.add("preg_match");
+            functions.add("preg_replace_callback");
+            functions.add("preg_replace");
+            functions.add("preg_split");
+        }
+
+        return functions;
+    }
+
+    static private Pattern regexWithModifiers = null;
+    static {
+        regexWithModifiers = Pattern.compile("^\\/(.*)\\/([a-zA-Z]+)?$");
+    }
+
+    @Override
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new BasePhpElementVisitor() {
+            public void visitPhpFunctionCall(FunctionReference reference) {
+                final String strFunctionName = reference.getName();
+                if (StringUtil.isEmpty(strFunctionName) || !getFunctions().contains(strFunctionName)) {
+                    return;
+                }
+
+                PsiElement[] params = reference.getParameters();
+                if (params.length < 2 || !(params[0] instanceof StringLiteralExpression)) {
+                    return;
+                }
+
+                String regex = ((StringLiteralExpression) params[0]).getContents();
+                if (!StringUtil.isEmpty(regex)) {
+                    Matcher regexMatcher = regexWithModifiers.matcher(regex);
+                    if (regexMatcher.find()) {
+                        String phpRegexPattern   = regexMatcher.group(1);
+                        String phpRegexModifiers = regexMatcher.group(2);
+
+                        checkCall(strFunctionName, (StringLiteralExpression) params[0], phpRegexPattern, phpRegexModifiers);
+                    }
+                }
+            }
+
+            private void checkCall (String strFunctionName, StringLiteralExpression target, String regex, String modifiers) {
+                /**
+                 * /no-az-chars/i => /no-az-chars/
+                 * /no-dot-.-char/s => /no-dot-.-char/
+                 * /no-^-or-$/m => /no-^-or-$/
+                 * /no-^-or-$/D => /no-^-or-$/
+                 *
+                 * +/regexp/e => mark as deprecated, use preg_replace_callback instead
+                 * +Check allowed PHP modifiers: eimsuxADJSUX
+                 */
+                AllowedModifierCheckStrategy.apply(modifiers, target, holder);
+                DeprecatedModifiersCheckStrategy.apply(modifiers, target, holder);
+
+                /**
+                 * Simplification
+                 *
+                 * /^text/ => 0 === strpos(...) (match)
+                 * /text/ => false !== strpos(...) (match) / str_replace (replace)
+                 * /^text/i => 0 === stripos(...) (match)
+                 * /text/i => false !== stripos(...) (match) / str_ireplace (replace)
+                 * preg_quote => warning if second argument is not presented
+                 */
+
+                /**
+                 * Classes
+                 *
+                 * [0-9] => \d
+                 * [^0-9] => \D
+                 * [:digit:] => \d
+                 * [:word:] => \w
+                 * [^\w] => \W
+                 * [^\s] => \S
+                 */
+
+                /**
+                 * Optimizations
+                 *
+                 * [seq][seq]... => [seq]{N}
+                 * [seq][seq]+ => [seq]{2,}
+                 * [seq][seq]* => [seq]+
+                 * [seq][seq]? => [seq]{1,2}
+                 * /. * ···/, /···. * / => /···/ (remove leading and trailing .* without ^ or $, note: if no back-reference to \0)
+                 * (...) => (?:...) (if there is no backreference)
+                 */
+            }
+        };
+    }
+}
---
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regualrExpressions/modifiersStrategy/DeprecatedModifiersCheckStrategy.java
@@ -0,0 +1,17 @@
+package com.kalessil.phpStorm.phpInspectionsEA.inspectors.regualrExpressions.modifiersStrategy;
+
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.util.text.StringUtil;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import org.jetbrains.annotations.NotNull;
+
+public class DeprecatedModifiersCheckStrategy {
+    private static final String strProblemDescription = "'e' modifier is deprecated, please use 'preg_replace_callback()' instead";
+
+    static public void apply(final String modifiers, @NotNull final StringLiteralExpression target, @NotNull final ProblemsHolder holder) {
+        if (!StringUtil.isEmpty(modifiers) && modifiers.indexOf('e') >= 0) {
+            holder.registerProblem(target, strProblemDescription, ProblemHighlightType.WEAK_WARNING);
+        }
+    }
+}

43be262c0b1ba7d1ef1c891358bf34924a713b85
Vladimir Reznichenko <kalessil@gmail.com>
2015-06-04 11:55:43 +0200

    ThrowsAnnotatedProperlyInspector: inherit-doc tag is also analyzed, messages separated to missing annotations and violations.

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/phpDoc/ThrowsAnnotatedProperlyInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/phpDoc/ThrowsAnnotatedProperlyInspector.java
@@ -7,21 +7,18 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocType;
-import com.jetbrains.php.lang.documentation.phpdoc.psi.impl.PhpDocCommentImpl;
-import com.jetbrains.php.lang.documentation.phpdoc.psi.impl.tags.PhpDocExpectedExceptionImpl;
-import com.jetbrains.php.lang.documentation.phpdoc.psi.impl.tags.PhpDocTagImpl;
-import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocReturnTag;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.phpDoc.ThrowsResolveUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.HashSet;
 
 public class ThrowsAnnotatedProperlyInspector extends BasePhpInspection {
-    private static final String strProblemDescription = "@throw annotation is missing for %c%";
+    private static final String strProblemMissing  = "@throw annotation for '%c%' is missing (ot exception handling is missing)";
+    private static final String strProblemViolates = "throwing '%c%' is not declared in inherited @throws (incomplete contract definition or contract violation)";
 
     @NotNull
     public String getShortName() {
@@ -37,46 +34,20 @@ public class ThrowsAnnotatedProperlyInspector extends BasePhpInspection {
                 if (StringUtil.isEmpty(strMethodName) || null == objMethodName) {
                     return;
                 }
-                PhpPsiElement previous = method.getPrevPsiSibling();
-                if (!(previous instanceof PhpDocCommentImpl)) {
-                    return;
-                }
-
-                // inherit doc not yet supported
-                Collection<PhpDocTagImpl> tags = PsiTreeUtil.findChildrenOfType(previous, PhpDocTagImpl.class);
-                for (PhpDocTagImpl tag : tags) {
-                    if (tag.getName().equals("@inheritdoc")) {
-                        return;
-                    }
-                }
-                tags.clear();
 
                 // find all throw statements
                 Collection<PhpThrow> throwStatements = PsiTreeUtil.findChildrenOfType(method, PhpThrow.class);
                 if (throwStatements.size() > 0) {
                     HashSet<String> declared = new HashSet<String>();
-
-                    // find all @throws and remember FQNs
-                    Collection<PhpDocReturnTag> returns = PsiTreeUtil.findChildrenOfType(previous, PhpDocReturnTag.class);
-                    for (PhpDocReturnTag returnOrThrow : returns) {
-                        if (returnOrThrow.getName().equals("@throws") && returnOrThrow.getFirstPsiChild() instanceof PhpDocType) {
-                            PhpDocType type = (PhpDocType) returnOrThrow.getFirstPsiChild();
-                            declared.add(type.getFQN());
-                            declared.add(type.getName());
-                        }
+                    /** if no doc-block defined, don't bother people */
+                    ThrowsResolveUtil.ResolveType resolvingStatus = ThrowsResolveUtil.resolveThrownExceptions(method, declared);
+                    if (ThrowsResolveUtil.ResolveType.NOT_RESOLVED == resolvingStatus) {
+                        throwStatements.clear();
+                        declared.clear();
+                        return;
                     }
-                    returns.clear();
 
-                    // find all @expectedException and remember FQNs
-                    Collection<PhpDocExpectedExceptionImpl> expectsTags = PsiTreeUtil.findChildrenOfType(previous, PhpDocExpectedExceptionImpl.class);
-                    for (PhpDocExpectedExceptionImpl expect : expectsTags) {
-                        if (expect.getFirstPsiChild() instanceof PhpDocType) {
-                            PhpDocType type = (PhpDocType) expect.getFirstPsiChild();
-                            declared.add(type.getFQN());
-                            declared.add(type.getName());
-                        }
-                    }
-                    expectsTags.clear();
+                    final boolean isInheritDoc = resolvingStatus ==ThrowsResolveUtil.ResolveType.RESOLVED_INHERIT_DOC;
 
                     // process all throw new ... expressions
                     for (PhpThrow throwStatement : throwStatements) {
@@ -87,8 +58,13 @@ public class ThrowsAnnotatedProperlyInspector extends BasePhpInspection {
                                 // match FQNs
                                 PhpClass resolved = (PhpClass) whatsClass.resolve();
                                 if (!declared.contains(resolved.getFQN()) && !declared.contains(resolved.getName())) {
-                                    String strError = strProblemDescription.replace("%c%", resolved.getName());
-                                    holder.registerProblem(objMethodName, strError, ProblemHighlightType.WEAK_WARNING);
+                                    String strError = isInheritDoc ? strProblemViolates : strProblemMissing;
+                                    ProblemHighlightType highlight = isInheritDoc ?
+                                            ProblemHighlightType.GENERIC_ERROR_OR_WARNING :
+                                            ProblemHighlightType.WEAK_WARNING;
+
+                                    strError = strError.replace("%c%", resolved.getName());
+                                    holder.registerProblem(objMethodName, strError, highlight);
                                 }
                             }
                         }

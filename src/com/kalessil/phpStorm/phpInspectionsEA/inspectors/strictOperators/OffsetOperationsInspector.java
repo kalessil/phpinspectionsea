81f36b6ed18626b0bbbf6b50b2b7b5ff1938f319
Vladimir Reznichenko <kalessil@gmail.com>
2015-07-30 23:00:42 +0200

    OffsetOperationsInspector: WIP, custom types resolver has some issues as well, try adding a native resolver wrapper compatible with custom api

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/strictOperators/OffsetOperationsInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/strictOperators/OffsetOperationsInspector.java
@@ -4,15 +4,18 @@ import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.jetbrains.php.lang.psi.elements.ArrayAccessExpression;
-import com.jetbrains.php.lang.psi.elements.ArrayIndex;
-import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
-import com.jetbrains.php.lang.psi.elements.PhpTypedElement;
+import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.psi.elements.*;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import com.jetbrains.php.refactoring.PhpRefactoringUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.TypeFromPsiResolvingUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.Types;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
 
 /**
  * @author Denis Ryabov
@@ -21,16 +24,6 @@ import org.jetbrains.annotations.NotNull;
 public class OffsetOperationsInspector extends BasePhpInspection {
     private static final String strProblemUseSquareBrackets = "Please use square brackets instead of curvy for deeper analysis.";
     private static final String strProblemInvalidIndex = "Wrong index type (%t% is resolved)";
-
-    private static final PhpType validIndexTypesSet = new PhpType();
-    static {
-        validIndexTypesSet.add(PhpType.STRING);
-        validIndexTypesSet.add(PhpType.FLOAT);
-        validIndexTypesSet.add(PhpType.INT);
-        validIndexTypesSet.add(PhpType.BOOLEAN);
-        validIndexTypesSet.add(PhpType.NULL);
-        validIndexTypesSet.add(PhpType.STRING);
-    }
 
     @NotNull
     public String getShortName() {
@@ -62,16 +55,35 @@ public class OffsetOperationsInspector extends BasePhpInspection {
                 if (null != indexHolder) {
                     PhpPsiElement indexValue = indexHolder.getValue();
                     if (null != indexValue) {
-                        PhpType indexValueType =
-                                PhpRefactoringUtil.getCompletedType((PhpTypedElement) indexValue, expression.getProject());
 
+                        // resolve types with custom resolver, native gives type sets which not comparable properly
+                        // TODO: wrap native resolver and normalize to be compatible with our wrapper
+                        HashSet<String> types = new HashSet<String>();
+                        TypeFromPsiResolvingUtil.resolveExpressionType(
+                                indexValue,
+                                ExpressionSemanticUtil.getScope(expression),
+                                PhpIndex.getInstance(holder.getProject()),
+                                types
+                        );
 
-                        // now check if any type provided and check sets relation
-                        String indexTypeAsString = indexValueType.toString();
-                        if (indexTypeAsString.length() > 0 && !PhpType.isSubType(indexValueType, validIndexTypesSet)) {
-                            String strError = strProblemInvalidIndex.replace("%t%", indexValueType.toString());
-                            holder.registerProblem(indexValue, strError, ProblemHighlightType.GENERIC_ERROR);
+                        // now check if any type provided and check sets validity
+                        if (types.size() > 0) {
+                            // remove valid types
+                            types.remove(Types.strString);
+                            types.remove(Types.strFloat);
+                            types.remove(Types.strInteger);
+                            types.remove(Types.strBoolean);
+                            types.remove(Types.strNull);
+                            types.remove(Types.strMixed);
+                            types.remove(Types.strClassNotResolved);
+                            types.remove(Types.strResolvingAbortedOnPsiLevel);
+
+                            if (types.size() > 0) {
+                                String strError = strProblemInvalidIndex.replace("%t%", types.iterator().next());
+                                holder.registerProblem(indexValue, strError, ProblemHighlightType.GENERIC_ERROR);
+                            }
                         }
+                        types.clear();
                     }
                 }
             }

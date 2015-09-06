f27cdf78904403910fb0a1595abf6d60e616f61d
Vladimir Reznichenko <kalessil@gmail.com>
2015-09-06 20:32:33 +0200

    IsEmptyFunctionUsageInspector: added options to report empty usage, suggesting count/null comparison

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/IsEmptyFunctionUsageInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/IsEmptyFunctionUsageInspector.java
@@ -9,17 +9,28 @@ import com.jetbrains.php.lang.psi.elements.ArrayAccessExpression;
 import com.jetbrains.php.lang.psi.elements.Function;
 import com.jetbrains.php.lang.psi.elements.PhpEmpty;
 import com.jetbrains.php.lang.psi.elements.PhpExpression;
+import com.kalessil.phpStorm.phpInspectionsEA.gui.PrettyListControl;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.TypeFromPsiResolvingUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.Types;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.TypesSemanticsUtil;
+import net.miginfocom.swing.MigLayout;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.util.HashSet;
 
 public class IsEmptyFunctionUsageInspector extends BasePhpInspection {
+    // configuration flags automatically saved by IDE
+    public boolean REPORT_EMPTY_USAGE = true;
+    public boolean SUGGEST_TO_USE_COUNT_CHECK = true;
+    public boolean SUGGEST_TO_USE_NULL_COMPARISON = true;
+
+    // static messages for triggered messages
     private static final String strProblemDescriptionDoNotUse = "'empty(...)' counts too much values as empty, consider refactoring with type sensitive checks";
     private static final String strProblemDescriptionUseCount = "'0 === count($...)' construction shall be used instead";
     private static final String strProblemDescriptionUseNullComparison = "Probably it should be 'null === $...' construction used";
@@ -51,21 +62,31 @@ public class IsEmptyFunctionUsageInspector extends BasePhpInspection {
                     /** Case 1: empty(array) - hidden logic - empty array */
                     if (this.isArrayType(objResolvedTypes)) {
                         objResolvedTypes.clear();
-                        holder.registerProblem(emptyExpression, strProblemDescriptionUseCount, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+
+                        if (SUGGEST_TO_USE_COUNT_CHECK) {
+                            holder.registerProblem(emptyExpression, strProblemDescriptionUseCount, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                        }
+
                         return;
                     }
 
                     /** case 2: nullable classes, int, float, resource */
                     if (this.isNullableCoreType(objResolvedTypes) || TypesSemanticsUtil.isNullableObjectInterface(objResolvedTypes)) {
                         objResolvedTypes.clear();
-                        holder.registerProblem(emptyExpression, strProblemDescriptionUseNullComparison, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+
+                        if (SUGGEST_TO_USE_NULL_COMPARISON) {
+                            holder.registerProblem(emptyExpression, strProblemDescriptionUseNullComparison, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                        }
+
                         return;
                     }
 
                     objResolvedTypes.clear();
                 }
 
-                holder.registerProblem(emptyExpression, strProblemDescriptionDoNotUse, ProblemHighlightType.WEAK_WARNING);
+                if (REPORT_EMPTY_USAGE) {
+                    holder.registerProblem(emptyExpression, strProblemDescriptionDoNotUse, ProblemHighlightType.WEAK_WARNING);
+                }
             }
 
 
@@ -86,5 +107,50 @@ public class IsEmptyFunctionUsageInspector extends BasePhpInspection {
                         resolvedTypesSet.contains(Types.strResource);
             }
         };
+    }
+
+    public JComponent createOptionsPanel() {
+        return (new IsEmptyFunctionUsageInspector.OptionsPanel()).getComponent();
+    }
+
+    public class OptionsPanel {
+        private JPanel optionsPanel;
+
+        private JCheckBox reportEmptyUsage;
+        private JCheckBox suggestToUseCountComparison;
+        private JCheckBox suggestToUseNullComparison;
+
+        public OptionsPanel() {
+            optionsPanel = new JPanel();
+            optionsPanel.setLayout(new MigLayout());
+
+            reportEmptyUsage = new JCheckBox("Report empty() usage", REPORT_EMPTY_USAGE);
+            reportEmptyUsage.addChangeListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    REPORT_EMPTY_USAGE = reportEmptyUsage.isSelected();
+                }
+            });
+            optionsPanel.add(reportEmptyUsage, "wrap");
+
+            suggestToUseCountComparison = new JCheckBox("Suggest to use count()-comparison", SUGGEST_TO_USE_COUNT_CHECK);
+            suggestToUseCountComparison.addChangeListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    SUGGEST_TO_USE_COUNT_CHECK = suggestToUseCountComparison.isSelected();
+                }
+            });
+            optionsPanel.add(suggestToUseCountComparison, "wrap");
+
+            suggestToUseNullComparison = new JCheckBox("Suggest to use null-comparison", SUGGEST_TO_USE_NULL_COMPARISON);
+            suggestToUseNullComparison.addChangeListener(new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    SUGGEST_TO_USE_NULL_COMPARISON = suggestToUseNullComparison.isSelected();
+                }
+            });
+            optionsPanel.add(suggestToUseNullComparison, "wrap");
+        }
+
+        public JPanel getComponent() {
+            return optionsPanel;
+        }
     }
 }

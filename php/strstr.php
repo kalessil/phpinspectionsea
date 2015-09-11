43017a53bbc4994c6466321b86afa21970e89d6d
Vladimir Reznichenko <kalessil@gmail.com>
2015-09-11 21:24:39 +0200

    StrStrUsedAsStrPosInspector: updated test cases

--- php/strstr.php
+++ php/strstr.php
@@ -10,13 +10,16 @@ function checkUseCases($strToTest) {
 
     if (strstr($strToTest, 'smth.') === false) { return; }
     if (strstr($strToTest, 'smth.') !== false) { return; }
-    /* no warnings here */
     if (strstr($strToTest, 'smth.') == false)  { return; }
     if (strstr($strToTest, 'smth.') != false)  { return; }
 
     if (false === strstr($strToTest, 'smth.')) { return; }
     if (false !== strstr($strToTest, 'smth.')) { return; }
-    /* no warnings here */
     if (false ==  strstr($strToTest, 'smth.')) { return; }
     if (false !=  strstr($strToTest, 'smth.')) { return; }
+
+    if (false === stristr($strToTest, 'smth.')) { return; }
+    if (false !== stristr($strToTest, 'smth.')) { return; }
+    if (false ==  stristr($strToTest, 'smth.')) { return; }
+    if (false !=  stristr($strToTest, 'smth.')) { return; }
 }

3afda1f3880170e77fdd3d9e3124861523b49f95
Vladimir Reznichenko <kalessil@gmail.com>
2016-01-26 20:03:04 +0100

    StrTrUsageAsStrReplaceInspector: report only single-char replacements  (fixed #163)

--- php/api/strtr.php
+++ php/api/strtr.php
@@ -1,4 +1,6 @@
 <?php
 
-    echo strtr('string to fix', 'from', 'to');
-    echo strtr('string to fix', array('from' => 'to'));
+    echo strtr('string to fix', ' ', '_'); // <- to be reported
+
+    echo strtr('string to fix', 'string', 'int'); // <- do not report
+    echo strtr('string to fix', array('to' => 'to be', 'to be' => 'to')); // <- do not report
--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/StrTrUsageAsStrReplaceInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/StrTrUsageAsStrReplaceInspector.java
@@ -6,8 +6,10 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class StrTrUsageAsStrReplaceInspector extends BasePhpInspection {
@@ -29,9 +31,20 @@ public class StrTrUsageAsStrReplaceInspector extends BasePhpInspection {
                     return;
                 }
 
-                // function usage with 3 parameters is the target pattern
+                /* function usage with 3 parameters is the target pattern */
                 PsiElement[] referenceParameters = reference.getParameters();
                 if (3 == referenceParameters.length) {
+                    /* ensure multiple search-replace are not packed into strings */
+                    StringLiteralExpression search = ExpressionSemanticUtil.resolveAsStringLiteral(referenceParameters[1]);
+                    if (null == search || StringUtil.isEmpty(search.getContents())) {
+                        return;
+                    }
+                    String searchContent = search.getContents().replaceAll("\\\\(.)", "$1");
+                    if (searchContent.length() > 1) {
+                        return;
+                    }
+
+                    /* report case */
                     String strError = strProblemDescription
                             .replace("%p%", referenceParameters[1].getText()+", "+referenceParameters[2].getText());
                     holder.registerProblem(reference, strError, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);

36d3f8088383c06858851cc0fe1a72c14780944d
Vladimir Reznichenko <kalessil@gmail.com>
2016-02-26 18:23:39 +0100

    UnNecessaryDoubleQuotesInspector: added quick-fix

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/UnNecessaryDoubleQuotesInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/UnNecessaryDoubleQuotesInspector.java
@@ -1,10 +1,19 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions;
 
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
+import com.jetbrains.php.lang.intentions.PhpReplaceQuotesIntention;
+import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.jetbrains.php.util.PhpStringUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
@@ -34,9 +43,35 @@ public class UnNecessaryDoubleQuotesInspector extends BasePhpInspection {
                 }
 
                 if (!(ExpressionSemanticUtil.getBlockScope(expression) instanceof PhpDocComment)) {
-                    holder.registerProblem(expression, strProblemDescription, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                    holder.registerProblem(expression, strProblemDescription, ProblemHighlightType.GENERIC_ERROR_OR_WARNING, new TheLocalFix());
                 }
             }
         };
+    }
+
+    private class TheLocalFix implements LocalQuickFix {
+        @NotNull
+        @Override
+        public String getName() {
+            return "Replace with single quotes";
+        }
+
+        @NotNull
+        @Override
+        public String getFamilyName() {
+            return getName();
+        }
+
+        @Override
+        public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+            PsiElement target = descriptor.getPsiElement();
+            if (target instanceof StringLiteralExpression) {
+                String unescaped      = PhpStringUtil.unescapeText(((StringLiteralExpression) target).getContents(), true);
+                String textExpression = '\'' + PhpStringUtil.escapeText(unescaped, true, '\n', '\t') + '\'';
+
+                PhpPsiElement replacement = PhpPsiElementFactory.createPhpPsiFromText(project, StringLiteralExpression.class, textExpression);
+                target.replace(replacement);
+            }
+        }
     }
 }

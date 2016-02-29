c1da949500fb2174c62431b3812463b2e0cb3e76
Vladimir Reznichenko <kalessil@gmail.com>
2016-02-29 18:15:02 +0100

    LongInheritanceChainInspector: do not report deprecated classes (fixed #178)

---
+++ php/classes/long-inheritance-chain.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @deprecated */
+class first  {}
+/* @deprecated */
+class second extends first {}
+/* @deprecated */
+class third  extends second {}
+
+/* @deprecated */
+class forthNotReported extends third {}
+class forthNotReported extends third {}
--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/LongInheritanceChainInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/LongInheritanceChainInspector.java
@@ -5,10 +5,16 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.impl.PhpDocCommentImpl;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.impl.tags.PhpDocTagImpl;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
 
 public class LongInheritanceChainInspector extends BasePhpInspection {
     private static final String strProblemDescription = "Class has 3 or more parent classes, consider using appropriate design patterns.";
@@ -23,32 +29,52 @@ public class LongInheritanceChainInspector extends BasePhpInspection {
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
             public void visitPhpClass(PhpClass clazz) {
-                String strClassFQN = clazz.getFQN();
-                /** skip un-explorable and test classes */
-                if (
-                    StringUtil.isEmpty(strClassFQN) ||
-                    strClassFQN.contains("\\Tests\\") || strClassFQN.contains("\\Test\\") ||
-                    strClassFQN.endsWith("Test") ||
-                    strClassFQN.endsWith("Exception")
-                ) {
+                final String strClassFQN = clazz.getFQN();
+                /* skip un-explorable classes */
+                if (StringUtil.isEmpty(strClassFQN) || strClassFQN.endsWith("Exception")) {
                     return;
                 }
 
-                PsiElement psiClassName = clazz.getNameIdentifier();
+                final PsiElement psiClassName = clazz.getNameIdentifier();
                 if (null != psiClassName) {
                     int intParentsCount = 0;
 
-                    /** count parents */
+                    /* count parents */
                     PhpClass classToCheck = clazz;
                     while (null != classToCheck.getSuperClass()) {
                         classToCheck = classToCheck.getSuperClass();
                         ++intParentsCount;
                     }
 
-                    if (intParentsCount >= 3) {
+                    if (intParentsCount >= 3 && !isDeprecated(clazz)) {
                         holder.registerProblem(psiClassName, strProblemDescription, ProblemHighlightType.WEAK_WARNING);
                     }
                 }
+            }
+
+            /**
+             * TODO: code duplicate of com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor#isDeprecated
+             * Personally I deprecating such classes before they can be dropped.
+             */
+            private boolean isDeprecated(PhpClass clazz) {
+                PhpPsiElement classDoc = clazz.getPrevPsiSibling();
+                if (!(classDoc instanceof PhpDocCommentImpl)) {
+                    return false;
+                }
+
+                /* try to find @deprecated */
+                Collection<PhpDocTagImpl> tags = PsiTreeUtil.findChildrenOfType(classDoc, PhpDocTagImpl.class);
+                if (tags.size() > 0) {
+                    for (PhpDocTagImpl subject : tags) {
+                        if (subject.getName().equals("@deprecated")) {
+                            tags.clear();
+                            return true;
+                        }
+                    }
+                    tags.clear();
+                }
+
+                return false;
             }
         };
     }

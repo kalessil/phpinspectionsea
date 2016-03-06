2a7c44c8afa7a55d8e9b05fe5486d8d5062a98ee
Vladimir Reznichenko <kalessil@gmail.com>
2016-03-06 12:22:14 +0100

    ReferencingObjectsInspector: resolved PHP-7 related false-positives.

--- php/code-style/referencing-objects.php
+++ php/code-style/referencing-objects.php
@@ -1,13 +1,21 @@
 <?php
 
-    $x = & new stdClass();
+    $x = & new stdClass(); // <- reported
 
-    function func(stdClass & $param1, array & $arr, & $str) {}
-
-    interface contract {
-        public function method(stdClass & $param2, array & $arr, & $str);
+    function func(stdClass & $param1, array & $arr, & $str) // <- reported
+    {
     }
 
-    abstract class impl implements contract {
-        abstract public function method2(stdClass & $param3, array & $arr, & $str);
+    interface contract
+    {
+        public function method(stdClass & $param2, array & $arr, & $str); // <- reported
+    }
+
+    abstract class impl implements contract
+    {
+        abstract public function method2(stdClass & $param3, array & $arr, & $str); // <- reported
+    }
+
+    function typeHinted(string & $param1, \string & $param2)
+    {
     }
--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/ReferencingObjectsInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/ReferencingObjectsInspector.java
@@ -12,8 +12,19 @@ import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import org.jetbrains.annotations.NotNull;
 
 public class ReferencingObjectsInspector extends BasePhpInspection {
-    private static final String strProblemParameter = "Objects are always passed by reference; please correct '& $%p%'";
+    private static final String strProblemParameter  = "Objects are always passed by reference; please correct '& $%p%'";
     private static final String strProblemAssignment = "Objects are always passed by reference; please correct '= & new '";
+
+    private static final PhpType php7Types;
+    static {
+        php7Types = (new PhpType())
+            .add(PhpType.STRING)
+            .add(PhpType.INT)
+            .add(PhpType.FLOAT)
+            .add(PhpType.BOOLEAN)
+            .add(PhpType.ARRAY)
+        ;
+    }
 
     @NotNull
     public String getShortName() {
@@ -24,7 +35,7 @@ public class ReferencingObjectsInspector extends BasePhpInspection {
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
-            /** re-dispatch to inspector */
+            /* re-dispatch to inspector */
             public void visitPhpMethod(Method method) {
                 this.inspectCallable(method);
             }
@@ -32,29 +43,32 @@ public class ReferencingObjectsInspector extends BasePhpInspection {
                 this.inspectCallable(function);
             }
 
-            private void inspectCallable (Function callable) {
+            private void inspectCallable (@NotNull Function callable) {
                 if (null == callable.getNameIdentifier()) {
                     return;
                 }
 
                 for (Parameter objParameter : callable.getParameters()) {
-                    /** TODO: php 7 scalar types */
-                    if (objParameter.isPassByRef() && !objParameter.getDeclaredType() .isEmpty() && !PhpType.isSubType(objParameter.getDeclaredType(), PhpType.ARRAY)) {
-                        String strWarning = strProblemParameter.replace("%p%", objParameter.getName());
-                        holder.registerProblem(callable.getNameIdentifier(), strWarning, ProblemHighlightType.WEAK_WARNING);
+                    if (
+                        objParameter.isPassByRef() && !objParameter.getDeclaredType().isEmpty() &&
+                        !PhpType.isSubType(objParameter.getDeclaredType(), php7Types)
+                    ) {
+                        final String message = strProblemParameter.replace("%p%", objParameter.getName());
+                        holder.registerProblem(objParameter, message, ProblemHighlightType.WEAK_WARNING);
                     }
                 }
             }
 
             public void visitPhpNewExpression(NewExpression expression) {
-                PsiElement parent = expression.getParent();
+                final PsiElement parent = expression.getParent();
                 if (parent instanceof AssignmentExpression) {
-                    AssignmentExpression assignment = (AssignmentExpression) parent;
+                    final AssignmentExpression assignment = (AssignmentExpression) parent;
                     if (assignment.getValue() == expression) {
                         PsiElement operation = assignment.getValue().getPrevSibling();
                         if (operation instanceof PsiWhiteSpace) {
                             operation = operation.getPrevSibling();
                         }
+
                         if (null != operation && operation.getText().replaceAll("\\s+","").equals("=&")) {
                             holder.registerProblem(expression, strProblemAssignment, ProblemHighlightType.WEAK_WARNING);
                         }

f56131977c45c334719705bbe10b7c953b5d4c65
Vladimir Reznichenko <reznichenko@beontra.com>
2016-03-17 14:54:20 +0100

    UnnecessaryUseAliasInspector: QF implemented.

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/UnnecessaryUseAliasInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/UnnecessaryUseAliasInspector.java
@@ -1,8 +1,12 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.codeSmell;
 
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.psi.elements.PhpUse;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
@@ -10,7 +14,7 @@ import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import org.jetbrains.annotations.NotNull;
 
 public class UnnecessaryUseAliasInspector extends BasePhpInspection {
-    private static final String strProblemDescription = "' as %a%' is redundant here";
+    private static final String messagePattern = "' as %a%' is redundant here";
 
     @NotNull
     public String getShortName() {
@@ -22,13 +26,40 @@ public class UnnecessaryUseAliasInspector extends BasePhpInspection {
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
             public void visitPhpUse(PhpUse expression) {
-                String clazz = expression.getOriginal();
-                String alias = expression.getAliasName();
+                if (expression.isTraitImport()) {
+                    return;
+                }
+
+                final String clazz = expression.getOriginal();
+                final String alias = expression.getAliasName();
                 if (!StringUtil.isEmpty(alias) && !StringUtil.isEmpty(clazz) && clazz.endsWith("\\"+alias)) {
-                    String message = strProblemDescription.replace("%a%", alias);
-                    holder.registerProblem(expression.getLastChild(), message, ProblemHighlightType.LIKE_UNUSED_SYMBOL);
+                    final String message = messagePattern.replace("%a%", alias);
+                    holder.registerProblem(expression.getLastChild(), message, ProblemHighlightType.LIKE_UNUSED_SYMBOL, new TheLocalFix());
                 }
             }
         };
+    }
+
+    private static class TheLocalFix implements LocalQuickFix {
+        @NotNull
+        @Override
+        public String getName() {
+            return "Remove alias";
+        }
+
+        @NotNull
+        @Override
+        public String getFamilyName() {
+            return getName();
+        }
+
+        @Override
+        public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+            final PsiElement expression = descriptor.getPsiElement().getParent();
+            if (expression instanceof PhpUse) {
+                expression.getLastChild().delete(); // alias itself
+                expression.getLastChild().delete(); // "as" keyword
+            }
+        }
     }
 }

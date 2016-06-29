40939f6b2e7d16304c90eb6d6a6c92fe1b4d3dc4
Vladimir Reznichenko <kalessil@gmail.com>
2016-06-29 22:17:55 +0200

    IsNullFunctionUsageInspector: correct QF for inverted expression (fixed #260)

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/IsNullFunctionUsageInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/IsNullFunctionUsageInspector.java
@@ -13,6 +13,7 @@ import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
 import com.jetbrains.php.lang.psi.elements.BinaryExpression;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.jetbrains.php.lang.psi.elements.UnaryExpression;
+import com.jetbrains.php.lang.psi.elements.impl.UnaryExpressionImpl;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import org.jetbrains.annotations.NotNull;
@@ -71,11 +72,19 @@ public class IsNullFunctionUsageInspector extends BasePhpInspection {
         public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
             final PsiElement expression = descriptor.getPsiElement();
             if (expression instanceof FunctionReference) {
-                final PsiElement replacement = PhpPsiElementFactory.createFromText(project, BinaryExpression.class, "null === null");
+                final PsiElement parent = expression.getParent();
+                boolean isInverted = false;
+                if (parent instanceof UnaryExpressionImpl) {
+                    final UnaryExpressionImpl not = (UnaryExpressionImpl) parent;
+                    isInverted = (null != not.getOperation() && PhpTokenTypes.opNOT == not.getOperation().getNode().getElementType());
+                }
+
+                final String pattern = isInverted ? "null !== null" : "null === null";
+                final PsiElement replacement = PhpPsiElementFactory.createFromText(project, BinaryExpression.class, pattern);
                 //noinspection ConstantConditions - expression is hardcoded so we safe from NPE here
                 ((BinaryExpression) replacement).getRightOperand().replace(((FunctionReference) expression).getParameters()[0]);
 
-                expression.replace(replacement);
+                (isInverted ? parent : expression).replace(replacement);
             }
         }
     }

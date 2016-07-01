9164eaed8cdb875c90b60313329fd9c94bbe3e56
Vladimir Reznichenko <kalessil@gmail.com>
2016-07-01 21:10:01 +0200

    Added comments to a test-case

--- php/ifs/if-and-or-words-operators.php
+++ php/ifs/if-and-or-words-operators.php
@@ -2,9 +2,9 @@
 
     $x = 0;
     if (
-        ($x > 0 and $x < 10)
-        or
-        ($x > 20 and $x < 30)
+        ($x > 0 and $x < 10)   // <-- reported
+        or                     // <-- reported
+        ($x > 20 and $x < 30)  // <-- reported
     ) {
         return;
     }

b604b667b3a5a3ce19f60fdc32a307b5fc35a675
Vladimir Reznichenko <kalessil@gmail.com>
2016-07-02 19:27:41 +0200

    UnnecessaryParenthesesInspector: report inclusion constructs (fixed function-like usage detection #248)

--- php/code-style/unnecessary-parentheses.php
+++ php/code-style/unnecessary-parentheses.php
@@ -1,5 +1,7 @@
 <?php
 
+    require_once ('whatever.php'); // <- reported
+
     (new stdClass())->with('data');                       // not reported
     $mixedIncludeReturn = (include __DIR__ . '/foo.php'); // not reported
 

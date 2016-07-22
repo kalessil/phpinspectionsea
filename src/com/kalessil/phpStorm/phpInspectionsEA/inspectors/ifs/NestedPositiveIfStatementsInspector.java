0751c30eb8b6d65432c6fa1a2928f883d87a9a47
Vladimir Reznichenko <kalessil@gmail.com>
2016-07-22 20:36:42 +0200

    NestedPositiveIfStatementsInspector: do not report when mixing and/or operators (fixed #272)

--- src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/ifs/NestedPositiveIfStatementsInspector.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/inspectors/ifs/NestedPositiveIfStatementsInspector.java
@@ -4,6 +4,9 @@ import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.tree.IElementType;
+import com.jetbrains.php.lang.lexer.PhpTokenTypes;
+import com.jetbrains.php.lang.psi.elements.BinaryExpression;
 import com.jetbrains.php.lang.psi.elements.GroupStatement;
 import com.jetbrains.php.lang.psi.elements.If;
 import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
@@ -11,6 +14,7 @@ import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class NestedPositiveIfStatementsInspector extends BasePhpInspection {
     private static final String strProblemDescription = "If statement can be merged into parent.";
@@ -43,7 +47,6 @@ public class NestedPositiveIfStatementsInspector extends BasePhpInspection {
                     return;
                 }
 
-
                 /** ensure that if is single expression in group */
                 GroupStatement objGroupStatement = (GroupStatement) ifStatement.getParent();
                 int intCountStatementsInParentGroup = ExpressionSemanticUtil.countExpressionsInGroup(objGroupStatement);
@@ -51,6 +54,12 @@ public class NestedPositiveIfStatementsInspector extends BasePhpInspection {
                     return;
                 }
 
+                /** ensure that the same logical operator being used (to not increase the visual complexity) */
+                IElementType operator       = getOperator(ifStatement);
+                IElementType parentOperator = getOperator((If) objParent);
+                if (operator != parentOperator) {
+                    return;
+                }
 
                 /** point on the issues */
                 PhpPsiElement objIfCondition = ifStatement.getCondition();
@@ -59,6 +68,28 @@ public class NestedPositiveIfStatementsInspector extends BasePhpInspection {
                 }
 
                 holder.registerProblem(objIfCondition, strProblemDescription, ProblemHighlightType.WEAK_WARNING);
+            }
+
+            private @Nullable IElementType getOperator(final @NotNull If statement) {
+                /* no condition or single condition*/
+                final PsiElement condition = ExpressionSemanticUtil.getExpressionTroughParenthesis(statement.getCondition());
+                if (null == condition) {
+                    return null;
+                }
+                if (!(condition instanceof BinaryExpression)) {
+                    return PhpTokenTypes.opAND;
+                }
+
+                /* we need only or/and operators to return */
+                final PsiElement operation = ((BinaryExpression) condition).getOperation();
+                if (null == operation) {
+                    return null;
+                }
+                final IElementType operationType = operation.getNode().getElementType();
+                if (operationType != PhpTokenTypes.opOR && operationType != PhpTokenTypes.opAND) {
+                    return null;
+                }
+                return operationType;
             }
         };
     }

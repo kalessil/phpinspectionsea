85f8abf48f0ec95cbcb49d381305b4b222db4402
Vladimir Reznichenko <kalessil@gmail.com>
2016-08-01 22:14:30 +0200

    Offline inspections: document target format (see #155)

--- cli/ps_inspect.sh
+++ cli/ps_inspect.sh
@@ -12,3 +12,13 @@ $PHP_STORM_DIR/bin/inspect.sh \
     $PROJECT_DIR \
     ~/WebIde100/config/inspection/Default.xml Default \
     -v2 -d $PROJECT_DIR/src
+
+# Target format we want to convert into is check-style; each PS xml converted individually
+# <?xml version="1.0" encoding="UTF-8"?>
+# <checkstyle version="*">
+#   <file name="File name">
+#       <error line="Line number" severity="info|level|error" message="Reported message" source="Inspection Title" />
+#       ...
+#   </file>
+#   ...
+# </checkstyle>

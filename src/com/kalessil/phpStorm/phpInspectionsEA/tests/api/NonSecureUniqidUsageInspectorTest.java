aed1729224b3c50a6b1ac002bf4a983d6d79adb6
Vladimir Reznichenko <kalessil@gmail.com>
2016-10-20 21:30:34 +0200

    NonSecureUniqidUsageInspector: sample test

---
+++ src/com/kalessil/phpStorm/phpInspectionsEA/tests/api/NonSecureUniqidUsageInspectorTest.java
@@ -0,0 +1,13 @@
+package com.kalessil.phpStorm.phpInspectionsEA.tests.api;
+
+import org.junit.Test;
+import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.security.NonSecureUniqidUsageInspector;
+
+public class NonSecureUniqidUsageInspectorTest extends CodeInsightFixtureTestCase {
+    public void testIfFindsAllPatterns() {
+        myFixture.configureByFiles("fixtures/uniqid.php");
+        myFixture.enableInspections(NonSecureUniqidUsageInspector.class);
+        myFixture.testHighlighting(true, false, true);
+    }
+}
---
+++ src/com/kalessil/phpStorm/phpInspectionsEA/tests/api/fixtures/uniqid.php
@@ -0,0 +1,4 @@
+<?php
+    <generic_error descr="Usage of a variable variable">uniqid()</generic_error>;
+    <generic_error descr="Usage of a variable variable">uniqid('q')</generic_error>;
+    uniqid('', true);

fc304627bab7d557e174607f0a9faf7db963240f
Vladimir Reznichenko <kalessil@gmail.com>
2016-10-20 21:53:02 +0200

    NonSecureUniqidUsageInspector: added test annotation

--- src/com/kalessil/phpStorm/phpInspectionsEA/tests/api/NonSecureUniqidUsageInspectorTest.java
+++ src/com/kalessil/phpStorm/phpInspectionsEA/tests/api/NonSecureUniqidUsageInspectorTest.java
@@ -5,6 +5,7 @@ import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.security.NonSecureUniqidUsageInspector;
 
 public class NonSecureUniqidUsageInspectorTest extends CodeInsightFixtureTestCase {
+    @Test
     public void testIfFindsAllPatterns() {
         myFixture.configureByFiles("fixtures/uniqid.php");
         myFixture.enableInspections(NonSecureUniqidUsageInspector.class);

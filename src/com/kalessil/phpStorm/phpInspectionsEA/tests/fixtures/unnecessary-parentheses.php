281b894d2b4d7ecd89229d0bcfac77ba2a85cbee
Vladimir Reznichenko <kalessil@gmail.com>
2016-10-26 12:39:21 +0200

    UnnecessaryParenthesesInspector: extended UTs fixtures

--- src/com/kalessil/phpStorm/phpInspectionsEA/tests/fixtures/unnecessary-parentheses.php
+++ src/com/kalessil/phpStorm/phpInspectionsEA/tests/fixtures/unnecessary-parentheses.php
@@ -3,11 +3,13 @@
     require_once <weak_warning descr="Unnecessary parentheses">('whatever.php')</weak_warning>;
     $x = <weak_warning descr="Unnecessary parentheses">($x)</weak_warning> + 1;
 
-
     $x = ($x + 1) / 2;
 
     (new stdClass())->with('data');
+    (new stdClass())->prop;
+
     (clone $stdClass)->with('data');
+    (clone $stdClass)->prop;
 
     ($x->callableProperty)();
     ($x->getCallableProperty())();

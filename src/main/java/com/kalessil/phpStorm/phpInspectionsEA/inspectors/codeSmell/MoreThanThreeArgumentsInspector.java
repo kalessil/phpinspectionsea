477c763bd1a1e395e7f3696d4b76637000e73a8b
Vladimir Reznichenko <kalessil@gmail.com>
2016-11-06 21:06:56 +0100

    MoreThanThreeArgumentsInspector: fixed JavaDoc

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/MoreThanThreeArgumentsInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/MoreThanThreeArgumentsInspector.java
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * Community request leaded to duplicating functionality of native PS inspection, so we just inherit it.
- * @see https://bitbucket.org/kalessil/phpinspectionsea/issues/289/allow-more-than-3-parameters-in
+ *  see https://bitbucket.org/kalessil/phpinspectionsea/issues/289/allow-more-than-3-parameters-in
  */
 public class MoreThanThreeArgumentsInspector extends PhpTooManyParametersInspection {
     @NotNull

2ef7c90e3f1e14a47e30833917c6bf6bd767b454
Vladimir Reznichenko <kalessil@gmail.com>
2016-11-20 10:29:35 +0100

    CurlSslServerSpoofingInspector: UTs updated

--- src/test/resources/fixtures/security/curl-ssl-spoofing.php
+++ src/test/resources/fixtures/security/curl-ssl-spoofing.php
@@ -5,32 +5,32 @@ class OpensslRsaPaddingOracle
     public function curlSllSpoofingCase1($ch)
     {
         $options = [
-            CURLOPT_SSL_VERIFYHOST => false, // <-reported
-            CURLOPT_SSL_VERIFYHOST => 0,     // <-reported
-            CURLOPT_SSL_VERIFYHOST => null,  // <-reported
-            CURLOPT_SSL_VERIFYHOST => true,  // <-reported
-            CURLOPT_SSL_VERIFYHOST => 1,     // <-reported
+            <error descr="CURLOPT_SSL_VERIFYHOST should be 2">CURLOPT_SSL_VERIFYHOST => false</error>,
+            <error descr="CURLOPT_SSL_VERIFYHOST should be 2">CURLOPT_SSL_VERIFYHOST => 0</error>,
+            <error descr="CURLOPT_SSL_VERIFYHOST should be 2">CURLOPT_SSL_VERIFYHOST => null</error>,
+            <error descr="CURLOPT_SSL_VERIFYHOST should be 2">CURLOPT_SSL_VERIFYHOST => true</error>,
+            <error descr="CURLOPT_SSL_VERIFYHOST should be 2">CURLOPT_SSL_VERIFYHOST => 1</error>,
             CURLOPT_SSL_VERIFYHOST => 2
         ];
 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // <-reported
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);     // <-reported
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, null);  // <-reported
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);  // <-reported
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);     // <-reported
+        <error descr="CURLOPT_SSL_VERIFYHOST should be 2">curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false)</error>;
+        <error descr="CURLOPT_SSL_VERIFYHOST should be 2">curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0)</error>;
+        <error descr="CURLOPT_SSL_VERIFYHOST should be 2">curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, null)</error>;
+        <error descr="CURLOPT_SSL_VERIFYHOST should be 2">curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true)</error>;
+        <error descr="CURLOPT_SSL_VERIFYHOST should be 2">curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1)</error>;
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 
         $options = [
-            CURLOPT_SSL_VERIFYPEER => 0,     // <-reported
-            CURLOPT_SSL_VERIFYPEER => false, // <-reported
-            CURLOPT_SSL_VERIFYPEER => null,  // <-reported
+            <error descr="CURLOPT_SSL_VERIFYPEER should be 1">CURLOPT_SSL_VERIFYPEER => 0</error>,
+            <error descr="CURLOPT_SSL_VERIFYPEER should be 1">CURLOPT_SSL_VERIFYPEER => false</error>,
+            <error descr="CURLOPT_SSL_VERIFYPEER should be 1">CURLOPT_SSL_VERIFYPEER => null</error>,
             CURLOPT_SSL_VERIFYPEER => true,
             CURLOPT_SSL_VERIFYPEER => 1
         ];
 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // <-reported
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);     // <-reported
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, null);  // <-reported
+        <error descr="CURLOPT_SSL_VERIFYPEER should be 1">curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false)</error>;
+        <error descr="CURLOPT_SSL_VERIFYPEER should be 1">curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0)</error>;
+        <error descr="CURLOPT_SSL_VERIFYPEER should be 1">curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, null)</error>;
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
 

8537a38724868351111422bde90381359f00e72b
Vladimir Reznichenko <kalessil@gmail.com>
2016-11-28 10:32:12 +0100

    CryptographicallySecureRandomnessInspector: report unchecked IV value usage

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/CryptographicallySecureRandomnessInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/CryptographicallySecureRandomnessInspector.java
@@ -1,17 +1,25 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.security;
 
+import com.intellij.codeInsight.PsiEquivalenceUtil;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.config.PhpLanguageFeature;
 import com.jetbrains.php.config.PhpLanguageLevel;
 import com.jetbrains.php.config.PhpProjectConfigurationFacade;
-import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.lexer.PhpTokenTypes;
+import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.PhpLanguageUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
 
 /*
  * This file is part of the Php Inspections (EA Extended) package.
@@ -33,10 +41,11 @@ import org.jetbrains.annotations.NotNull;
 
 public class CryptographicallySecureRandomnessInspector extends BasePhpInspection {
     private static final String messageUseRandomBytes                    = "Consider using cryptographically secure random_bytes() instead";
+    private static final String messageVerifyBytes                       = "The IV generated can be false, please add necessary checks";
     private static final String messageOpenssl2ndArgumentNotDefined      = "Use 2nd parameter for determining if the algorithm used was cryptographically strong";
     private static final String messageMcrypt2ndArgumentNotDefined       = "Please provide 2nd parameter implicitly as default value has changed between PHP versions";
 
-    private static final String messageVerifyBytes                       = "..."; // error
+
     private static final String messageOpenssl2ndArgumentNotVerified     = "..."; // error
     private static final String messageMcrypt2ndArgumentStrongNotSecure  = "..."; // error
 
@@ -61,11 +70,13 @@ public class CryptographicallySecureRandomnessInspector extends BasePhpInspectio
                 }
                 final boolean isOpenSSL = functionName.equals("openssl_random_pseudo_bytes");
 
+
                 /* Case 1: use random_bytes in PHP7 */
                 PhpLanguageLevel php = PhpProjectConfigurationFacade.getInstance(holder.getProject()).getLanguageLevel();
                 if (php.hasFeature(PhpLanguageFeature.SCALAR_TYPE_HINTS)) { // PHP7 and newer
                     holder.registerProblem(reference, messageUseRandomBytes, ProblemHighlightType.WEAK_WARNING);
                 }
+
 
                 /* Case 2: report missing 2nd argument */
                 final boolean hasSecondArgument = 2 == params.length;
@@ -74,7 +85,48 @@ public class CryptographicallySecureRandomnessInspector extends BasePhpInspectio
                     holder.registerProblem(reference, message, ProblemHighlightType.GENERIC_ERROR);
                 }
 
+
                 /* Case 3: unchecked generation result */
+                boolean resultVerified = false;
+                if (reference.getParent() instanceof AssignmentExpression) {
+                    final AssignmentExpression assignment = (AssignmentExpression) reference.getParent();
+                    final PsiElement assignmentContainer  = assignment.getVariable();
+                    if (assignment.getValue() == reference && null != assignmentContainer) {
+                        final Function scope      = ExpressionSemanticUtil.getScope(reference);
+                        final GroupStatement body = null == scope ? null : ExpressionSemanticUtil.getGroupStatement(scope);
+                        if (null != body) {
+                            Collection<BinaryExpression> checks = PsiTreeUtil.findChildrenOfType(body, BinaryExpression.class);
+                            for (BinaryExpression expression : checks) {
+                                /* ensure binary expression is complete */
+                                final PsiElement left      = expression.getLeftOperand();
+                                final PsiElement right     = expression.getRightOperand();
+                                final PsiElement operation = expression.getOperation();
+                                if (null == operation || null == left || null == right) {
+                                    continue;
+                                }
+
+                                /* expression should be a comparison with a false */
+                                if (!PhpLanguageUtil.isFalse(left) && !PhpLanguageUtil.isFalse(right)) {
+                                    continue;
+                                }
+                                final IElementType operator = operation.getNode().getElementType();
+                                if (PhpTokenTypes.opIDENTICAL != operator && PhpTokenTypes.opNOT_IDENTICAL != operator) {
+                                    continue;
+                                }
+
+                                final PsiElement operatorValue = PhpLanguageUtil.isFalse(left) ? right : left;
+                                if (PsiEquivalenceUtil.areElementsEquivalent(operatorValue, assignmentContainer)) {
+                                    resultVerified = true;
+                                    break;
+                                }
+                            }
+                            checks.clear();
+                        }
+                    }
+                }
+                if (!resultVerified) {
+                    holder.registerProblem(reference, messageVerifyBytes, ProblemHighlightType.GENERIC_ERROR);
+                }
 
                 /* Case 4: is 2nd argument verified/strong enough */
             }
--- src/test/resources/fixtures/security/crypto-secure-randomness.php
+++ src/test/resources/fixtures/security/crypto-secure-randomness.php
@@ -5,12 +5,23 @@ class CryptoSecureRandomness
     public function missingSecondParameters() {
         $x = <error descr="Use 2nd parameter for determining if the algorithm used was cryptographically strong">openssl_random_pseudo_bytes</error> (32);
         if ($x === false) {
-            return;
+            return false;
         }
 
         $x = <error descr="Please provide 2nd parameter implicitly as default value has changed between PHP versions">mcrypt_create_iv</error> (32);
         if ($x === false) {
-            return;
+            return false;
         }
+
+        return $x;
+    }
+
+    public function missingResultsVerification()
+    {
+        $x = <error descr="The IV generated can be false, please add necessary checks">openssl_random_pseudo_bytes</error> (32, $isCryptoStrong);
+
+        $x = <error descr="The IV generated can be false, please add necessary checks">mcrypt_create_iv</error> (32, MCRYPT_DEV_URANDOM);
+
+        return $x;
     }
 }

0fe80c8220919ce5ab5c5bb380e7d876352e71dc
Vladimir Reznichenko <kalessil@gmail.com>
2016-11-28 21:21:52 +0100

    PregQuoteUsageInspector: implemented

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/PregQuoteUsageInspector.java
@@ -0,0 +1,45 @@
+package com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage;
+
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+public class PregQuoteUsageInspector extends BasePhpInspection {
+    private static final String message = "Please provide regex delimiter as the second argument for proper escaping";
+
+    @NotNull
+    public String getShortName() {
+        return "PregQuoteUsageInspection";
+    }
+
+    @Override
+    @NotNull
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new BasePhpElementVisitor() {
+            public void visitPhpFunctionCall(FunctionReference reference) {
+                final String functionName = reference.getName();
+                final PsiElement[] params = reference.getParameters();
+                if (1 != params.length || StringUtil.isEmpty(functionName) || !functionName.equals("preg_quote")) {
+                    return;
+                }
+
+                holder.registerProblem(reference, message, ProblemHighlightType.GENERIC_ERROR);
+            }
+        };
+    }
+}
---
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/pitfalls/PregQuoteUsageInspectorTest.java
@@ -0,0 +1,12 @@
+package com.kalessil.phpStorm.phpInspectionsEA.pitfalls;
+
+import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.PregQuoteUsageInspector;
+
+public class PregQuoteUsageInspectorTest extends CodeInsightFixtureTestCase {
+    public void testIfFindsAllPatterns() {
+        myFixture.configureByFile("fixtures/pitfalls/preg-quote.php");
+        myFixture.enableInspections(PregQuoteUsageInspector.class);
+        myFixture.testHighlighting(true, false, true);
+    }
+}

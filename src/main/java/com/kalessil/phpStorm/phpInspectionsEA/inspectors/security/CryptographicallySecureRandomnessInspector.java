ea0082f16abaff6cd71c3245d8aecc2220bf6618
Vladimir Reznichenko <kalessil@gmail.com>
2016-11-28 15:16:12 +0100

    Code grooming: unused imports

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/CryptographicallySecureRandomnessInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/CryptographicallySecureRandomnessInspector.java
@@ -17,7 +17,6 @@ import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.PhpLanguageUtil;
-import org.jaxen.expr.UnaryExpr;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;

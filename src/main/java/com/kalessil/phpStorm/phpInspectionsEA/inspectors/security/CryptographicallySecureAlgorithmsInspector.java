6d4a1e87c16a05c598d3d281dc2c936f13743d37
Vladimir Reznichenko <kalessil@gmail.com>
2016-12-02 11:13:35 +0100

    Cod

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/CryptographicallySecureAlgorithmsInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/CryptographicallySecureAlgorithmsInspector.java
@@ -25,24 +25,29 @@ public class CryptographicallySecureAlgorithmsInspector extends BasePhpInspectio
 
     final private static Map<String, String> constants = new HashMap<>();
     static {
+        /* notes for future me, to not check things twice */
+        // OPENSSL_ALGO_SHA1, OPENSSL_ALGO_MD5, OPENSSL_ALGO_MD4, OPENSSL_ALGO_MD2 -> has no Blowfish replacement
+
         /* known bugs */
         constants.put("MCRYPT_RIJNDAEL_192",    "mcrypt's MCRYPT_RIJNDAEL_192 is not AES compliant, MCRYPT_RIJNDAEL_128 should be used instead");
         constants.put("MCRYPT_RIJNDAEL_256",    "mcrypt's MCRYPT_RIJNDAEL_256 is not AES compliant, MCRYPT_RIJNDAEL_128 + 256-bit key should be used instead");
-        /* weak algorithms */
+        /* weak algorithms, mcrypt constants */
         constants.put("MCRYPT_3DES",            "3DES has known vulnerabilities, consider using MCRYPT_RIJNDAEL_128 instead");
         constants.put("MCRYPT_DES",             "DES has known vulnerabilities, consider using MCRYPT_RIJNDAEL_128 instead");
         constants.put("MCRYPT_RC2",             "RC2 has known vulnerabilities, consider using MCRYPT_RIJNDAEL_128 instead");
         constants.put("MCRYPT_RC4",             "RC4 has known vulnerabilities, consider using MCRYPT_RIJNDAEL_128 instead");
+        /* weak algorithms, openssl constants */
         constants.put("OPENSSL_CIPHER_3DES",    "3DES has known vulnerabilities, consider using AES-128-* instead");
         constants.put("OPENSSL_CIPHER_DES",     "DES has known vulnerabilities, consider using AES-128-* instead");
         constants.put("OPENSSL_CIPHER_RC2_40",  "RC2 has known vulnerabilities, consider using AES-128-* instead");
         constants.put("OPENSSL_CIPHER_RC2_64",  "RC2 has known vulnerabilities, consider using AES-128-* instead");
+        /* weak algorithms, crypt constants */
+        constants.put("CRYPT_MD5",              "MD5 has known vulnerabilities, consider using CRYPT_BLOWFISH instead");
+        constants.put("CRYPT_STD_DES",          "DES has known vulnerabilities, consider using CRYPT_BLOWFISH instead");
 
         /*
-            Constants:
-                OPENSSL_ALGO_SHA1, OPENSSL_ALGO_MD5, OPENSSL_ALGO_MD4, OPENSSL_ALGO_MD2
             Functions:
-                md5|sha1|crc32
+                md5|sha1|crc32 => use crypt(.., CRYPT_BLOWFISH);
             Strings (possibly only resolved as string literals):
                 '(tripledes)|(des3)|(des(-(ede|ede3))?(-(cbc|cfb|ecb|cfb1|cfb8|ofb))?)'
                 'sha[01]?'

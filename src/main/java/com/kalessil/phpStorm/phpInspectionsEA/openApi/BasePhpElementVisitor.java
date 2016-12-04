880fa0eff20052c46c84533fe1709af83789ef1a
Vladimir Reznichenko <kalessil@gmail.com>
2016-12-04 18:42:42 +0100

    Implemented PhpEval visitor (fixed #55)

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/openApi/BasePhpElementVisitor.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/openApi/BasePhpElementVisitor.java
@@ -1,6 +1,25 @@
 package com.kalessil.phpStorm.phpInspectionsEA.openApi;
 
+import com.jetbrains.php.lang.psi.elements.PhpEval;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class BasePhpElementVisitor extends PhpElementVisitor {
+
+    /** adds PhpEval visitor */
+    @Override
+    final public void visitPhpElement(PhpPsiElement element) {
+        if (element instanceof PhpEval) {
+            visitPhpEval((PhpEval) element);
+            return;
+        }
+
+        this.visitElement(element);
+    }
+
+    public void visitPhpEval(@NotNull PhpEval eval) {
+        this.visitElement(eval);
+    }
+
 }

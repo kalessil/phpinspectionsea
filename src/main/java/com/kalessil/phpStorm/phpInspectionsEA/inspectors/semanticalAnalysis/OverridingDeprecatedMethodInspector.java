c0f05d3051f17611eb7a68533d6201faf7d97a22
Vladimir Reznichenko <kalessil@gmail.com>
2016-12-15 15:22:44 +0100

    OverridingDeprecatedMethodInspector: skeleton (WIP #41)

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/OverridingDeprecatedMethodInspector.java
@@ -0,0 +1,41 @@
+package com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis;
+
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.psi.elements.Method;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+public class OverridingDeprecatedMethodInspector extends BasePhpInspection {
+    private static final String messagePattern = "%s% overrides/implements a deprecated method. Consider refactoring or deprecate is as well.";
+
+    @NotNull
+    public String getShortName() {
+        return "OverridingDeprecatedMethodInspection";
+    }
+
+    @Override
+    @NotNull
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new BasePhpElementVisitor() {
+            public void visitPhpMethod(Method method) {
+                /* 1: if the method already deprecated, do not process it */
+                /* 1: method must be in real class: we are targeting to spot technical debt only; ignore test classes; */
+
+                /* 2: if super class has no method, lookup interfaces; */
+                /* 2: lookup all interfaces for a deprecated method */
+
+            }
+        };
+    }
+}

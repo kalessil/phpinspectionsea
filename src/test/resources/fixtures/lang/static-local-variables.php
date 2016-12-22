738e83cefacf3ceedc53e5fc36602f8d9d735701
Vladimir Reznichenko <kalessil@gmail.com>
2016-12-22 17:01:33 +0100

    StaticLocalVariablesUsageInspector: added test-cases

--- src/test/resources/fixtures/lang/static-local-variables.php
+++ src/test/resources/fixtures/lang/static-local-variables.php
@@ -2,6 +2,7 @@
 
 class SLVPatterns {
     public function f() {
-        ...
+        <weak_warning descr="Variable can be static: property or 'static $array = [...]' (compile-time initialization).">$array</weak_warning> = [''];
+        return $array;
     }
 }

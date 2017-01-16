c5a7d7e39eb517975b62ddc8802e4e05da55c41c
Vladimir Reznichenko <kalessil@gmail.com>
2017-01-16 16:33:03 +0100

    [#15] ComparisonOperandsOrderInspector - added settings

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/ComparisonOperandsOrderInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/ComparisonOperandsOrderInspector.java
@@ -1,14 +1,5 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.codeSmell;
 
-/*
- * This file is part of the Php Inspections (EA Extended) package.
- *
- * (c) Vladimir Reznichenko <kalessil@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
@@ -20,12 +11,29 @@ import com.jetbrains.php.lang.psi.elements.ConstantReference;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import net.miginfocom.swing.MigLayout;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 public class ComparisonOperandsOrderInspector extends BasePhpInspection {
+    // configuration flags automatically saved by IDE
+    @SuppressWarnings("WeakerAccess")
+    public boolean CONFIGURED           = false;
+    public boolean PREFER_YODA_STYLE    = false;
+    public boolean PREFER_REGULAR_STYLE = false;
+
     final static private String messageUseYoda    = "Yoda conditions style should be used instead";
     final static private String messageUseRegular = "Regular conditions style should be used instead";
 
@@ -48,9 +56,9 @@ public class ComparisonOperandsOrderInspector extends BasePhpInspection {
         return new BasePhpElementVisitor() {
             public void visitPhpBinaryExpression(BinaryExpression expression) {
                 /* verify general structure */
-                final IElementType operator = expression.getOperationType();
-                final PsiElement left       = expression.getLeftOperand();
-                final PsiElement right      = expression.getRightOperand();
+                final IElementType operator = CONFIGURED ? expression.getOperationType() : null;
+                final PsiElement left       = CONFIGURED ? expression.getLeftOperand() : null;
+                final PsiElement right      = CONFIGURED ? expression.getRightOperand() : null;
                 if (null == operator || null == left || null == right || !operations.contains(operator)) {
                     return;
                 }
@@ -68,10 +76,60 @@ public class ComparisonOperandsOrderInspector extends BasePhpInspection {
                     return;
                 }
 
-                if (!isLeftConstant) {
+                if (PREFER_YODA_STYLE && !isLeftConstant) {
                     holder.registerProblem(expression, messageUseYoda, ProblemHighlightType.WEAK_WARNING);
+                    return;
+                }
+                if (PREFER_REGULAR_STYLE && !isRightConstant) {
+                    holder.registerProblem(expression, messageUseRegular, ProblemHighlightType.WEAK_WARNING);
                 }
             }
         };
     }
+
+    public JComponent createOptionsPanel() {
+        return (new ComparisonOperandsOrderInspector.OptionsPanel()).getComponent();
+    }
+
+    private class OptionsPanel {
+        final private JPanel optionsPanel;
+
+        final private JCheckBox preferYodaStyle;
+        final private JCheckBox preferRegularStyle;
+
+        public OptionsPanel() {
+            optionsPanel = new JPanel();
+            optionsPanel.setLayout(new MigLayout());
+
+            preferYodaStyle    = new JCheckBox("Prefer yoda style", PREFER_YODA_STYLE);
+            preferRegularStyle = new JCheckBox("Prefer regular style", PREFER_REGULAR_STYLE);
+
+            preferYodaStyle.addChangeListener(e -> {
+                PREFER_YODA_STYLE = preferYodaStyle.isSelected();
+                if (PREFER_YODA_STYLE) {
+                    preferRegularStyle.setSelected(false);
+                    PREFER_REGULAR_STYLE = false;
+                }
+
+                CONFIGURED = PREFER_YODA_STYLE || PREFER_REGULAR_STYLE;
+            });
+            optionsPanel.add(preferYodaStyle, "wrap");
+
+            preferRegularStyle.addChangeListener(e -> {
+                PREFER_REGULAR_STYLE = preferRegularStyle.isSelected();
+                if (PREFER_REGULAR_STYLE) {
+                    preferYodaStyle.setSelected(false);
+                    PREFER_YODA_STYLE = false;
+                }
+
+                CONFIGURED = PREFER_YODA_STYLE || PREFER_REGULAR_STYLE;
+            });
+            optionsPanel.add(preferRegularStyle, "wrap");
+        }
+
+        JPanel getComponent() {
+            return optionsPanel;
+        }
+    }
+
 }

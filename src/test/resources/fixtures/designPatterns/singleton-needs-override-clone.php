050283533b3fa4b3b45a1f51ae088c019dbb324a
Vladimir Reznichenko <kalessil@gmail.com>
2017-02-09 20:23:28 +0100

    [#114] SingletonFactoryPatternViolationInspector: one more test case

--- src/test/resources/fixtures/designPatterns/singleton-needs-override-clone.php
+++ src/test/resources/fixtures/designPatterns/singleton-needs-override-clone.php
@@ -5,3 +5,10 @@ class <weak_warning descr="Singleton should also override __clone to prevent cop
     private function __construct()       {}
     public static function getInstance() {}
 }
+
+class EntityOverridesClone
+{
+    public function __clone()            {} // <- clone overridden
+    private function __construct()       {}
+    public static function getInstance() {}
+}

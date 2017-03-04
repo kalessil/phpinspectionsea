6a99492c28fa84fc1a7d541bbbb7f7cafc2b73c2
Vladimir Reznichenko <kalessil@gmail.com>
2017-03-04 16:38:49 +0100

    InArrayMissUseInspector: fix broken tests

--- src/test/resources/fixtures/api/in-array-missuse.php
+++ src/test/resources/fixtures/api/in-array-missuse.php
@@ -1,18 +1,25 @@
 <?php
 
     /* comparison equivalent cases */
+    $x = <error descr="''2' == '1'' should be used instead.">in_array</error>  ('1', ['2'], false);
+    $x = <error descr="''2' === '1'' should be used instead.">in_array</error> ('1', ['2'], true);
+    $x = <error descr="''2' == '1'' should be used instead.">in_array</error>  ('1', ['2']);
+    $x = <error descr="''2' == '1'' should be used instead.">in_array</error>  ('1', [0 => '2'], false);
+    $x = <error descr="''2' === '1'' should be used instead.">in_array</error> ('1', [0 => '2'], true);
+    $x = <error descr="''2' == '1'' should be used instead.">in_array</error>  ('1', [0 => '2']);
+    $x = <error descr="''2' == '1'' should be used instead.">in_array</error>  ('1', ['0' => '2']);
+
     $x = <error descr="''2' == '1'' should be used instead.">in_array</error> ('1', array('2'), false);
-    $x = <error descr="''2' === '1'' should be used instead.">in_array</error> ('1', array('2'), true);
-    $x = <error descr="''2' == '1'' should be used instead.">in_array</error> ('1', array('2'));
-    $x = <error descr="''2' == '1'' should be used instead.">in_array</error> ('1', array(0 => '2'), false);
-    $x = <error descr="''2' === '1'' should be used instead.">in_array</error> ('1', array(0 => '2'), true);
-    $x = <error descr="''2' == '1'' should be used instead.">in_array</error> ('1', array(0 => '2'));
-    $x = <error descr="''2' == '1'' should be used instead.">in_array</error> ('1', array('0' => '2'));
+    $x = true  && <error descr="''2' == '1'' should be used instead.">in_array</error> ('1', array('2'), false);
+    $x = false || <error descr="''2' == '1'' should be used instead.">in_array</error> ('1', array('2'), false);
+
+
 
     /* array_key_exists equivalent cases */
-    $y = <error descr="This looks equivalent to a array_key_exists(...) call. It is safe to refactor for type-safe code when the indexes are integers/strings only.">in_array</error> ('0', array_keys(array('item')), false);
-    $y = <error descr="This looks equivalent to a array_key_exists(...) call. It is safe to refactor for type-safe code when the indexes are integers/strings only.">in_array</error> ('0', array_keys(array('item')), true);
-    $y = <error descr="This looks equivalent to a array_key_exists(...) call. It is safe to refactor for type-safe code when the indexes are integers/strings only.">in_array</error> ('0', array_keys(array('item')));
+    $y = !<error descr="'array_key_exists('0', ['item'])' should be used instead. It is safe to refactor for type-safe code when the indexes are integers/strings only.">in_array('0', array_keys(['item']), false)</error>;
+    $y =  <error descr="'array_key_exists('0', ['item'])' should be used instead. It is safe to refactor for type-safe code when the indexes are integers/strings only.">in_array('0', array_keys(['item']), false)</error>;
+    $y =  <error descr="'array_key_exists('0', ['item'])' should be used instead. It is safe to refactor for type-safe code when the indexes are integers/strings only.">in_array('0', array_keys(['item']), true)</error>;
+    $y =  <error descr="'array_key_exists('0', ['item'])' should be used instead. It is safe to refactor for type-safe code when the indexes are integers/strings only.">in_array('0', array_keys(['item']))</error>;
 
 
     /* false-positives */

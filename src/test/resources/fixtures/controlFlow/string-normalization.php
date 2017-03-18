cba15aad26e0faeb575f90fc9d04ae5a0fe7e85b
Vladimir Reznichenko <kalessil@gmail.com>
2017-03-18 15:11:33 +0100

    [#168] StringNormalizationInspector: added more test-cases

--- src/test/resources/fixtures/controlFlow/string-normalization.php
+++ src/test/resources/fixtures/controlFlow/string-normalization.php
@@ -1,24 +1,27 @@
 <?php
 
     /* pattern: in-efficient nesting */
-    echo <weak_warning descr="'strtolower(trim($string))' should be used instead.">trim</weak_warning>(strtolower($string));
-    echo <weak_warning descr="'strtolower(ltrim($string))' should be used instead.">ltrim</weak_warning>(strtolower($string));
-    echo <weak_warning descr="'strtolower(rtrim($string))' should be used instead.">rtrim</weak_warning>(strtolower($string));
-    echo <weak_warning descr="'strtolower(substr($string, 1))' should be used instead.">substr</weak_warning>(strtolower($string), 1);
-    echo <weak_warning descr="'strtolower(mb_substr($string, 1))' should be used instead.">mb_substr</weak_warning>(strtolower($string), 1);
+    echo <weak_warning descr="'strtolower(trim($string))' should be used instead.">trim(strtolower($string))</weak_warning>;
+    echo <weak_warning descr="'strtolower(ltrim($string))' should be used instead.">ltrim(strtolower($string))</weak_warning>;
+    echo <weak_warning descr="'strtolower(rtrim($string))' should be used instead.">rtrim(strtolower($string))</weak_warning>;
+    echo <weak_warning descr="'strtolower(substr($string, 1))' should be used instead.">substr(strtolower($string), 1)</weak_warning>;
+    echo <weak_warning descr="'strtolower(mb_substr($string, 1))' should be used instead.">mb_substr(strtolower($string), 1)</weak_warning>;
 
     /* valid cases */
+    echo $x->trim($x->strtolower($string));
+    echo $x->trim(strtolower($string));
+    echo trim($x->strtolower($string));
     echo strtolower(trim($string));
 
     /* pattern: senseless nesting; same function */
-    echo strtolower(<weak_warning descr="'strtolower(...)' makes no sense here.">strtolower</weak_warning>($string));
-    echo ucfirst(<weak_warning descr="'ucfirst(...)' makes no sense here.">ucfirst</weak_warning>($string));
+    echo strtolower(<weak_warning descr="'strtolower(...)' makes no sense here.">strtolower($string)</weak_warning>);
+    echo ucfirst(<weak_warning descr="'ucfirst(...)' makes no sense here.">ucfirst($string)</weak_warning>);
     /* pattern: senseless nesting; different functions */
-    echo strtolower(<weak_warning descr="'ucfirst(...)' makes no sense here.">ucfirst</weak_warning>($string));
-    echo strtoupper(<weak_warning descr="'lcfirst(...)' makes no sense here.">lcfirst</weak_warning>($string));
-    echo mb_convert_case(<weak_warning descr="'ucwords(...)' makes no sense here.">ucwords</weak_warning>($string));
-    echo mb_strtolower(<weak_warning descr="'ucfirst(...)' makes no sense here.">ucfirst</weak_warning>($string));
-    echo mb_strtoupper(<weak_warning descr="'lcfirst(...)' makes no sense here.">lcfirst</weak_warning>($string));
+    echo strtolower(<weak_warning descr="'ucfirst(...)' makes no sense here.">ucfirst($string)</weak_warning>);
+    echo strtoupper(<weak_warning descr="'lcfirst(...)' makes no sense here.">lcfirst($string)</weak_warning>);
+    echo mb_convert_case(<weak_warning descr="'ucwords(...)' makes no sense here.">ucwords($string)</weak_warning>);
+    echo mb_strtolower(<weak_warning descr="'ucfirst(...)' makes no sense here.">ucfirst($string)</weak_warning>);
+    echo mb_strtoupper(<weak_warning descr="'lcfirst(...)' makes no sense here.">lcfirst($string)</weak_warning>);
 
     /* valid cases */
     echo ucfirst(strtolower($string));

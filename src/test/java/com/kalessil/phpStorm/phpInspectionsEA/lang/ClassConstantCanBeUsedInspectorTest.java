3acfef707fb6e01136d00bfd75dacafdc9ef3aca
Gareth Parker <blackfireweb@gmail.com>
2017-03-24 20:39:24 +0000

    Adding test with missing fix file

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/lang/ClassConstantCanBeUsedInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/lang/ClassConstantCanBeUsedInspectorTest.java
@@ -1,7 +1,10 @@
 package com.kalessil.phpStorm.phpInspectionsEA.lang;
 
+import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions.ClassConstantCanBeUsedInspector;
+
+import java.util.List;
 
 final public class ClassConstantCanBeUsedInspectorTest extends CodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
@@ -9,5 +12,20 @@ final public class ClassConstantCanBeUsedInspectorTest extends CodeInsightFixtur
         myFixture.configureByFile("fixtures/lang/classConstant/class-name-constant-ns.php");
         myFixture.enableInspections(ClassConstantCanBeUsedInspector.class);
         myFixture.testHighlighting(true, false, true);
+    }
+
+    public void testQuickFixOutput() {
+        myFixture.configureByFile("fixtures/lang/classConstant/class-in-the-same-namespace.php");
+        myFixture.configureByFile("fixtures/lang/classConstant/class-name-constant-ns.php");
+        myFixture.enableInspections(ClassConstantCanBeUsedInspector.class);
+        myFixture.testHighlighting(true, false, false);
+
+        List<IntentionAction> quickFixes = myFixture.getAllQuickFixes();
+        for (IntentionAction fix: quickFixes) {
+            myFixture.launchAction(fix);
+        }
+
+        myFixture.setTestDataPath("./");
+        myFixture.checkResultByFile("fixtures/lang/classConstant/class-name-constant-ns.fixed.php");
     }
 }

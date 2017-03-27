2231339bc2c05f141580c74db799d6b3d7430e18
Vladimir Reznichenko <kalessil@gmail.com>
2017-03-27 19:31:20 +0200

    Code grooming: optimized imports

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/UnnecessaryParenthesesInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/UnnecessaryParenthesesInspector.java
@@ -9,7 +9,6 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.*;
-import com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions.nullCoalescing.NullCoalescingOperatorCanBeUsedInspector;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;

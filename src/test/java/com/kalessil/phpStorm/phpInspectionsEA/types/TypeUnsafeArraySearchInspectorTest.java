b882f7f7f4045d328660c6f0b3a3e5c6abc331fa
Gareth Parker <blackfireweb@gmail.com>
2017-03-30 10:31:51 +0100

    Rename for clarity

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/types/TypeUnsafeArraySearchInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/types/TypeUnsafeArraySearchInspectorTest.java
@@ -5,7 +5,7 @@ import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.arrays.TypeUnsafeArraySearchInspector;
 
 final public class TypeUnsafeArraySearchInspectorTest extends CodeInsightFixtureTestCase {
-    public void testIfFindsAllPatterns() {
+    public void testIfFindsAllPatterns4() {
         myFixture.configureByFile("fixtures/api/strict-array-search.php");
         myFixture.enableInspections(TypeUnsafeArraySearchInspector.class);
         myFixture.testHighlighting(true, false, true);

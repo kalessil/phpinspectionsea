c3aeebc10e45badbbf914a3dcce2563f64b48031
David Rodrigues <david.proweb@gmail.com>
2017-04-08 17:29:18 -0300

    [#221] SideEffectAnalysisInspector: identify possibles side-effects when exists refs on function parameters;

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/SideEffectAnalysisInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/SideEffectAnalysisInspector.java
@@ -14,6 +14,7 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.psi.elements.Function;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.Parameter;
 import com.jetbrains.php.lang.psi.elements.Statement;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
@@ -24,6 +25,7 @@ import java.util.HashMap;
 public class SideEffectAnalysisInspector extends BasePhpInspection {
     private static final String message = "This call can be removed because it have no side-effect.";
     private static HashMap<String, SideEffect> mappedSideEffects = new HashMap<>();
+    private static HashMap<String, Integer> mappedRefPositions = new HashMap<>();
 
     private enum SideEffect {NONE, POSSIBLE, UNKNOW, INTERNAL, EXTERNAL}
 
@@ -34,7 +36,23 @@ public class SideEffectAnalysisInspector extends BasePhpInspection {
             return SideEffect.UNKNOW;
         }
 
+        if (function.hasRefParams()) {
+            saveRefPosition(function);
+            return SideEffect.POSSIBLE;
+        }
+
         return SideEffect.NONE;
+    }
+
+    private static void saveRefPosition(@NotNull final Function function) {
+        final Parameter[] functionParameters = function.getParameters();
+
+        for (int functionParametersIndex = 0; functionParametersIndex < functionParameters.length; functionParametersIndex++) {
+            if (functionParameters[functionParametersIndex].isPassByRef()) {
+                mappedRefPositions.put(function.getFQN(), functionParametersIndex + 1);
+                break;
+            }
+        }
     }
 
     @NotNull
@@ -62,8 +80,17 @@ public class SideEffectAnalysisInspector extends BasePhpInspection {
                 final SideEffect functionSideEffect = getIdentifiedSideEffect(functionReference);
 
                 if (functionSideEffect.equals(SideEffect.NONE) && functionReference.getParent() instanceof Statement) {
-                    holder.registerProblem(functionReference.getParent(), message, ProblemHighlightType.WEAK_WARNING);
+                    registerProblem(functionReference);
                 }
+
+                if (functionSideEffect.equals(SideEffect.POSSIBLE) &&
+                    functionReference.getParameters().length < mappedRefPositions.get(functionReference.getFQN())) {
+                    registerProblem(functionReference);
+                }
+            }
+
+            private void registerProblem(@NotNull final FunctionReference functionReference) {
+                holder.registerProblem(functionReference.getParent(), message, ProblemHighlightType.WEAK_WARNING);
             }
         };
     }
--- src/test/resources/fixtures/deadCode/side-effects.php
+++ src/test/resources/fixtures/deadCode/side-effects.php
@@ -7,3 +7,9 @@ function sideEffectNone($number = null) {
 
 <weak_warning descr="This call can be removed because it have no side-effect.">sideEffectNone();</weak_warning>
 <weak_warning descr="This call can be removed because it have no side-effect.">sideEffectNone(1);</weak_warning>
+
+function sideEffectPossible($arg1, &$arg2 = null) {
+}
+
+<weak_warning descr="This call can be removed because it have no side-effect.">sideEffectPossible(1);</weak_warning>
+sideEffectPossible(1, $arg2);

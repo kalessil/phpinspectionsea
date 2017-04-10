e9edfb32b891fe5164f70c3b8185b67c7926046c
Vladimir Reznichenko <kalessil@gmail.com>
2017-04-10 14:59:21 +0200

    [#215] OverridingDeprecatedMethodInspector: more patterns

--- src/test/resources/fixtures/classes/overriding-deprecated-constructs.php
+++ src/test/resources/fixtures/classes/overriding-deprecated-constructs.php
@@ -8,6 +8,7 @@
     class DeprecatedClass implements DeprecatedInterface {
         public function <warning descr="'deprecatedInInterface' overrides/implements a deprecated method. Consider refactoring or deprecate it as well.">deprecatedInInterface</warning> () {}
     }
+
     /* pattern: overriding deprecation */
     class DeprecatedMethod extends DeprecatedClass {
         /** @deprecated */
@@ -20,6 +21,17 @@
         public function <warning descr="'deprecatedInInterface' overrides/implements a deprecated method. Consider refactoring or deprecate it as well.">deprecatedInInterface</warning>();
     }
 
+    /* pattern: child deprecation instead of parent */
+    abstract class DeprecationHolderParent {
+        abstract public function abstractToDeprecate();
+        public function implementationToDeprecate() {}
+    }
+    abstract class DeprecationHolderChild extends DeprecationHolderParent {
+        /** @deprecated */
+        public function <weak_warning descr="Parent 'abstractToDeprecate' probably needs to be deprecated as well.">abstractToDeprecate</weak_warning> () {}
+        /** @deprecated */
+        public function <weak_warning descr="Parent 'implementationToDeprecate' probably needs to be deprecated as well.">implementationToDeprecate</weak_warning> () {}
+    }
 
     /* false-positives: implemented deprecation and deprecated */
     class DeprecatedClassFixed implements DeprecatedInterface {

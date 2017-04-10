45ef77323ed89e10399bf125664259657687eac2
Vladimir Reznichenko <kalessil@gmail.com>
2017-04-10 22:13:11 +0200

    [#220] SuspiciousAssignmentsInspector: new patterns - more test cases, bugs discovered

--- src/test/resources/fixtures/pitfalls/suspiciousAssignments/suspicious-assignments-sequential.php
+++ src/test/resources/fixtures/pitfalls/suspiciousAssignments/suspicious-assignments-sequential.php
@@ -2,8 +2,15 @@
 
 function immediateOverrides()
 {
-    /* case 1: possible override */
+    /* case 1: possible override, if itself */
     if ($x) {
+        $y = '';
+    }
+    <error descr="$y is immediately overridden, perhaps it was intended to use 'else' here.">$y = ''</error>;
+    /* case 1: possible override, alternative branch */
+    if ($x) {
+        ;
+    } else {
         $y = '';
     }
     <error descr="$y is immediately overridden, perhaps it was intended to use 'else' here.">$y = ''</error>;
@@ -28,6 +35,9 @@ function immediateOverrides()
     /* false-positive: depends on itself */
     $a = '';
     $a = trim($a);
+    /* false-positive: accumulation */
+    $v[] = '';
+    $v[] = '';
 
 
     return [$y, $z, $t, $a];

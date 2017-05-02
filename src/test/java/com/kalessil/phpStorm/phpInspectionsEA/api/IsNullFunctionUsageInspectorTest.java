60c6b60985fd0a6381c025618b5a787200ef9c12
Vladimir Reznichenko <kalessil@gmail.com>
2017-05-02 08:59:53 +0200

    IsNullFunctionUsageInspector: QF tests added

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/IsNullFunctionUsageInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/IsNullFunctionUsageInspectorTest.java
@@ -1,12 +1,20 @@
 package com.kalessil.phpStorm.phpInspectionsEA.api;
 
+import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.IsNullFunctionUsageInspector;
 
 final public class IsNullFunctionUsageInspectorTest extends CodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
-        myFixture.configureByFile("fixtures/api/is-null-function.php");
         myFixture.enableInspections(IsNullFunctionUsageInspector.class);
+
+        myFixture.configureByFile("fixtures/api/is-null-function.php");
         myFixture.testHighlighting(true, false, true);
+
+        for (IntentionAction fix : myFixture.getAllQuickFixes()) {
+            myFixture.launchAction(fix);
+        }
+        myFixture.setTestDataPath(".");
+        myFixture.checkResultByFile("fixtures/api/is-null-function.fixed.php");
     }
 }

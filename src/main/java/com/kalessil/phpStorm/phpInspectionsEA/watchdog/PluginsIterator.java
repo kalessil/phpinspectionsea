6192bf75a54e0e71e7c3d260e8f26e8bfdfd6be7
Vladimir Reznichenko <kalessil@gmail.com>
2017-05-08 22:40:34 +0200

    Watchdog: enhance plugins iterator

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/watchdog/PluginsIterator.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/watchdog/PluginsIterator.java
@@ -1,7 +1,6 @@
 package com.kalessil.phpStorm.phpInspectionsEA.watchdog;
 
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.IdeaPluginDescriptorImpl;
 import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.util.Pair;
@@ -11,22 +10,38 @@ import org.jetbrains.annotations.NotNull;
 import java.util.*;
 
 final public class PluginsIterator implements Iterator {
-    @NotNull
-    final private List<Pair<String, String>> pluginsIds = new ArrayList<>();
+    private static List<String> jetBrainsPlugins = new ArrayList<>();
+    static {
+        jetBrainsPlugins.add("com.jetbrains.php");
+    }
 
     @NotNull
     final private Iterator<Pair<String, String>> pluginsIterator;
 
     PluginsIterator() {
+        final List<Pair<String, String>> corePlugins = new ArrayList<>();
+        final List<Pair<String, String>> plugins     = new ArrayList<>();
         for (final IdeaPluginDescriptor plugin : PluginManager.getPlugins()) {
             final PluginId pluginId = plugin.getPluginId();
             final String stringId   = pluginId.getIdString();
             if (StringUtils.countMatches(stringId, ".") > 1) {
-                this.pluginsIds.add(Pair.create(stringId, plugin.getName()));
+                /* core plugins needs to be pushed into the end and are basically fallbacks */
+                if (jetBrainsPlugins.contains(stringId)) {
+                    corePlugins.add(Pair.create(stringId, plugin.getName()));
+                } else {
+                    plugins.add(Pair.create(stringId, plugin.getName()));
+                }
             }
         }
+        plugins.addAll(corePlugins);
+        corePlugins.clear();
 
-        this.pluginsIterator = this.pluginsIds.iterator();
+        /* TODO: find this in stack traces directly */
+        // org.intellij.plugins.*.
+        // org.intellij.lang.*.
+        // com.intellij.lang.*.
+
+        this.pluginsIterator = plugins.iterator();
     }
 
     @Override

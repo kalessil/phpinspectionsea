ed55a16cee150beb6e666a6a379f53fbcc3937d2
David Rodrigues <david.proweb@gmail.com>
2017-05-16 12:29:19 -0300

    [#73] ParameterEqualsDefaultValueInspector: term "method()" replaced by "test()";

--- src/test/resources/fixtures/codeStyle/argument-default-equals.fixed.php
+++ src/test/resources/fixtures/codeStyle/argument-default-equals.fixed.php
@@ -1,14 +1,14 @@
 <?php
 
-function method($a = 0, $b = 0) {
+function test($a = 0, $b = 0) {
 }
 
 // Positives.
-method();
-method(1);
-method(0, 1);
+test();
+test(1);
+test(0, 1);
 
 // Warnings.
-method();
-method(1);
-method();
+test();
+test(1);
+test();
--- src/test/resources/fixtures/codeStyle/argument-default-equals.php
+++ src/test/resources/fixtures/codeStyle/argument-default-equals.php
@@ -1,14 +1,14 @@
 <?php
 
-function method($a = 0, $b = 0) {
+function test($a = 0, $b = 0) {
 }
 
 // Positives.
-method();
-method(1);
-method(0, 1);
+test();
+test(1);
+test(0, 1);
 
 // Warnings.
-method(<weak_warning descr="The argument can be safely dropped, as identical to the default value.">0</weak_warning>);
-method(1, <weak_warning descr="The argument can be safely dropped, as identical to the default value.">0</weak_warning>);
-method(<weak_warning descr="The argument can be safely dropped, as identical to the default value.">0, 0</weak_warning>);
+test(<weak_warning descr="The argument can be safely dropped, as identical to the default value.">0</weak_warning>);
+test(1, <weak_warning descr="The argument can be safely dropped, as identical to the default value.">0</weak_warning>);
+test(<weak_warning descr="The argument can be safely dropped, as identical to the default value.">0, 0</weak_warning>);

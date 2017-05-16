4362c3c643b77b01aef57561cdac2557e011e617
Vladimir Reznichenko <kalessil@gmail.com>
2017-05-16 13:39:44 +0200

    PassingByReferenceCorrectnessInspector: UTs extended

--- src/test/resources/fixtures/pitfalls/passing-by-reference-correctness.php
+++ src/test/resources/fixtures/pitfalls/passing-by-reference-correctness.php
@@ -14,12 +14,18 @@ class Clazz
     {
         return '';
     }
+
+    public static function y()
+    {
+        return '';
+    }
 }
 
 $obj = new Clazz();
 $obj->method($obj->property);
 $obj->method(<warning descr="Emits a notice (only variable references should be returned by reference)">explode(...[])</warning>);
 $obj->method(<warning descr="Emits a notice (only variable references should be returned by reference)">$obj->x()</warning>);
+$obj->method(<warning descr="Emits a notice (only variable references should be returned by reference)">Clazz::y()</warning>);
 
 $array = [0];
 $obj->method($array[0]);

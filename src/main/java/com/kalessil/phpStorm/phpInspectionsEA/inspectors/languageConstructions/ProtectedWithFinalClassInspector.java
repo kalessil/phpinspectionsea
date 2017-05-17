7e08d735fc00afb39df11b54ca1ff68a8bf16478
David Rodrigues <david.proweb@gmail.com>
2017-05-17 18:17:47 -0300

    [#294] ProtectedWithFinalClassInspector: implemented for methods;

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/ProtectedWithFinalClassInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/ProtectedWithFinalClassInspector.java
@@ -2,11 +2,11 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions;
 
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.php.lang.psi.elements.Field;
-import com.jetbrains.php.lang.psi.elements.PhpClass;
-import com.jetbrains.php.lang.psi.elements.PhpModifierList;
+import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 
@@ -24,25 +24,44 @@ public class ProtectedWithFinalClassInspector extends BasePhpInspection {
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder problemsHolder, final boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
+            private void checkElement(final PhpElementWithModifier element) {
+                if (!element.getModifier().isProtected()) {
+                    return;
+                }
+
+                final PhpClass elementClass = ((PhpClassMember) element).getContainingClass();
+
+                assert elementClass != null;
+
+                if (!elementClass.isFinal()) {
+                    return;
+                }
+
+                final PsiElement elementModifierScope = (element instanceof Field)
+                                                        ? ((Field) element).getParent()
+                                                        : (PsiElement) element;
+
+                final PhpModifierList  elementModifierList = PsiTreeUtil.findChildOfType(elementModifierScope, PhpModifierList.class);
+                final LeafPsiElement[] elementModifiers    = PsiTreeUtil.getChildrenOfType(elementModifierList, LeafPsiElement.class);
+
+                assert elementModifiers != null;
+
+                for (final LeafPsiElement elementModifier : elementModifiers) {
+                    if ("protected".equals(elementModifier.getText())) {
+                        problemsHolder.registerProblem(elementModifier, message, ProblemHighlightType.WEAK_WARNING);
+                        break;
+                    }
+                }
+            }
+
             @Override
             public void visitPhpField(final Field field) {
-                if (!field.getModifier().isProtected()) {
-                    return;
-                }
+                checkElement(field);
+            }
 
-                final PhpClass fieldClass = field.getContainingClass();
-
-                assert fieldClass != null;
-
-                if (!fieldClass.isFinal()) {
-                    return;
-                }
-
-                final PhpModifierList fieldProtectedModifier = PsiTreeUtil.findChildOfType(field.getParent(), PhpModifierList.class);
-
-                assert fieldProtectedModifier != null;
-
-                problemsHolder.registerProblem(fieldProtectedModifier, message, ProblemHighlightType.WEAK_WARNING);
+            @Override
+            public void visitPhpMethod(final Method method) {
+                checkElement(method);
             }
         };
     }
--- src/test/resources/fixtures/codeSmell/protected-with-final-class.php
+++ src/test/resources/fixtures/codeSmell/protected-with-final-class.php
@@ -11,3 +11,33 @@ final class PropertyClass2 {
     <weak_warning descr="Protected modifier could be replaced by private.">protected</weak_warning> $isProtected;
     private $isPrivate;
 }
+
+class MethodClass1 {
+    function method0() { }
+    public function method1() { }
+    protected function method2() { }
+    private function method3() { }
+
+    static function method0s() { }
+    static public function method1s() { }
+    static protected function method2s() { }
+    static private function method3s() { }
+}
+
+abstract class MethodClass2 {
+    abstract function method0();
+    abstract public function method1();
+    abstract protected function method2();
+}
+
+final class MethodClass3 {
+    function method0() { }
+    public function method1() { }
+    <weak_warning descr="Protected modifier could be replaced by private.">protected</weak_warning> function method2() { }
+    private function method3() { }
+
+    static function method0s() { }
+    static public function method1s() { }
+    static <weak_warning descr="Protected modifier could be replaced by private.">protected</weak_warning> function method2s() { }
+    static private function method3s() { }
+}

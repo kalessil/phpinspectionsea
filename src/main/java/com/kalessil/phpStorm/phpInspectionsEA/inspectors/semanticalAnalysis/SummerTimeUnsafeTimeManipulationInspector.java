818ba681ee8f865690a6392b9e1654b4ba27fc9f
Vladimir Reznichenko <kalessil@gmail.com>
2017-05-19 22:37:47 +0200

    [#319] SummerTimeUnsafeTimeManipulationInspector: resolved false-positives (context of using 24 is considered)

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/SummerTimeUnsafeTimeManipulationInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/SummerTimeUnsafeTimeManipulationInspector.java
@@ -4,13 +4,27 @@ import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
+import com.jetbrains.php.lang.parser.PhpElementTypes;
 import com.jetbrains.php.lang.psi.elements.BinaryExpression;
+import com.jetbrains.php.lang.psi.elements.ParenthesizedExpression;
+import com.jetbrains.php.lang.psi.elements.PhpExpression;
 import com.jetbrains.php.lang.psi.elements.SelfAssignmentExpression;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import org.jetbrains.annotations.NotNull;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 public class SummerTimeUnsafeTimeManipulationInspector extends BasePhpInspection {
     private static final String message = "Consider using \\DateTime for DST safe date/time manipulation.";
@@ -28,7 +42,7 @@ public class SummerTimeUnsafeTimeManipulationInspector extends BasePhpInspection
                 final PsiElement operation = expression.getOperation();
                 final PsiElement left      = expression.getLeftOperand();
                 final PsiElement right     = expression.getRightOperand();
-                if (null == operation || null == left || null == right) {
+                if (operation == null || left == null || right == null) {
                     return;
                 }
 
@@ -40,14 +54,11 @@ public class SummerTimeUnsafeTimeManipulationInspector extends BasePhpInspection
                     operationType == PhpTokenTypes.opMINUS ||
                     operationType == PhpTokenTypes.opPLUS
                 ) {
-                    final boolean isOneDayNumberUsed = (
-                        right.textMatches("24") || right.textMatches("86400") ||
-                        (
-                            !(left instanceof BinaryExpression) &&
-                            (left.textMatches("24") || left.textMatches("86400"))
-                        )
-                    );
-                    if (isOneDayNumberUsed) {
+                    if (this.isTargetMagicNumber(right) && this.isTargetContext(right)) {
+                        holder.registerProblem(expression, message, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                        return;
+                    }
+                    if (this.isTargetMagicNumber(left) && this.isTargetContext(left)) {
                         holder.registerProblem(expression, message, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
                     }
                 }
@@ -55,9 +66,8 @@ public class SummerTimeUnsafeTimeManipulationInspector extends BasePhpInspection
 
             public void visitPhpSelfAssignmentExpression(SelfAssignmentExpression expression) {
                 final PsiElement operation = expression.getOperation();
-                final PsiElement left      = expression.getVariable();
-                final PsiElement right     = expression.getValue();
-                if (null == operation || null == left || null == right) {
+                final PsiElement value     = expression.getValue();
+                if (operation == null || value == null) {
                     return;
                 }
 
@@ -69,10 +79,38 @@ public class SummerTimeUnsafeTimeManipulationInspector extends BasePhpInspection
                     operationType == PhpTokenTypes.opMINUS_ASGN ||
                     operationType == PhpTokenTypes.opPLUS_ASGN
                 ) {
-                    if (right.textMatches("24") || right.textMatches("86400")) {
+                    if (this.isTargetMagicNumber(value)) {
                         holder.registerProblem(expression, message, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
                     }
                 }
+            }
+
+            private boolean isTargetContext(@NotNull PsiElement magicNumber) {
+                boolean result = magicNumber.textMatches("86400");
+                if (!result) {
+                    PsiElement expression = magicNumber.getParent();
+                    while (expression instanceof ParenthesizedExpression || expression instanceof BinaryExpression) {
+                        expression = expression.getParent();
+                    }
+                    for (PsiElement candidate : PsiTreeUtil.findChildrenOfType(expression, PhpExpression.class)) {
+                        if (candidate.getNode().getElementType() == PhpElementTypes.NUMBER) {
+                            final String text = candidate.getText();
+                            if (text.equals("60") || text.endsWith("3600")) {
+                                result = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                return result;
+            }
+
+            private boolean isTargetMagicNumber(@NotNull PsiElement candidate) {
+                boolean result = false;
+                if (candidate.getNode().getElementType() == PhpElementTypes.NUMBER) {
+                    result = candidate.textMatches("24") || candidate.textMatches("86400");
+                }
+                return result;
             }
        };
     }

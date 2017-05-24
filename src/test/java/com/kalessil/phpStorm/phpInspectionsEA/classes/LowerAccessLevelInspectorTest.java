7683ed943eedc8ac5ea304767a0827f894fa78cc
Vladimir Reznichenko <kalessil@gmail.com>
2017-05-24 09:06:32 +0200

    LowerAccessLevelInspector: added UTs

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/LowerAccessLevelInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/LowerAccessLevelInspectorTest.java
@@ -7,7 +7,7 @@ import com.jetbrains.php.config.PhpProjectConfigurationFacade;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.classes.lowerAccessLevel.LowerAccessLevelInspector;
 
 public class LowerAccessLevelInspectorTest extends CodeInsightFixtureTestCase {
-    public void testIfFindsAllPatterns() {
+    public void testProtectedMembersOfFinalClass() {
         PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP710);
         myFixture.enableInspections(new LowerAccessLevelInspector());
 
@@ -20,5 +20,18 @@ public class LowerAccessLevelInspectorTest extends CodeInsightFixtureTestCase {
 
         myFixture.setTestDataPath(".");
         myFixture.checkResultByFile("fixtures/classes/weakerAccess/protected-with-final-class.fixed.php");
+    }
+    public void testProtectedFieldsInPrivateContext() {
+        myFixture.enableInspections(new LowerAccessLevelInspector());
+
+        myFixture.configureByFile("fixtures/classes/weakerAccess/protected-fields-in-private-context.php");
+        myFixture.testHighlighting(true, false, true);
+
+        for (final IntentionAction fix : myFixture.getAllQuickFixes()) {
+            myFixture.launchAction(fix);
+        }
+
+        myFixture.setTestDataPath(".");
+        myFixture.checkResultByFile("fixtures/classes/weakerAccess/protected-fields-in-private-context.fixed.php");
     }
 }

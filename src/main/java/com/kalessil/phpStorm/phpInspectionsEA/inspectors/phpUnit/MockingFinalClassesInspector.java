4b340bd6202bdb7b1292270f2f63cbf8552d7601
Vladimir Reznichenko <kalessil@gmail.com>
2017-06-02 18:50:05 +0200

    MockingFinalClassesInspector: cleanup

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/phpUnit/MockingFinalClassesInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/phpUnit/MockingFinalClassesInspector.java
@@ -28,10 +28,17 @@ public class MockingFinalClassesInspector extends BasePhpInspection {
 
     private final static Map<String, String> methods = new HashMap<>();
     static {
-        methods.put("\\PHPUnit_Framework_TestCase.getMockBuilder",   "getMockBuilder");
-        methods.put("\\PHPUnit\\Framework\\TestCase.getMockBuilder", "getMockBuilder");
-        methods.put("\\PhpSpec\\ObjectBehavior.shouldHaveType",      "shouldHaveType");
-        methods.put("\\PhpSpec\\ObjectBehavior.shouldNotHaveType",   "shouldNotHaveType");
+        /* PhpUnit-related */
+        methods.put("\\PHPUnit_Framework_TestCase.getMockBuilder",        "getMockBuilder");
+        methods.put("\\PHPUnit_Framework_TestCase.getMock",               "getMock");
+        methods.put("\\PHPUnit_Framework_TestCase.getMockClass",          "getMockClass");
+        methods.put("\\PHPUnit_Framework_MockObject_Generator.getMock",   "getMock");
+        methods.put("\\PHPUnit_Framework_MockObject_MockBuilder.getMock", "getMock");
+        methods.put("\\PHPUnit\\Framework\\TestCase.getMockBuilder",      "getMockBuilder");
+        methods.put("\\PHPUnit\\Framework\\TestCase.getMockClass",        "getMockClass");
+        /* PhpSpec-related */
+        methods.put("\\Prophecy\\Prophet.prophesize",                     "prophesize");
+        methods.put("\\Prophecy\\Prophecy\\ObjectProphecy.willExtend",    "willExtend");
     }
 
     @NotNull
@@ -47,7 +54,7 @@ public class MockingFinalClassesInspector extends BasePhpInspection {
             public void visitPhpMethodReference(@NotNull MethodReference reference) {
                 final String methodName      = reference.getName();
                 final PsiElement[] arguments = reference.getParameters();
-                if (methodName != null && arguments.length == 1 && methods.containsValue(methodName)) {
+                if (methodName != null && arguments.length > 0 && methods.containsValue(methodName)) {
                     final PsiElement resolved = reference.resolve();
                     if (resolved instanceof Method && methods.get(((Method) resolved).getFQN()) != null) {
                         final PhpClass referencedClass = this.getClass(arguments[0]);
--- src/test/resources/fixtures/pitfalls/mocking-final-classes.php
+++ src/test/resources/fixtures/pitfalls/mocking-final-classes.php
@@ -1,37 +1,36 @@
 <?php
 
-namespace PhpSpec {
-    class ObjectBehavior
+namespace PHPUnit\Framework {
+    class TestCase
     {
-        public function shouldHaveType()    {}
-        public function shouldNotHaveType() {}
+        public function getMockBuilder() {}
+        public function getMockClass()   {}
     }
 }
-namespace PHPUnit\Framework {
-    class TestCase                   { public function getMockBuilder() {} }
-}
 namespace {
-    class PHPUnit_Framework_TestCase { public function getMockBuilder() {} }
+    class PHPUnit_Framework_TestCase
+    {
+        public function getMockBuilder() {}
+        public function getMock()        {}
+        public function getMockClass()   {}
+    }
 }
 
 namespace {
     final class Clazz extends stdClass {}
 
-    $phpSpec    = new \PhpSpec\ObjectBehavior();
     $phpUnitOld = new \PHPUnit_Framework_TestCase();
     $phpUnitNew = new \PHPUnit\Framework\TestCase();
 
-    $phpSpec->shouldHaveType(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
-    $phpSpec->shouldNotHaveType(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
-    $phpSpec->shouldHaveType(stdClass::class);
-    $phpSpec->shouldNotHaveType(stdClass::class);
-
     $phpUnitOld->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
+    $phpUnitOld->getMock(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
+    $phpUnitOld->getMockClass(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
     $phpUnitOld->getMockBuilder(stdClass::class);
 
     $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
     $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">'Clazz'</error>);
     $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">'\Clazz'</error>);
     $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">"\\Clazz"</error>);
+    $phpUnitNew->getMockClass(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
     $phpUnitNew->getMockBuilder(stdClass::class);
 }

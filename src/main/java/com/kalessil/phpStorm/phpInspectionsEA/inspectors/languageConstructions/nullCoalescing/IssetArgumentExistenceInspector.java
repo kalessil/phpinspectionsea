37a02bc9dd3040e92f46a8b281fb90ead28f3de3
Vladimir Reznichenko <kalessil@gmail.com>
2017-06-09 19:31:10 +0200

    IssetArgumentExistenceInspector: new patterns

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/nullCoalescing/IssetArgumentExistenceInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/nullCoalescing/IssetArgumentExistenceInspector.java
@@ -1,32 +1,18 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions.nullCoalescing;
 
-/*
- * This file is part of the Php Inspections (EA Extended) package.
- *
- * (c) Vladimir Reznichenko <kalessil@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.codeInsight.controlFlow.PhpControlFlowUtil;
 import com.jetbrains.php.codeInsight.controlFlow.instructions.PhpAccessVariableInstruction;
-import com.jetbrains.php.config.PhpLanguageFeature;
-import com.jetbrains.php.config.PhpLanguageLevel;
-import com.jetbrains.php.config.PhpProjectConfigurationFacade;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
-import com.jetbrains.php.lang.psi.elements.BinaryExpression;
-import com.jetbrains.php.lang.psi.elements.Function;
-import com.jetbrains.php.lang.psi.elements.Parameter;
-import com.jetbrains.php.lang.psi.elements.Variable;
+import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.List;
@@ -41,37 +27,54 @@ import java.util.Set;
  * file that was distributed with this source code.
  */
 
-public class NullCoalescingArgumentExistenceInspector extends BasePhpInspection {
+public class IssetArgumentExistenceInspector extends BasePhpInspection {
     private static final String messagePattern = "'$%v%' seems to be not defined in the scope.";
 
     @NotNull
     public String getShortName() {
-        return "NullCoalescingArgumentExistenceInspection";
+        return "IssetArgumentExistenceInspection";
     }
 
     @Override
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
-            public void visitPhpBinaryExpression(BinaryExpression expression) {
-                final PhpLanguageLevel phpVersion
-                        = PhpProjectConfigurationFacade.getInstance(holder.getProject()).getLanguageLevel();
-                if (!phpVersion.hasFeature(PhpLanguageFeature.COALESCE_OPERATOR)) {
-                    return;
-                }
-
+            @Override
+            public void visitPhpBinaryExpression(@NotNull BinaryExpression expression) {
                 final PsiElement leftOperand = expression.getLeftOperand();
                 if (PhpTokenTypes.opCOALESCE == expression.getOperationType() && leftOperand instanceof Variable) {
                     final Variable variable = (Variable) leftOperand;
-                    if (!this.isSuppliedFromOutside(variable)) {
+                    if (!this.isSuppliedFromOutside(variable, this.getSuppliedVariables(expression))) {
                         analyzeExistence(variable);
+                    }
+                }
+            }
+
+            @Override
+            public void visitPhpEmpty(@NotNull PhpEmpty expression) {
+                this.analyzeArgumentsExistence(expression.getVariables());
+            }
+
+            @Override
+            public void visitPhpIsset(@NotNull PhpIsset expression) {
+                this.analyzeArgumentsExistence(expression.getVariables());
+            }
+
+            private void analyzeArgumentsExistence(@NotNull PhpExpression[] arguments) {
+                final Set<String> parameters = arguments.length > 0 ? this.getSuppliedVariables(arguments[0]) : null;
+                for (final PhpExpression argument : arguments) {
+                    if (argument instanceof Variable) {
+                        final Variable variable = (Variable) argument;
+                        if (!this.isSuppliedFromOutside(variable, parameters)) {
+                            analyzeExistence(variable);
+                        }
                     }
                 }
             }
 
             private void analyzeExistence (@NotNull Variable variable) {
                 final Function scope = ExpressionSemanticUtil.getScope(variable);
-                if (null != scope) {
+                if (scope != null) {
                     final String variableName = variable.getName();
                     final PhpAccessVariableInstruction[] usages
                         = PhpControlFlowUtil.getFollowingVariableAccessInstructions(
@@ -81,7 +84,7 @@ public class NullCoalescingArgumentExistenceInspector extends BasePhpInspection 
                         );
                     final PhpAccessVariableInstruction firstUsage = usages.length > 0 ? usages[0] : null;
                     final PsiElement candidate                    = null == firstUsage ? null : firstUsage.getAnchor();
-                    if (null != candidate) {
+                    if (candidate != null) {
                         if (candidate == variable || candidate.getParent() == variable.getParent()) {
                             final String message = messagePattern.replace("%v%", variableName);
                             holder.registerProblem(variable, message, ProblemHighlightType.GENERIC_ERROR);
@@ -90,22 +93,27 @@ public class NullCoalescingArgumentExistenceInspector extends BasePhpInspection 
                 }
             }
 
-            private boolean isSuppliedFromOutside(@NotNull Variable variable) {
-                final Set<String> suppliedVariables = new HashSet<>();
-                final Function scope                = ExpressionSemanticUtil.getScope(variable);
-                if (null != scope) {
+            @Nullable
+            private Set<String> getSuppliedVariables(@NotNull PsiElement expression) {
+                final Function scope  = ExpressionSemanticUtil.getScope(expression);
+                Set<String> variables = null;
+                if (scope != null) {
+                    variables = new HashSet<>();
                     for (final Parameter parameter : scope.getParameters()) {
-                        suppliedVariables.add(parameter.getName());
+                        variables.add(parameter.getName());
                     }
                     final List<Variable> usedVariables = ExpressionSemanticUtil.getUseListVariables(scope);
-                    if (null != usedVariables) {
+                    if (usedVariables != null) {
                         for (final Variable usedVariable : usedVariables) {
-                            suppliedVariables.add(usedVariable.getName());
+                            variables.add(usedVariable.getName());
                         }
                     }
                 }
+                return variables;
+            }
 
-                return suppliedVariables.contains(variable.getName());
+            private boolean isSuppliedFromOutside(@NotNull Variable variable, @Nullable Set<String> suppliedVariables) {
+                return suppliedVariables != null && suppliedVariables.contains(variable.getName());
             }
         };
     }
--- src/test/resources/fixtures/controlFlow/isset-operations-variable-existence.php
+++ src/test/resources/fixtures/controlFlow/isset-operations-variable-existence.php
@@ -5,10 +5,10 @@ class CasesHolder
     public function interfaceVariables($x): array
     {
         $a = function ($y) use ($x) {
-            return $x ?? $y;
+            return [ $x ?? $y, isset($x), empty($x) ];
         };
         $b = function ($y) use ($x) {
-            return $y ?? $x;
+            return [ $y ?? $x, isset($y), empty($y) ];
         };
         return [$a, $b];
     }
@@ -16,8 +16,10 @@ class CasesHolder
     public function reportedCases()
     {
         $x = <error descr="'$x' seems to be not defined in the scope.">$x</error> ?? '';
+        $e = empty(<error descr="'$e' seems to be not defined in the scope.">$e</error>);
+        $i = isset(<error descr="'$i' seems to be not defined in the scope.">$i</error>);
         $z = <error descr="'$y' seems to be not defined in the scope.">$y</error> ?? '';
-        return [$x, $z];
+        return [$x, $z, $e, $i];
     }
 
     public function relyOnIde() {

f6887a150fba1d98c0ee8103298b690e34d9cad8
David Rodrigues <david.proweb@gmail.com>
2017-06-22 04:53:26 -0300

    [#361] DateIntervalSpecificationInspector: bug-fix (some cases has not been reported)
    * DateIntervalSpecificationInspector: code style;
    * DateIntervalSpecificationInspector: fixing the time specification ("T") to be not empty;

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/dateTime/DateIntervalSpecificationInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/dateTime/DateIntervalSpecificationInspector.java
@@ -2,7 +2,6 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.dateTime;
 
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -13,14 +12,16 @@ import com.jetbrains.php.lang.psi.elements.Variable;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.jetbrains.annotations.NotNull;
+
 /*
  * This file is part of the Php Inspections (EA Extended) package.
  *
+ * (c) David Rodrigues <david.proweb@gmail.com>
  * (c) Vladimir Reznichenko <kalessil@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
@@ -29,53 +30,64 @@ import java.util.regex.Pattern;
 
 public class DateIntervalSpecificationInspector extends BasePhpInspection {
     private static final String strProblemDescription = "Date interval specification seems to be invalid.";
+    private static final Pattern regexDateTimeAlike;
+    private static final Pattern regexRegular;
+    static {
+        regexDateTimeAlike = Pattern.compile("^P\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$");
+        regexRegular = Pattern.compile("^P((\\d+Y)?(\\d+M)?(\\d+D)?(\\d+W)?)?(T(?=\\d)(\\d+H)?(\\d+M)?(\\d+S)?)?$");
+    }
 
     @NotNull
     public String getShortName() {
         return "DateIntervalSpecificationInspection";
     }
 
-    final static private Pattern regexDateTimeAlike;
-    final static private Pattern regexRegular;
-    static {
-        regexDateTimeAlike = Pattern.compile("^P\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$");
-        regexRegular       = Pattern.compile("^P((\\d+Y)?(\\d+M)?(\\d+D)?(\\d+W)?)?(T(\\d+H)?(\\d+M)?(\\d+S)?)?$");
-    }
     @Override
     @NotNull
-    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+    public PsiElementVisitor buildVisitor(
+        @NotNull final ProblemsHolder holder,
+        final boolean isOnTheFly
+    ) {
         return new BasePhpElementVisitor() {
-            public void visitPhpNewExpression(NewExpression expression) {
+            public void visitPhpNewExpression(final NewExpression expression) {
                 /* before inspecting check parameters amount */
                 final PsiElement[] params = expression.getParameters();
-                if (1 != params.length) {
+
+                if (params.length != 1) {
                     return;
                 }
 
                 /* check if it's the target class */
                 final ClassReference classReference = expression.getClassReference();
-                if (null == classReference) {
+
+                if (classReference == null) {
                     return;
                 }
+
                 /* TODO: child classes support */
                 final String classFQN = classReference.getFQN();
-                if (StringUtil.isEmpty(classFQN) || !classFQN.equals("\\DateInterval")) {
+
+                if (!"\\DateInterval".equals(classFQN)) {
                     return;
                 }
 
                 /* now try getting string literal and test against valid patterns */
                 final StringLiteralExpression pattern = ExpressionSemanticUtil.resolveAsStringLiteral(params[0]);
-                if (null != pattern) {
+
+                if (pattern != null) {
                     final String patternText = pattern.getContents();
+
                     /* do not process patterns with inline variables */
-                    if (patternText.indexOf('$') >= 0 && PsiTreeUtil.findChildrenOfType(pattern, Variable.class).size() > 0) {
+                    if ((patternText.indexOf('$') >= 0) &&
+                        !PsiTreeUtil.findChildrenOfType(pattern, Variable.class).isEmpty()) {
                         return;
                     }
 
-                    Matcher regexMatcher = regexRegular.matcher(patternText);
+                    final Matcher regexMatcher = regexRegular.matcher(patternText);
                     if (!regexMatcher.find()) {
-                        regexMatcher = regexDateTimeAlike.matcher(patternText);
-                        if (!regexMatcher.find()) {
+                        final Matcher matcher = regexDateTimeAlike.matcher(patternText);
+
+                        if (!matcher.find()) {
                             /* report the issue */
                             holder.registerProblem(pattern, strProblemDescription, ProblemHighlightType.GENERIC_ERROR);
                         }

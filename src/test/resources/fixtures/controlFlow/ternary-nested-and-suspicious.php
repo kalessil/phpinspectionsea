db60ead9208e4114b1969583a51851ae7161fa27
Vladimir Reznichenko <kalessil@gmail.com>
2017-06-26 17:31:20 +0200

    Code grooming: review BO refactoring inspectors (test, more grooming, 9/13)

--- src/test/resources/fixtures/controlFlow/ternary-nested-and-suspicious.php
+++ src/test/resources/fixtures/controlFlow/ternary-nested-and-suspicious.php
@@ -1,6 +1,6 @@
 <?php
 
-    /* Pattern: values corruption might take place */
+    /* pattern: values corruption might take place */
     echo <weak_warning descr="This may not work as expected (wrap condition into '()' to specify intention).">$a & $b</weak_warning> ? 0 : 1;
     echo <weak_warning descr="This may not work as expected (wrap condition into '()' to specify intention).">$a | $b</weak_warning> ? 0 : 1;
     echo <weak_warning descr="This may not work as expected (wrap condition into '()' to specify intention).">$a - $b</weak_warning> ? 0 : 1;
@@ -14,10 +14,10 @@
     echo <error descr="This may not work as expected (wrap condition into '()' to specify intention).">$a and $b ? 0 : 1</error>;
     echo <error descr="This may not work as expected (wrap condition into '()' to specify intention).">$a or $b ? 0 : 1</error>;
 
-    /* Pattern: bugs */
+    /* pattern: bugs */
     echo (<error descr="True and false variants are identical, most probably this is a bug.">$b ? 0 : 0</error>);
 
-    /* Pattern: nested ternaries */
+    /* pattern: nested ternaries */
     echo $a ? $a : (<weak_warning descr="Nested ternary operator should not be used (maintainability issues).">$b ? $b : null</weak_warning>);
     echo !$a ? (<weak_warning descr="Nested ternary operator should not be used (maintainability issues).">$b ? $b : null</weak_warning>) : $a;
     echo (<weak_warning descr="Nested ternary operator should not be used (maintainability issues).">$b ? $b : null</weak_warning>) ? !$a : $a;
@@ -37,6 +37,3 @@
     echo $a === $b ? 0 : 1;
     echo $a !== $b ? 0 : 1;
     echo $a instanceof stdClass ? 0 : 1;
-
-
-

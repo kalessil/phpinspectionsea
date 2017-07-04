0821585c63ac17803cc804d2368b4748e3c7f855
Vladimir Reznichenko <kalessil@gmail.com>
2017-07-04 22:04:43 +0200

    VariableFunctionsUsageInspector: QF bug-fix

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/lang/VariableFunctionsUsageInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/lang/VariableFunctionsUsageInspectorTest.java
@@ -8,18 +8,19 @@ import com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions.V
 
 /* Test the same scenario, but for different language levels */
 final public class VariableFunctionsUsageInspectorTest extends CodeInsightFixtureTestCase {
-
     public void testIfFindsAllPatternsPhp53() {
         PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP530);
+        myFixture.enableInspections(new VariableFunctionsUsageInspector());
+
         myFixture.configureByFile("fixtures/lang/variable-functions-php53.php");
-        myFixture.enableInspections(VariableFunctionsUsageInspector.class);
         myFixture.testHighlighting(true, false, true);
     }
 
     public void testIfFindsAllPatternsPhp54() {
         PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP540);
+        myFixture.enableInspections(new VariableFunctionsUsageInspector());
+
         myFixture.configureByFile("fixtures/lang/variable-functions-php54.php");
-        myFixture.enableInspections(VariableFunctionsUsageInspector.class);
         myFixture.testHighlighting(true, false, true);
 
         for (IntentionAction fix : myFixture.getAllQuickFixes()) {
@@ -31,9 +32,9 @@ final public class VariableFunctionsUsageInspectorTest extends CodeInsightFixtur
 
     public void testFalsePositives() {
         PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP530);
+        myFixture.enableInspections(new VariableFunctionsUsageInspector());
+
         myFixture.configureByFile("fixtures/lang/variable-functions-false-positives.php");
-        myFixture.enableInspections(VariableFunctionsUsageInspector.class);
         myFixture.testHighlighting(true, false, true);
     }
-
 }

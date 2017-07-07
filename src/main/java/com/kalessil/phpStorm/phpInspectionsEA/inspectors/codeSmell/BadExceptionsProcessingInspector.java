82031ba78e718b597923dfe88cb20fb48f1eadb5
Vladimir Reznichenko <kalessil@gmail.com>
2017-07-07 09:42:01 +0200

    [#383] BadExceptionsProcessingInspection: new patterns

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/BadExceptionsProcessingInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/BadExceptionsProcessingInspector.java
@@ -2,9 +2,13 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.codeSmell;
 
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.lang.psi.elements.Catch;
 import com.jetbrains.php.lang.psi.elements.GroupStatement;
 import com.jetbrains.php.lang.psi.elements.Try;
+import com.jetbrains.php.lang.psi.elements.Variable;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
@@ -20,7 +24,9 @@ import org.jetbrains.annotations.NotNull;
  */
 
 public class BadExceptionsProcessingInspector extends BasePhpInspection {
-    private static final String messagePattern = "Consider moving non-related statements (%c% in total) outside the try-block or refactoring the try-body into a function/method.";
+    private static final String messagePattern          = "Consider moving non-related statements (%c% in total) outside the try-block or refactoring the try-body into a function/method.";
+    private static final String messageFailSilently     = "The exception being ignored, please don't fail silently and at least log it.";
+    private static final String messageChainedException = "The exception being ignored, please log it or use chained exceptions.";
 
     @NotNull
     public String getShortName() {
@@ -31,12 +37,36 @@ public class BadExceptionsProcessingInspector extends BasePhpInspection {
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
-            public void visitPhpTry(Try tryStatement) {
+            @Override
+            public void visitPhpTry(@NotNull Try tryStatement) {
                 final GroupStatement body  = ExpressionSemanticUtil.getGroupStatement(tryStatement);
-                final int expressionsCount = null == body ? 0 : ExpressionSemanticUtil.countExpressionsInGroup(body);
+                final int expressionsCount = body == null ? 0 : ExpressionSemanticUtil.countExpressionsInGroup(body);
                 if (expressionsCount > 3) {
                     final String message = messagePattern.replace("%c%", String.valueOf(expressionsCount));
                     holder.registerProblem(tryStatement.getFirstChild(), message, ProblemHighlightType.WEAK_WARNING);
+                }
+            }
+
+            @Override
+            public void visitPhpCatch(@NotNull Catch catchStatement) {
+                final Variable variable   = catchStatement.getException();
+                final GroupStatement body = ExpressionSemanticUtil.getGroupStatement(catchStatement);
+                if (variable != null && body != null) {
+                    final String variableName = variable.getName();
+                    boolean isVariableUsed    = false;
+                    for (final Variable usedVariable :PsiTreeUtil.findChildrenOfType(body, Variable.class)) {
+                        if (usedVariable.getName().equals(variableName)) {
+                            isVariableUsed = true;
+                            break;
+                        }
+                    }
+                    if (!isVariableUsed) {
+                        if (ExpressionSemanticUtil.countExpressionsInGroup(body) == 0) {
+                            holder.registerProblem(variable, messageFailSilently, ProblemHighlightType.WEAK_WARNING);
+                        } else {
+                            holder.registerProblem(variable, messageChainedException, ProblemHighlightType.WEAK_WARNING);
+                        }
+                    }
                 }
             }
         };
--- src/test/resources/fixtures/codeStyle/bad-exceptions-handling.php
+++ src/test/resources/fixtures/codeStyle/bad-exceptions-handling.php
@@ -4,7 +4,10 @@
         echo 1;
         echo 2;
         echo 3;
-    } catch (Exception $failSilently) {
+    } catch (RuntimeException <weak_warning descr="The exception being ignored, please don't fail silently and at least log it.">$failSilently</weak_warning>) {
+        /* fail silently */
+    } catch (Exception <weak_warning descr="The exception being ignored, please log it or use chained exceptions.">$chainedCalls</weak_warning>) {
+        throw new RuntimeException('...');
     }
 
     <weak_warning descr="Consider moving non-related statements (4 in total) outside the try-block or refactoring the try-body into a function/method.">try</weak_warning> {
@@ -12,5 +15,6 @@
         echo 2;
         echo 3;
         echo 4;
-    } catch (Exception $failSilently) {
+    } catch (Exception $exception) {
+        throw new RuntimeException('', null, $exception);
     }

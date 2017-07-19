48f3a2a07194aaefc7a111452d06396cb8ff18a0
Vladimir Reznichenko <kalessil@gmail.com>
2017-07-19 21:31:10 +0200

    [#407] BadExceptionsProcessingInspector: fixed "Empty PSI element" exception

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/BadExceptionsProcessingInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/BadExceptionsProcessingInspector.java
@@ -11,6 +11,7 @@ import com.jetbrains.php.lang.psi.elements.Variable;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 /*
@@ -42,7 +43,7 @@ public class BadExceptionsProcessingInspector extends BasePhpInspection {
                 final int expressionsCount = body == null ? 0 : ExpressionSemanticUtil.countExpressionsInGroup(body);
                 if (expressionsCount > 3) {
                     final String message = messagePattern.replace("%c%", String.valueOf(expressionsCount));
-                    holder.registerProblem(tryStatement.getFirstChild(), message, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                    holder.registerProblem(tryStatement.getFirstChild(), message);
                 }
             }
 
@@ -52,18 +53,20 @@ public class BadExceptionsProcessingInspector extends BasePhpInspection {
                 final GroupStatement body = ExpressionSemanticUtil.getGroupStatement(catchStatement);
                 if (variable != null && body != null) {
                     final String variableName = variable.getName();
-                    boolean isVariableUsed    = false;
-                    for (final Variable usedVariable :PsiTreeUtil.findChildrenOfType(body, Variable.class)) {
-                        if (usedVariable.getName().equals(variableName)) {
-                            isVariableUsed = true;
-                            break;
+                    if (!StringUtils.isEmpty(variableName)) { /* incomplete catch statement */
+                        boolean isVariableUsed = false;
+                        for (final Variable usedVariable : PsiTreeUtil.findChildrenOfType(body, Variable.class)) {
+                            if (usedVariable.getName().equals(variableName)) {
+                                isVariableUsed = true;
+                                break;
+                            }
                         }
-                    }
-                    if (!isVariableUsed) {
-                        if (ExpressionSemanticUtil.countExpressionsInGroup(body) == 0) {
-                            holder.registerProblem(variable, messageFailSilently, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
-                        } else {
-                            holder.registerProblem(variable, messageChainedException, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                        if (!isVariableUsed) {
+                            if (ExpressionSemanticUtil.countExpressionsInGroup(body) == 0) {
+                                holder.registerProblem(variable, messageFailSilently);
+                            } else {
+                                holder.registerProblem(variable, messageChainedException);
+                            }
                         }
                     }
                 }

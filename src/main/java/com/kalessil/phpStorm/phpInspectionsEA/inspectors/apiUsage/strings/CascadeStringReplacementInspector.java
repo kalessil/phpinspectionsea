d7853b3bee1420cb4aa8a42467d4c845b70b4654
Vladimir Reznichenko <kalessil@gmail.com>
2017-07-21 08:47:40 +0200

    [#399] CascadeStringReplacementInspector: resolved false-positives (used/stored expressions are not matching)

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/strings/CascadeStringReplacementInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/strings/CascadeStringReplacementInspector.java
@@ -1,8 +1,8 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.strings;
 
+import com.intellij.codeInsight.PsiEquivalenceUtil;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
@@ -10,6 +10,7 @@ import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
@@ -30,10 +31,11 @@ public class CascadeStringReplacementInspector extends BasePhpInspection {
         return new BasePhpElementVisitor() {
             /* TODO: support mb_* function */
 
-            public void visitPhpAssignmentExpression(AssignmentExpression assignmentExpression) {
+            @Override
+            public void visitPhpAssignmentExpression(@NotNull AssignmentExpression assignmentExpression) {
                 /* try getting function reference, indicating pattern match */
                 final FunctionReference functionCall = getStrReplaceReference(assignmentExpression);
-                if (null != functionCall) {
+                if (functionCall != null) {
                     /* get previous non-comment, non-php-doc expression */
                     PsiElement previous = assignmentExpression.getParent().getPrevSibling();
                     while (null != previous && !(previous instanceof PhpPsiElement)) {
@@ -44,7 +46,7 @@ public class CascadeStringReplacementInspector extends BasePhpInspection {
                     }
                     final PsiElement[] params = functionCall.getParameters();
 
-
+                    /* TODO: dedicated method */
                     /* === cascade calls check === */
                     /* previous assignment should be inspected, probably we can merge this one into it */
                     if (
@@ -54,34 +56,36 @@ public class CascadeStringReplacementInspector extends BasePhpInspection {
                         /* ensure linking variable discoverable and call contains all params */
                         final PsiElement glueVariable = ((AssignmentExpression) previous.getFirstChild()).getVariable();
                         if (3 == params.length && glueVariable instanceof Variable && params[2] instanceof Variable) {
-                            /* extract variable names from link points */
-                            final String previousVariable = ((Variable) glueVariable).getName();
-                            final String callSubject      = ((Variable) params[2]).getName();
+                            /* ensure previous, used and result storage is the same variable */
+                            final String previousVariableName  = ((Variable) glueVariable).getName();
+                            final String callSubjectName       = ((Variable) params[2]).getName();
+                            final PsiElement callResultStorage = assignmentExpression.getVariable();
                             if (
-                                !StringUtil.isEmpty(callSubject) && !StringUtil.isEmpty(previousVariable) &&
-                                callSubject.equals(previousVariable)
+                                callResultStorage != null && callSubjectName.equals(previousVariableName) &&
+                                PsiEquivalenceUtil.areElementsEquivalent(glueVariable, callResultStorage)
                             ) {
-                                holder.registerProblem(functionCall, messageCascading, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
+                                holder.registerProblem(functionCall, messageCascading);
                             }
                         }
                     }
 
 
+                    /* TODO: dedicated method */
                     /* === nested calls check === */
-                    if (3 == params.length && params[2] instanceof FunctionReference) {
+                    if (3 == params.length && OpenapiTypesUtil.isFunctionReference(params[2])) {
                         /* ensure 3rd argument is nested call of str_replace */
                         final String functionName = ((FunctionReference) params[2]).getName();
-                        if (!StringUtil.isEmpty(functionName) && functionName.equals("str_replace")) {
+                        if (functionName != null && functionName.equals("str_replace")) {
                             holder.registerProblem(params[2], messageNesting, ProblemHighlightType.GENERIC_ERROR_OR_WARNING);
                         }
                     }
 
-
+                    /* TODO: dedicated method */
                     /* === replacements uniqueness check === */
                     if (3 == params.length && params[1] instanceof ArrayCreationExpression) {
                         final HashSet<String> replacements = new HashSet<>();
 
-                        for (PsiElement oneReplacement : params[1].getChildren()) {
+                        for (final PsiElement oneReplacement : params[1].getChildren()) {
                             if (oneReplacement instanceof PhpPsiElement) {
                                 final PhpPsiElement item = ((PhpPsiElement) oneReplacement).getFirstPsiChild();
                                 /* abort on non-string entries  */
@@ -97,7 +101,7 @@ public class CascadeStringReplacementInspector extends BasePhpInspection {
                         final int uniqueReplacements = replacements.size();
                         replacements.clear();
 
-                        if (1 == uniqueReplacements) {
+                        if (uniqueReplacements == 1) {
                             holder.registerProblem(params[1], messageReplacements, ProblemHighlightType.WEAK_WARNING);
                         }
                     }
@@ -106,17 +110,15 @@ public class CascadeStringReplacementInspector extends BasePhpInspection {
         };
     }
 
-    private FunctionReference getStrReplaceReference(AssignmentExpression assignment) {
+    private FunctionReference getStrReplaceReference(@NotNull AssignmentExpression assignment) {
+        FunctionReference result = null;
         final PsiElement value = ExpressionSemanticUtil.getExpressionTroughParenthesis(assignment.getValue());
-        /* ensure function and not method reference */
-        if (value instanceof FunctionReference) {
+        if (OpenapiTypesUtil.isFunctionReference(value)) {
             final String functionName = ((FunctionReference) value).getName();
-            /* is target function */
-            if (!StringUtil.isEmpty(functionName) && functionName.equals("str_replace")) {
-                return (FunctionReference) value;
+            if (functionName != null && functionName.equals("str_replace")) {
+                result = (FunctionReference) value;
             }
         }
-
-        return null;
+        return result;
     }
 }
--- src/test/resources/fixtures/api/cascade-str-replace.php
+++ src/test/resources/fixtures/api/cascade-str-replace.php
@@ -6,10 +6,10 @@
     $str  = str_replace('-', '_', 'Some text here');
     /** dock-block should not break inspection */
     /** multiple dock-blocks should not break inspection */
-    $str = <warning descr="This str_replace(...) call can be merged with the previous.">str_replace</warning> (' ', '-', $str);
+    $str = <warning descr="This str_replace(...) call can be merged with the previous.">str_replace(' ', '-', $str)</warning>;
     /** dock-block should not break inspection */
     /** multiple dock-blocks should not break inspection */
-    $str = <warning descr="This str_replace(...) call can be merged with the previous.">str_replace</warning> (' ', '-', $str);
+    $str = <warning descr="This str_replace(...) call can be merged with the previous.">str_replace(' ', '-', $str)</warning>;
 
     /* nested calls pattern */
     $str = str_replace(
@@ -23,3 +23,7 @@
         <weak_warning descr="Can be replaced with the string duplicated in array.">array('1', '1')</weak_warning>,
         $str1
     );
+
+    /* false-positives: source and result storage containers are not matching */
+    $a = str_replace('', '', '');
+    $b = str_replace('', '', $a);

e794810d59f7f2ec5c196aa6394ccaa4fa32905d
David Rodrigues <david.proweb@gmail.com>
2017-07-23 17:10:30 -0300

    [#146] SelfClassReferencingInspector: implemented
    * SelfClassReferencingInspector: base implementation;
    * SelfClassReferencingInspector: suggests replacement for `MyClass::class` to just `__CLASS__`;
    * SelfClassReferencingInspector: added an option to do it on opposite way;
    * SelfClassReferencingInspector: quick-fix implemented with tests;
    * SelfClassReferencingInspector: false-positive - self::class and static::class should be ignored;
    * SelfClassReferencingInspector: false-positive - on reserve mode, self::class should be replaced from self to MyClass, not from __CLASS__;
    * SelfClassReferencingInspector: false-positive - on reserve mode, MyClass::class and similar should be ignored;
    * SelfClassReferencingInspector: dead code removed;
    * SelfClassReferencingInspector: added copyright;
    * SelfClassReferencingInspector: travis - checking for anonymous classes from another way;

---
+++ src/test/resources/fixtures/codeStyle/self-class-referencing.fixed.php
@@ -0,0 +1,30 @@
+<?php
+
+class MyClass {
+    public function method() {
+        new self;
+        self::CONSTANT;
+        self::staticMethod();
+        self::$staticProperty;
+
+        __CLASS__;
+
+        // False-positives:
+        self::class;
+        static::class;
+    }
+
+    // Not applicable: anonymous class is another context.
+    public function anonymousClass() {
+        return new class {
+            public function method() {
+                new MyClass;
+                MyClass::CONSTANT;
+                MyClass::staticMethod();
+                MyClass::$staticProperty;
+
+                MyClass::class;
+            }
+        };
+    }
+}
---
+++ src/test/resources/fixtures/codeStyle/self-class-referencing.php
@@ -0,0 +1,30 @@
+<?php
+
+class MyClass {
+    public function method() {
+        new <weak_warning descr="Class reference \"MyClass\" could be replaced by \"self\"">MyClass</weak_warning>;
+        <weak_warning descr="Class reference \"MyClass\" could be replaced by \"self\"">MyClass</weak_warning>::CONSTANT;
+        <weak_warning descr="Class reference \"MyClass\" could be replaced by \"self\"">MyClass</weak_warning>::staticMethod();
+        <weak_warning descr="Class reference \"MyClass\" could be replaced by \"self\"">MyClass</weak_warning>::$staticProperty;
+
+        <weak_warning descr="Class reference \"MyClass::class\" could be replaced by \"__CLASS__\"">MyClass::class</weak_warning>;
+
+        // False-positives:
+        self::class;
+        static::class;
+    }
+
+    // Not applicable: anonymous class is another context.
+    public function anonymousClass() {
+        return new class {
+            public function method() {
+                new MyClass;
+                MyClass::CONSTANT;
+                MyClass::staticMethod();
+                MyClass::$staticProperty;
+
+                MyClass::class;
+            }
+        };
+    }
+}

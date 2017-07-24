12db504b8be86676f76d58d7377e45bd728c60d8
Vladimir Reznichenko <kalessil@gmail.com>
2017-07-24 10:00:49 +0200

    [#302] HostnameSubstitutionInspector: more test cases.

--- src/test/resources/fixtures/security/hostname-substitution.php
+++ src/test/resources/fixtures/security/hostname-substitution.php
@@ -4,6 +4,11 @@
     echo '@' . <error descr="...">$_SERVER['HTTP_HOST']</error>;
     echo '@' . <error descr="...">strtolower($_SERVER['HTTP_HOST'])</error>;
 
+    /* false-positives: there are no email-alike criterion matched */
+    echo $_SERVER['HTTP_HOST'];
+    echo 'HTTP_HOST: ' . $_SERVER['HTTP_HOST'];
+    echo 'SERVER_NAME: ' . $_SERVER['SERVER_NAME'];
+
     $host   = strtolower(<error descr="...">$_SERVER['HTTP_HOST']</error>);
     $domain = strtolower(<error descr="...">$_SERVER['SERVER_NAME']</error>);
     $email  = strtolower(<error descr="...">$_SERVER['HTTP_HOST']</error>);
@@ -15,4 +20,20 @@
             $domain = $_SERVER['SERVER_NAME'];
         }
         echo '' . '@' . <error descr="...">$domain</error>;
+    }
+
+    function valid_cases() {
+        $server = $_SERVER['SERVER_NAME'] ?? gethostname();
+        $host   = $_SERVER['HTTP_HOST']   ?? gethostname();
+        /* TODO: this needs to be reported as no in_array presented */
+        $domain = $server === $host /*&& in_array($server, [], true)*/ ? $server : 'localhost';
+        return $domain;
+    }
+
+    function reported_case_simplified(){
+        $server = strtolower($_SERVER['SERVER_NAME']);
+        if (strpos($server,'www.') === 0) {
+            $server = substr($server, 4);
+        }
+        return 'me@' . <error descr="...">$server</error>;
     }

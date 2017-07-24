35213ef448a6c71b8cc4c8ca9b3bf8244263661e
Vladimir Reznichenko <kalessil@gmail.com>
2017-07-24 10:37:58 +0200

    ConstantCanBeUsedInspectorTest: added a todo.

--- src/test/resources/fixtures/api/constants-usage.fixed.php
+++ src/test/resources/fixtures/api/constants-usage.fixed.php
@@ -4,6 +4,7 @@
     echo PHP_VERSION;
     echo __CLASS__;
     echo M_PI;
+    // echo [weak_warning descr="PHP_OS constant should be used instead."]php_uname('s')[/weak_warning>];
 
     echo phpversion('extension-name');
     echo phpversion($object);
--- src/test/resources/fixtures/api/constants-usage.php
+++ src/test/resources/fixtures/api/constants-usage.php
@@ -4,6 +4,7 @@
     echo <weak_warning descr="PHP_VERSION constant should be used instead.">phpversion()</weak_warning>;
     echo <weak_warning descr="__CLASS__ constant should be used instead.">get_class()</weak_warning>;
     echo <weak_warning descr="M_PI constant should be used instead.">pi()</weak_warning>;
+    // echo [weak_warning descr="PHP_OS constant should be used instead."]php_uname('s')[/weak_warning>];
 
     echo phpversion('extension-name');
     echo phpversion($object);

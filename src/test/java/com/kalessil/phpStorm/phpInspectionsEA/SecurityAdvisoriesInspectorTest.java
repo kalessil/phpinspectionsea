7bb41ae77714ce365a8b893d1fab7609646c0918
David Rodrigues <david.proweb@gmail.com>
2017-07-30 03:38:56 -0300

    SecurityAdvisoriesInspector: code-coverage;

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/SecurityAdvisoriesInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/SecurityAdvisoriesInspectorTest.java
@@ -3,6 +3,13 @@ package com.kalessil.phpStorm.phpInspectionsEA;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.security.SecurityAdvisoriesInspector;
 
 public final class SecurityAdvisoriesInspectorTest extends PhpCodeInsightFixtureTestCase {
+    // Code-coverage.
+    public void testNoComposerJson() {
+        myFixture.enableInspections(new SecurityAdvisoriesInspector());
+        myFixture.configureByFile("fixtures/securityAdvisories/any.json");
+        myFixture.testHighlighting(true, false, true);
+    }
+
     public void testHasAdvisory() {
         myFixture.enableInspections(new SecurityAdvisoriesInspector());
         myFixture.configureByFile("fixtures/securityAdvisories/hasAdvisories/composer.json");
@@ -36,6 +43,24 @@ public final class SecurityAdvisoriesInspectorTest extends PhpCodeInsightFixture
     public void testDevDependenciesInProdPackage() {
         myFixture.enableInspections(new SecurityAdvisoriesInspector());
         myFixture.configureByFile("fixtures/securityAdvisories/devDependenciesInProdPackage/composer.json");
+        myFixture.testHighlighting(true, false, true);
+    }
+
+    public void testNoRequire() {
+        myFixture.enableInspections(new SecurityAdvisoriesInspector());
+        myFixture.configureByFile("fixtures/securityAdvisories/noRequire/composer.json");
+        myFixture.testHighlighting(true, false, true);
+    }
+
+    public void testInvalidRequireList() {
+        myFixture.enableInspections(new SecurityAdvisoriesInspector());
+        myFixture.configureByFile("fixtures/securityAdvisories/invalidRequireList/composer.json");
+        myFixture.testHighlighting(true, false, true);
+    }
+
+    public void testInvalidRequirePropertyValue() {
+        myFixture.enableInspections(new SecurityAdvisoriesInspector());
+        myFixture.configureByFile("fixtures/securityAdvisories/invalidRequirePropertyValue/composer.json");
         myFixture.testHighlighting(true, false, true);
     }
 }

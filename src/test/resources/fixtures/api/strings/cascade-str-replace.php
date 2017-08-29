4be256cf5501b739c29b15c490e196d35a8c7700
Vladimir Reznichenko <kalessil@gmail.com>
2017-08-29 09:57:07 +0200

    [#443] CascadeStringReplacementInspector: QF implemented (#491)

--- src/test/resources/fixtures/api/strings/cascade-str-replace.php
+++ src/test/resources/fixtures/api/strings/cascade-str-replace.php
@@ -1,9 +1,9 @@
 <?php
 
-    $str1 = str_replace('-', '_', 'Some text here');
+    $str1 = str_replace('-', '_', '...');
 
     /* sequential calls pattern */
-    $str  = str_replace('-', '_', 'Some text here');
+    $str  = str_replace('-', '_', '...');
     /** dock-block should not break inspection */
     /** multiple dock-blocks should not break inspection */
     $str = <warning descr="This str_replace(...) call can be merged with the previous.">str_replace(' ', '-', $str)</warning>;
@@ -12,18 +12,36 @@
     $str = <warning descr="This str_replace(...) call can be merged with the previous.">str_replace(' ', '-', $str)</warning>;
 
     /* nested calls pattern */
-    $str = str_replace(
+    $nested = str_replace(
         ' ', '-',
-        <warning descr="This str_replace(...) call can be merged with its parent.">str_replace(' ', '-', $str)</warning>
+        <warning descr="This str_replace(...) call can be merged with its parent.">str_replace(' ', '-', '...')</warning>
     );
 
     /* parameters simplification pattern */
-    $str2 = str_replace(
-        array('1', '1'),
+    $simplify = str_replace(
+        array ('1', '1'),
         <weak_warning descr="Can be replaced with the string duplicated in array.">array('1', '1')</weak_warning>,
-        $str1
+        '...'
     );
 
     /* false-positives: source and result storage containers are not matching */
     $a = str_replace('', '', '');
     $b = str_replace('', '', $a);
+
+    /* QF correctness cases */
+    function fix_correctness()
+    {
+        $one = str_replace('1', '.', '...');
+        $one = <warning descr="This str_replace(...) call can be merged with the previous.">str_replace('2', '.', $one)</warning>;
+        $one = <warning descr="This str_replace(...) call can be merged with the previous.">str_replace('3', '_', $one)</warning>;
+
+        $two = str_replace('3', '_', <warning descr="This str_replace(...) call can be merged with its parent.">str_replace('2', '.', str_replace('1', '.', '...'))</warning>);
+
+        $three = str_replace('1', '.', <warning descr="This str_replace(...) call can be merged with its parent.">str_replace('2', '.', '...')</warning>);
+        $four  = str_replace('1', '.', <warning descr="This str_replace(...) call can be merged with its parent.">str_replace(['2'], ['.'], '...')</warning>);
+        $five  = str_replace(['1'], '.', <warning descr="This str_replace(...) call can be merged with its parent.">str_replace('2', '.', '...')</warning>);
+        $six   = str_replace(['1'], '.', <warning descr="This str_replace(...) call can be merged with its parent.">str_replace(['2'], ['.'], '...')</warning>);
+
+        $seven = str_replace(['1'], 'x', <warning descr="This str_replace(...) call can be merged with its parent.">str_replace(['2'], ['y'], '...')</warning>);
+        $eight = str_replace(['1', '2'], 'x', <warning descr="This str_replace(...) call can be merged with its parent.">str_replace(['3'], 'y', '...')</warning>);
+    }

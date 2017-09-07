043d36a12cae0b4cd9a99106fc369e8f67784e12
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-07 19:07:54 +0200

    [#443] MkdirRaceConditionInspector: enhance test cases.

--- src/test/resources/fixtures/pitfalls/mkdir-race-conditions.php
+++ src/test/resources/fixtures/pitfalls/mkdir-race-conditions.php
@@ -1,21 +1,21 @@
 <?php
 
+    /* case: just a call -> conditionally throw exception */
     <error descr="Following construct should be used: 'if (!mkdir('...') && !is_dir('...')) { ... }'.">mkdir('...');</error>
     <error descr="Following construct should be used: 'if (!mkdir('...') && !is_dir('...')) { ... }'.">@mkdir('...');</error>
-
-    /* false-positive: result saved */
-    $result = mkdir('...');
-
-
     if (!is_dir('...')) {
         <error descr="Following construct should be used: 'if (!mkdir('...') && !is_dir('...')) { ... }'.">mkdir('...');</error>
     }
 
-    <error descr="Following construct should be used: 'if (!mkdir('...') && !is_dir('...')) { ... }'.">if</error> ((!mkdir('...'))) {}
+    /* false-positive: result saved */
+    $result = mkdir('...');
 
-
+    /* case: incomplete conditions */
+    if (<error descr="Some check are missing: 'mkdir('...') || is_dir('...')'.">(!mkdir('...'))</error>) {}
+    if (<error descr="Some check are missing: 'mkdir('...') || is_dir('...')'.">(!@mkdir('...'))</error>) {}
     if (!is_dir('...') && <error descr="Some check are missing: '!mkdir('...') && !is_dir('...')'.">!mkdir('...')</error>) {}
     if (is_dir('...') || <error descr="Some check are missing: 'mkdir('...') || is_dir('...')'.">mkdir('...')</error>) {}
 
     /* false-positive: re-checked afterwards */
     if (!is_dir('...') && !mkdir('...') && !is_dir('...')) {}
+    if (is_dir('...') || mkdir('...') || is_dir('...')) {}

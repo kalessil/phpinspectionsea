261fd11915fee9365df056cc5724a9167081c06c
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-07 09:33:02 +0200

    [#505] ReferencingObjectsInspector: resolved false-positives (nullable scalars).

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/codeStyle/ReferencingObjectsInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/codeStyle/ReferencingObjectsInspectorTest.java
@@ -1,13 +1,19 @@
 package com.kalessil.phpStorm.phpInspectionsEA.codeStyle;
 
+import com.intellij.codeInsight.intention.IntentionAction;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.codeStyle.ReferencingObjectsInspector;
 
 final public class ReferencingObjectsInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new ReferencingObjectsInspector());
-
         myFixture.configureByFile("fixtures/codeStyle/referencing-objects.php");
         myFixture.testHighlighting(true, false, true);
+
+        for (final IntentionAction fix : myFixture.getAllQuickFixes()) {
+            myFixture.launchAction(fix);
+        }
+        myFixture.setTestDataPath(".");
+        myFixture.checkResultByFile("fixtures/codeStyle/referencing-objects.fixed.php");
     }
 }

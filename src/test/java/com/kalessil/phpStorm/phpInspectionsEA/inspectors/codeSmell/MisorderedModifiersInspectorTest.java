6a14f0d0894ca6d7336ab35e3db3dd421ea63dae
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-08 11:29:54 +0200

    MisorderedModifiersInspector: code grooming

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/MisorderedModifiersInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeSmell/MisorderedModifiersInspectorTest.java
@@ -4,17 +4,15 @@ import com.intellij.codeInsight.intention.IntentionAction;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.codeStyle.MisorderedModifiersInspector;
 
-public class MisorderedModifiersInspectorTest extends PhpCodeInsightFixtureTestCase {
+final public class MisorderedModifiersInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new MisorderedModifiersInspector());
-
         myFixture.configureByFile("fixtures/codeStyle/misordered-modifiers.php");
         myFixture.testHighlighting(true, false, true);
 
         for (final IntentionAction fix : myFixture.getAllQuickFixes()) {
             myFixture.launchAction(fix);
         }
-
         myFixture.setTestDataPath(".");
         myFixture.checkResultByFile("fixtures/codeStyle/misordered-modifiers.fixed.php");
     }

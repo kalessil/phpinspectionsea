3faf2578a2430ea9efa525f51dbb1ae8bcedc465
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-09 21:23:33 +0200

    Code grooming: don't use impl-classes (ControlStatementImpl)

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/UsingInclusionOnceReturnValueInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/UsingInclusionOnceReturnValueInspector.java
@@ -2,20 +2,28 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions;
 
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
+import com.jetbrains.php.lang.psi.elements.ControlStatement;
 import com.jetbrains.php.lang.psi.elements.Include;
 import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
-import com.jetbrains.php.lang.psi.elements.impl.ControlStatementImpl;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 public class UsingInclusionOnceReturnValueInspector extends BasePhpInspection {
     private static final String message = "Only the first call returns the proper/expected result. Subsequent calls will return 'true'.";
@@ -29,14 +37,13 @@ public class UsingInclusionOnceReturnValueInspector extends BasePhpInspection {
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
-            public void visitPhpInclude(Include include) {
+            @Override
+            public void visitPhpInclude(@NotNull Include include) {
                 final PsiElement parent = include.getParent();
-                if (parent instanceof ControlStatementImpl || !OpenapiTypesUtil.isStatementImpl(parent)) {
-                    if (null == include.getArgument() || !include.getFirstChild().getText().endsWith("_once")) {
-                        return;
+                if (parent instanceof ControlStatement || !OpenapiTypesUtil.isStatementImpl(parent)) {
+                    if (include.getArgument() != null && include.getFirstChild().getText().endsWith("_once")) {
+                        holder.registerProblem(include, message, new TheLocalFix());
                     }
-
-                    holder.registerProblem(include, message, ProblemHighlightType.GENERIC_ERROR, new TheLocalFix());
                 }
             }
         };
@@ -57,12 +64,12 @@ public class UsingInclusionOnceReturnValueInspector extends BasePhpInspection {
 
         @Override
         public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-            PsiElement target = descriptor.getPsiElement();
+            final PsiElement target = descriptor.getPsiElement();
             if (target instanceof Include) {
-                boolean isInclude = PhpTokenTypes.kwINCLUDE_ONCE == target.getFirstChild().getNode().getElementType();
-                String pattern    = isInclude ? "include ''" : "require ''";
-
-                PhpPsiElement replacement = PhpPsiElementFactory.createPhpPsiFromText(project, Include.class, pattern);
+                final boolean isInclude = OpenapiTypesUtil.is(target.getFirstChild(), PhpTokenTypes.kwINCLUDE_ONCE);
+                final String pattern    = isInclude ? "include ''" : "require ''";
+                final PhpPsiElement replacement
+                        = PhpPsiElementFactory.createPhpPsiFromText(project, Include.class, pattern);
                 //noinspection ConstantConditions pattern hardcoded and arg is checked - safe
                 ((Include) replacement).getArgument().replace(((Include) target).getArgument());
                 target.replace(replacement);

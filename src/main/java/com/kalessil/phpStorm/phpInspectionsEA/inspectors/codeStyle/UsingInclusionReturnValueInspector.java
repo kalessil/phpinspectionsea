e723b2093cb31c210ccd9c49e561f623c50a0eb9
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-10 12:37:59 +0200

    UsingInclusionReturnValueInspector: added UTs

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeStyle/UsingInclusionReturnValueInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeStyle/UsingInclusionReturnValueInspector.java
@@ -1,18 +1,25 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.codeStyle;
 
-import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.psi.elements.Include;
-import com.jetbrains.php.lang.psi.elements.impl.ControlStatementImpl;
-import com.jetbrains.php.lang.psi.elements.impl.StatementImpl;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 public class UsingInclusionReturnValueInspector  extends BasePhpInspection {
-    private static final String message = "Operating on this return mechanism is considered a bad practice. OOP can be used instead.";
+    private static final String message
+            = "Operating on this return mechanism is considered a bad practice. OOP can be used instead.";
 
     @NotNull
     public String getShortName() {
@@ -23,10 +30,10 @@ public class UsingInclusionReturnValueInspector  extends BasePhpInspection {
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
-            public void visitPhpInclude(Include include) {
-                final PsiElement parent = include.getParent();
-                if (parent instanceof ControlStatementImpl || !(parent instanceof StatementImpl)) {
-                    holder.registerProblem(include, message, ProblemHighlightType.WEAK_WARNING);
+            @Override
+            public void visitPhpInclude(@NotNull Include include) {
+                if (!OpenapiTypesUtil.isStatementImpl(include.getParent())) {
+                    holder.registerProblem(include, message);
                 }
             }
         };
---
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/codeStyle/UsingInclusionReturnValueInspectorTest.java
@@ -0,0 +1,12 @@
+package com.kalessil.phpStorm.phpInspectionsEA.codeStyle;
+
+import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.codeStyle.UsingInclusionReturnValueInspector;
+
+final public class UsingInclusionReturnValueInspectorTest extends PhpCodeInsightFixtureTestCase {
+    public void testIfFindsAllPatterns() {
+        myFixture.enableInspections(new UsingInclusionReturnValueInspector());
+        myFixture.configureByFile("fixtures/codeStyle/using-inclusion-result.php");
+        myFixture.testHighlighting(true, false, true);
+    }
+}

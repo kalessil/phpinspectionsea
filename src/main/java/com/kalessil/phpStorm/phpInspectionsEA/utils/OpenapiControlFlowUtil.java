c93b0ba7f8faae84c7f158dafcc8557d4a7c9414
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-12 15:53:51 +0200

    Code grooming: introduce skeleton of OpenapiControlFlowUtil

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/utils/OpenapiControlFlowUtil.java
@@ -0,0 +1,5 @@
+package com.kalessil.phpStorm.phpInspectionsEA.utils;
+
+final public class OpenapiControlFlowUtil {
+    /* TODO: isolate PhpControlFlowUtil.getFollowingVariableAccessInstructions */
+}

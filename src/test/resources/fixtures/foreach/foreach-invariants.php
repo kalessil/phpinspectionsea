5486f48a0934a556b0747fcbfc5d2a37b2ab2512
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-14 12:44:09 +0200

    [#451] ForeachInvariantsInspector: added a new test-cases

--- src/test/resources/fixtures/foreach/foreach-invariants.php
+++ src/test/resources/fixtures/foreach/foreach-invariants.php
@@ -25,19 +25,26 @@
         }
     }
 
-    /* false-positives: no subject index based access */
-    for ($i = 0, $max = count($arr); $i < $max; ++$i, $z = 0) {
-        echo $i;
-    }
-    for ($max = count($arr), $i = 0; $i < $max; $i++, $z = 0) {
-        echo $i;
-    }
-    for ($i = 0; $i < 10; $i++, $z = 0) {
-        echo $arr[$i];
-    }
+    function false_positives_holder($arr) {
+        /* false-positives: complex repetitive checks */
+        for ($i = 0, $max = count($arr); $i < $max || $condition; ++$i, $z = 0) {
+            $condition = $arr[$i] % 0;
+        }
 
-    /* false-positives: multiple containers */
-    $col = array();
-    for ($i = 0, $max = count($arr); $i < $max; $i++, $z = 0) {
-        echo $arr[$i], $col[$i];
+        /* false-positives: no subject index based access */
+        for ($i = 0, $max = count($arr); $i < $max; ++$i, $z = 0) {
+            echo $i;
+        }
+        for ($max = count($arr), $i = 0; $i < $max; $i++, $z = 0) {
+            echo $i;
+        }
+        for ($i = 0; $i < 10; $i++, $z = 0) {
+            echo $arr[$i];
+        }
+
+        /* false-positives: multiple containers */
+        $col = array();
+        for ($i = 0, $max = count($arr); $i < $max; $i++, $z = 0) {
+            echo $arr[$i], $col[$i];
+        }
     }

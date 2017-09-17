b5c3463ec9fbce41059e5048a269822f0da7fa0f
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-17 08:39:01 +0200

    Exceptions handling: RedundantElseClauseInspector - drop redundant check.

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/RedundantElseClauseInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/RedundantElseClauseInspector.java
@@ -3,13 +3,11 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.fixers.UnnecessaryElseFixer;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
-import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -47,9 +45,6 @@ public class RedundantElseClauseInspector extends BasePhpInspection {
                 /* general construct expectations: body wrapped into {} and has alternative branches */
                 final GroupStatement ifBody = ExpressionSemanticUtil.getGroupStatement(ifStatement);
                 if (null == ifBody || !ExpressionSemanticUtil.hasAlternativeBranches(ifStatement)) {
-                    return;
-                }
-                if (!OpenapiTypesUtil.is(ifBody.getFirstChild(), PhpTokenTypes.chLBRACE)) {
                     return;
                 }
 

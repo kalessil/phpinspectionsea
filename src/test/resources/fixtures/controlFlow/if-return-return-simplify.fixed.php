1cd4eacb7310fd2cb3d88456bcbfb77c3013cee4
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-21 07:47:33 +0200

    [#532] IfReturnReturnSimplificationInspector: new patterns (if-return-else-return)

--- src/test/resources/fixtures/controlFlow/if-return-return-simplify.fixed.php
+++ src/test/resources/fixtures/controlFlow/if-return-return-simplify.fixed.php
@@ -1,24 +1,41 @@
 <?php
 
-function suggestSimplifyingInverted($a) {
-    return !($a > 0);
-}
-
-function suggestSimplifying($a) {
-    return $a > 0;
-}
-
-function suggestSimplifyingReportedCases($a) {
-    return !(!$a || in_array($a, 'whatever', true));
-}
-
-function noFalsePositivesCase($a) {
-    if ($a === 0) {
+class CasesHolder {
+    public function ifReturnReturnOne($x) {
+        if ($x > 0) { return false; }
+        return true;
+    }
+    public function ifReturnReturnTwo($x) {
+        if ($x > 0) { return true; }
         return false;
     }
-    /* comment: ensures that comments are not processed */
-    if ($a > 0) {
+    public function ifReturnReturnThree($x) {
+        if ($x === 0) { return true; }
+        /* a comment here */
+        if ($x > 0) { return true; }
         return false;
     }
-    return true;
+    public function ifReturnReturnFour($x) {
+        if ($x > 0) { return true; }
+        return true;
+    }
+
+    public function ifReturnElseReturnOne($x) {
+        if ($x > 0) { return true; }
+        else { return false; }
+    }
+    public function ifReturnElseReturnTwo($x) {
+        if ($x > 0) { return false; }
+        else { return true; }
+    }
+    public function ifReturnElseReturnThree($x) {
+        if ($x > 0) { return true; }
+        else { return true; }
+    }
+    public function ifReturnElseReturnFour($x) {
+        if ($x === 0) { return true; }
+        /* a comment here */
+        if ($x > 0) { return false; }
+        else { return true; }
+    }
 }

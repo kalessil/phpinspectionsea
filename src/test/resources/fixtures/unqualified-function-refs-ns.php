ab37f19710aff8a122f3741ae88340feff60d63b
Jacob Dreesen <jacob@hdreesen.de>
2017-10-24 08:43:43 +0200

    UnqualifiedReferenceInspector: fix message wording

--- src/test/resources/fixtures/unqualified-function-refs-ns.php
+++ src/test/resources/fixtures/unqualified-function-refs-ns.php
@@ -6,17 +6,17 @@ namespace {
 
 namespace Unqualified\References {
 
-    echo <weak_warning descr="Using '\uniqid(...)' would enable some of opcache optimizations">uniqid()</weak_warning>;
-    echo <weak_warning descr="Using '\PHP_INT_MAX' would enable some of opcache optimizations">PHP_INT_MAX</weak_warning>;
+    echo <weak_warning descr="Using '\uniqid(...)' would enable some of opcode optimizations">uniqid()</weak_warning>;
+    echo <weak_warning descr="Using '\PHP_INT_MAX' would enable some of opcode optimizations">PHP_INT_MAX</weak_warning>;
 
-    echo \call_user_func(<weak_warning descr="Using '\my_function' would enable some of opcache optimizations">'my_function'</weak_warning>, '');
-    echo \call_user_func_array(<weak_warning descr="Using '\my_function' would enable some of opcache optimizations">'my_function'</weak_warning>, []);
-    echo \array_filter([], <weak_warning descr="Using '\my_function' would enable some of opcache optimizations">'my_function'</weak_warning>);
-    echo \array_map(<weak_warning descr="Using '\my_function' would enable some of opcache optimizations">'my_function'</weak_warning>, []);
-    echo \array_walk([], <weak_warning descr="Using '\my_function' would enable some of opcache optimizations">'my_function'</weak_warning>);
-    echo \array_reduce([], <weak_warning descr="Using '\my_function' would enable some of opcache optimizations">'my_function'</weak_warning>);
+    echo \call_user_func(<weak_warning descr="Using '\my_function' would enable some of opcode optimizations">'my_function'</weak_warning>, '');
+    echo \call_user_func_array(<weak_warning descr="Using '\my_function' would enable some of opcode optimizations">'my_function'</weak_warning>, []);
+    echo \array_filter([], <weak_warning descr="Using '\my_function' would enable some of opcode optimizations">'my_function'</weak_warning>);
+    echo \array_map(<weak_warning descr="Using '\my_function' would enable some of opcode optimizations">'my_function'</weak_warning>, []);
+    echo \array_walk([], <weak_warning descr="Using '\my_function' would enable some of opcode optimizations">'my_function'</weak_warning>);
+    echo \array_reduce([], <weak_warning descr="Using '\my_function' would enable some of opcode optimizations">'my_function'</weak_warning>);
 
-    echo \array_reduce([], <weak_warning descr="Using '\my_function' would enable some of opcache optimizations">"my_function"</weak_warning>);
+    echo \array_reduce([], <weak_warning descr="Using '\my_function' would enable some of opcode optimizations">"my_function"</weak_warning>);
 
     /* false-positives: a qualified call */
     echo \uniqid();

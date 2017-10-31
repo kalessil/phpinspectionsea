730f60f45587cdbfc58245472bdd06f4766b4428
Vladimir Reznichenko <kalessil@gmail.com>
2017-10-31 18:16:33 +0100

    NonSecureArrayRandUsageInspector: updated test-case

--- src/test/resources/fixtures/security/array_rand.php
+++ src/test/resources/fixtures/security/array_rand.php
@@ -1,6 +1,8 @@
 <?php
 
+function cases_holder() {
     $value = $values[<error descr="Insufficient entropy, mt_rand/random_int based solution would be more secure.">array_rand($values)</error>];
 
     $values = $values[array_rand($values, 5)];
     $key    = array_rand($values);
+}

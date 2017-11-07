9593699fcda5164f90963261402c37d4d5adc707
Vladimir Reznichenko <kalessil@gmail.com>
2017-11-07 09:21:59 +0100

    [#621] DisconnectedForeachInstructionInspector: resolved false-positives (possible objects modification)

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/foreach/DisconnectedForeachInstructionInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/foreach/DisconnectedForeachInstructionInspectorTest.java
@@ -5,18 +5,16 @@ import com.kalessil.phpStorm.phpInspectionsEA.inspectors.forEach.DisconnectedFor
 
 final public class DisconnectedForeachInstructionInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
-        DisconnectedForeachInstructionInspector inspector = new DisconnectedForeachInstructionInspector();
-        inspector.SUGGEST_USING_CLONE = true;
-
+        final DisconnectedForeachInstructionInspector inspector = new DisconnectedForeachInstructionInspector();
+        inspector.SUGGEST_USING_CLONE                           = true;
         myFixture.configureByFile("fixtures/foreach/disconnected-statements-foreach.php");
         myFixture.enableInspections(inspector);
         myFixture.testHighlighting(true, false, true);
     }
 
     public void testFalsePositives() {
-        DisconnectedForeachInstructionInspector inspector = new DisconnectedForeachInstructionInspector();
-        inspector.SUGGEST_USING_CLONE = true;
-
+        final DisconnectedForeachInstructionInspector inspector = new DisconnectedForeachInstructionInspector();
+        inspector.SUGGEST_USING_CLONE                           = true;
         myFixture.configureByFile("fixtures/foreach/disconnected-statements-foreach-false-positives.php");
         myFixture.enableInspections(inspector);
         myFixture.testHighlighting(true, false, true);

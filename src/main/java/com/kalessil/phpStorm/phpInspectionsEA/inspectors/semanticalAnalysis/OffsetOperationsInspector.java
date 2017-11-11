fd84a9ad29e24f402e2c7c6c9e2f2a44860649ca
Adrian Föder <adrian@foeder.de>
2017-11-11 19:12:43 +0100

    Wording: it's -> its

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/OffsetOperationsInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/OffsetOperationsInspector.java
@@ -31,7 +31,7 @@ import java.util.HashSet;
  */
 public class OffsetOperationsInspector extends BasePhpInspection {
     private static final String messageUseSquareBrackets = "Using [ ] instead of { } makes possible to analyze this expression.";
-    private static final String patternNoOffsetSupport   = "'%c%' may not support offset operations (or it's type not annotated properly: %t%).";
+    private static final String patternNoOffsetSupport   = "'%c%' may not support offset operations (or its type not annotated properly: %t%).";
     private static final String patternInvalidIndex      = "Resolved index type (%p%) is incompatible with possible %a%. Probably just proper type hinting needed.";
 
     @NotNull

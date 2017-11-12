8621d0cd64c90bba95c609ba66e91258d2d187d4
Vladimir Reznichenko <kalessil@gmail.com>
2017-11-12 20:22:47 +0100

    Ultimate: link the purchase page

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/license/PurchaseLicenseAction.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/license/PurchaseLicenseAction.java
@@ -1,18 +1,11 @@
 package com.kalessil.phpStorm.phpInspectionsEA.license;
 
+import com.intellij.ide.BrowserUtil;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.notification.*;
 import org.jetbrains.annotations.NotNull;
 
 final public class PurchaseLicenseAction {
     public void perform(@NotNull IdeaPluginDescriptor plugin) {
-        final String pluginName       = plugin.getName();
-        final NotificationGroup group = new NotificationGroup(pluginName, NotificationDisplayType.STICKY_BALLOON, true);
-        Notifications.Bus.notify(group.createNotification(
-            "<b>" + pluginName + "</b>",
-            PurchaseLicenseAction.class.getName(),
-            NotificationType.INFORMATION,
-            NotificationListener.URL_OPENING_LISTENER
-        ));
+        BrowserUtil.browse("https://kalessil.github.io/php-inspections-ultimate.html");
     }
 }

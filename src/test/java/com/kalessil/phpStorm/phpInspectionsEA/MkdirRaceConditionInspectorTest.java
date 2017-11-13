cf2265c69138e0bae033ae20134c0b2278acbf1d
Vladimir Reznichenko <kalessil@gmail.com>
2017-11-13 12:08:29 +0100

    FilePutContentsRaceConditionInspector: implemented

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/MkdirRaceConditionInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/MkdirRaceConditionInspectorTest.java
@@ -1,18 +1,18 @@
 package com.kalessil.phpStorm.phpInspectionsEA;
 
 import com.intellij.codeInsight.intention.IntentionAction;
-import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.MkdirRaceConditionInspector;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.raceConditions.MkdirRaceConditionInspector;
 
 final public class MkdirRaceConditionInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new MkdirRaceConditionInspector());
-        myFixture.configureByFile("fixtures/pitfalls/mkdir-race-conditions.php");
+        myFixture.configureByFile("fixtures/pitfalls/raceCondition/mkdir-race-conditions.php");
         myFixture.testHighlighting(true, false, true);
 
         for (final IntentionAction fix : myFixture.getAllQuickFixes()) {
             myFixture.launchAction(fix);
         }
         myFixture.setTestDataPath(".");
-        myFixture.checkResultByFile("fixtures/pitfalls/mkdir-race-conditions.fixed.php");
+        myFixture.checkResultByFile("fixtures/pitfalls/raceCondition/mkdir-race-conditions.fixed.php");
     }
 }

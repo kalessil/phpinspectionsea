f654fa4167e11d153fc4586b8c736afc88651cc6
Vladimir Reznichenko <kalessil@gmail.com>
2017-11-20 20:16:26 +0100

    [#639] MagicMethodsValidityInspector: updated test cases

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/MagicMethodsValidityInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/MagicMethodsValidityInspectorTest.java
@@ -1,43 +1,44 @@
 package com.kalessil.phpStorm.phpInspectionsEA.classes;
 
+import com.intellij.codeInsight.intention.IntentionAction;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.magicMethods.MagicMethodsValidityInspector;
 
 final public class MagicMethodsValidityInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsInconsistentGetsSetsPatterns() {
         myFixture.enableInspections(new MagicMethodsValidityInspector());
-
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-get-set.php");
         myFixture.testHighlighting(true, false, true);
     }
 
     public void testIfFindsSetStatePatterns() {
         myFixture.enableInspections(new MagicMethodsValidityInspector());
-
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-set-state.php");
         myFixture.testHighlighting(true, false, true);
     }
 
     public void testAbstractMethods() {
         myFixture.enableInspections(new MagicMethodsValidityInspector());
-
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-abstract.php");
         myFixture.testHighlighting(true, false, true);
     }
 
     public void testCallsParentMethods() {
         myFixture.enableInspections(new MagicMethodsValidityInspector());
-
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-calls-parent.php");
         myFixture.testHighlighting(true, false, true);
     }
 
     public void testMissingUnderscoreMethods() {
-        // TODO Test not validated for correctness
         myFixture.enableInspections(new MagicMethodsValidityInspector());
-
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-missing-underscore.php");
         myFixture.testHighlighting(true, false, true);
+
+        for (final IntentionAction fix : myFixture.getAllQuickFixes()) {
+            myFixture.launchAction(fix);
+        }
+        myFixture.setTestDataPath(".");
+        myFixture.checkResultByFile("fixtures/classes/magicMethods/magic-methods-missing-underscore.fixed.php");
     }
 }
 

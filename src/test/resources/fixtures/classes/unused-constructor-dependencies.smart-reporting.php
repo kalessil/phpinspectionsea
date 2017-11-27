b021b50ab732d3abd8c98bb73cf8970f3f1d44cf
Vladimir Reznichenko <kalessil@gmail.com>
2017-11-27 18:40:51 +0100

    [#656] UnusedConstructorDependenciesInspector: reporting usability

---
+++ src/test/resources/fixtures/classes/unused-constructor-dependencies.smart-reporting.php
@@ -0,0 +1,22 @@
+<?php
+
+class ReportingCasesHolder {
+
+    private $one;
+    private $two;
+    private $three;
+
+    public function __construct($dependency) {
+        <weak_warning descr="Property is used only in constructor, perhaps we are dealing with dead code here.">$this->one</weak_warning> = $dependency;
+
+        <weak_warning descr="Property is used only in constructor, perhaps we are dealing with dead code here.">$this->two</weak_warning> = new Clazz();
+        $this->two->method();
+
+        <weak_warning descr="Property is used only in constructor, perhaps we are dealing with dead code here.">$this->three</weak_warning> = new Clazz();
+        <weak_warning descr="Property is used only in constructor, perhaps we are dealing with dead code here.">$this->three</weak_warning> = new Clazz();
+        <weak_warning descr="Property is used only in constructor, perhaps we are dealing with dead code here.">$this->three</weak_warning>->method();
+    }
+
+    public function method() {
+    }
+}

eb46e602ff519c878e15d880204d7f5864933f56
Vladimir Reznichenko <kalessil@gmail.com>
2017-12-04 18:18:16 +0100

    ArrayKeysMissUseInspector: updated test-cases

--- src/test/resources/fixtures/api/array/array-keys-misused.fixed.php
+++ src/test/resources/fixtures/api/array/array-keys-misused.fixed.php
@@ -3,8 +3,8 @@
 function cases_holder() {
     return [
         array_keys([]),
-        array_keys(),
 
+        array_unique(array_keys()),
         array_unique(array_keys(), SORT_STRING),
         array_unique(array_keyz(), SORT_STRING),
 
--- src/test/resources/fixtures/api/array/array-keys-misused.php
+++ src/test/resources/fixtures/api/array/array-keys-misused.php
@@ -3,8 +3,8 @@
 function cases_holder() {
     return [
         <warning descr="'array_unique(...)' is not making any sense here (array keys are unique).">array_unique(array_keys([]))</warning>,
-        <warning descr="'array_unique(...)' is not making any sense here (array keys are unique).">array_unique(array_keys())</warning>,
 
+        array_unique(array_keys()),
         array_unique(array_keys(), SORT_STRING),
         array_unique(array_keyz(), SORT_STRING),
 

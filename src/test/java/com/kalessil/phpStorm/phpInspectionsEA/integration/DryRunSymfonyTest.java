637d735f736394bc8b6dc763d1d2692c97826092
Vladimir Reznichenko <kalessil@gmail.com>
2017-12-06 20:10:22 +0100

    [#680] CoreInterfacesUsageInspector: bug-fix test-cases, updated release notes

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/integration/DryRunSymfonyTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/integration/DryRunSymfonyTest.java
@@ -1,19 +1,7 @@
 package com.kalessil.phpStorm.phpInspectionsEA.integration;
 
-import com.intellij.analysis.AnalysisScope;
-import com.intellij.codeInspection.InspectionManager;
 import com.intellij.codeInspection.InspectionsReportConverter;
-import com.intellij.codeInspection.ex.InspectionManagerEx;
-import com.intellij.codeInspection.ex.PlainTextFormatter;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.util.ProgressIndicatorBase;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.InspectionTestCase;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DryRunSymfonyTest extends InspectionTestCase {
     public void test() throws InspectionsReportConverter.ConversionException {

1448e7dc0beb8fe5838baad0b93b880437ea3c48
Vladimir Reznichenko <kalessil@gmail.com>
2017-12-16 21:39:15 +0100

    NonSecureHtmlentitiesUsageInspector: copyright note added

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/NonSecureHtmlentitiesUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/NonSecureHtmlentitiesUsageInspector.java
@@ -11,6 +11,15 @@ import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import org.jetbrains.annotations.NotNull;
 
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 public class NonSecureHtmlentitiesUsageInspector extends BasePhpInspection {
     private static final String message = "Single quotes handling is not specified, please use ENT_QUOTES or ENT_COMPAT as second argument.";
 

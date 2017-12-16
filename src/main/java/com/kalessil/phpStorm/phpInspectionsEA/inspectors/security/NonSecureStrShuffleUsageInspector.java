d6808ddb8a36f55a0c5a0fded399127fda1ffbed
Vladimir Reznichenko <kalessil@gmail.com>
2017-12-16 16:06:22 +0100

    [#692] NonSecureStrShuffleUsageInspector: implemented

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/NonSecureStrShuffleUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/NonSecureStrShuffleUsageInspector.java
@@ -4,13 +4,10 @@ import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.jetbrains.php.lang.psi.elements.ArrayAccessExpression;
-import com.jetbrains.php.lang.psi.elements.ArrayIndex;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationComponent;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
-import com.kalessil.phpStorm.phpInspectionsEA.utils.OpeanapiEquivalenceUtil;
 import org.jetbrains.annotations.NotNull;
 
 /*
@@ -22,12 +19,12 @@ import org.jetbrains.annotations.NotNull;
  * file that was distributed with this source code.
  */
 
-public class NonSecureArrayRandUsageInspector extends BasePhpInspection {
+public class NonSecureStrShuffleUsageInspector extends BasePhpInspection {
     private static final String message = "Insufficient entropy, random_int/random_bytes based solution would be more secure.";
 
     @NotNull
     public String getShortName() {
-        return "NonSecureArrayRandUsageInspection";
+        return "NonSecureStrShuffleUsageInspection";
     }
 
     @Override
@@ -39,16 +36,10 @@ public class NonSecureArrayRandUsageInspector extends BasePhpInspection {
                 if (!EAUltimateApplicationComponent.areFeaturesEnabled()) { return; }
 
                 final String functionName = reference.getName();
-                if (functionName != null && functionName.equals("array_rand")) {
+                if (functionName != null && functionName.equals("str_shuffle")) {
                     final PsiElement[] arguments = reference.getParameters();
                     if (arguments.length == 1) {
-                        final PsiElement parent = reference.getParent();
-                        if (parent instanceof ArrayIndex) {
-                            final PsiElement container = ((ArrayAccessExpression) parent.getParent()).getValue();
-                            if (container != null && OpeanapiEquivalenceUtil.areEqual(container, arguments[0])) {
-                                holder.registerProblem(reference, message, ProblemHighlightType.GENERIC_ERROR);
-                            }
-                        }
+                        holder.registerProblem(reference, message, ProblemHighlightType.GENERIC_ERROR);
                     }
                 }
             }

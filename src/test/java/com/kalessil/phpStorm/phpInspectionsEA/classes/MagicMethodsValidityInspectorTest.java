dff6f46a829d6df140f31485b009b95eece6173d
Vladimir Reznichenko <kalessil@gmail.com>
2017-12-21 22:37:06 +0100

    [#713] MagicMethodsValidityInspector: code grooming, added UTs for __toString method

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/MagicMethodsValidityInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/MagicMethodsValidityInspectorTest.java
@@ -10,25 +10,26 @@ final public class MagicMethodsValidityInspectorTest extends PhpCodeInsightFixtu
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-get-set.php");
         myFixture.testHighlighting(true, false, true);
     }
-
     public void testIfFindsSetStatePatterns() {
         myFixture.enableInspections(new MagicMethodsValidityInspector());
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-set-state.php");
         myFixture.testHighlighting(true, false, true);
     }
-
     public void testAbstractMethods() {
         myFixture.enableInspections(new MagicMethodsValidityInspector());
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-abstract.php");
         myFixture.testHighlighting(true, false, true);
     }
-
     public void testCallsParentMethods() {
         myFixture.enableInspections(new MagicMethodsValidityInspector());
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-calls-parent.php");
         myFixture.testHighlighting(true, false, true);
     }
-
+    public void testToStringMethod() {
+        myFixture.enableInspections(new MagicMethodsValidityInspector());
+        myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-toString.php");
+        myFixture.testHighlighting(true, false, true);
+    }
     public void testMissingUnderscoreMethods() {
         myFixture.enableInspections(new MagicMethodsValidityInspector());
         myFixture.configureByFile("fixtures/classes/magicMethods/magic-methods-missing-underscore.php");

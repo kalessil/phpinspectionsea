9147c49470dc636af5d7b9506ad2119d04ab3750
Vladimir Reznichenko <kalessil@gmail.com>
2017-12-24 20:55:04 +0100

    [#694] ExceptionsAnnotatingAndHandlingInspector: bug-fix test fixture

--- src/test/resources/fixtures/controlFlow/exception-workflow/missing-phpdoc.php
+++ src/test/resources/fixtures/controlFlow/exception-workflow/missing-phpdoc.php
@@ -6,13 +6,13 @@ class CasesHolder {
         if ('' === '-') {
             /* not properly supported ATM */
             $exception = new \LogicException();
-            <weak_warning descr="Throws a non-annotated/unhandled exception: '\Exception1'.">throw $exception;</weak_warning>
+            <weak_warning descr="Throws a non-annotated/unhandled exception: '\LogicException'.">throw $exception;</weak_warning>
         }
         throw new \RuntimeException();
     }
 
     public function trigger() {
-        <weak_warning descr="Throws a non-annotated/unhandled exception: '\Exception1'.">$this->emitter();</weak_warning>
+        <weak_warning descr="Throws a non-annotated/unhandled exception: '\RuntimeException'.">$this->emitter();</weak_warning>
     }
 
 }

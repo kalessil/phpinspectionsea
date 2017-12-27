7bd5b476e3f53cb5d05d1784e423bb435bbe15db
Vladimir Reznichenko <kalessil@gmail.com>
2017-12-27 23:23:48 +0100

    [#94] UnnecessaryAssertionInspector: check language level

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/phpUnit/UnnecessaryAssertionInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/phpUnit/UnnecessaryAssertionInspector.java
@@ -1,8 +1,12 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.phpUnit;
 
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.config.PhpLanguageFeature;
+import com.jetbrains.php.config.PhpLanguageLevel;
+import com.jetbrains.php.config.PhpProjectConfigurationFacade;
 import com.jetbrains.php.lang.psi.elements.Function;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
@@ -11,20 +15,27 @@ import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiElementsUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiResolveUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.Types;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UnnecessaryAssertionInspector extends BasePhpInspection {
-    private final static String message = "...";
+    private final static String message = "Perhaps this assertion can probably be skipped (argument implicitly declares return type).";
 
-    final private static Map<String, Integer> targets = new HashMap<>();
+    final private static Map<String, Integer> targetPositions = new HashMap<>();
+    final private static Map<String, String> targetType       = new HashMap<>();
     static {
-        targets.put("assertInstanceOf",   1);
-        targets.put("assertEmpty",        0);
-        targets.put("assertNull",         0);
-        targets.put("assertInternalType", 1);
+        targetPositions.put("assertInstanceOf",   1);
+        targetPositions.put("assertEmpty",        0);
+        targetPositions.put("assertNull",         0);
+        targetPositions.put("assertInternalType", 1);
+
+        targetType.put("assertEmpty",        Types.strVoid);
+        targetType.put("assertNull",         Types.strVoid);
+        targetType.put("assertInstanceOf",   null);
+        targetType.put("assertInternalType", null);
     }
 
     @NotNull
@@ -38,18 +49,24 @@ public class UnnecessaryAssertionInspector extends BasePhpInspection {
         return new BasePhpElementVisitor() {
             @Override
             public void visitPhpMethodReference(@NotNull MethodReference reference) {
+                final Project project      = holder.getProject();
+                final PhpLanguageLevel php = PhpProjectConfigurationFacade.getInstance(project).getLanguageLevel();
+                if (!php.hasFeature(PhpLanguageFeature.RETURN_TYPES)) {
+                    return;
+                }
+
                 final String methodName = reference.getName();
-                if (methodName != null && targets.containsKey(methodName)) {
-                    final int position           = targets.get(methodName);
+                if (methodName != null && targetPositions.containsKey(methodName)) {
+                    final int position           = targetPositions.get(methodName);
                     final PsiElement[] arguments = reference.getParameters();
                     if (arguments.length >= position + 1 && arguments[position] instanceof FunctionReference) {
-                        final FunctionReference call      = (FunctionReference) arguments[position];
-                        final PsiElement resolvedFunction = OpenapiResolveUtil.resolveReference(call);
-                        if (resolvedFunction instanceof Function) {
-                            final boolean hasReturnType = OpenapiElementsUtil.getReturnType((Function) resolvedFunction) != null;
-                            if (hasReturnType) {
-                                final PhpType type = OpenapiResolveUtil.resolveType(call, holder.getProject());
-                                if (type != null && !type.hasUnknown() && type.size() == 1) {
+                        final FunctionReference call = (FunctionReference) arguments[position];
+                        final PsiElement function    = OpenapiResolveUtil.resolveReference(call);
+                        if (function instanceof Function && OpenapiElementsUtil.getReturnType((Function) function) != null) {
+                            final PhpType resolved = OpenapiResolveUtil.resolveType(call, project);
+                            if (resolved != null && !resolved.hasUnknown() && resolved.size() == 1) {
+                                final String expected = targetType.get(methodName);
+                                if (expected == null || resolved.getTypes().stream().anyMatch(type -> Types.getType(type).equals(expected))) {
                                     holder.registerProblem(reference, message);
                                 }
                             }

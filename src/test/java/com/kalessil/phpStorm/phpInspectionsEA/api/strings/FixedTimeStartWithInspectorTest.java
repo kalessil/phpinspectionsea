4e22e8b43d36cd445788427f0705988a38d92eef
Vladimir Reznichenko <kalessil@gmail.com>
2018-01-12 15:43:59 +0100

    StartsWithOnLongStringsInspector: added UTs.

---
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/strings/FixedTimeStartWithInspectorTest.java
@@ -0,0 +1,16 @@
+package com.kalessil.phpStorm.phpInspectionsEA.api.strings;
+
+import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.strings.FixedTimeStartWithInspector;
+
+final public class FixedTimeStartWithInspectorTest extends PhpCodeInsightFixtureTestCase {
+    public void testIfFindsAllPatterns() {
+        myFixture.enableInspections(new FixedTimeStartWithInspector());
+        myFixture.configureByFile("fixtures/api/strings/fixed-time-start-with.php");
+        myFixture.testHighlighting(true, false, true);
+
+        myFixture.getAllQuickFixes().forEach(fix -> myFixture.launchAction(fix));
+        myFixture.setTestDataPath(".");
+        myFixture.checkResultByFile("fixtures/api/strings/fixed-time-start-with.fixed.php");
+    }
+}
---
+++ src/test/resources/fixtures/api/strings/fixed-time-start-with.fixed.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    0 === strncmp('', 'what', 4),
+    strncmp('', 'what', 4) === 0,
+    strncmp('', 'what', 4) !== 0,
+
+    0 === strncasecmp('', 'what', 4),
+    strncasecmp('', 'what', 4) === 0,
+    strncasecmp('', 'what', 4) !== 0,
+
+    /* false-positives: not targeted contexts */
+    0 === strpos('', $what),
+    0 === strpos('', 'what', 1),
+];

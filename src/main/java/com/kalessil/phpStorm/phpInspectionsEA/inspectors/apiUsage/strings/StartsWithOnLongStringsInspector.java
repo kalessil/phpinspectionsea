b5e47378b4c27eda1804184f0746c814cac1f030
Vladimir Reznichenko <kalessil@gmail.com>
2018-01-12 14:59:00 +0100

    StartsWithOnLongStringsInspector: started implementation

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/strings/StartsWithOnLongStringsInspector.java
@@ -0,0 +1,70 @@
+package com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.strings;
+
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.tree.IElementType;
+import com.jetbrains.php.lang.lexer.PhpTokenTypes;
+import com.jetbrains.php.lang.psi.elements.BinaryExpression;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiElementsUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+public class StartsWithOnLongStringsInspector extends BasePhpInspection {
+
+    private static final Map<String, String> mapping = new HashMap<>();
+    static {
+        mapping.put("stripos", "strncasecmp");
+        mapping.put("strpos",  "strncmp");
+    }
+
+    private static final String message  = "'%e%' should be used instead.";
+
+    @NotNull
+    public String getShortName() {
+        return "StartsWithOnLongStringsInspection";
+    }
+
+    @Override
+    @NotNull
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new BasePhpElementVisitor() {
+            @Override
+            public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
+                final String functionName = reference.getName();
+                if (functionName != null && mapping.containsKey(functionName)) {
+                    final PsiElement parent = reference.getParent();
+                    if (parent instanceof BinaryExpression) {
+                        final BinaryExpression binary = (BinaryExpression) parent;
+                        final IElementType operator   = binary.getOperationType();
+                        if (operator == PhpTokenTypes.opIDENTICAL || operator == PhpTokenTypes.opNOT_IDENTICAL) {
+                            final PsiElement[] arguments = reference.getParameters();
+                            if (arguments.length == 2 && arguments[1] instanceof StringLiteralExpression) {
+                                final PsiElement zeroCandidate = OpenapiElementsUtil.getSecondOperand(binary, reference);
+                                if (zeroCandidate != null && zeroCandidate.getText().equals("0")) {
+
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    }
+}

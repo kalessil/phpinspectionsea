1997b9232a5759c3bc1e03d15c6394e3658b5e87
Vladimir Reznichenko <kalessil@gmail.com>
2018-01-16 10:58:56 +0100

    [#749] MagicMethodsValidityInspector: code grooming

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/magicMethods/strategy/NormallyCallsParentMethodStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/magicMethods/strategy/NormallyCallsParentMethodStrategy.java
@@ -8,7 +8,7 @@ import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.NamedElementUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiResolveUtil;
-import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
 
 /*
  * This file is part of the Php Inspections (EA Extended) package.
@@ -20,36 +20,29 @@ import org.apache.commons.lang.StringUtils;
  */
 
 public class NormallyCallsParentMethodStrategy {
-    private static final String messagePattern = "%m% is probably missing %c%::%m% call.";
+    private static final String messagePattern = "%s is probably missing %s::%s call.";
 
-    static public void apply(final Method method, final ProblemsHolder holder) {
-        final String methodName   = method.getName();
-        final PhpClass clazz      = method.getContainingClass();
-        final PsiElement nameNode = NamedElementUtil.getNameIdentifier(method);
-        if (null == nameNode || null == clazz || StringUtils.isEmpty(methodName)) {
-            return;
-        }
+    static public void apply(@NotNull Method method, @NotNull ProblemsHolder holder) {
+        final PhpClass clazz = method.getContainingClass();
+        if (clazz != null) {
+            final String methodName           = method.getName();
+            final PhpClass parentClazz        = OpenapiResolveUtil.resolveSuperClass(clazz);
+            final Method   parentMethod       = parentClazz == null ? null : parentClazz.findMethodByName(methodName);
+            final PhpClass parentMethodHolder = parentMethod == null ? null : parentMethod.getContainingClass();
+            if (parentMethodHolder != null && !parentMethod.isAbstract() && !parentMethod.getAccess().isPrivate()) {
+                final boolean isUsed = PsiTreeUtil.findChildrenOfType(method, MethodReference.class).stream()
+                        .anyMatch(call -> methodName.equals(call.getName()));
+                if (!isUsed) {
+                    final PsiElement nameNode = NamedElementUtil.getNameIdentifier(method);
+                    if (nameNode != null) {
+                        holder.registerProblem(
+                                nameNode,
+                                String.format(messagePattern, methodName, parentMethodHolder.getName(), methodName)
+                        );
+                    }
 
-        /* find parent with protected/public method, if not overrides anything, terminate inspection */
-        final PhpClass parent       = OpenapiResolveUtil.resolveSuperClass(clazz);
-        final Method   parentMethod = parent == null ? null : parent.findMethodByName(methodName);
-        final PhpClass methodHolder = parentMethod == null ? null : parentMethod.getContainingClass();
-        if (methodHolder == null || parentMethod.isAbstract() || parentMethod.getAccess().isPrivate()) {
-            return;
-        }
-
-        /* find all calls inside methods and check if any of them calls parent */
-        for (final MethodReference call : PsiTreeUtil.findChildrenOfType(method, MethodReference.class)) {
-            final String innerCallName = call.getName();
-            if (innerCallName != null && innerCallName.equals(methodName)) {
-                return;
+                }
             }
         }
-
-        /* report the issue */
-        final String message = messagePattern
-            .replace("%m%", methodName)
-            .replace("%c%", methodHolder.getName());
-        holder.registerProblem(nameNode, message);
     }
 }

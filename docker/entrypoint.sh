1dab87cda95aa825ae2b9c3c39d09db333ce3ea2
root <root@ecademo.ebusiness1nfdev.net>
2018-01-17 14:03:03 +0100

    Add docker headless PHPStorm configuration files

---
+++ docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+if [ -z "$INSPECTIONCONFIGFILE" ]
+    then INSPECTIONCONFIGFILE=/var/ci/project/.idea/inspectionProfiles/Project_Default.xml
+fi
+if [ -z "$INSPECTIONRESULTFILE" ]
+    then INSPECTIONRESULTFILE=/var/ci/phpinspections_cs.xml
+fi
+if [ -n "$INSPECTIONCONFIG" ]
+    then echo $INSPECTIONCONFIG > /var/ci/Project_Default.xml
+    INSPECTIONCONFIGFILE=/var/ci/Project_Default.xml
+fi
+
+/root/vendor/bin/phpstorm-inspect `ls -d ~/PhpStorm-*/`bin/inspect.sh /root/.PhpStorm${PHPSTORMVERSION}/system /var/ci ${INSPECTIONCONFIGFILE} /var/ci/project checkstyle > ${INSPECTIONRESULTFILE}
+

2c0ae57d7030737ec2e1d95b025644dd7ad03706
Vladimir Reznichenko <kalessil@gmail.com>
2018-01-19 13:25:41 +0100

    [#747] GetClassUsageInspector: resolved a false-negative (nullable object parameter)

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/GetClassUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/GetClassUsageInspector.java
@@ -6,14 +6,20 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.config.PhpLanguageLevel;
 import com.jetbrains.php.config.PhpProjectConfigurationFacade;
+import com.jetbrains.php.lang.psi.elements.Function;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.jetbrains.php.lang.psi.elements.PhpTypedElement;
+import com.jetbrains.php.lang.psi.elements.Variable;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiResolveUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.PhpLanguageUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.Types;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 /*
  * This file is part of the Php Inspections (EA Extended) package.
@@ -50,13 +56,27 @@ public class GetClassUsageInspector extends BasePhpInspection {
                             if (resolved != null) {
                                 final boolean hasNull = resolved.filterUnknown().getTypes().stream()
                                         .anyMatch(t -> Types.getType(t).equals(Types.strNull));
-                                if (hasNull) {
+                                if (hasNull || isNullableParameter(arguments[0])) {
                                     holder.registerProblem(reference, message);
                                 }
                             }
                         }
                     }
                 }
+            }
+
+            private boolean isNullableParameter(@NotNull PsiElement expression) {
+                boolean result = false;
+                if (expression instanceof Variable) {
+                    final Function scope = ExpressionSemanticUtil.getScope(expression);
+                    if (scope != null) {
+                        final String name = ((Variable) expression).getName();
+                        result = Arrays.stream(scope.getParameters()).anyMatch(parameter ->
+                                name.equals(parameter.getName()) && PhpLanguageUtil.isNull(parameter.getDefaultValue())
+                        );
+                    }
+                }
+                return result;
             }
         };
     }

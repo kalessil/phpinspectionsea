78f26c8b00654bb3cc25901e6a423dbee3624a8c
Vladimir Reznichenko <kalessil@gmail.com>
2018-01-21 22:15:26 +0100

    [#695] TypesCastingCanBeUsedInspector: renamed test class

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/TypesCastingCanBeUsedInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/TypesCastingCanBeUsedInspectorTest.java
@@ -3,7 +3,7 @@ package com.kalessil.phpStorm.phpInspectionsEA.api;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.TypesCastingCanBeUsedInspector;
 
-final public class TypesCastingWithFunctionsInspectorTest extends PhpCodeInsightFixtureTestCase {
+final public class TypesCastingCanBeUsedInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new TypesCastingCanBeUsedInspector());
         myFixture.configureByFile("fixtures/api/type-casting-can-be-used.php");

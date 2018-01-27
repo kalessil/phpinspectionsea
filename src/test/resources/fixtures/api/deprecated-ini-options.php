03d9ca74a14b6630ff1a0867a381df7160b41854
Vladimir Reznichenko <kalessil@gmail.com>
2018-01-27 06:36:05 +0100

    DeprecatedIniOptionsInspector: tweak name/arguments checks, added UTs.

---
+++ src/test/resources/fixtures/api/deprecated-ini-options.php
@@ -0,0 +1,6 @@
+<?php
+
+    <error descr="'safe_mode' is a deprecated option since PHP 5.3.0 (removed in PHP 5.4.0).">ini_set('safe_mode', '')</error>;
+    <error descr="'safe_mode' is a deprecated option since PHP 5.3.0 (removed in PHP 5.4.0).">ini_get('safe_mode')</error>;
+    <error descr="'safe_mode' is a deprecated option since PHP 5.3.0 (removed in PHP 5.4.0).">ini_alter('safe_mode', '')</error>;
+    <error descr="'safe_mode' is a deprecated option since PHP 5.3.0 (removed in PHP 5.4.0).">ini_restore('safe_mode')</error>;

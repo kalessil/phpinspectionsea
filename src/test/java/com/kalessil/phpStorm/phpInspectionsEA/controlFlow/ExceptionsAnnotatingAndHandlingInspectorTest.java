debc6086032b26e4269308a6f77691e70b75e3c8
Vladimir Reznichenko <kalessil@gmail.com>
2018-02-01 09:11:32 +0100

    [#775] ExceptionsAnnotatingAndHandlingInspector: added test cases

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/controlFlow/ExceptionsAnnotatingAndHandlingInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/controlFlow/ExceptionsAnnotatingAndHandlingInspectorTest.java
@@ -20,6 +20,10 @@ final public class ExceptionsAnnotatingAndHandlingInspectorTest extends PhpCodeI
         myFixture.enableInspections(new ExceptionsAnnotatingAndHandlingInspector());
         myFixture.configureByFile("fixtures/controlFlow/exception-workflow/missing-phpdoc.php");
         myFixture.testHighlighting(true, false, true);
-
+    }
+    public void testCatchExceptionAndThrowable() {
+        myFixture.enableInspections(new ExceptionsAnnotatingAndHandlingInspector());
+        myFixture.configureByFile("fixtures/controlFlow/exception-workflow/catch-exception-throwable.php");
+        myFixture.testHighlighting(true, false, true);
     }
 }

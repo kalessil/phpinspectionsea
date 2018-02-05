8646192f764e3bb171f6ea7e5081c28f51afc088
Vladimir Reznichenko <kalessil@gmail.com>
2018-02-05 22:32:27 +0100

    [#420] UnsupportedStringOffsetOperationsInspector: updated test cases

--- src/test/resources/fixtures/lang/unsupported-string-offset.php
+++ src/test/resources/fixtures/lang/unsupported-string-offset.php
@@ -3,7 +3,7 @@
 function cases_holder(string $string)
 {
     $one = $string[0][0];
-    $one = [ $string[0][0] ];
+    $one = [$string[0][0]];
     $string[0] = 0;
 
     <error descr="Provokes a PHP Fatal error (cannot use string offset as an array).">$string[0][0]</error> = 0;
@@ -12,9 +12,7 @@ function cases_holder(string $string)
         = <error descr="Provokes a PHP Fatal error (cannot use string offset as an array).">$string[1]['...']</error>
         = 0;
 
-    [<error descr="Provokes a PHP Fatal error (cannot use string offset as an array).">$string[0]['...']</error>]
-        = ['...'];
-
-    array(<error descr="Provokes a PHP Fatal error (cannot use string offset as an array).">$string[0]['...']</error>)
-        = array('...', '...');
+    /* gets reported too, but PS 2016.2 parser fails the test cases */
+    //[$string[0]['...']] = ['...'];
+    //array($string[0]['...']) = array('...');
 }

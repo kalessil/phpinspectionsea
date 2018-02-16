c4eaa1e7d22907f1327c18c1d02c93b9c3debccb
Vladimir Reznichenko <kalessil@gmail.com>
2018-02-16 19:55:55 +0100

    [#792] AliasFunctionsUsageInspector: new functions

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/AliasFunctionsUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/AliasFunctionsUsageInspector.java
@@ -35,32 +35,53 @@ public class AliasFunctionsUsageInspector extends BasePhpInspection {
         return "AliasFunctionsUsageInspection";
     }
 
-    private static final HashMap<String, String> mapping = new HashMap<>();
+    private static final Map<String, String> mapping = new HashMap<>();
     static {
-        mapping.put("is_double",            "is_float");
-        mapping.put("is_integer",           "is_int");
-        mapping.put("is_long",              "is_int");
-        mapping.put("is_real",              "is_float");
-        mapping.put("sizeof",               "count");
-        mapping.put("doubleval",            "floatval");
-        mapping.put("fputs",                "fwrite");
-        mapping.put("join",                 "implode");
-        mapping.put("key_exists",           "array_key_exists");
-        mapping.put("chop",                 "rtrim");
-        mapping.put("ini_alter",            "ini_set");
-        mapping.put("is_writeable",         "is_writable");
-        mapping.put("magic_quotes_runtime", "set_magic_quotes_runtime");
-        mapping.put("pos",                  "current");
-        mapping.put("show_source",          "highlight_file");
-        mapping.put("strchr",               "strstr");
-        mapping.put("set_file_buffer",      "stream_set_write_buffer");
+        mapping.put("is_double",              "is_float");
+        mapping.put("is_integer",             "is_int");
+        mapping.put("is_long",                "is_int");
+        mapping.put("is_real",                "is_float");
+        mapping.put("sizeof",                 "count");
+        mapping.put("doubleval",              "floatval");
+        mapping.put("fputs",                  "fwrite");
+        mapping.put("join",                   "implode");
+        mapping.put("key_exists",             "array_key_exists");
+        mapping.put("chop",                   "rtrim");
+        mapping.put("ini_alter",              "ini_set");
+        mapping.put("is_writeable",           "is_writable");
+        mapping.put("magic_quotes_runtime",   "set_magic_quotes_runtime");
+        mapping.put("pos",                    "current");
+        mapping.put("show_source",            "highlight_file");
+        mapping.put("strchr",                 "strstr");
+        mapping.put("set_file_buffer",        "stream_set_write_buffer");
+        mapping.put("session_commit",         "session_write_close");
+        mapping.put("mysqli_escape_string",   "mysqli_real_escape_string");
+        mapping.put("ocifreecursor",          "oci_free_statement");
+        mapping.put("recode",                 "recode_string");
+        mapping.put("imap_listmailbox",       "imap_list");
+        mapping.put("socket_getopt",          "socket_get_option");
+        mapping.put("socket_setopt",          "socket_set_option");
+        mapping.put("openssl_get_privatekey", "openssl_pkey_get_private");
+        mapping.put("posix_errno",            "posix_get_last_error");
+        mapping.put("ldap_close",             "ldap_unbind");
+        mapping.put("pcntl_errno",            "pcntl_get_last_error");
+        mapping.put("ftp_quit",               "ftp_close");
+        mapping.put("odbc_do",                "odbc_exec");
+
+        /* also aliases, but original naming is so ugly, that I'm not going to promote it */
+        //mapping.put("close", "closedir");
+        //mapping.put("rewind", "rewinddir");
+        //mapping.put("odbc_field_precision", "odbc_field_len");
+        //mapping.put("mysqli_execute", "mysqli_stmt_execute");
+        //mapping.put("imap_header", "imap_headerinfo");
     }
 
     @Override
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
-            public void visitPhpFunctionCall(FunctionReference reference) {
+            @Override
+            public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
                 final String functionName = reference.getName();
                 if (null != functionName && mapping.containsKey(functionName)) {
                     /* avoid complaining to imported functions */

e546de2d7c00873b26dcf270c26a53cfb30dd979
Vladimir Reznichenko <kalessil@gmail.com>
2018-02-17 21:39:41 +0100

    [#60] DeprecatedIniOptionsInspector: updated test cases

--- src/test/resources/fixtures/api/deprecated-ini-options.php
+++ src/test/resources/fixtures/api/deprecated-ini-options.php
@@ -1,6 +1,8 @@
 <?php
 
-    ini_set(<warning descr="'safe_mode' is a deprecated option since PHP 5.3.0 (removed in PHP 5.4.0).">'safe_mode'</warning>, '');
-    ini_get(<warning descr="'safe_mode' is a deprecated option since PHP 5.3.0 (removed in PHP 5.4.0).">'safe_mode'</warning>);
-    ini_alter(<warning descr="'safe_mode' is a deprecated option since PHP 5.3.0 (removed in PHP 5.4.0).">'safe_mode'</warning>, '');
-    ini_restore(<warning descr="'safe_mode' is a deprecated option since PHP 5.3.0 (removed in PHP 5.4.0).">'safe_mode'</warning>);
+    ini_set(<warning descr="'iconv.input_encoding' is a deprecated since PHP 5.6.0.">'iconv.input_encoding'</warning>);
+    ini_get(<warning descr="'iconv.input_encoding' is a deprecated since PHP 5.6.0.">'iconv.input_encoding'</warning>);
+    ini_alter(<warning descr="'iconv.input_encoding' is a deprecated since PHP 5.6.0.">'iconv.input_encoding'</warning>);
+    ini_restore(<warning descr="'iconv.input_encoding' is a deprecated since PHP 5.6.0.">'iconv.input_encoding'</warning>);
+
+    ini_restore(<warning descr="'session.bug_compat_42' was removed in PHP 5.4.0.">'session.bug_compat_42'</warning>);

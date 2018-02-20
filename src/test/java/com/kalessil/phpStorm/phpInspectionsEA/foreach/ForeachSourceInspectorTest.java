d783308b7b2e0b4775e806f0ebdbdc9adcc8a212
David Rodrigues <david.proweb@gmail.com>
2018-02-20 13:34:58 -0300

    [#800, #801] ForeachSourceInspector: resolved a false-positive (object[] type resolve issues)

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/foreach/ForeachSourceInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/foreach/ForeachSourceInspectorTest.java
@@ -3,15 +3,16 @@ package com.kalessil.phpStorm.phpInspectionsEA.foreach;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.forEach.ForeachSourceInspector;
 
-final public class ForeachSourceInspectorTest extends PhpCodeInsightFixtureTestCase {
+public final class ForeachSourceInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         final ForeachSourceInspector inspector = new ForeachSourceInspector();
-        inspector.REPORT_MIXED_TYPES           = true;
-        inspector.REPORT_UNRECOGNIZED_TYPES    = true;
+        inspector.REPORT_MIXED_TYPES = true;
+        inspector.REPORT_UNRECOGNIZED_TYPES = true;
         myFixture.enableInspections(inspector);
         myFixture.configureByFile("fixtures/foreach/foreach-source.php");
         myFixture.testHighlighting(true, false, true);
     }
+
     public void testFalsePositives() {
         myFixture.enableInspections(new ForeachSourceInspector());
         myFixture.configureByFile("fixtures/foreach/foreach-source-false-positives.setup.php");

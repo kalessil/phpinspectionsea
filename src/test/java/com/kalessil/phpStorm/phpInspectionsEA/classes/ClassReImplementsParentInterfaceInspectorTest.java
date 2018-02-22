3930a688238716e21ddec2226d81906658b116d8
Vladimir Reznichenko <kalessil@gmail.com>
2018-02-22 21:40:23 +0100

    ClassReImplementsParentInterfaceInspector: crash reports and code grooming

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/ClassReImplementsParentInterfaceInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/ClassReImplementsParentInterfaceInspectorTest.java
@@ -6,7 +6,6 @@ import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.clas
 final public class ClassReImplementsParentInterfaceInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new ClassReImplementsParentInterfaceInspector());
-
         myFixture.configureByFile("fixtures/classes/re-implements-parent-interface.php");
         myFixture.testHighlighting(true, false, true);
     }
--- src/test/resources/fixtures/classes/re-implements-parent-interface.php
+++ src/test/resources/fixtures/classes/re-implements-parent-interface.php
@@ -11,18 +11,18 @@ abstract class AParent2 implements IParent1, IParent2
 { private $y; }
 
 class CParent1 extends AParent1
-    implements <error descr="IParent1 is already announced in \AParent1.">IParent1</error>
+    implements <error descr="'\IParent1' is already announced in '\AParent1'.">IParent1</error>
 { private $xx; }
 
 class CParent2 extends AParent2
     implements
-        <error descr="IParent1 is already announced in \AParent2.">IParent1</error>,
-        <error descr="IParent2 is already announced in \AParent2.">IParent2</error>
+        <error descr="'\IParent1' is already announced in '\AParent2'.">IParent1</error>,
+        <error descr="'\IParent2' is already announced in '\AParent2'.">IParent2</error>
 { private $xx; }
 
 class ClassImplementsSameInterfaceTwice
     implements
         IParent2,
-        <error descr="Class cannot implement previously implemented interface">IParent2</error>
+        <error descr="Class cannot implement previously implemented interface.">IParent2</error>
 {
 }

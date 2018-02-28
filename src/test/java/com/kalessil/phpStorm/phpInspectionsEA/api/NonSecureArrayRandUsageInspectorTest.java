1fc1f2634a2397acb68729715e83c2aaf9409a7c
Vladimir Reznichenko <kalessil@gmail.com>
2018-02-28 14:53:05 +0100

    Code grooming: make test final

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/NonSecureArrayRandUsageInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/NonSecureArrayRandUsageInspectorTest.java
@@ -3,7 +3,7 @@ package com.kalessil.phpStorm.phpInspectionsEA.api;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.security.NonSecureArrayRandUsageInspector;
 
-public class NonSecureArrayRandUsageInspectorTest extends PhpCodeInsightFixtureTestCase {
+final public class NonSecureArrayRandUsageInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new NonSecureArrayRandUsageInspector());
         myFixture.configureByFile("fixtures/security/array_rand.php");
--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/controlFlow/RepetitiveMethodCallsInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/controlFlow/RepetitiveMethodCallsInspectorTest.java
@@ -3,11 +3,10 @@ package com.kalessil.phpStorm.phpInspectionsEA.controlFlow;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.RepetitiveMethodCallsInspector;
 
-public class RepetitiveMethodCallsInspectorTest extends PhpCodeInsightFixtureTestCase {
+final public class RepetitiveMethodCallsInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new RepetitiveMethodCallsInspector());
         myFixture.configureByFile("fixtures/controlFlow/repetitive-method-calls.php");
         myFixture.testHighlighting(true, false, true);
-
     }
 }

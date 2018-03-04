b8c9e3d04a7a00d68cf8f06667031ddcc281b3ba
Vladimir Reznichenko <kalessil@gmail.com>
2018-03-04 11:39:23 +0100

    [#837] AliasFunctionsUsageInspector: bug-fixing

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/deprecations/AliasFunctionsUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/deprecations/AliasFunctionsUsageInspector.java
@@ -7,11 +7,9 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiNameIdentifierOwner;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
-import com.kalessil.phpStorm.phpInspectionsEA.utils.NamedElementUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -96,21 +94,15 @@ public class AliasFunctionsUsageInspector extends BasePhpInspection {
                 final String functionName = reference.getName();
                 if (functionName != null) {
                     if (relevantAliases.containsKey(functionName) && this.isFromRootNamespace(reference)) {
-                        final PsiElement target = NamedElementUtil.getNameIdentifier((PsiNameIdentifierOwner) reference);
-                        if (target != null) {
-                            final String original = relevantAliases.get(functionName);
-                            holder.registerProblem(
-                                    target,
-                                    String.format(messagePattern, functionName, original),
-                                    ProblemHighlightType.LIKE_DEPRECATED,
-                                    new TheLocalFix(original)
-                            );
-                        }
+                        final String original = relevantAliases.get(functionName);
+                        holder.registerProblem(
+                                reference.getNameNode().getPsi(),
+                                String.format(messagePattern, functionName, original),
+                                ProblemHighlightType.LIKE_DEPRECATED,
+                                new TheLocalFix(original)
+                        );
                     } else if (deprecatedAliases.containsKey(functionName) && this.isFromRootNamespace(reference)) {
-                        final PsiElement target = NamedElementUtil.getNameIdentifier((PsiNameIdentifierOwner) reference);
-                        if (target != null) {
-                            holder.registerProblem(reference, deprecatedAliases.get(functionName));
-                        }
+                        holder.registerProblem(reference.getNameNode().getPsi(), deprecatedAliases.get(functionName));
                     }
                 }
             }

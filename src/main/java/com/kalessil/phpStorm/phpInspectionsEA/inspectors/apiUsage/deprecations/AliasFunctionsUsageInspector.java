f893d45f98c7776e3dd8d27f56a84b855bcd1ac4
Vladimir Reznichenko <kalessil@gmail.com>
2018-03-04 11:21:45 +0100

    [#837] AliasFunctionsUsageInspector: usability (reporting target)

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/deprecations/AliasFunctionsUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/deprecations/AliasFunctionsUsageInspector.java
@@ -7,9 +7,11 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiNameIdentifierOwner;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.NamedElementUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -94,11 +96,21 @@ public class AliasFunctionsUsageInspector extends BasePhpInspection {
                 final String functionName = reference.getName();
                 if (functionName != null) {
                     if (relevantAliases.containsKey(functionName) && this.isFromRootNamespace(reference)) {
-                        final String original = relevantAliases.get(functionName);
-                        final String message  = String.format(messagePattern, functionName, original);
-                        holder.registerProblem(reference, message, ProblemHighlightType.LIKE_DEPRECATED, new TheLocalFix(original));
+                        final PsiElement target = NamedElementUtil.getNameIdentifier((PsiNameIdentifierOwner) reference);
+                        if (target != null) {
+                            final String original = relevantAliases.get(functionName);
+                            holder.registerProblem(
+                                    target,
+                                    String.format(messagePattern, functionName, original),
+                                    ProblemHighlightType.LIKE_DEPRECATED,
+                                    new TheLocalFix(original)
+                            );
+                        }
                     } else if (deprecatedAliases.containsKey(functionName) && this.isFromRootNamespace(reference)) {
-                        holder.registerProblem(reference, deprecatedAliases.get(functionName));
+                        final PsiElement target = NamedElementUtil.getNameIdentifier((PsiNameIdentifierOwner) reference);
+                        if (target != null) {
+                            holder.registerProblem(reference, deprecatedAliases.get(functionName));
+                        }
                     }
                 }
             }
@@ -127,7 +139,7 @@ public class AliasFunctionsUsageInspector extends BasePhpInspection {
 
         @Override
         public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-            final PsiElement expression = descriptor.getPsiElement();
+            final PsiElement expression = descriptor.getPsiElement().getParent();
             if (expression instanceof FunctionReference && !project.isDisposed()) {
                 ((FunctionReference) expression).handleElementRename(this.suggestedName);
             }

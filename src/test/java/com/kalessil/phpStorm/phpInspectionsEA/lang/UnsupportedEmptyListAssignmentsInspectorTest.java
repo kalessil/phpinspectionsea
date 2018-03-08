2d7e1a42111968b1de73162ec6add8f906a969e6
Vladimir Reznichenko <kalessil@gmail.com>
2018-03-08 08:59:17 +0100

    [#822] UnsupportedEmptyListAssignmentsInspector: added UTs

---
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/lang/UnsupportedEmptyListAssignmentsInspectorTest.java
@@ -0,0 +1,15 @@
+package com.kalessil.phpStorm.phpInspectionsEA.lang;
+
+import com.jetbrains.php.config.PhpLanguageLevel;
+import com.jetbrains.php.config.PhpProjectConfigurationFacade;
+import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions.UnsupportedEmptyListAssignmentsInspector;
+
+final public class UnsupportedEmptyListAssignmentsInspectorTest extends PhpCodeInsightFixtureTestCase {
+    public void testIfFindsAllPatterns() {
+        PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP700);
+        myFixture.enableInspections(new UnsupportedEmptyListAssignmentsInspector());
+        myFixture.configureByFile("fixtures/lang/unsupported-empty-list-assignments.php");
+        myFixture.testHighlighting(true, false, true);
+    }
+}

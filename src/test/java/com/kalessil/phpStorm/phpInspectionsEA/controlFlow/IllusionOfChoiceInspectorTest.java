1206926148eb5eddcc4f66a154ddb67949e7c3dc
Vladimir Reznichenko <kalessil@gmail.com>
2018-04-08 17:11:53 +0200

    [#869] IllusionOfChoiceInspector: added UT without fixtures

---
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/controlFlow/IllusionOfChoiceInspectorTest.java
@@ -0,0 +1,17 @@
+package com.kalessil.phpStorm.phpInspectionsEA.controlFlow;
+
+import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.IllusionOfChoiceInspector;
+
+final public class IllusionOfChoiceInspectorTest extends PhpCodeInsightFixtureTestCase {
+    public void testIfFindsAllPatterns() {
+        myFixture.enableInspections(new IllusionOfChoiceInspector());
+        myFixture.configureByFile("fixtures/controlFlow/illusion-of-choice.php");
+        myFixture.testHighlighting(true, false, true);
+
+        myFixture.getAllQuickFixes().forEach(fix -> myFixture.launchAction(fix));
+        myFixture.setTestDataPath(".");
+        myFixture.checkResultByFile("fixtures/controlFlow/illusion-of-choice.fixed.php");
+    }
+}
+

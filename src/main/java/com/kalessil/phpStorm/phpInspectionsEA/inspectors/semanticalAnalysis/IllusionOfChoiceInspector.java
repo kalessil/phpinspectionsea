48c17dd5b0f748033c2076ac3166f88095fab7d8
Vladimir Reznichenko <kalessil@gmail.com>
2018-04-08 15:54:13 +0200

    [#869] IllusionOfChoiceInspector: WIP

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/IllusionOfChoiceInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/IllusionOfChoiceInspector.java
@@ -4,17 +4,21 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.tree.IElementType;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpeanapiEquivalenceUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class IllusionOfChoiceInspector extends BasePhpInspection {
+    private static final String messageSameValueConditional = "...";
+    private static final String messageSameValueTernary     = "...";
 
     static private final Set<IElementType> targetOperations = new HashSet<>();
     static {
@@ -39,6 +43,17 @@ public class IllusionOfChoiceInspector extends BasePhpInspection {
         return new BasePhpElementVisitor() {
             @Override
             public void visitPhpTernaryExpression(@NotNull TernaryExpression expression) {
+                final PsiElement condition = ExpressionSemanticUtil.getExpressionTroughParenthesis(expression.getCondition());
+                if (condition instanceof BinaryExpression) {
+                    final BinaryExpression binary = (BinaryExpression) condition;
+                    if (targetOperations.contains(binary.getOperationType())) {
+                        final PsiElement trueVariant  = expression.getTrueVariant();
+                        final PsiElement falseVariant = expression.getFalseVariant();
+                        if (trueVariant != null && falseVariant != null) {
+                            this.analyze(binary, trueVariant, falseVariant);
+                        }
+                    }
+                }
             }
 
             @Override
@@ -52,30 +67,43 @@ public class IllusionOfChoiceInspector extends BasePhpInspection {
                         if (ifLast instanceof PhpReturn && ExpressionSemanticUtil.countExpressionsInGroup(ifBody) == 1) {
                             final PsiElement elseStatement = expression.getElseBranch();
                             if (elseStatement != null) {
-                                /* both if-else are returns */
+                                /* both if-else has returns only */
                                 final GroupStatement elseBody = ExpressionSemanticUtil.getGroupStatement(elseStatement);
                                 final PsiElement elseLast     = elseBody == null ? null : ExpressionSemanticUtil.getLastStatement(elseBody);
-                                if (elseLast instanceof PhpReturn) {
+                                if (elseLast instanceof PhpReturn && ExpressionSemanticUtil.countExpressionsInGroup(elseBody) == 1) {
                                     final PsiElement ifReturnValue   = ExpressionSemanticUtil.getReturnValue((PhpReturn) ifLast);
                                     final PsiElement elseReturnValue = ExpressionSemanticUtil.getReturnValue((PhpReturn) elseLast);
                                     if (ifReturnValue != null && elseReturnValue != null) {
-                                        this.analyze(expression.getFirstChild(), ifReturnValue, elseReturnValue);
+                                        this.analyze(binary, ifReturnValue, elseReturnValue);
                                     }
                                 }
                             } else {
-                                /* both if-next are returns */
+                                /* if has return only and the next statement is return */
+                                PsiElement nextStatement = expression.getNextPsiSibling();
+                                while (nextStatement instanceof PhpDocComment) {
+                                    nextStatement = ((PhpDocComment) nextStatement).getNextPsiSibling();
+                                }
+                                if (nextStatement instanceof PhpReturn) {
+                                    final PsiElement ifReturnValue   = ExpressionSemanticUtil.getReturnValue((PhpReturn) ifLast);
+                                    final PsiElement nextReturnValue = ExpressionSemanticUtil.getReturnValue((PhpReturn) nextStatement);
+                                    if (ifReturnValue != null && nextReturnValue != null) {
+                                        this.analyze(binary, ifReturnValue, nextReturnValue);
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
 
-            private void analyze(@NotNull PsiElement target, @NotNull PsiElement trueVarian, @NotNull PsiElement falseVarian) {
-                holder.registerProblem(target, "Nope!");
+            private void analyze(@NotNull PsiElement target, @NotNull PsiElement trueVariant, @NotNull PsiElement falseVariant) {
+                if (OpeanapiEquivalenceUtil.areEqual(trueVariant, falseVariant)) {
+                    final boolean isConditional = falseVariant.getParent() instanceof PhpReturn;
+                    holder.registerProblem(falseVariant, isConditional ? messageSameValueConditional : messageSameValueTernary);
+                } else {
+                    /* TODO: check if the construct makes sense at all */
+                }
             }
         };
     }
-
-    /* SenselessTernaryOperatorInspector + https://github.com/kalessil/phpinspectionsea/issues/869 */
-
 }

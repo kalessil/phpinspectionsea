a4ee23a22a0249f6f1a974a9789992f9394624ba
Vladimir Reznichenko <kalessil@gmail.com>
2018-04-24 20:31:00 +0200

    [#883] UnnecessaryClosureInspector: updated test fixtures

--- src/test/resources/fixtures/codeStyle/unnecessary-closures.fixed.php
+++ src/test/resources/fixtures/codeStyle/unnecessary-closures.fixed.php
@@ -1,8 +1,8 @@
 <?php
 
 function cases_holder() {
-    array_map(<weak_warning descr="The closure can be replaced with 'trim' (reduces cognitive load).">function ($value) { return trim($value); }</weak_warning>, []);
-    array_filter([], <weak_warning descr="The closure can be replaced with 'is_numeric' (reduces cognitive load).">function ($value) { return is_numeric($value); }</weak_warning>);
+    array_map('trim', []);
+    array_filter([], 'is_numeric');
 
     array_map(function (string $value) { return trim($value); }, []);
     array_filter([], function (string $value) { return is_numeric($value); });

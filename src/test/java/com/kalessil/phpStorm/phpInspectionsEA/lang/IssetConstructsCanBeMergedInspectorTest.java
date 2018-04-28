9cf28e9843718c72c6ae8c850c909442334981b1
Vladimir Reznichenko <kalessil@gmail.com>
2018-04-28 22:06:45 +0200

    [#79] IssetConstructsCanBeMergedInspector: WIP, added UTs skeleton

---
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/lang/IssetConstructsCanBeMergedInspectorTest.java
@@ -0,0 +1,16 @@
+package com.kalessil.phpStorm.phpInspectionsEA.lang;
+
+import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions.IssetConstructsCanBeMergedInspector;
+
+final public class IssetConstructsCanBeMergedInspectorTest extends PhpCodeInsightFixtureTestCase {
+    public void testIfFindsAllPatterns() {
+        myFixture.enableInspections(new IssetConstructsCanBeMergedInspector());
+        myFixture.configureByFile("fixtures/lang/isset-merge.php");
+        myFixture.testHighlighting(true, false, true);
+
+        myFixture.getAllQuickFixes().forEach(fix -> myFixture.launchAction(fix));
+        myFixture.setTestDataPath(".");
+        myFixture.checkResultByFile("fixtures/lang/isset-merge.fixed.php");
+    }
+}

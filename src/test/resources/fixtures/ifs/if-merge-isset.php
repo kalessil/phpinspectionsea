f29771d36dfe86904cb211fd140212d74d9ced8a
Vladimir Reznichenko <kalessil@gmail.com>
2018-04-28 22:25:54 +0200

    [#79] IssetConstructsCanBeMergedInspector: WIP, cleanup refactoring artifacts

--- src/test/resources/fixtures/ifs/if-merge-isset.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    if (isset($a) && <warning descr="This can be merged into the previous 'isset(..., ...[, ...])'.">isset($b)</warning>)    {}
-    if (!isset($a) || !<warning descr="This can be merged into the previous '!isset(..., ...[, ...])'.">isset($b)</warning>) {}
-
-    if (isset($a) || isset($b))   {}
-    if (!isset($a) && !isset($b)) {}

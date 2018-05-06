3c17e25b85d4c657e2f48cc874eb100cf59859ea
Vladimir Reznichenko <kalessil@gmail.com>
2018-05-06 15:50:37 +0200

    CompositionAndInheritanceInspector: WIP, updated test cases

--- src/test/resources/fixtures/classes/composition-inheritance-force.php
+++ src/test/resources/fixtures/classes/composition-inheritance-force.php
@@ -6,5 +6,5 @@ interface _Interface     {}
 abstract class _Abstract {}
 final class _Final       {}
 
-class <weak_warning descr="The class needs to be abstract (since it has children).">_Parent</weak_warning> {}
-class <weak_warning descr="The class needs to be either final (for aggregation) or abstract (for inheritance).">_Child</weak_warning> extends _Parent {}
+class <warning descr="The class needs to be abstract (since it has children).">_Parent</warning> {}
+class <warning descr="The class needs to be either final (for aggregation) or abstract (for inheritance).">_Child</warning> extends _Parent {}

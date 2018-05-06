67dbebe3a51d665c9df910c468b7d614161cdeed
Vladimir Reznichenko <kalessil@gmail.com>
2018-05-06 16:06:41 +0200

    CompositionAndInheritanceInspector: WIP, added new test cases

--- src/test/resources/fixtures/classes/composition-inheritance.php
+++ src/test/resources/fixtures/classes/composition-inheritance.php
@@ -3,12 +3,9 @@
 interface _Interface {
     public function method();
 }
-
-
 class <warning descr="The class needs to be either final (for aggregation) or abstract (for inheritance).">_ClassFollowsContract</warning> implements _Interface {
     public function method() {}
 }
-
 class _ClassBreakContract implements _Interface {
     public function method() {}
     public function breakContract() {}
@@ -18,5 +15,10 @@ class _ClassBreakContract implements _Interface {
 class <warning descr="The class needs to be abstract (since it has children).">_ParentFollowsContract</warning> implements _Interface {
     public function method() {}
 }
+class _ChildFollowsContract extends _ParentFollowsContract {}
 
-class <warning descr="The class needs to be either final (for aggregation) or abstract (for inheritance).">_ChildFollowsContract</warning> extends _ParentFollowsContract {}
+
+abstract class _AbstractFollowsContract implements _Interface {
+    public function method() {}
+}
+class <warning descr="The class needs to be either final (for aggregation) or abstract (for inheritance).">_AbstractChildFollowsContract</warning> extends _AbstractFollowsContract {}

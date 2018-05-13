afc08e2ea03eb6793282dc0060d467aed418116b
Vladimir Reznichenko <kalessil@gmail.com>
2018-05-13 08:37:11 +0200

    [#847] CallableMethodValidityInspector: set language level in test

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/CallableMethodValidityInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/classes/CallableMethodValidityInspectorTest.java
@@ -1,10 +1,13 @@
 package com.kalessil.phpStorm.phpInspectionsEA.classes;
 
+import com.jetbrains.php.config.PhpLanguageLevel;
+import com.jetbrains.php.config.PhpProjectConfigurationFacade;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.classes.CallableMethodValidityInspector;
 
 final public class CallableMethodValidityInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsPatterns() {
+        PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP710);
         myFixture.enableInspections(new CallableMethodValidityInspector());
         myFixture.configureByFile("fixtures/classes/callable-methods-validity.php");
         myFixture.configureByFile("fixtures/classes/callable-exceptions-handler-validity.php");

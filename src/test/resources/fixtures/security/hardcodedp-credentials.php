f59097d8eb3b578e8ddecfa5d1d7947bd8f74782
Vladimir Reznichenko <kalessil@gmail.com>
2018-06-02 14:16:21 +0200

    [#495] HardcodedCredentialsInspector: updated a fixture

--- src/test/resources/fixtures/security/hardcodedp-credentials.php
+++ src/test/resources/fixtures/security/hardcodedp-credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-    password_hash('...');
+    password_hash(<warning descr="It would be better idea to not hard-code credentials but to use a managed environment instead.">'...'</warning>);
     password_hash(<warning descr="It would be better idea to not hard-code credentials but to use a managed environment instead.">'...'</warning>, 'algorithm');
 
     /* @var ZipArchive $zip */

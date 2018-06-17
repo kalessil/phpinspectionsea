80263f964f03962c1c26181664b768d62642aec8
Vladimir Reznichenko <kalessil@gmail.com>
2018-06-17 16:59:57 +0200

    [#931] ArrayColumnCanBeUsedInspector: WIP

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/ArrayColumnCanBeUsedInspector.java
@@ -0,0 +1,81 @@
+package com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.arrays;
+
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.psi.elements.*;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.NamedElementUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpeanapiEquivalenceUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+public class ArrayColumnCanBeUsedInspector extends BasePhpInspection {
+    private static final String messagePattern = "'%s' would fit more here (it also faster).";
+
+    @NotNull
+    public String getShortName() {
+        return "ArrayColumnCanBeUsedInspection";
+    }
+
+    @Override
+    @NotNull
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new BasePhpElementVisitor() {
+            @Override
+            public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
+                final String functionName = reference.getName();
+                if (functionName != null && functionName.equals("array_map")) {
+                    final PsiElement[] arguments = reference.getParameters();
+                    if (arguments.length == 2 && arguments[1] != null && OpenapiTypesUtil.isLambda(arguments[0])) {
+                        final Function closure       = (Function) (arguments[0] instanceof Function ? arguments[0] : arguments[0].getFirstChild());
+                        final Parameter[] parameters = closure.getParameters();
+                        if (parameters.length > 0) {
+                            final GroupStatement body = ExpressionSemanticUtil.getGroupStatement(closure);
+                            if (body != null && ExpressionSemanticUtil.countExpressionsInGroup(body) == 1) {
+                                final PsiElement last = ExpressionSemanticUtil.getLastStatement(body);
+                                if (last instanceof PhpReturn) {
+                                    final PsiElement candidate = ExpressionSemanticUtil.getReturnValue((PhpReturn) last);
+                                    if (candidate instanceof ArrayAccessExpression) {
+                                        final ArrayAccessExpression access = (ArrayAccessExpression) candidate;
+                                        final PsiElement value             = access.getValue();
+                                        if (value instanceof Variable && OpeanapiEquivalenceUtil.areEqual(value, parameters[0])) {
+                                            final ArrayIndex index = access.getIndex();
+                                            final PsiElement key   = index == null ? null : index.getValue();
+                                            if (key instanceof StringLiteralExpression || key instanceof Variable) {
+                                                final PsiElement nameNode = NamedElementUtil.getNameIdentifier(reference);
+                                                if (nameNode != null) {
+                                                    final String replacement = String.format(
+                                                            "array_column(%s, %s)",
+                                                            arguments[1].getText(),
+                                                            key.getText()
+                                                    );
+                                                    holder.registerProblem(
+                                                            nameNode,
+                                                            String.format(messagePattern, replacement),
+                                                            new UseArrayColumnFixer(replacement)
+                                                    );
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    }
+}

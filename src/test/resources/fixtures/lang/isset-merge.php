4fa5dcc487971eadc70e4c1a97cb27f5d95d4743
Vladimir Reznichenko <kalessil@gmail.com>
2018-06-22 20:44:22 +0200

    [#956] IssetConstructsCanBeMergedInspector: QF bug-fix (lost parenthesises)

--- src/test/resources/fixtures/lang/isset-merge.php
+++ src/test/resources/fixtures/lang/isset-merge.php
@@ -4,6 +4,7 @@ function cases_holder() {
     return [
         isset($a) && <weak_warning descr="This can be merged into the previous 'isset(..., ...[, ...])'.">isset($b)</weak_warning>,
         isset($a) && (<weak_warning descr="This can be merged into the previous 'isset(..., ...[, ...])'.">isset($b)</weak_warning> && isset($c)),
+        isset($a) && <weak_warning descr="This can be merged into the previous 'isset(..., ...[, ...])'.">isset($b)</weak_warning> && ($c || $d),
 
         isset($a) && !isset($b),
 

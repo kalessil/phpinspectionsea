20ddcee1179bbd249a734805ffa650e6dd9419ab
Vladimir Reznichenko <kalessil@gmail.com>
2018-06-25 21:42:10 +0200

    SuspiciousBinaryOperationInspector: new patterns

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/binaryOperations/strategy/MultipleValuesEqualityStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/binaryOperations/strategy/MultipleValuesEqualityStrategy.java
@@ -4,9 +4,10 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.IElementType;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
-import com.jetbrains.php.lang.psi.elements.BinaryExpression;
-import com.jetbrains.php.lang.psi.elements.ParenthesizedExpression;
+import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpeanapiEquivalenceUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -28,15 +29,17 @@ import java.util.stream.Stream;
 /* ... != ... || ... != ... */
 
 final public class MultipleValuesEqualityStrategy {
+    private static final String messageAlwaysTrue  = "'%s' seems to be always true.";
+    private static final String messageAlwaysFalse = "'%s' seems to be always false.";
 
-    private static final Set<IElementType> equalOperators    = new HashSet<>();
-    private static final Set<IElementType> notEqualOperators = new HashSet<>();
+    private static final Map<IElementType, String> equalOperators    = new HashMap<>();
+    private static final Map<IElementType, String> notEqualOperators = new HashMap<>();
     static {
-        equalOperators.add(PhpTokenTypes.opEQUAL);
-        equalOperators.add(PhpTokenTypes.opIDENTICAL);
+        equalOperators.put(PhpTokenTypes.opEQUAL, "%s == %s && %s == %s");
+        equalOperators.put(PhpTokenTypes.opIDENTICAL, "%s === %s && %s === %s");
 
-        notEqualOperators.add(PhpTokenTypes.opNOT_EQUAL);
-        notEqualOperators.add(PhpTokenTypes.opNOT_IDENTICAL);
+        notEqualOperators.put(PhpTokenTypes.opNOT_EQUAL, "%s != %s || %s != %s");
+        notEqualOperators.put(PhpTokenTypes.opNOT_IDENTICAL, "%s != %s || %s != %s");
     }
 
     public static boolean apply(@NotNull BinaryExpression expression, @NotNull ProblemsHolder holder) {
@@ -49,11 +52,11 @@ final public class MultipleValuesEqualityStrategy {
             if (!(context instanceof BinaryExpression) || ((BinaryExpression) context).getOperationType() != operator) {
                 final List<PsiElement> fragments = extractFragments(expression, operator);
                 if (fragments.size() > 1) {
-                    final Set<IElementType> operators     = operator == PhpTokenTypes.opAND ? equalOperators : notEqualOperators;
-                    final List<BinaryExpression> filtered = fragments.stream()
+                    final Map<IElementType, String> operators = operator == PhpTokenTypes.opAND ? equalOperators : notEqualOperators;
+                    final List<BinaryExpression> filtered     = fragments.stream()
                             .filter(fragment -> fragment instanceof BinaryExpression)
                             .map(fragment    -> (BinaryExpression) fragment)
-                            .filter(fragment -> operators.contains(fragment.getOperationType()))
+                            .filter(fragment -> operators.containsKey(fragment.getOperationType()))
                             .collect(Collectors.toList());
                     if (filtered.size() > 1) {
                         result = analyze(filtered, operator, holder);
@@ -71,6 +74,61 @@ final public class MultipleValuesEqualityStrategy {
             @NotNull IElementType operator,
             @NotNull ProblemsHolder holder
     ) {
+        for (final BinaryExpression current : filtered) {
+            final PsiElement currentLeft  = current.getLeftOperand();
+            final PsiElement currentRight = current.getRightOperand();
+            if (currentLeft != null && currentRight != null) {
+                final IElementType operation  = current.getOperationType();
+                boolean reached               = false;
+                for (final BinaryExpression following : filtered) {
+                    if (following.getOperationType() == operation) {
+                        if (reached) {
+                            final PsiElement followingLeft  = following.getLeftOperand();
+                            final PsiElement followingRight = following.getRightOperand();
+                            if (followingLeft != null && followingRight != null) {
+                                PsiElement source             = null;
+                                final List<PsiElement> values = new ArrayList<>();
+                                if (OpeanapiEquivalenceUtil.areEqual(currentLeft, followingLeft)) {
+                                    source = followingLeft;
+                                    values.add(currentRight);
+                                    values.add(followingRight);
+                                } else if (OpeanapiEquivalenceUtil.areEqual(currentRight, followingRight)) {
+                                    source = followingRight;
+                                    values.add(currentLeft);
+                                    values.add(followingLeft);
+                                }
+                                if (
+                                    source != null &&
+                                    !(source instanceof StringLiteralExpression) &&
+                                    !(source instanceof ConstantReference) &&
+                                    !(source instanceof ClassConstantReference) &&
+                                    !OpenapiTypesUtil.isNumber(source)
+                                ) {
+                                    final boolean isAndOperator = operator == PhpTokenTypes.opAND;
+                                    final String fragment       = String.format(
+                                            (isAndOperator ? equalOperators : notEqualOperators).get(operation),
+                                            source.getText(),
+                                            values.get(0).getText(),
+                                            source.getText(),
+                                            values.get(1).getText()
+                                    );
+                                    values.clear();
+
+                                    holder.registerProblem(
+                                            following.getParent(),
+                                            String.format(isAndOperator ? messageAlwaysFalse : messageAlwaysTrue, fragment)
+                                    );
+                                    return true;
+                                }
+                                values.clear();
+                            }
+                        }
+                        reached = reached || following == current;
+                    }
+                }
+            }
+        }
+
         return false;
     }
 

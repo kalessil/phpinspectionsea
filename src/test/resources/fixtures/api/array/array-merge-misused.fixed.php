6c37c331f5b2dc644f2dd851d8b36c608d860d0c
Vladimir Reznichenko <kalessil@gmail.com>
2018-06-30 11:53:49 +0200

    ArrayMergeMissUseInspector: updated test fixtures

--- src/test/resources/fixtures/api/array/array-merge-misused.fixed.php
+++ src/test/resources/fixtures/api/array/array-merge-misused.fixed.php
@@ -1,1 +1,12 @@
 <?php
+
+function cases_holder($x) {
+    return [
+        array_merge([], array_merge([], []), []),
+
+        array_push($x, 0),
+        array_push($x, 0, 1, 2),
+        $x = array_merge($x, []),
+        $x = array_merge($x, ['key' => 'value']),
+    ];
+}
--- src/test/resources/fixtures/api/array/array-merge-misused.php
+++ src/test/resources/fixtures/api/array/array-merge-misused.php
@@ -1,1 +1,12 @@
 <?php
+
+function cases_holder($x) {
+    return [
+        <warning descr="Inlining nested 'array_merge(...)' in arguments is possible here (it also faster).">array_merge([], array_merge([], []), [])</warning>,
+
+        <warning descr="'array_push(...)' would fit more here (it also faster).">$x = array_merge($x, [0])</warning>,
+        <warning descr="'array_push(...)' would fit more here (it also faster).">$x = array_merge($x, [0, 1, 2])</warning>,
+        $x = array_merge($x, []),
+        $x = array_merge($x, ['key' => 'value']),
+    ];
+}

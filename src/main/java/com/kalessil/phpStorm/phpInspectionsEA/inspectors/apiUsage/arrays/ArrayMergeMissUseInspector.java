97e3f99cf2465b294436706c362af47d777ed29d
Vladimir Reznichenko <kalessil@gmail.com>
2018-06-30 07:04:18 +0200

    [#964] ArrayMergeMissUseInspector: QFes

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/ArrayMergeMissUseInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/ArrayMergeMissUseInspector.java
@@ -8,13 +8,16 @@ import com.jetbrains.php.lang.psi.elements.ArrayHashElement;
 import com.jetbrains.php.lang.psi.elements.AssignmentExpression;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationComponent;
+import com.kalessil.phpStorm.phpInspectionsEA.fixers.UseSuggestedReplacementFixer;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpeanapiEquivalenceUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /*
  * This file is part of the Php Inspections (EA Extended) package.
@@ -55,7 +58,14 @@ public class ArrayMergeMissUseInspector extends BasePhpInspection {
                                 if (OpenapiTypesUtil.isAssignment(parent)) {
                                     final PsiElement container = ((AssignmentExpression) parent).getVariable();
                                     if (container != null && OpeanapiEquivalenceUtil.areEqual(container, arguments[0])) {
-                                        holder.registerProblem(parent, messageArrayPush);
+                                        final List<String> fragments = new ArrayList<>();
+                                        fragments.add(arguments[0].getText());
+                                        Arrays.stream(elements).forEach(e -> fragments.add(e.getText()));
+
+                                        final String replacement = String.format("array_push(%s)", String.join(", ", fragments));
+                                        holder.registerProblem(parent, messageArrayPush, new UseArrayPushFixer(replacement));
+
+                                        fragments.clear();
                                     }
                                 }
                             }
@@ -71,21 +81,23 @@ public class ArrayMergeMissUseInspector extends BasePhpInspection {
                                 }
                             }
                         }
-
-                        /* case 3: duplicate arguments */
-                        for (final PsiElement argument : arguments) {
-                            boolean reached = false;
-                            for (final PsiElement candidate : arguments) {
-                                if (reached && OpeanapiEquivalenceUtil.areEqual(candidate, argument)) {
-                                    holder.registerProblem(argument, messageDuplicateArgument);
-                                    break;
-                                }
-                                reached = reached || candidate == argument;
-                            }
-                        }
                     }
                 }
             }
         };
+    }
+
+    private static final class UseArrayPushFixer extends UseSuggestedReplacementFixer {
+        private static final String title = "Use array_push(...) instead";
+
+        @NotNull
+        @Override
+        public String getName() {
+            return title;
+        }
+
+        UseArrayPushFixer(@NotNull String expression) {
+            super(expression);
+        }
     }
 }

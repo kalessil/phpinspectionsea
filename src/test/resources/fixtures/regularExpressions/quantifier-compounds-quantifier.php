e7fd44f99a21c12ed28340b4cc00c1b17466491d
vladimir.reznichenko <vladimir.reznichenko@intellishop.ag>
2018-07-03 08:50:15 +0200

    NotOptimalRegularExpressionsInspector: fixing broken build

--- src/test/resources/fixtures/regularExpressions/quantifier-compounds-quantifier.php
+++ src/test/resources/fixtures/regularExpressions/quantifier-compounds-quantifier.php
@@ -13,9 +13,9 @@
     preg_match(<error descr="( \D* )* might be exploited (ReDoS, Regular Expression Denial of Service).">'/(|\D*|)*/'</error>, '');
     preg_match(<error descr="( \D* )* might be exploited (ReDoS, Regular Expression Denial of Service).">'/(|\D*)*/'</error>, '');
 
-    preg_match(<error descr="\D and \W are not mutually exclusive in '\D|\W' which can be exploited (ReDoS, Regular Expression Denial of Service).">'/(\D|\W)+/'</error>, '');
+    preg_match(<error descr="\D and \W are not mutually exclusive in '\D|\W' which can be exploited (ReDoS, Regular Expression Denial of Service).">'/(\D|\W|xxx)+/'</error>, '');
     preg_match('/(\D|\w)+/', '');
-    preg_match(<error descr="\d and \w are not mutually exclusive in '\d|\w' which can be exploited (ReDoS, Regular Expression Denial of Service).">'/(\d|\w)+/'</error>, '');
+    preg_match(<error descr="\d and \w are not mutually exclusive in '\d|\w' which can be exploited (ReDoS, Regular Expression Denial of Service).">'/(\d|\w|xxx)+/'</error>, '');
     preg_match('/(\d|\W)+/', '');
 
     preg_match('/(\D*){1,10}/', '');

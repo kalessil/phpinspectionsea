41d23ce5a07f2bc3afc44a1b01cc508a0230d175
vladimir.reznichenko <vladimir.reznichenko@intellishop.ag>
2018-07-03 10:03:24 +0200

    NotOptimalRegularExpressionsInspector: updated test fixture

--- src/test/resources/fixtures/regularExpressions/quantifier-compounds-quantifier.php
+++ src/test/resources/fixtures/regularExpressions/quantifier-compounds-quantifier.php
@@ -14,9 +14,9 @@
     preg_match(<error descr="( \D* )* might be exploited (ReDoS, Regular Expression Denial of Service).">'/(|\D*)*/'</error>, '');
 
     preg_match(<error descr="\D and \W are not mutually exclusive in '\D|\W' which can be exploited (ReDoS, Regular Expression Denial of Service).">'/(\D|\W)+/'</error>, '');
-    preg_match(<error descr="'(\D|\w)' is 'greedy'. Please use '([\D\w])' instead.">'/(\D|\w)+/'</error>, '');
+    preg_match(<warning descr="'(\D|\w)' is 'greedy'. Please use '([\D\w])' instead.">'/(\D|\w)+/'</warning>, '');
     preg_match(<error descr="\d and \w are not mutually exclusive in '\d|\w' which can be exploited (ReDoS, Regular Expression Denial of Service).">'/(\d|\w)+/'</error>, '');
-    preg_match(<error descr="'(\d|\W)' is 'greedy'. Please use '([\d\W])' instead.">'/(\d|\W)+/'</error>, '');
+    preg_match(<warning descr="'(\d|\W)' is 'greedy'. Please use '([\d\W])' instead.">'/(\d|\W)+/'</warning>, '');
 
     preg_match('/(\D*){1,10}/', '');
     preg_match('/(\D*){1,}/', '');

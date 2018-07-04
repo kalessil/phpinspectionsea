d2468d6ae352fdeeef1819e23de9303bd9c72c52
Vladimir Reznichenko <kalessil@gmail.com>
2018-07-04 20:43:50 +0200

    StrTrUsageAsStrReplaceInspector: updated test fixture

--- src/test/resources/fixtures/controlFlow/strtr-used-as-strreplace.php
+++ src/test/resources/fixtures/controlFlow/strtr-used-as-strreplace.php
@@ -7,7 +7,7 @@
     <weak_warning descr="'str_replace(\"\\\\\", '_', '...')' can be used instead (improves maintainability).">strtr('...', "\\", '_')</weak_warning>;
 
     <weak_warning descr="'str_replace('\'', '_', '...')' can be used instead (improves maintainability).">strtr('...', '\'', '_')</weak_warning>;
-    <weak_warning descr="'str_replace(\"\\\\\"\", '_', '...')' can be used instead (improves maintainability).">strtr('...', "\"", '_')</weak_warning>;
+    <weak_warning descr="'str_replace(\"\\\"\", '_', '...')' can be used instead (improves maintainability).">strtr('...', "\"", '_')</weak_warning>;
 
     /* false-positives */
     strtr('...', '...', 'int');

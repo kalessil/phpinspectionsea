5c09e487f89b628239d04044d4cb0fcc9849f3d4
Vladimir Reznichenko <kalessil@gmail.com>
2018-07-04 19:36:46 +0200

    StrTrUsageAsStrReplaceInspector: new test cases

--- src/test/resources/fixtures/controlFlow/strtr-used-as-strreplace.fixed.php
+++ src/test/resources/fixtures/controlFlow/strtr-used-as-strreplace.fixed.php
@@ -1,10 +1,16 @@
 <?php
 
-    /* Main case: characters replacement */
-    str_replace(' ', '_', 'string to fix');
-    str_replace(" ", "_", 'string to fix');
-    str_replace('\\', '_', 'string to fix');
+    strtr('...', ' ', '_');
+    strtr('...', " ", "_");
+    
+    strtr('...', '\\', '_');
+    strtr('...', "\\", '_');
+    
+    strtr('...', '\'', '_');
+    strtr('...', "\"", '_');
 
-    /* False-positives */
-    strtr('string to fix', 'string', 'int');
-    strtr('string to fix', array('to' => 'to be', 'to be' => 'to'));
+    /* false-positives */
+    strtr('...', '...', 'int');
+    strtr('...', '\n', 'int');
+    strtr('...', "\'", 'int');
+    strtr('...', []);

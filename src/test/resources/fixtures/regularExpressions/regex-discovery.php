e6c36bda549a5c3bb43f0346b1ee5d2b89ea22c4
vladimir.reznichenko <vladimir.reznichenko@intellishop.ag>
2018-07-04 10:07:07 +0200

    NotOptimalRegularExpressionsInspector: updated test cases

--- src/test/resources/fixtures/regularExpressions/regex-discovery.php
+++ src/test/resources/fixtures/regularExpressions/regex-discovery.php
@@ -5,7 +5,6 @@ class CasesHolder {
     const REGEX2 = '1';
 
     public function case() {
-        $regex   = '/'. self::REGEX1 . self::REGEX2 . '/i';
-        preg_match(<weak_warning descr="'i' modifier is ambiguous here (no alphabet characters in given pattern).">$regex</weak_warning>, '...');
+        preg_match(<weak_warning descr="'i' modifier is ambiguous here (no alphabet characters in given pattern).">'/'. self::REGEX1 . self::REGEX2 . '/i'</weak_warning>, '...');
     }
 }

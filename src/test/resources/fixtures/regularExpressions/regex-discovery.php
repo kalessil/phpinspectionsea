aa80ac487e4cda54969846058cfd6e430b94661d
vladimir.reznichenko <vladimir.reznichenko@intellishop.ag>
2018-07-04 10:13:11 +0200

    NotOptimalRegularExpressionsInspector: updated test cases

--- src/test/resources/fixtures/regularExpressions/regex-discovery.php
+++ src/test/resources/fixtures/regularExpressions/regex-discovery.php
@@ -1,8 +1,8 @@
 <?php
 
 class CasesHolder {
-    const REGEX1 = '0';
-    const REGEX2 = '1';
+    const REGEX1 = '123';
+    const REGEX2 = '.+';
 
     public function case() {
         preg_match(<weak_warning descr="'i' modifier is ambiguous here (no alphabet characters in given pattern).">'/'. self::REGEX1 . self::REGEX2 . '/i'</weak_warning>, '...');

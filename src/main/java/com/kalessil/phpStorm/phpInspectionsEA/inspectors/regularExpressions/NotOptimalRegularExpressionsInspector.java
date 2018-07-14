089ff0ef6764822719fb52260d0599b439a5ecc0
Vladimir Reznichenko <kalessil@gmail.com>
2018-07-14 17:09:16 +0200

    NotOptimalRegularExpressionsInspector: cleaned up

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regularExpressions/NotOptimalRegularExpressionsInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regularExpressions/NotOptimalRegularExpressionsInspector.java
@@ -59,11 +59,6 @@ public class NotOptimalRegularExpressionsInspector extends BasePhpInspection {
     static {
         regexWithModifiers      = Pattern.compile("^([^{])(.*)\\1([a-zA-Z]+)?$");
         regexWithModifiersCurvy = Pattern.compile("^\\{(.*)\\}([a-zA-Z]+)?$");
-
-        /* original regex: \.\([a-z\?]+(?:\|[a-z\?]+)*\)\??$ */
-        // fileExtensions = "\\.\\([a-z\\?]+(?:\\|[a-z\\?]+)*\\)\\??$";
-        /* original regex: ^(?:\\b)?[\[\(\?:]*(http|https|ftp|ssh|git)*/
-        // protocoll = "^(?:\\\\b)?[\\[\\(\\?:]*(http|https|ftp|ssh|git)";
     }
 
     @Override

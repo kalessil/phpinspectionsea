cb059b096f4e05efc8d6fad3d4764588094f424a
Vladimir Reznichenko <kalessil@gmail.com>
2018-07-15 11:39:38 +0200

    BypassedPathTraversalProtectionInspector: registered

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/BypassedPathTraversalProtectionInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/BypassedPathTraversalProtectionInspector.java
@@ -5,11 +5,11 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.kalessil.phpStorm.phpInspectionsEA.fixers.UseSuggestedReplacementFixer;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
 import java.util.Set;
 
 /*
@@ -22,6 +22,7 @@ import java.util.Set;
  */
 
 public class BypassedPathTraversalProtectionInspector extends LocalInspectionTool {
+    private static final String messageFilterVar = "The call doesn't prevent path traversal, as can be bypassed (e.g. '....//').";
 
     @NotNull
     public String getShortName() {
@@ -39,10 +40,11 @@ public class BypassedPathTraversalProtectionInspector extends LocalInspectionToo
                     final PsiElement[] arguments = reference.getParameters();
                     if (arguments.length >= 3) {
                         final Set<String> second = ExpressionSemanticUtil.resolveAsString(arguments[1]);
-                        if (!second.isEmpty() && second.stream().anyMatch(String::isEmpty)) {
+                        if (!second.isEmpty() && second.contains("")) {
                             final Set<String> first = ExpressionSemanticUtil.resolveAsString(arguments[0]);
-                            if (!first.isEmpty()) {
-                                // ../ or ..\
+                            if (!first.isEmpty() && (first.contains("../") || first.contains("..\\"))) {
+                                final String replacement = String.format("preg_replace('/\\.+[\\/\\\\]+/', '', %s)", arguments[2].getText());
+                                holder.registerProblem(reference, messageFilterVar, new UsePregReplaceFix(replacement));
                             }
                             first.clear();
                         }
@@ -51,5 +53,19 @@ public class BypassedPathTraversalProtectionInspector extends LocalInspectionToo
                 }
             }
         };
+    }
+
+    private static final class UsePregReplaceFix extends UseSuggestedReplacementFixer {
+        private static final String title = "Harden with preg_replace(...)";
+
+        @NotNull
+        @Override
+        public String getName() {
+            return title;
+        }
+
+        UsePregReplaceFix(@NotNull String expression) {
+            super(expression);
+        }
     }
 }

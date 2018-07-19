3623ab44e6bf7acb7a3a1c42b7fc75f42545148f
Vladimir Reznichenko <kalessil@gmail.com>
2018-07-19 22:32:43 +0200

    [#981] ClassMockingCorrectnessInspector: new test cases

--- src/test/resources/fixtures/pitfalls/class-mocking-correctness.php
+++ src/test/resources/fixtures/pitfalls/class-mocking-correctness.php
@@ -7,8 +7,10 @@ namespace PhpSpec {
 namespace PHPUnit\Framework {
     class TestCase
     {
-        public function getMockBuilder() {}
-        public function getMockClass()   {}
+        public function getMockBuilder()          {}
+        public function getMockForTrait()         {}
+        public function getMockForAbstractClass() {}
+        public function getMockClass()            {}
     }
 }
 namespace {
@@ -21,25 +23,33 @@ namespace {
 }
 
 namespace {
-    final class Clazz extends stdClass {}
+    final class Clazz {}
+    abstract class AbstractClazz {}
+    trait TraitClazz {}
 
     $phpUnitOld = new \PHPUnit_Framework_TestCase();
     $phpUnitNew = new \PHPUnit\Framework\TestCase();
 
-    $phpUnitOld->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
-    $phpUnitOld->getMock(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
-    $phpUnitOld->getMockClass(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
+    $phpUnitOld->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">FinalClazz::class</error>);
+    $phpUnitOld->getMock(<error descr="Causes reflection errors as the referenced class is final.">FinalClazz::class</error>);
+    $phpUnitOld->getMockClass(<error descr="Causes reflection errors as the referenced class is final.">FinalClazz::class</error>);
     $phpUnitOld->getMockBuilder(stdClass::class);
 
-    $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
+    $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">FinalClazz::class</error>);
     $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">'Clazz'</error>);
     $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">'\Clazz'</error>);
     $phpUnitNew->getMockBuilder(<error descr="Causes reflection errors as the referenced class is final.">"\\Clazz"</error>);
-    $phpUnitNew->getMockClass(<error descr="Causes reflection errors as the referenced class is final.">Clazz::class</error>);
+    $phpUnitNew->getMockClass(<error descr="Causes reflection errors as the referenced class is final.">FinalClazz::class</error>);
     $phpUnitNew->getMockBuilder(stdClass::class);
+
+    $phpUnitNew->getMockForAbstractClass(<error descr="Needs an abstract class here.">FinalClazz::class</error>);
+    $phpUnitNew->getMockForTrait(<error descr="Needs a trait here.">FinalClazz::class</error>);
+
+    $phpUnitOld->getMockBuilder(<error descr="Perhaps it was intended to mock it with getMockForAbstractClass method.">AbstractClazz::class</error>);
+    $phpUnitOld->getMockBuilder(<error descr="Perhaps it was intended to mock it with getMockForTrait method.">TraitClazz::class</error>);
 
     class ClazzSpec extends \PhpSpec\ObjectBehavior
     {
-        function it_does_something(<error descr="Causes reflection errors as the referenced class is final.">Clazz</error> $clazz) {}
+        function it_does_something(<error descr="Causes reflection errors as the referenced class is final.">FinalClazz</error> $clazz) {}
     }
 }

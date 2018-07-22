e70e44d154f39b40a76964c48bc8aa2a08a15402
Vladimir Reznichenko <kalessil@gmail.com>
2018-07-22 14:55:41 +0200

    [#921] PhpUnitTestsInspector: updated a test fixture

--- src/test/resources/fixtures/phpUnit/expects-once.php
+++ src/test/resources/fixtures/phpUnit/expects-once.php
@@ -4,6 +4,6 @@ class CasesHolder
 {
     public function testExpectsOnce() {
         $mock = $this->getMockBuilder(\SimpleXMLElement::class)->getMock();
-        $mock->expects(<weak_warning descr="'->once()' would make more sense here.">$this->exactly(1))</weak_warning>)->method('asXML')->willReturn('...');
+        $mock->expects(<weak_warning descr="'->once()' would make more sense here.">$this->exactly(1)</weak_warning>)->method('asXML')->willReturn('...');
     }
 }

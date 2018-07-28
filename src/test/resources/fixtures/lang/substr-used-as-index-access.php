6541177e778fafb49dc290a1ba09070392291d96
Vladimir Reznichenko <kalessil@gmail.com>
2018-07-28 20:48:53 +0200

    [#980] SubStrUsedAsArrayAccessInspector: resolved false-positives

--- src/test/resources/fixtures/lang/substr-used-as-index-access.php
+++ src/test/resources/fixtures/lang/substr-used-as-index-access.php
@@ -1,8 +1,8 @@
 <?php
 
     <warning descr="'$string[$position]' might be used instead (invalid index accesses might show up).">substr($string, $position, 1)</warning>;
-    <warning descr="'$string[strlen($string) -1]' might be used instead (invalid index accesses might show up).">substr($string, -1, 1)</warning>;
-    <warning descr="'$string[strlen($string) -$offset]' might be used instead (invalid index accesses might show up).">substr($string, -$offset, 1)</warning>;
+    <warning descr="'$string[strlen($string) - 1]' might be used instead (invalid index accesses might show up).">substr($string, -1, 1)</warning>;
+    <warning descr="'$string[strlen($string) - $offset]' might be used instead (invalid index accesses might show up).">substr($string, -$offset, 1)</warning>;
 
     /* false-positives: ms_substr */
     mb_substr($string, $position, 1);
@@ -12,3 +12,7 @@
     /* false-positives: offset differs from 1 */
     substr($string, $position, -1);
     substr($string, $position, 5);
+
+    /* false-positives: source types */
+    substr(call(), $position, 1);
+    substr((string)$source, $position, 1);

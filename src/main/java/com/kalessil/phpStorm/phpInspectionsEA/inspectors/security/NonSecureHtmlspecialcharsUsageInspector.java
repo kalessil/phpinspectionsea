3592592dffc2b1c81cc68e2df74e5b3982681748
Vladimir Reznichenko <kalessil@gmail.com>
2018-07-28 20:02:44 +0200

    NonSecureHtmlspecialcharsUsageInspector: new patterns

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/NonSecureHtmlspecialcharsUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/NonSecureHtmlspecialcharsUsageInspector.java
@@ -9,6 +9,7 @@ import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.BinaryExpression;
 import com.jetbrains.php.lang.psi.elements.ConstantReference;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationComponent;
 import com.kalessil.phpStorm.phpInspectionsEA.fixers.UseSuggestedReplacementFixer;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
@@ -30,12 +31,17 @@ import java.util.stream.Collectors;
  */
 
 public class NonSecureHtmlspecialcharsUsageInspector extends BasePhpInspection {
-    private static final String message = "Single quotes handling is not specified, please use ENT_QUOTES or ENT_COMPAT as second argument.";
+    private static final String messageHarden   = "Single quotes handling is not specified, please use ENT_QUOTES or ENT_COMPAT as second argument.";
+    private static final String messageCallback = "Single quotes are not handled, please make use of ENT_QUOTES or ENT_COMPAT flags.";
 
-    private static final Set<String> flags = new HashSet<>();
+    private static final Set<String> flags     = new HashSet<>();
+    private static final Set<String> functions = new HashSet<>();
     static {
         flags.add("ENT_QUOTES");
         flags.add("ENT_COMPAT");
+
+        functions.add("call_user_func");
+        functions.add("array_map");
     }
 
     @NotNull
@@ -52,36 +58,45 @@ public class NonSecureHtmlspecialcharsUsageInspector extends BasePhpInspection {
                 if (!EAUltimateApplicationComponent.areFeaturesEnabled()) { return; }
 
                 final String functionName = reference.getName();
-                if (functionName != null && functionName.equals("htmlspecialchars")) {
-                    final PsiElement[] arguments = reference.getParameters();
-                    if (arguments.length > 0 && !this.isTestContext(reference)) {
-                        final PsiElement usedFlags = arguments.length > 1 ? arguments[1] : null;
-                        String updatedFlags        = null;
-                        if (usedFlags == null) {
-                            updatedFlags = "ENT_QUOTES | ENT_HTML5";
-                        } else if (usedFlags instanceof ConstantReference) {
-                            if (!flags.contains(((ConstantReference) usedFlags).getName())) {
-                                updatedFlags = String.format("ENT_QUOTES | %s", usedFlags.getText());
-                            }
-                        } else if (usedFlags instanceof BinaryExpression) {
-                            if (((BinaryExpression) usedFlags).getOperationType() == PhpTokenTypes.opBIT_OR) {
-                                final boolean usesQuoting = PsiTreeUtil.findChildrenOfType(usedFlags, ConstantReference.class)
-                                        .stream().anyMatch(constant -> flags.contains(constant.getName()));
-                                if (!usesQuoting) {
+                if (functionName != null) {
+                    if (functionName.equals("htmlspecialchars")) {
+                        final PsiElement[] arguments = reference.getParameters();
+                        if (arguments.length > 0 && !this.isTestContext(reference)) {
+                            final PsiElement usedFlags = arguments.length > 1 ? arguments[1] : null;
+                            String updatedFlags        = null;
+                            if (usedFlags == null) {
+                                updatedFlags = "ENT_QUOTES | ENT_HTML5";
+                            } else if (usedFlags instanceof ConstantReference) {
+                                if (!flags.contains(((ConstantReference) usedFlags).getName())) {
                                     updatedFlags = String.format("ENT_QUOTES | %s", usedFlags.getText());
                                 }
+                            } else if (usedFlags instanceof BinaryExpression) {
+                                if (((BinaryExpression) usedFlags).getOperationType() == PhpTokenTypes.opBIT_OR) {
+                                    final boolean usesQuoting = PsiTreeUtil.findChildrenOfType(usedFlags, ConstantReference.class)
+                                            .stream().anyMatch(constant -> flags.contains(constant.getName()));
+                                    if (!usesQuoting) {
+                                        updatedFlags = String.format("ENT_QUOTES | %s", usedFlags.getText());
+                                    }
+                                }
+                            }
+                            if (updatedFlags != null) {
+                                final String[] updatedArguments = new String[Math.max(2, arguments.length)];
+                                Arrays.stream(arguments).map(PsiElement::getText).collect(Collectors.toList()).toArray(updatedArguments);
+                                updatedArguments[1]             = updatedFlags;
+                                final String replacement        = String.format("htmlspecialchars(%s)", String.join(", ", updatedArguments));
+                                holder.registerProblem(reference, messageHarden, ProblemHighlightType.GENERIC_ERROR, new EscapeAllQuotesFix(replacement));
                             }
                         }
-                        if (updatedFlags != null) {
-                            final String[] updatedArguments = new String[Math.max(2, arguments.length)];
-                            Arrays.stream(arguments).map(PsiElement::getText).collect(Collectors.toList()).toArray(updatedArguments);
-                            updatedArguments[1]             = updatedFlags;
-                            final String replacement        = String.format("htmlspecialchars(%s)", String.join(", ", updatedArguments));
-                            holder.registerProblem(reference, message, ProblemHighlightType.GENERIC_ERROR, new EscapeAllQuotesFix(replacement));
+                    } else if (functions.contains(functionName)) {
+                        final PsiElement[] arguments = reference.getParameters();
+                        if (arguments.length == 2 && arguments[0] instanceof StringLiteralExpression) {
+                            final String callback = ((StringLiteralExpression) arguments[0]).getContents();
+                            if (callback.equals("htmlspecialchars")) {
+                                holder.registerProblem(arguments[0], messageCallback);
+                            }
                         }
                     }
                 }
-                /* TODO: call_user_func, array_walk - dynamic contexts */
             }
         };
     }
--- src/test/resources/fixtures/security/htmlspecialchars.fixed.php
+++ src/test/resources/fixtures/security/htmlspecialchars.fixed.php
@@ -10,5 +10,8 @@ function cases_holder() {
         htmlspecialchars('', ENT_COMPAT),
         htmlspecialchars('', ENT_QUOTES | ENT_HTML5),
         htmlspecialchars('', ENT_COMPAT | ENT_HTML5),
+
+        call_user_func('htmlspecialchars', '...'),
+        array_map('htmlspecialchars', []),
     ];
 }

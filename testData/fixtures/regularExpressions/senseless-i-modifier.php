19f546b1c7fd2b846b2b3c2236e5bd7b0a299866
Vladimir Reznichenko <kalessil@gmail.com>
2018-09-07 21:01:42 +0200

    Fixing merge: revert regex fixtures partially

--- testData/fixtures/regularExpressions/senseless-i-modifier.php
+++ testData/fixtures/regularExpressions/senseless-i-modifier.php
@@ -2,5 +2,5 @@
 
     preg_match(<weak_warning descr="'i' modifier is ambiguous here (no alphabet characters in given pattern).">'/123.+/i'</weak_warning>, '');
 
-    preg_match(<error descr="'а-я' range in '[а-яё]' is looking rather suspicious, please check.">'/[а-яё].+/iu'</error>, '');
+    preg_match('/[а-яё].+/iu', '');
     preg_match('/abc.+/i', '');

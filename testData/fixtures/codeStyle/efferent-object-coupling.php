34aa07c816fd54e57a0ada76a3e7f3ab1de5fd3d
Vladimir Reznichenko <kalessil@gmail.com>
2018-09-27 17:57:10 +0200

    EfferentObjectCouplingInspector: code grooming

--- testData/fixtures/codeStyle/efferent-object-coupling.php
+++ testData/fixtures/codeStyle/efferent-object-coupling.php
@@ -1,11 +1,12 @@
 <?php
-class A {
 
+class A {
 }
+
 class MyTime {
     public function setTime(\DateTime $time){}
 }
 
-class <weak_warning descr="Efferent coupling is 2.">B</weak_warning> extends A{
+class <weak_warning descr="High efferent coupling (2).">B</weak_warning> extends A{
     public function setTime(\DateTime $time){}
 }

ed93543de6f1b21b368c3a7919f1f14a85f4701f
vladimir.reznichenko <vladimir.reznichenko@intellishop.ag>
2018-09-28 08:39:26 +0200

    CI: script to check missing distraction toggles

---
+++ .travis/missing-toggles-check.php
@@ -0,0 +1,48 @@
+<?php
+
+    $basePath    = __DIR__ . '/..';
+    $sourcesPath = $basePath . '/src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors';
+
+    $missingDistractionTogglesFiles = [];
+    $partialDistractionTogglesFiles = [];
+
+    /* @var \SplFileInfo $file */
+    foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($sourcesPath)) as $file) {
+        if ($file->getExtension() === 'java') {
+            $content = file_get_contents($file->getPathname());
+
+            $distractionToggles = 0;
+            $lastPosition       = 0;
+            $searchFragment     = '.isContainingFileSkipped';
+            while (($lastPosition = strpos($content, $searchFragment, $lastPosition)) !== false) {
+                ++$distractionToggles;
+                $lastPosition += strlen($searchFragment);
+            }
+
+            if ($distractionToggles > 0) {
+                /* ensure each visitor uses toggles */
+                $visitors       = 0;
+                $lastPosition   = 0;
+                $searchFragment = 'public void visit';
+                while (($lastPosition = strpos($content, $searchFragment, $lastPosition)) !== false) {
+                    ++$visitors;
+                    $lastPosition += strlen($searchFragment);
+                }
+
+                if ($visitors != $distractionToggles) {
+                    $partialDistractionTogglesFiles[] = $file->getFilename();
+                }
+            } else {
+                $missingDistractionTogglesFiles[] = $file->getFilename();
+            }
+        }
+    }
+
+    if (count($partialDistractionTogglesFiles) > 0) {
+        echo 'Following files has inconsistent distraction toggles: ' . PHP_EOL . implode(PHP_EOL, $partialDistractionTogglesFiles) . PHP_EOL;
+        exit(-1);
+    }
+    if (count($missingDistractionTogglesFiles) > 0) {
+        echo 'Following files are missing distraction toggles: ' . PHP_EOL . implode(PHP_EOL, $missingDistractionTogglesFiles) . PHP_EOL;
+        exit(-1);
+    }

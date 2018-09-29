78e4f92d08b42ecd22637991ecdb7cd47f1a562f
Vladimir Reznichenko <kalessil@gmail.com>
2018-09-29 22:07:56 +0200

    [#1044] StreamSelectUsageInspector: cleaned up test fixtures

--- testData/fixtures/pitfalls/stream-select.php
+++ testData/fixtures/pitfalls/stream-select.php
@@ -4,13 +4,14 @@ class CasesHolder {
     const THRESHOLD_CORRECT = 200000;
     const THRESHOLD_LOW     = 100000;
 
-    public function cases_hlder() {
+    public function cases_holder() {
         return [
             stream_select ([], [], [], 0, <warning descr="Might cause high CPU usage connectivity issues (documentation advices using 200000 here, 200 ms).">self::THRESHOLD_LOW</warning>),
             stream_select ([], [], [], 0, <warning descr="Might cause high CPU usage connectivity issues (documentation advices using 200000 here, 200 ms).">100000</warning>),
 
             stream_select ([], [], [], 0),
             stream_select ([], [], [], 0, 200000),
+            stream_select ([], [], [], 1, self::THRESHOLD_LOW),
             stream_select ([], [], [], 0, self::THRESHOLD_CORRECT),
         ];
     }

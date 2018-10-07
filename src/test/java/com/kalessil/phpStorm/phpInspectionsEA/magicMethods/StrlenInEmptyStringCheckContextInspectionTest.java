edf69737c1ce0c966bb5b96a1e5f44305886baf4
Vladimir Reznichenko <kalessil@gmail.com>
2018-10-07 20:59:09 +0200

    StrlenInEmptyStringCheckContextInspection: respect plugin settings

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/magicMethods/StrlenInEmptyStringCheckContextInspectionTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/magicMethods/StrlenInEmptyStringCheckContextInspectionTest.java
@@ -2,9 +2,12 @@ package com.kalessil.phpStorm.phpInspectionsEA.magicMethods;
 
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalTransformations.StrlenInEmptyStringCheckContextInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.settings.ComparisonStyle;
 
 final public class StrlenInEmptyStringCheckContextInspectionTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
+        ComparisonStyle.force(ComparisonStyle.REGULAR);
+
         myFixture.enableInspections(new StrlenInEmptyStringCheckContextInspection());
         myFixture.configureByFile("testData/fixtures/magicMethods/empty-string-comparison.php");
         myFixture.testHighlighting(true, false, true);

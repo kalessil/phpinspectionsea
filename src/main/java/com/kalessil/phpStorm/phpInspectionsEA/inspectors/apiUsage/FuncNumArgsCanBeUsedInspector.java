4837b0352a08ae99f6ed6b739d11cbb900c2cc8b
vladimir.reznichenko <vladimir.reznichenko@intellishop.ag>
2018-10-18 09:34:31 +0200

    Ultimate: updated release notes

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/FuncNumArgsCanBeUsedInspector.java
@@ -0,0 +1,69 @@
+package com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage;
+
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationComponent;
+import com.kalessil.phpStorm.phpInspectionsEA.fixers.UseSuggestedReplacementFixer;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+public class FuncNumArgsCanBeUsedInspector extends BasePhpInspection {
+    private static final String message = "'func_num_args()' can be used instead.";
+
+
+    @NotNull
+    public String getShortName() {
+        return "FuncNumArgsCanBeUsedInspection";
+    }
+
+    @Override
+    @NotNull
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new BasePhpElementVisitor() {
+            @Override
+            public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
+                if (!EAUltimateApplicationComponent.areFeaturesEnabled()) { return; }
+                if (this.isContainingFileSkipped(reference))              { return; }
+
+                final String functionName = reference.getName();
+                if (functionName != null && functionName.equals("count")) {
+                    final PsiElement[] arguments = reference.getParameters();
+                    if (arguments.length == 1 && OpenapiTypesUtil.isFunctionReference(arguments[0])) {
+                        final FunctionReference innerReference = (FunctionReference) arguments[0];
+                        final String innerName                 = innerReference.getName();
+                        if (innerName != null && innerName.equals("func_get_args")) {
+                            holder.registerProblem(reference, message, new UseAlternativeFix("func_num_args()"));
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+    private static final class UseAlternativeFix extends UseSuggestedReplacementFixer {
+        private static final String title = "Use the suggested alternative";
+
+        @NotNull
+        @Override
+        public String getName() {
+            return title;
+        }
+
+        UseAlternativeFix(@NotNull String expression) {
+            super(expression);
+        }
+    }
+}

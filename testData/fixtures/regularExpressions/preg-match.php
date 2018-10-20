a4727f16fc177f0f3d01d04e7bc85723b4b94aba
Vladimir Reznichenko <kalessil@gmail.com>
2018-10-20 11:07:45 +0200

    [#1064] NotOptimalRegularExpressionsInspector: updated test fixtures

--- testData/fixtures/regularExpressions/preg-match.php
+++ testData/fixtures/regularExpressions/preg-match.php
@@ -2,28 +2,25 @@
 
 function cases_holder() {
     return [
-        // "text" !== '...'
-        !preg_match('/^text$/', '...'),
-        preg_match('/^text$/', '...') < 1,
-        preg_match('/^text$/', '...') == 0,
-        preg_match('/^text$/', '...') === 0,
-        preg_match('/^text$/', '...') != 1,
-        preg_match('/^text$/', '...') !== 1,
+        <warning descr="'\"text\" !== '...'' can be used instead.">!preg_match('/^text$/', '...')</warning>,
+        <warning descr="'\"text\" !== '...'' can be used instead.">preg_match('/^text$/', '...') < 1</warning>,
+        <warning descr="'\"text\" !== '...'' can be used instead.">preg_match('/^text$/', '...') == 0</warning>,
+        <warning descr="'\"text\" !== '...'' can be used instead.">preg_match('/^text$/', '...') === 0</warning>,
+        <warning descr="'\"text\" !== '...'' can be used instead.">preg_match('/^text$/', '...') != 1</warning>,
+        <warning descr="'\"text\" !== '...'' can be used instead.">preg_match('/^text$/', '...') !== 1</warning>,
 
-        // 0 !== strpos('...', "text")
-        !preg_match('/^text/', '...'),
-        preg_match('/^text/', '...') < 1,
-        preg_match('/^text/', '...') == 0,
-        preg_match('/^text/', '...') === 0,
-        preg_match('/^text/', '...') != 1,
-        preg_match('/^text/', '...') !== 1,
+        <warning descr="'0 !== strpos('...', \"text\")' can be used instead.">!preg_match('/^text/', '...')</warning>,
+        <warning descr="'0 !== strpos('...', \"text\")' can be used instead.">preg_match('/^text/', '...') < 1</warning>,
+        <warning descr="'0 !== strpos('...', \"text\")' can be used instead.">preg_match('/^text/', '...') == 0</warning>,
+        <warning descr="'0 !== strpos('...', \"text\")' can be used instead.">preg_match('/^text/', '...') === 0</warning>,
+        <warning descr="'0 !== strpos('...', \"text\")' can be used instead.">preg_match('/^text/', '...') != 1</warning>,
+        <warning descr="'0 !== strpos('...', \"text\")' can be used instead.">preg_match('/^text/', '...') !== 1</warning>,
 
-        // false === strpos('...', "text")
-        !preg_match('/text/', '...'),
-        preg_match('/text/', '...') < 1,
-        preg_match('/text/', '...') == 0,
-        preg_match('/text/', '...') === 0,
-        preg_match('/text/', '...') != 1,
-        preg_match('/text/', '...') !== 1,
+        <warning descr="'false === strpos('...', \"text\")' can be used instead.">!preg_match('/text/', '...')</warning>,
+        <warning descr="'false === strpos('...', \"text\")' can be used instead.">preg_match('/text/', '...') < 1</warning>,
+        <warning descr="'false === strpos('...', \"text\")' can be used instead.">preg_match('/text/', '...') == 0</warning>,
+        <warning descr="'false === strpos('...', \"text\")' can be used instead.">preg_match('/text/', '...') === 0</warning>,
+        <warning descr="'false === strpos('...', \"text\")' can be used instead.">preg_match('/text/', '...') != 1</warning>,
+        <warning descr="'false === strpos('...', \"text\")' can be used instead.">preg_match('/text/', '...') !== 1</warning>,
     ];
 }

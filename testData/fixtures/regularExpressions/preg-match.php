2076af2c0d503dea2bfba8f1aa9b19236a9f8333
Vladimir Reznichenko <kalessil@gmail.com>
2018-10-20 10:34:18 +0200

    [#1064] NotOptimalRegularExpressionsInspector: bug-fix fixture

--- testData/fixtures/regularExpressions/preg-match.php
+++ testData/fixtures/regularExpressions/preg-match.php
@@ -2,25 +2,25 @@
 
 function cases_holder() {
     return [
-        !preg_match('^text$', '...'),
-        preg_match('^text$', '...') < 1,
-        preg_match('^text$', '...') == 0,
-        preg_match('^text$', '...') === 0,
-        preg_match('^text$', '...') != 1,
-        preg_match('^text$', '...') !== 1,
+        !preg_match('/^text$/', '...'),
+        preg_match('/^text$/', '...') < 1,
+        preg_match('/^text$/', '...') == 0,
+        preg_match('/^text$/', '...') === 0,
+        preg_match('/^text$/', '...') != 1,
+        preg_match('/^text$/', '...') !== 1,
 
-        !preg_match('^text', '...'),
-        preg_match('^text', '...') < 1,
-        preg_match('^text', '...') == 0,
-        preg_match('^text', '...') === 0,
-        preg_match('^text', '...') != 1,
-        preg_match('^text', '...') !== 1,
+        !preg_match('/^text/', '...'),
+        preg_match('/^text/', '...') < 1,
+        preg_match('/^text/', '...') == 0,
+        preg_match('/^text/', '...') === 0,
+        preg_match('/^text/', '...') != 1,
+        preg_match('/^text/', '...') !== 1,
 
-        !preg_match('text', '...'),
-        preg_match('text', '...') < 1,
-        preg_match('text', '...') == 0,
-        preg_match('text', '...') === 0,
-        preg_match('text', '...') != 1,
-        preg_match('text', '...') !== 1,
+        !preg_match('/text/', '...'),
+        preg_match('/text/', '...') < 1,
+        preg_match('/text/', '...') == 0,
+        preg_match('/text/', '...') === 0,
+        preg_match('/text/', '...') != 1,
+        preg_match('/text/', '...') !== 1,
     ];
 }

d9f41d8558deb69f94f78922f4e1d40958f69d7d
Vladimir Reznichenko <kalessil@gmail.com>
2018-10-24 21:03:05 +0200

    RealpathInStreamContextInspector: code grooming

--- testData/fixtures/pitfalls/realpath-in-stream-context.fixed.php
+++ testData/fixtures/pitfalls/realpath-in-stream-context.fixed.php
@@ -3,14 +3,14 @@
     /* the main case */
     dirname(__DIR__) . '/';
     dirname(dirname(dirname(__DIR__))) . '/';
-    realpath (__DIR__ . '/..' . '/..');
+    realpath(__DIR__ . '/..' . '/..');
 
     /* similar cases, when the issues will popup */
     include '/whatever/file.php';
     include_once '/whatever/file.php';
     require '/whatever/file.php';
     require_once '/whatever/file.php';
-    require_once realpath ($x);
+    require_once realpath($x);
 
     /* the same, but wrapped with () */
     include ('/whatever/file.php');

1ab899017895ed364ac5a1f3a1d777bc0e7789e1
Vladimir Reznichenko <kalessil@gmail.com>
2018-10-25 18:00:34 +0200

    [#1081] PassingByReferenceCorrectnessInspector: new patterns

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/byReference/PassingByReferenceCorrectnessInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/byReference/PassingByReferenceCorrectnessInspector.java
@@ -4,6 +4,8 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiWhiteSpace;
+import com.jetbrains.php.config.PhpLanguageLevel;
+import com.jetbrains.php.config.PhpProjectConfigurationFacade;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
@@ -13,6 +15,9 @@ import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiResolveUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /*
  * This file is part of the Php Inspections (EA Extended) package.
@@ -25,6 +30,13 @@ import org.jetbrains.annotations.Nullable;
 
 public class PassingByReferenceCorrectnessInspector extends BasePhpInspection {
     private static final String message = "Emits a notice (only variable references should be returned by reference).";
+
+    private static final Set<String> skippedFunctions = new HashSet<>();
+    static {
+        /* workaround for https://youtrack.jetbrains.com/issue/WI-37984 */
+        skippedFunctions.add("current");
+        skippedFunctions.add("key");
+    }
 
     @NotNull
     public String getShortName() {
@@ -39,28 +51,32 @@ public class PassingByReferenceCorrectnessInspector extends BasePhpInspection {
             public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
                 final String functionName = reference.getName();
                 if (functionName != null) {
-                    /* workaround for https://youtrack.jetbrains.com/issue/WI-37984 */
-                    final boolean shouldSkip =
-                            (functionName.equals("current") || functionName.equals("key")) &&
-                            this.isFromRootNamespace(reference);
-                    if (!shouldSkip) {
-                        this.analyze(reference);
+                    final boolean skip = skippedFunctions.contains(functionName) && this.isFromRootNamespace(reference);
+                    if (!skip) {
+                        this.analyze(
+                                reference,
+                                PhpProjectConfigurationFacade.getInstance(reference.getProject()).getLanguageLevel()
+                        );
                     }
                 }
             }
 
             @Override
             public void visitPhpMethodReference(@NotNull MethodReference reference) {
-                this.analyze(reference);
+                this.analyze(
+                        reference,
+                        PhpProjectConfigurationFacade.getInstance(reference.getProject()).getLanguageLevel()
+                );
             }
 
-            private void analyze(@NotNull FunctionReference reference) {
+            private void analyze(@NotNull FunctionReference reference, @NotNull PhpLanguageLevel php) {
                 final PsiElement[] arguments = reference.getParameters();
                 if (arguments.length > 0) {
                     /* lazy analysis: if all args are valid, not even bother resolving the reference */
                     boolean doAnalyze = false;
                     for (final PsiElement argument : arguments) {
-                        boolean isRefCompatible = argument instanceof Variable || argument instanceof NewExpression;
+                        boolean isRefCompatible = argument instanceof Variable ||
+                                                  (argument instanceof NewExpression && php.compareTo(PhpLanguageLevel.PHP560) <= 0);
                         if (!isRefCompatible) {
                             doAnalyze = true;
                             break;
@@ -71,17 +87,15 @@ public class PassingByReferenceCorrectnessInspector extends BasePhpInspection {
                     if (resolved instanceof Function) {
                         final Parameter[] parameters = ((Function) resolved).getParameters();
                         for (int index = 0, max = Math.min(parameters.length, arguments.length); index < max; ++index) {
-                            final PsiElement argument = arguments[index];
-                            if (
-                                argument instanceof FunctionReference &&
-                                parameters[index].isPassByRef() &&
-                                !this.isByReference(argument)
-                            ) {
-                                final PsiElement resolvedArgument = OpenapiResolveUtil.resolveReference((FunctionReference) argument);
-                                if (resolvedArgument instanceof Function) {
-                                    final PsiElement name = NamedElementUtil.getNameIdentifier((Function) resolvedArgument);
-                                    if (!this.isByReference(name)) {
-                                        holder.registerProblem(argument, message);
+                            if (parameters[index].isPassByRef()) {
+                                final PsiElement argument = arguments[index];
+                                if (argument instanceof FunctionReference && !this.isByReference(argument)) {
+                                    final PsiElement function = OpenapiResolveUtil.resolveReference((FunctionReference) argument);
+                                    if (function instanceof Function) {
+                                        final PsiElement name = NamedElementUtil.getNameIdentifier((Function) function);
+                                        if (!this.isByReference(name)) {
+                                            holder.registerProblem(argument, message);
+                                        }
                                     }
                                 }
                             }
@@ -93,11 +107,11 @@ public class PassingByReferenceCorrectnessInspector extends BasePhpInspection {
             private boolean isByReference(@Nullable PsiElement element) {
                 boolean result = false;
                 if (element != null) {
-                    PsiElement refCandidate = element.getPrevSibling();
-                    if (refCandidate instanceof PsiWhiteSpace) {
-                        refCandidate = refCandidate.getPrevSibling();
+                    PsiElement ampersandCandidate = element.getPrevSibling();
+                    if (ampersandCandidate instanceof PsiWhiteSpace) {
+                        ampersandCandidate = ampersandCandidate.getPrevSibling();
                     }
-                    result = OpenapiTypesUtil.is(refCandidate, PhpTokenTypes.opBIT_AND);
+                    result = OpenapiTypesUtil.is(ampersandCandidate, PhpTokenTypes.opBIT_AND);
                 }
                 return result;
             }
--- testData/fixtures/pitfalls/passing-by-reference-correctness.php
+++ testData/fixtures/pitfalls/passing-by-reference-correctness.php
@@ -23,6 +23,7 @@ $obj->method($obj->property);
 $obj->method(<warning descr="Emits a notice (only variable references should be returned by reference).">explode(...[])</warning>);
 $obj->method(<warning descr="Emits a notice (only variable references should be returned by reference).">$obj->x()</warning>);
 $obj->method(<warning descr="Emits a notice (only variable references should be returned by reference).">Clazz::y()</warning>);
+$obj->method(<warning descr="Emits a notice (only variable references should be returned by reference).">new Clazz()</warning>);
 
 $array = [0];
 $obj->method($array[0]);

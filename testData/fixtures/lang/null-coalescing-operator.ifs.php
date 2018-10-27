f43b8d7f1c27ce1a047e5f7c3c767e58b6e07cec
Vladimir Reznichenko <kalessil@gmail.com>
2018-10-27 11:34:16 +0200

    NullCoalescingOperatorCanBeUsedInspector: new test fixtures

--- testData/fixtures/lang/null-coalescing-operator.ifs.php
+++ testData/fixtures/lang/null-coalescing-operator.ifs.php
@@ -14,3 +14,20 @@
     if (isset($value)) {
         $value = $value;
     }
+
+    <weak_warning descr="'$container = $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
+        $container = $value;
+    } else {
+        $container = 'default';
+    }
+
+    <weak_warning descr="'return $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
+        return $value;
+    } else {
+        return 'default';
+    }
+
+    <weak_warning descr="'return $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
+        return $value;
+    }
+    return 'default';

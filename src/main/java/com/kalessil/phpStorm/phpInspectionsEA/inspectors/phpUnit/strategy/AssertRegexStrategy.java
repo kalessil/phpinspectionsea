b4d8168b760d13d80da946b0177abc2d36766aed
Vladimir Reznichenko <kalessil@gmail.com>
2018-10-28 21:30:06 +0100

    Fixed merge

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/phpUnit/strategy/AssertRegexStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/phpUnit/strategy/AssertRegexStrategy.java
@@ -2,6 +2,8 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.phpUnit.strategy;
 
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
+import com.jetbrains.php.lang.lexer.PhpTokenTypes;
+import com.jetbrains.php.lang.psi.elements.BinaryExpression;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
@@ -38,6 +40,39 @@ public class AssertRegexStrategy {
                         final PsiElement[] innerArguments = candidate.getParameters();
                         if (innerArguments.length == 2) {
                             final boolean isDirect = arguments[0].getText().equals(numberCompareTargets.get(methodName));
+                            holder.registerProblem(
+                                    reference,
+                                    (isDirect ? "assertRegExp" : "assertNotRegExp") + " should be used instead"
+                            );
+                            result = true;
+                        }
+                    }
+                }
+            }
+        } else if (binaryTargets.contains(methodName)) {
+            final PsiElement[] arguments = reference.getParameters();
+            if (arguments.length > 0 && arguments[0] instanceof BinaryExpression) {
+                final BinaryExpression binary = (BinaryExpression) arguments[0];
+                if (binary.getOperationType() == PhpTokenTypes.opGREATER) {
+                    final PsiElement left  = binary.getLeftOperand();
+                    final PsiElement right = binary.getRightOperand();
+                    if (OpenapiTypesUtil.isNumber(right) && OpenapiTypesUtil.isFunctionReference(left)) {
+                        final boolean isTargetNumber = right.getText().equals("0");
+                        if (isTargetNumber) {
+                            final FunctionReference candidate = (FunctionReference) left;
+                            final String candidateName        = candidate.getName();
+                            if (candidateName != null && candidateName.equals("preg_match")) {
+                                final PsiElement[] innerArguments = candidate.getParameters();
+                                if (innerArguments.length == 2) {
+                                    final boolean isDirect = methodName.equals("assertTrue");
+                                    holder.registerProblem(
+                                            reference,
+                                            (isDirect ? "assertRegExp" : "assertNotRegExp") + " should be used instead"
+                                    );
+                                    result = true;
+
+                                }
+                            }
                         }
                     }
                 }

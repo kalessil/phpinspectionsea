8393b9eb1bbd86e060db173deb4086fedf903b65
Vladimir Reznichenko <kalessil@gmail.com>
2018-11-03 20:12:14 +0100

    SuspiciousReturnInspector: new patterns

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/SuspiciousReturnInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/SuspiciousReturnInspector.java
@@ -4,8 +4,9 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.lang.psi.elements.*;
+import com.jetbrains.php.lang.psi.resolve.types.PhpType;
+import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationComponent;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
@@ -21,7 +22,8 @@ import org.jetbrains.annotations.NotNull;
  */
 
 public class SuspiciousReturnInspector extends BasePhpInspection {
-    private static final String message = "Voids all return and throw statements from the try-block (returned values and exceptions are lost)";
+    private static final String messageFinally = "Voids all returned values and thrown exceptions from the try-block (returned values and exceptions are lost).";
+    private static final String messageYield   = "It was probably intended to use yield here (currently the returned values is getting ignored).";
 
     @NotNull
     public String getShortName() {
@@ -34,7 +36,8 @@ public class SuspiciousReturnInspector extends BasePhpInspection {
         return new BasePhpElementVisitor() {
             @Override
             public void visitPhpReturn(@NotNull PhpReturn statement) {
-                if (this.isContainingFileSkipped(statement)) { return; }
+                if (!EAUltimateApplicationComponent.areFeaturesEnabled()) { return; }
+                if (this.isContainingFileSkipped(statement))              { return; }
 
                 PsiElement parent = statement.getParent();
                 while (parent != null && !(parent instanceof PsiFile)) {
@@ -52,12 +55,18 @@ public class SuspiciousReturnInspector extends BasePhpInspection {
 
             private void analyzeReturnFromFinally(@NotNull PhpReturn statement, @NotNull Try scope) {
                 final GroupStatement body = ExpressionSemanticUtil.getGroupStatement(scope);
-                if (body != null && PsiTreeUtil.findChildOfAnyType(body, PhpReturn.class, PhpThrow.class) != null) {
-                    holder.registerProblem(statement, message);
+                if (body != null && ExpressionSemanticUtil.countExpressionsInGroup(body) > 0) {
+                    holder.registerProblem(statement, messageFinally);
                 }
             }
 
             private void analyzeReturnFromGenerator(@NotNull PhpReturn statement, @NotNull Function scope) {
+                if (ExpressionSemanticUtil.getReturnValue(statement) != null) {
+                    final PhpType type = scope.getType();
+                    if (type.filterUnknown().getTypes().stream().anyMatch(t -> t.equals("\\Generator"))) {
+                        holder.registerProblem(statement, messageYield);
+                    }
+                }
             }
         };
     }
--- testData/fixtures/pitfalls/return-in-finally.php
+++ testData/fixtures/pitfalls/return-in-finally.php
@@ -1,19 +1,31 @@
 <?php
 
-try {
-    if ($x > 0) {
-        return $x;
+function finally_cases_holder($argument, $exception)
+{
+    try {
+        if ($argument) { return $argument; }
+    } finally {
+        <error descr="Voids all returned values and thrown exceptions from the try-block (returned values and exceptions are lost).">return $argument;</error>
     }
-} catch (Exception $e) {
-} finally {
-    <error descr="Voids all return and throw statements from the try-block (returned values and exceptions are lost)">return -1;</error>
+
+    try {
+        if ($argument) { throw $exception; }
+    } finally {
+        <error descr="Voids all returned values and thrown exceptions from the try-block (returned values and exceptions are lost).">return $exception;</error>
+    }
+
+    /* false-positives */
+    try {
+    } finally {
+        return $argument;
+    }
 }
 
-try {
-    if ($x > 0) {
-        throw new RuntimeException();
-    }
-} catch (Exception $e) {
-} finally {
-    <error descr="Voids all return and throw statements from the try-block (returned values and exceptions are lost)">return -1;</error>
+function yield_cases_holder($argument): Generator
+{
+    yield $argument;
+    <error descr="It was probably intended to use yield here (currently the returned values is getting ignored).">return $argument;</error>
+
+    /* false-positives */
+    return;
 }

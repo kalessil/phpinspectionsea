c9cfe8568c70cea54983d8fa91ad09cc7d0f2152
Vladimir Reznichenko <kalessil@gmail.com>
2018-11-24 08:32:17 +0100

    TransitiveDependenciesUsageInspector: removed outdated comment

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/TransitiveDependenciesUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/TransitiveDependenciesUsageInspector.java
@@ -67,8 +67,6 @@ public class TransitiveDependenciesUsageInspector extends BasePhpInspection {
                     final Project project    = holder.getProject();
                     final String ownManifest = this.getManifest(reference, project);
                     if (ownManifest != null) {
-                        // if we can not identify class or it is in root NS, then we do resolve reference
-
                         final PsiElement resolved = OpenapiResolveUtil.resolveReference(reference);
                         if (resolved != null) {
                             final String dependencyManifest = this.getManifest(resolved, project);

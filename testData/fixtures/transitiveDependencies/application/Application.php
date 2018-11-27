6e2c0b69dc060d7e288fcbb19f5f5af8225511a8
Vladimir Reznichenko <kalessil@gmail.com>
2018-11-27 16:44:23 +0100

    TransitiveDependenciesUsageInspector: new test cases

--- testData/fixtures/transitiveDependencies/application/Application.php
+++ testData/fixtures/transitiveDependencies/application/Application.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Company {
+    use First\Subsplit\Clazz as SubsplitClazz;
     use First\Clazz as FirstClass;
     use <warning descr="The class belongs to a package which is not directly required in your composer.json. Please add the package into your composer.json">Second\Clazz</warning> as SecondClass;
     use Ignored\Clazz as IgnoredClass;
@@ -14,10 +15,12 @@ namespace Company {
                 new static(),
 
                 new \stdClass(),
+                new \First\Subsplit\Clazz(),
                 new \First\Clazz(),
                 new <warning descr="The class belongs to a package which is not directly required in your composer.json. Please add the package into your composer.json">\Second\Clazz()</warning>,
                 new \Ignored\Clazz(),
 
+                new SubsplitClazz(),
                 new FirstClass(),
                 new SecondClass(),
                 new IgnoredClass(),
---
+++ testData/fixtures/transitiveDependencies/application/vendor/first/subsplit/Clazz.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace First\Subsplit;
+
+class Clazz {}

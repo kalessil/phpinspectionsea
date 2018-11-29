ba2827a1a5e18330e88543361e22f4b80385d2a7
Vladimir Reznichenko <kalessil@gmail.com>
2018-11-29 11:23:03 +0100

    [#1123] AutoloadingIssuesInspector: performance fix

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/classes/AutoloadingIssuesInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/classes/AutoloadingIssuesInspector.java
@@ -4,13 +4,16 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.containers.MultiMap;
 import com.jetbrains.php.lang.psi.PhpFile;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.NamedElementUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -49,9 +52,10 @@ public class AutoloadingIssuesInspector extends BasePhpInspection {
             public void visitPhpFile(@NotNull PhpFile file) {
                 final String fileName = file.getName();
                 if (fileName.endsWith(".php") && !ignoredFiles.contains(fileName) && !laravelMigration.matcher(fileName).matches()) {
-                    /* find out how many named classes has been defined in the file */
-                    final List<PhpClass> classes = PsiTreeUtil.findChildrenOfType(file, PhpClass.class).stream()
-                            .filter(c -> !c.isAnonymous()).collect(Collectors.toList());
+                    final List<PhpClass> classes = new ArrayList<>();
+                    file.getTopLevelDefs().values().stream()
+                            .filter(definition  -> definition instanceof PhpClass)
+                            .forEach(definition -> classes.add((PhpClass) definition));
                     /* multiple classes defined, do nothing - this doesn't comply to PSRs */
                     if (classes.size() == 1) {
                         final PhpClass clazz = classes.get(0);

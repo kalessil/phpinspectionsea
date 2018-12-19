dcb231c79439f499c0d2d0db3720f6769c26b584
Vladimir Reznichenko <kalessil@gmail.com>
2018-12-19 20:10:06 +0100

    [#1142] StrlenInEmptyStringCheckContextInspection: actualize test fixtures

--- testData/fixtures/magicMethods/empty-string-comparison.fixed.php
+++ testData/fixtures/magicMethods/empty-string-comparison.fixed.php
@@ -1,55 +1,33 @@
 <?php
 
-interface IWithToString                { public function __toString(); }
-interface IWithoutToString             {}
-abstract class AbstractWithToString    { abstract public function __toString(); }
-abstract class AbstractWithoutToString {}
-class ClassWithToString                { public function __toString() {} }
-class ClassWithoutToString             {}
+function checkUseCases(string $string, float $float, int $int)
+{
+    if ('' !== $string)  {}
+    if ('' === $string) {}
+    if ('' !== $string)     {}
+    if ('' === $string)    {}
 
-function checkInterfaces(IWithToString $i1, IWithoutToString $i2) {
-    if (
-        '' != $i1 ||
-        strlen($i2)
-    ) {}
-}
-function checkAbstractClasses(AbstractWithToString $a1, AbstractWithoutToString $a2) {
-    if (
-        '' != $a1 ||
-        strlen($a2)
-    ) {}
-}
-function checkClasses(ClassWithToString $c1, ClassWithoutToString $c2) {
-    if (
-        '' != $c1 ||
-        strlen($c2)
-    ) {}
-}
+    if ($string || '' !== $string) {}
+    if ($string && '' !== $string) {}
 
-function checkUseCases($strToTest) {
-    if ('' !== $strToTest)  {}
-    if ('' === $strToTest) {}
-    if ('' !== $strToTest)     {}
-    if ('' === $strToTest)    {}
+    if ('' === $string)  {}
+    if ('' !== $string)  {}
+    if ('' === $string) {}
+    if ('' !== $string) {}
 
-    if ($strToTest || '' !== $strToTest) {}
-    if ($strToTest && '' !== $strToTest) {}
+    if ('' === $string) {}
+    if ('' !== $string) {}
+    if ('' === $string) {}
+    if ('' !== $string) {}
 
-    if ('' === $strToTest)  {}
-    if ('' !== $strToTest)  {}
-    if ('' === $strToTest) {}
-    if ('' !== $strToTest) {}
+    if ('' === $string) {}
+    if ('' !== $string)  {}
+    if ('' !== $string) {}
 
-    if ('' === $strToTest) {}
-    if ('' !== $strToTest) {}
-    if ('' === $strToTest) {}
-    if ('' !== $strToTest) {}
-
-    if ('' === $strToTest)   {}
-    if ('' !== $strToTest)  {}
-    if ('' !== $strToTest)   {}
+    if ('' != $float) {}
+    if ('' != $int) {}
 
     /* not yet supported */
-    if (1 >  strlen($strToTest))   {}
-    if (1 <= strlen($strToTest))  {}
+    if (1 >  strlen($string)) {}
+    if (1 <= strlen($string)) {}
 }
--- testData/fixtures/magicMethods/empty-string-comparison.php
+++ testData/fixtures/magicMethods/empty-string-comparison.php
@@ -1,53 +1,31 @@
 <?php
 
-interface IWithToString                { public function __toString(); }
-interface IWithoutToString             {}
-abstract class AbstractWithToString    { abstract public function __toString(); }
-abstract class AbstractWithoutToString {}
-class ClassWithToString                { public function __toString() {} }
-class ClassWithoutToString             {}
+function checkUseCases(string $string, float $float, int $int)
+{
+    if (<weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">mb_strlen($string)</weak_warning>)  {}
+    if (<weak_warning descr="''' === $string' would make more sense here (it also slightly faster).">!mb_strlen($string)</weak_warning>) {}
+    if (<weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">strlen($string)</weak_warning>)     {}
+    if (<weak_warning descr="''' === $string' would make more sense here (it also slightly faster).">!strlen($string)</weak_warning>)    {}
 
-function checkInterfaces(IWithToString $i1, IWithoutToString $i2) {
-    if (
-        <weak_warning descr="''' != $i1' can be used instead.">strlen($i1)</weak_warning> ||
-        <error descr="\IWithoutToString miss __toString() implementation.">strlen($i2)</error>
-    ) {}
-}
-function checkAbstractClasses(AbstractWithToString $a1, AbstractWithoutToString $a2) {
-    if (
-        <weak_warning descr="''' != $a1' can be used instead.">strlen($a1)</weak_warning> ||
-        <error descr="\AbstractWithoutToString miss __toString() implementation.">strlen($a2)</error>
-    ) {}
-}
-function checkClasses(ClassWithToString $c1, ClassWithoutToString $c2) {
-    if (
-        <weak_warning descr="''' != $c1' can be used instead.">strlen($c1)</weak_warning> ||
-        <error descr="\ClassWithoutToString miss __toString() implementation.">strlen($c2)</error>
-    ) {}
-}
+    if ($string || <weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">strlen($string)</weak_warning>) {}
+    if ($string && <weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">strlen($string)</weak_warning>) {}
 
-function checkUseCases(string $string) {
-    if (<weak_warning descr="''' !== $string' can be used instead.">mb_strlen($string)</weak_warning>)  {}
-    if (<weak_warning descr="''' === $string' can be used instead.">!mb_strlen($string)</weak_warning>) {}
-    if (<weak_warning descr="''' !== $string' can be used instead.">strlen($string)</weak_warning>)     {}
-    if (<weak_warning descr="''' === $string' can be used instead.">!strlen($string)</weak_warning>)    {}
+    if (<weak_warning descr="''' === $string' would make more sense here (it also slightly faster).">strlen($string) == 0</weak_warning>)  {}
+    if (<weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">strlen($string) != 0</weak_warning>)  {}
+    if (<weak_warning descr="''' === $string' would make more sense here (it also slightly faster).">strlen($string) === 0</weak_warning>) {}
+    if (<weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">strlen($string) !== 0</weak_warning>) {}
 
-    if ($string || <weak_warning descr="''' !== $string' can be used instead.">strlen($string)</weak_warning>) {}
-    if ($string && <weak_warning descr="''' !== $string' can be used instead.">strlen($string)</weak_warning>) {}
+    if (<weak_warning descr="''' === $string' would make more sense here (it also slightly faster).">0 ==  strlen($string)</weak_warning>) {}
+    if (<weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">0 !=  strlen($string)</weak_warning>) {}
+    if (<weak_warning descr="''' === $string' would make more sense here (it also slightly faster).">0 === strlen($string)</weak_warning>) {}
+    if (<weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">0 !== strlen($string)</weak_warning>) {}
 
-    if (<weak_warning descr="''' === $string' can be used instead.">strlen($string) == 0</weak_warning>)  {}
-    if (<weak_warning descr="''' !== $string' can be used instead.">strlen($string) != 0</weak_warning>)  {}
-    if (<weak_warning descr="''' === $string' can be used instead.">strlen($string) === 0</weak_warning>) {}
-    if (<weak_warning descr="''' !== $string' can be used instead.">strlen($string) !== 0</weak_warning>) {}
+    if (<weak_warning descr="''' === $string' would make more sense here (it also slightly faster).">strlen($string) < 1</weak_warning>) {}
+    if (<weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">strlen($string) >= 1</weak_warning>)  {}
+    if (<weak_warning descr="''' !== $string' would make more sense here (it also slightly faster).">strlen($string) > 0</weak_warning>) {}
 
-    if (<weak_warning descr="''' === $string' can be used instead.">0 ==  strlen($string)</weak_warning>) {}
-    if (<weak_warning descr="''' !== $string' can be used instead.">0 !=  strlen($string)</weak_warning>) {}
-    if (<weak_warning descr="''' === $string' can be used instead.">0 === strlen($string)</weak_warning>) {}
-    if (<weak_warning descr="''' !== $string' can be used instead.">0 !== strlen($string)</weak_warning>) {}
-
-    if (<weak_warning descr="''' === $string' can be used instead.">strlen($string) < 1</weak_warning>)   {}
-    if (<weak_warning descr="''' !== $string' can be used instead.">strlen($string) >= 1</weak_warning>)  {}
-    if (<weak_warning descr="''' !== $string' can be used instead.">strlen($string) > 0</weak_warning>)   {}
+    if (<weak_warning descr="''' != $float' would make more sense here (it also slightly faster).">strlen($float) > 0</weak_warning>) {}
+    if (<weak_warning descr="''' != $int' would make more sense here (it also slightly faster).">strlen($int) > 0</weak_warning>) {}
 
     /* not yet supported */
     if (1 >  strlen($string)) {}

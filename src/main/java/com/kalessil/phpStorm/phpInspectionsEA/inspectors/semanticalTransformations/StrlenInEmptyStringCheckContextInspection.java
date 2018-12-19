a135adb451aa39f9ae55e8ba823c69befaeb77d8
Vladimir Reznichenko <kalessil@gmail.com>
2018-12-19 19:08:53 +0100

    [#1142] StrlenInEmptyStringCheckContextInspection: code grooming

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalTransformations/StrlenInEmptyStringCheckContextInspection.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalTransformations/StrlenInEmptyStringCheckContextInspection.java
@@ -44,74 +44,74 @@ public class StrlenInEmptyStringCheckContextInspection extends BasePhpInspection
             public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
                 /* check if it's the target function */
                 final String functionName = reference.getName();
-                if (functionName == null || (!functionName.equals("strlen") && !functionName.equals("mb_strlen"))) {
-                    return;
-                }
+                if (functionName != null && (functionName.equals("strlen") || functionName.equals("mb_strlen"))) {
+                    final PsiElement[] arguments = reference.getParameters();
+                    if (arguments.length > 0) {
+                        boolean isMatchedPattern = false;
+                        boolean isEmptyString    = false;
+                        PsiElement target        = null;
 
-                boolean isMatchedPattern = false;
-                boolean isEmptyString    = false;
-                PsiElement target        = null;
+                        /* check explicit numbers comparisons */
+                        final PsiElement parent = reference.getParent();
+                        if (parent instanceof BinaryExpression) {
+                            final BinaryExpression binary  = (BinaryExpression) parent;
+                            final PsiElement left          = binary.getLeftOperand();
+                            final PsiElement secondOperand = OpenapiElementsUtil.getSecondOperand(binary, reference);
+                            /* second operand should be a number */
+                            if (OpenapiTypesUtil.isNumber(secondOperand)) {
+                                final String number = secondOperand.getText();
 
-                /* check explicit numbers comparisons */
-                final PsiElement parent = reference.getParent();
-                if (parent instanceof BinaryExpression) {
-                    final BinaryExpression binary  = (BinaryExpression) parent;
-                    final PsiElement left          = binary.getLeftOperand();
-                    final PsiElement secondOperand = OpenapiElementsUtil.getSecondOperand(binary, reference);
-                    /* second operand should be a number */
-                    if (OpenapiTypesUtil.isNumber(secondOperand)) {
-                        final String strNumber = secondOperand.getText();
+                                /* check cases when comparing with 1 */
+                                final IElementType operator = binary.getOperationType();
+                                if (operator == PhpTokenTypes.opGREATER) {
+                                    isMatchedPattern = left == reference && number.equals("0");
+                                    target           = binary;
+                                    isEmptyString    = false;
+                                } else if (operator == PhpTokenTypes.opLESS || operator == PhpTokenTypes.opGREATER_OR_EQUAL) {
+                                    isMatchedPattern = left == reference && number.equals("1");
+                                    target           = binary;
+                                    isEmptyString    = operator == PhpTokenTypes.opLESS;
+                                }
 
-                        /* check cases when comparing with 1 */
-                        final IElementType operator = binary.getOperationType();
-                        if (operator == PhpTokenTypes.opGREATER) {
-                            isMatchedPattern = left == reference && strNumber.equals("0");
-                            target           = binary;
-                            isEmptyString    = false;
-                        } else if (operator == PhpTokenTypes.opLESS || operator == PhpTokenTypes.opGREATER_OR_EQUAL) {
-                            isMatchedPattern = left == reference && strNumber.equals("1");
-                            target           = binary;
-                            isEmptyString    = operator == PhpTokenTypes.opLESS;
+                                /* check cases when checking equality to 0 */
+                                if (!isMatchedPattern && OpenapiTypesUtil.tsCOMPARE_EQUALITY_OPS.contains(operator)) {
+                                    isMatchedPattern = number.equals("0");
+                                    target           = binary;
+                                    isEmptyString    = operator == PhpTokenTypes.opIDENTICAL || operator == PhpTokenTypes.opEQUAL;
+                                }
+                            }
                         }
 
-                        /* check cases when checking equality to 0 */
-                        if (!isMatchedPattern && OpenapiTypesUtil.tsCOMPARE_EQUALITY_OPS.contains(operator)) {
-                            isMatchedPattern = strNumber.equals("0");
-                            target           = binary;
-                            isEmptyString    = operator == PhpTokenTypes.opIDENTICAL || operator == PhpTokenTypes.opEQUAL;
+                        /* checks NON-implicit boolean comparison patterns */
+                        if (!isMatchedPattern && ExpressionSemanticUtil.isUsedAsLogicalOperand(reference)) {
+                            isMatchedPattern           = true;
+                            target                     = reference;
+                            final PsiElement operation = parent instanceof UnaryExpression ? ((UnaryExpression) parent).getOperation() : null;
+                            if (operation != null) {
+                                isEmptyString = OpenapiTypesUtil.is(operation, PhpTokenTypes.opNOT);
+                                target        = parent;
+                            }
+                        }
+
+                        /* investigate possible issues */
+                        if (isMatchedPattern) {
+                            final boolean isStrict
+                                = !ClassInStringContextStrategy.apply(arguments[0], holder, target, patternMissingToStringMethod);
+
+                            final boolean isRegular  = ComparisonStyle.isRegular();
+                            final String operator    = (isEmptyString ? "=" : "!") + (isStrict ? "==" : "=");
+                            final String replacement = String.format(
+                                    isRegular ? "%s %s ''" : "'' %s %s",
+                                    isRegular ? arguments[0].getText() : operator,
+                                    isRegular ? operator : arguments[0].getText()
+                            );
+                            holder.registerProblem(
+                                    target,
+                                    messagePattern.replace("%e%", replacement),
+                                    new CompareToEmptyStringFix(replacement)
+                            );
                         }
                     }
-                }
-
-                /* checks NON-implicit boolean comparison patterns */
-                if (!isMatchedPattern && ExpressionSemanticUtil.isUsedAsLogicalOperand(reference)) {
-                    isMatchedPattern           = true;
-                    target                     = reference;
-                    final PsiElement operation = parent instanceof UnaryExpression ? ((UnaryExpression) parent).getOperation() : null;
-                    if (operation != null) {
-                        isEmptyString = OpenapiTypesUtil.is(operation, PhpTokenTypes.opNOT);
-                        target        = parent;
-                    }
-                }
-
-                /* investigate possible issues */
-                final PsiElement[] arguments = reference.getParameters();
-                if (isMatchedPattern && arguments.length > 0) {
-                    final boolean isStrict
-                        = !ClassInStringContextStrategy.apply(arguments[0], holder, target, patternMissingToStringMethod);
-
-                    final boolean isRegular  = ComparisonStyle.isRegular();
-                    final String operator    = (isEmptyString ? "=" : "!") + (isStrict ? "==" : "=");
-                    final String replacement = String.format(
-                            isRegular ? "%s %s ''" : "'' %s %s",
-                            isRegular ? arguments[0].getText() : operator,
-                            isRegular ? operator : arguments[0].getText()
-                    );
-                    holder.registerProblem(
-                            target,
-                            messagePattern.replace("%e%", replacement),
-                            new CompareToEmptyStringFix(replacement)
-                    );
                 }
             }
         };

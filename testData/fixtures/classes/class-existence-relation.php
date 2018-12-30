b15a24ba5fdd0ed5a5e4b0abf663b554d47323ae
Vladimir Reznichenko <kalessil@gmail.com>
2018-12-30 10:10:50 +0100

    [#1151] ClassExistenceCheckInspector: updated test fixture

--- testData/fixtures/classes/class-existence-relation.php
+++ testData/fixtures/classes/class-existence-relation.php
@@ -10,26 +10,26 @@ namespace ClassExistenceAndRelation {
     $string = '...';
 
     return [
-        <error descr="This call seems to always return false, please inspect the ::class expression.">class_exists</error> (I::class),
-        <error descr="This call seems to always return false, please inspect the ::class expression.">class_exists</error> (T::class),
+        <error descr="This call seems to always return false, please inspect the ::class expression.">class_exists(I::class)</error>,
+        <error descr="This call seems to always return false, please inspect the ::class expression.">class_exists(T::class)</error>,
         class_exists(C::class),
 
         interface_exists(I::class),
-        <error descr="This call seems to always return false, please inspect the ::class expression.">interface_exists</error> (T::class),
-        <error descr="This call seems to always return false, please inspect the ::class expression.">interface_exists</error> (C::class),
+        <error descr="This call seems to always return false, please inspect the ::class expression.">interface_exists(T::class)</error>,
+        <error descr="This call seems to always return false, please inspect the ::class expression.">interface_exists(C::class)</error>,
 
-        <error descr="This call seems to always return false, please inspect the ::class expression.">trait_exists</error> (I::class),
+        <error descr="This call seems to always return false, please inspect the ::class expression.">trait_exists(I::class)</error>,
         trait_exists(T::class),
-        <error descr="This call seems to always return false, please inspect the ::class expression.">trait_exists</error> (C::class),
+        <error descr="This call seems to always return false, please inspect the ::class expression.">trait_exists(C::class)</error>,
 
         is_subclass_of($object, I::class),
-        <error descr="This call seems to always return false, please inspect the ::class expression.">is_subclass_of</error> ($object, T::class),
+        <error descr="This call seems to always return false, please inspect the ::class expression.">is_subclass_of($object, T::class)</error>,
         is_subclass_of($object, C::class),
         is_subclass_of($string, C::class),
 
         is_a($object, I::class),
-        <error descr="This call seems to always return false, please inspect the ::class expression.">is_a</error> ($object, T::class),
+        <error descr="This call seems to always return false, please inspect the ::class expression.">is_a($object, T::class)</error>,
         is_a($object, C::class),
-        <error descr="This call might work not as expected, please specify the third argument.">is_a</error> ($string, C::class),
+        <error descr="This call might work not as expected, please specify the third argument.">is_a($string, C::class)</error>,
     ];
 }

46c1dff33b51914e4c827589fcd06757fb79c5c4
Vladimir Reznichenko <kalessil@gmail.com>
2018-12-31 13:41:58 +0100

    [#1148] InArrayCanBeUsedInspector: prototyping

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/InArrayCanBeUsedInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/InArrayCanBeUsedInspector.java
@@ -8,10 +8,8 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationComponent;
-import com.kalessil.phpStorm.phpInspectionsEA.fixers.UseSuggestedReplacementFixer;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
-import com.kalessil.phpStorm.phpInspectionsEA.options.OptionsComponent;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiElementsUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiEquivalenceUtil;
@@ -19,7 +17,6 @@ import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;

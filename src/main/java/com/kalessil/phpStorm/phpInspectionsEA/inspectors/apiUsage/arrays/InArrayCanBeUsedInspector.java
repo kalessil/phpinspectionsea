e1622bfafe626ddcc4771a45bbd9f0242ea4d323
Vladimir Reznichenko <kalessil@gmail.com>
2018-12-31 13:22:27 +0100

    [#1148] InArrayCanBeUsedInspector: prototyping

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/InArrayCanBeUsedInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/InArrayCanBeUsedInspector.java
@@ -4,14 +4,15 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
-import com.jetbrains.php.lang.psi.elements.BinaryExpression;
-import com.jetbrains.php.lang.psi.elements.ParenthesizedExpression;
-import com.jetbrains.php.lang.psi.elements.UnaryExpression;
+import com.jetbrains.php.lang.psi.elements.*;
 import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationComponent;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiElementsUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiEquivalenceUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -54,14 +55,57 @@ public class InArrayCanBeUsedInspector extends BasePhpInspection {
                         return;
                     }
 
-                    final List<BinaryExpression> binaries = this.filter(this.extract(expression, operator), operator);
-                    if (binaries.size() > 1) {
+                    final List<BinaryExpression> conditions = this.filter(this.extract(expression, operator), operator);
+                    if (conditions.size() > 1) {
                         /* `x !=[=] ... && x !=[=] ...`, `x ==[=] ... || x ==[=] ...` */
-                        /* TODO: group (values should be a number, string, or not contain calls) */
-                        /* TODO: if subject contains calls, report 2+ values, otherwise 3+ values */
+                        final Map<PsiElement, List<PsiElement>> groups = this.group(conditions);
+                        for (final Map.Entry<PsiElement, List<PsiElement>> entry : groups.entrySet()) {
+                            final PsiElement subject      = entry.getKey();
+                            final List<PsiElement> values = entry.getValue();
+                            /* if subject contains calls, report 2+ values, otherwise 3+ values */
+                            final boolean isTarget = values.size() > 2 ||
+                                                     PsiTreeUtil.findChildOfType(subject, FunctionReference.class) != null;
+                            if (isTarget) {
+                                /* TODO: reporting */
+                            }
+                            values.clear();
+                        }
+                        groups.clear();
                     }
-                    binaries.clear();
+                    conditions.clear();
                 }
+            }
+
+            @NotNull
+            private Map<PsiElement, List<PsiElement>> group(@NotNull List<BinaryExpression> conditions) {
+                final HashMap<PsiElement, List<PsiElement>> result = new HashMap<>();
+                for (final BinaryExpression binary : conditions) {
+                    final PsiElement left  = binary.getLeftOperand();
+                    final PsiElement right = binary.getRightOperand();
+                    if (left != null && right != null) {
+                        final List<PsiElement> values = Stream.of(left, right)
+                                .filter(operand -> operand instanceof StringLiteralExpression ||
+                                                   operand instanceof ConstantReference ||
+                                                   operand instanceof ClassConstantReference ||
+                                                   OpenapiTypesUtil.isNumber(operand)
+                                )
+                                .collect(Collectors.toList());
+                        if (values.size() == 1) {
+                            final PsiElement value   = values.get(0);
+                            final PsiElement subject = OpenapiElementsUtil.getSecondOperand(binary, value);
+                            final Optional<PsiElement> key = result.keySet().stream()
+                                    .filter(s -> s != null && subject != null && OpenapiEquivalenceUtil.areEqual(s, subject))
+                                    .findFirst();
+                            if (!key.isPresent()) {
+                                result.put(subject, new ArrayList<>(Collections.singletonList(value)));
+                            } else {
+                                result.get(key.get()).add(value);
+                            }
+                        }
+                        /* alternatives as variables, fields array (as I understand currently) better to not report */
+                    }
+                }
+                return result;
             }
 
             @NotNull
@@ -89,13 +133,6 @@ public class InArrayCanBeUsedInspector extends BasePhpInspection {
                             .forEach(expression -> {
                                 if (expression instanceof BinaryExpression) {
                                     result.addAll(this.extract((BinaryExpression) expression, operator));
-                                } else if (expression instanceof UnaryExpression) {
-                                    final UnaryExpression unary = (UnaryExpression) expression;
-                                    if (OpenapiTypesUtil.is(unary.getOperation(), PhpTokenTypes.opNOT)) {
-                                        result.add(ExpressionSemanticUtil.getExpressionTroughParenthesis(unary.getValue()));
-                                    } else {
-                                        result.add(unary);
-                                    }
                                 } else {
                                     result.add(expression);
                                 }

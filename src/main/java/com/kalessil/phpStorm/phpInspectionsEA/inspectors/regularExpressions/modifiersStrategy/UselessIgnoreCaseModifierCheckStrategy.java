cc67f15aab8b6583e79f9fd283d6eaf9ef5cee4e
Vladimir Reznichenko <kalessil@gmail.com>
2019-02-13 22:08:30 +0100

    [#1187] NotOptimalRegularExpressionsInspector: code grooming

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regularExpressions/modifiersStrategy/UselessIgnoreCaseModifierCheckStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regularExpressions/modifiersStrategy/UselessIgnoreCaseModifierCheckStrategy.java
@@ -3,7 +3,6 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.regularExpressions.mod
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
-import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 /*
@@ -19,8 +18,8 @@ public class UselessIgnoreCaseModifierCheckStrategy {
     private static final String message = "'i' modifier is ambiguous here (no alphabet characters in given pattern).";
 
     static public void apply(final String modifiers, final String pattern, @NotNull final StringLiteralExpression target, @NotNull final ProblemsHolder holder) {
-        if (modifiers != null && pattern != null && !modifiers.isEmpty() && !pattern.isEmpty()) {
-            final boolean check = modifiers.indexOf('i') != -1;
+        if (modifiers != null && !modifiers.isEmpty() && modifiers.indexOf('i') != -1) {
+            final boolean check = pattern != null && !pattern.isEmpty();
             if (check && !pattern.replaceAll("\\\\[\\\\dDwWsS]", "").matches(".*\\p{L}.*")) {
                 holder.registerProblem(target, message, ProblemHighlightType.WEAK_WARNING);
             }

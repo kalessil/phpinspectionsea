f072328f06c252b736b785de61e575e07af8b77f
Vladimir Reznichenko <kalessil@gmail.com>
2019-02-13 21:47:35 +0100

    [#1187] NotOptimalRegularExpressionsInspector: resolved a false-negative (i-modifier)

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regularExpressions/modifiersStrategy/UselessIgnoreCaseModifierCheckStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regularExpressions/modifiersStrategy/UselessIgnoreCaseModifierCheckStrategy.java
@@ -6,12 +6,22 @@ import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 public class UselessIgnoreCaseModifierCheckStrategy {
     private static final String message = "'i' modifier is ambiguous here (no alphabet characters in given pattern).";
 
     static public void apply(final String modifiers, final String pattern, @NotNull final StringLiteralExpression target, @NotNull final ProblemsHolder holder) {
-        if (!StringUtils.isEmpty(modifiers) && !StringUtils.isEmpty(pattern)) {
-            if (-1 != modifiers.indexOf('i') && !pattern.matches(".*\\p{L}.*")) {
+        if (modifiers != null && pattern != null && !modifiers.isEmpty() && !pattern.isEmpty()) {
+            final boolean check = modifiers.indexOf('i') != -1;
+            if (check && !pattern.replaceAll("\\\\[\\\\dDwWsS]", "").matches(".*\\p{L}.*")) {
                 holder.registerProblem(target, message, ProblemHighlightType.WEAK_WARNING);
             }
         }

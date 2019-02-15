92595f1253fbe4e7b596cb28268d48118dd70c7f
Vladimir Reznichenko <kalessil@gmail.com>
2019-02-15 13:01:59 +0100

    Fixed merge

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regularExpressions/modifiersStrategy/UselessIgnoreCaseModifierCheckStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/regularExpressions/modifiersStrategy/UselessIgnoreCaseModifierCheckStrategy.java
@@ -2,6 +2,7 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.regularExpressions.mod
 
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,7 +18,7 @@ import org.jetbrains.annotations.NotNull;
 public class UselessIgnoreCaseModifierCheckStrategy {
     private static final String message = "'i' modifier is ambiguous here (no alphabet characters in given pattern).";
 
-    static public void apply(final String modifiers, final String pattern, @NotNull final StringLiteralExpression target, @NotNull final ProblemsHolder holder) {
+    static public void apply(final String modifiers, final String pattern, @NotNull final PsiElement target, @NotNull final ProblemsHolder holder) {
         if (modifiers != null && !modifiers.isEmpty() && modifiers.indexOf('i') != -1) {
             final boolean check = pattern != null && !pattern.isEmpty();
             if (check && !pattern.replaceAll("\\\\[\\\\dDwWsS]", "").matches(".*\\p{L}.*")) {

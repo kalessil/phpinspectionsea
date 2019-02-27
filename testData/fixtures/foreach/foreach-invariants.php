165faee4e226da3585e232d87d87ac243e970248
Vladimir Reznichenko <kalessil@gmail.com>
2019-02-27 20:17:04 +0100

    [#1210] ForeachInvariantsInspector: enhance QF (eliminate unused limit variable)

--- testData/fixtures/foreach/foreach-invariants.php
+++ testData/fixtures/foreach/foreach-invariants.php
@@ -16,7 +16,8 @@
             $x = &$arr[$i];
             $x =& $arr[$i];
         }
-        <warning descr="Foreach can probably be used instead (easier to read and support).">for</warning> ($i = 0, $max = count($arr); $i < $max; $i++, $z = 0) {
+        $iMax = count($arr);
+        <warning descr="Foreach can probably be used instead (easier to read and support).">for</warning> ($i = 0; $i < $iMax; $i++, $z = 0) {
             echo $arr[$i]->property, $arr[$i]->property[0], $arr[$i][0];
             echo $arr[$i], $arr[$i] . " $arr[$i] " . " {$arr[$i]} ";
         }

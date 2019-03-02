c32b6aaf39c6ffaf921ea86f54f9a16e2f437b0e
Vladimir Reznichenko <kalessil@gmail.com>
2019-03-02 21:55:08 +0100

    BasenameCallsContextInspector: WIP, prototyping

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/fileSystem/BasenameCallsContextInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/fileSystem/BasenameCallsContextInspector.java
@@ -9,7 +9,10 @@ import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationComponent;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.PossibleValuesDiscoveryUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
 
 /*
  * This file is part of the Php Inspections (EA Extended) package.
@@ -21,7 +24,7 @@ import org.jetbrains.annotations.NotNull;
  */
 
 public class BasenameCallsContextInspector extends BasePhpInspection {
-    private static final String messagePattern = "'%e%' can be used instead (reduces amount of calls).";
+    private static final String messagePattern = "'%s' can be used instead (reduces amount of calls).";
 
     @NotNull
     public String getShortName() {
@@ -43,13 +46,30 @@ public class BasenameCallsContextInspector extends BasePhpInspection {
                     if (arguments.length >= 3 && arguments[1] instanceof StringLiteralExpression) {
                         final String replacedWith = ((StringLiteralExpression) arguments[1]).getContents();
                         if (replacedWith.isEmpty() && OpenapiTypesUtil.isFunctionReference(arguments[2])) {
-                            final FunctionReference candidate = (FunctionReference) arguments[2];
-                            final String candidateName        = candidate.getName();
-                            if (candidateName != null && candidateName.equals("basename")) {
-                                final PsiElement[] candidateArguments = candidate.getParameters();
-                                if (candidateArguments.length == 1) {
-
+                            final FunctionReference candidate     = (FunctionReference) arguments[2];
+                            final String candidateName            = candidate.getName();
+                            final PsiElement[] candidateArguments = candidate.getParameters();
+                            if (candidateArguments.length == 1 && candidateName != null && candidateName.equals("basename")) {
+                                final Set<PsiElement> searches = PossibleValuesDiscoveryUtil.discover(arguments[0]);
+                                for (final PsiElement search : searches) {
+                                    if (search instanceof StringLiteralExpression) {
+                                        final String searched = ((StringLiteralExpression) search).getContents();
+                                        if (searched.matches("^\\.[a-z0-9]+$")) {
+                                            final String replacement = String.format(
+                                                    "%sbasename(%s, %s)",
+                                                    candidate.getImmediateNamespaceName(),
+                                                    candidateArguments[0].getText(),
+                                                    arguments[0].getText()
+                                            );
+                                            holder.registerProblem(
+                                                    reference,
+                                                    String.format(messagePattern, replacement)
+                                            );
+                                            break;
+                                        }
+                                    }
                                 }
+                                searches.clear();
                             }
                         }
                     }

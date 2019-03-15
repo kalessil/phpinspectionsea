67e013802aa5700e490238eb6d78823490cf2b1e
Vladimir Reznichenko <kalessil@gmail.com>
2019-03-15 15:42:56 +0100

    docker: minor tweaks

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,4 +10,6 @@ if [ -n "$INSPECTIONCONFIG" ]
     INSPECTIONCONFIGFILE=/var/ci/Project_Default.xml
 fi
 
+rm -f ${INSPECTIONRESULTFILE} >> /dev/null 2>&1
 /vendor/bin/phpstorm-inspect `ls -d /PhpStorm-*/`bin/inspect.sh /root/.PhpStorm${PHPSTORMVERSION}/system /var/ci ${INSPECTIONCONFIGFILE} /var/ci/project checkstyle > ${INSPECTIONRESULTFILE}
+(stat ${INSPECTIONRESULTFILE} >> /dev/null 2>&1 && echo 'Inspections results: exported') || echo 'Inspections results: missing'

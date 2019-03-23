137778cf29e63f0c37687e63dbb00297f14cd8b3
Vladimir Reznichenko <kalessil@gmail.com>
2019-03-23 20:33:53 +0100

    SuspiciousLoopInspector: new test cases

--- testData/fixtures/controlFlow/suspicious-loop-conditions.php
+++ testData/fixtures/controlFlow/suspicious-loop-conditions.php
@@ -1,6 +1,10 @@
 <?php
 
     /* case: anomalies in conditions, which conflicts with looping itself */
+    if (!$wrong || $correct) {
+        foreach ($correct as $value) {}
+        <error descr="A parent condition 'empty($wrong)' looks suspicious.">foreach</error> ($wrong as $value) {}
+    }
     if (empty($wrong) || !empty($correct)) {
         foreach ($correct as $value) {}
         <error descr="A parent condition 'empty($wrong)' looks suspicious.">foreach</error> ($wrong as $value) {}

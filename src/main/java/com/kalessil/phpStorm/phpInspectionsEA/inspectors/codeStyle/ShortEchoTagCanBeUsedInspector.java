077e3c14284e952804b264311ef816b548f7b192
Vladimir Reznichenko <kalessil@gmail.com>
2019-03-24 23:12:28 +0100

    [#224] ShortEchoTagCanBeUsedInspector: bug-fixing

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeStyle/ShortEchoTagCanBeUsedInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeStyle/ShortEchoTagCanBeUsedInspector.java
@@ -4,16 +4,19 @@ import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiWhiteSpace;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
+import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
 import com.jetbrains.php.lang.psi.elements.PhpEchoStatement;
 import com.jetbrains.php.lang.psi.elements.PhpPrintExpression;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
  * This file is part of the Php Inspections (EA Extended) package.
@@ -48,7 +51,7 @@ public class ShortEchoTagCanBeUsedInspector extends BasePhpInspection {
                         closingTag = closingTag.getNextSibling();
                     }
                     if (OpenapiTypesUtil.is(closingTag, PhpTokenTypes.PHP_CLOSING_TAG)) {
-                        holder.registerProblem(echo.getFirstChild(), message, new UseShortEchoTagInspector(openingTag));
+                        holder.registerProblem(echo.getFirstChild(), message, new UseShortEchoTagInspector(openingTag, echo));
                     }
                 }
             }
@@ -67,7 +70,7 @@ public class ShortEchoTagCanBeUsedInspector extends BasePhpInspection {
                         closingTag = closingTag.getNextSibling();
                     }
                     if (OpenapiTypesUtil.is(closingTag, PhpTokenTypes.PHP_CLOSING_TAG)) {
-                        holder.registerProblem(print.getFirstChild(), message, new UseShortEchoTagInspector(openingTag));
+                        holder.registerProblem(print.getFirstChild(), message, new UseShortEchoTagInspector(openingTag, expression));
                     }
                 }
             }
@@ -77,7 +80,14 @@ public class ShortEchoTagCanBeUsedInspector extends BasePhpInspection {
     private static final class UseShortEchoTagInspector implements LocalQuickFix {
         private static final String title = "Use '<?= ... ?>' instead";
 
-        private UseShortEchoTagInspector(@NotNull PsiElement tag) {
+        private final SmartPsiElementPointer<PsiElement> expression;
+        private final SmartPsiElementPointer<PsiElement> tag;
+
+        private UseShortEchoTagInspector(@NotNull PsiElement tag, @NotNull PsiElement expression) {
+            super();
+            final SmartPointerManager factory = SmartPointerManager.getInstance(tag.getProject());
+            this.tag        = factory.createSmartPsiElementPointer(tag);
+            this.expression = factory.createSmartPsiElementPointer(expression);
         }
 
         @NotNull
@@ -96,8 +106,19 @@ public class ShortEchoTagCanBeUsedInspector extends BasePhpInspection {
         public void applyFix(@NotNull final Project project, @NotNull final ProblemDescriptor descriptor) {
             final PsiElement target = descriptor.getPsiElement();
             if (target != null && !project.isDisposed()) {
+                final PsiElement tag        = this.tag.getElement();
+                final PsiElement expression = this.expression.getElement();
+                if (tag != null && expression != null) {
+                    final PsiElement newTag = PhpPsiElementFactory.createFromText(project, PhpTokenTypes.PHP_ECHO_OPENING_TAG, "?><?=?>");
+                    final String arguments  = Stream.of(target.getParent().getChildren()).map(PsiElement::getText).collect(Collectors.joining(", "));
+                    final PsiElement echo   = PhpPsiElementFactory.createFromText(project, PhpEchoStatement.class, String.format("echo %s", arguments));
+                    if (newTag != null && echo != null) {
+                        echo.getFirstChild().delete();
+                        expression.replace(echo);
+                        tag.replace(newTag);
+                    }
+                }
             }
         }
     }
-
 }

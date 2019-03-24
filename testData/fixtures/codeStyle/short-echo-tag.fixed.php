c32fb03d8eb2f4e3d2018b24888caaf83e8c9848
Vladimir Reznichenko <kalessil@gmail.com>
2019-03-24 21:42:22 +0100

    [#224] ShortEchoTagCanBeUsedInspector: added UTs

---
+++ testData/fixtures/codeStyle/short-echo-tag.fixed.php
@@ -0,0 +1,12 @@
+<?= $value ?>
+<?= $value ?>
+<?= $value, $value ?>
+<?= $value ?>
+<?= $value ?>
+
+<?= $value; ?>
+<?= $value ?>
+<?= $value, $value ?>
+
+<?php echo $value; echo $value; ?>
+<?php print $value; print $value; ?>
---
+++ testData/fixtures/codeStyle/short-echo-tag.php
@@ -0,0 +1,12 @@
+<?php <weak_warning descr="">echo</weak_warning> $value; ?>
+<?php <weak_warning descr="">echo</weak_warning> $value ?>
+<?php <weak_warning descr="">echo</weak_warning> $value, $value ?>
+<?php <weak_warning descr="">print</weak_warning> $value; ?>
+<?php <weak_warning descr="">print</weak_warning> $value ?>
+
+<?= $value; ?>
+<?= $value ?>
+<?= $value, $value ?>
+
+<?php echo $value; echo $value; ?>
+<?php print $value; print $value; ?>

de27ac9027c80faeb27106329af83f6a038eea57
Vladimir Reznichenko <kalessil@gmail.com>
2019-03-25 11:59:36 +0100

    ExplodeMissUseInspector: relocated

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/arrays/ExplodeMissUseInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/arrays/ExplodeMissUseInspectorTest.java
@@ -1,17 +1,17 @@
-package com.kalessil.phpStorm.phpInspectionsEA.controlFlow;
+package com.kalessil.phpStorm.phpInspectionsEA.api.arrays;
 
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
-import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.ExplodeMissUseInspector;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.arrays.ExplodeMissUseInspector;
 
 final public class ExplodeMissUseInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new ExplodeMissUseInspector());
-        myFixture.configureByFile("testData/fixtures/controlFlow/explode-misuse.php");
+        myFixture.configureByFile("testData/fixtures/api/array/explode-misuse.php");
         myFixture.testHighlighting(true, false, true);
 
         myFixture.getAllQuickFixes().forEach(fix -> myFixture.launchAction(fix));
         myFixture.setTestDataPath(".");
-        myFixture.checkResultByFile("testData/fixtures/controlFlow/explode-misuse.fixed.php");
+        myFixture.checkResultByFile("testData/fixtures/api/array/explode-misuse.fixed.php");
     }
 }
 

296094fbc5801981eabb0cbed909cda67446487d
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-02 20:29:25 +0200

    ProperNullCoalescingOperatorUsageInspector: updated test fixture

--- testData/fixtures/codeStyle/proper-null-coalescing-usage.php
+++ testData/fixtures/codeStyle/proper-null-coalescing-usage.php
@@ -15,7 +15,7 @@ namespace {
             return [
                 <weak_warning descr="It possible to use '$this->method()' instead (reduces cognitive load).">$this->method() ?? null</weak_warning>,
 
-                <weak_warning descr="Resolved operands types are not complimentary, while they should be ([\CasesHolder, null] vs [array]).">$this->property ?? []</weak_warning>,
+                <weak_warning descr="Resolved operands types are not complimentary, while they should be ([\CasesHolder] vs [array]).">$this->property ?? []</weak_warning>,
                 $this->property ?? null,
 
                 $one ?? $two,

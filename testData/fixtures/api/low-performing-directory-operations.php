aafca308e8b50652d0fe849968acc81980c97ce2
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-03 15:37:24 +0200

    LowPerformingDirectoryOperationsInspector: updated test fixtures

--- testData/fixtures/api/low-performing-directory-operations.php
+++ testData/fixtures/api/low-performing-directory-operations.php
@@ -1,7 +1,7 @@
 <?php
 
-    <error descr="'scandir(...)' sorts results by default, please specify the second argument.">scandir('...')</error>;
+    <error descr="'scandir(...)' sorts results by default, please provide second argument for specifying the intention.">scandir('...')</error>;
     scandir('...', SCANDIR_SORT_ASCENDING);
 
-    <error descr="'glob(...)' sorts results by default, please specify the second argument.">glob('...')</error>;
+    <error descr="'glob(...)' sorts results by default, please provide second argument for specifying the intention.">glob('...')</error>;
     glob('...', null);

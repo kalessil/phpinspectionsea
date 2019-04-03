049207a6679cf727939fa044dfa2bb9f1c21230b
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-03 16:29:04 +0200

    NotOptimalRegularExpressionsInspector: new test cases

--- testData/fixtures/regularExpressions/alternative-regex-functions-usage.php
+++ testData/fixtures/regularExpressions/alternative-regex-functions-usage.php
@@ -1,6 +1,6 @@
 <?php
 
-    preg_quote('/./');
+    <warning descr="Second parameter should be provided (for proper symbols escaping).">preg_quote('/./')</warning>;
     preg_quote('/./', '/');
 
     !<weak_warning descr="'preg_match(...)' would fit more here (also performs better).">preg_match_all</weak_warning>('/./', '...');

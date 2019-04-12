501e95dcacb64549036c3260a6675c69663336b5
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-12 16:06:57 +0200

    [#1290] SuspiciousBinaryOperationInspector: resolved a false-positive

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/binaryOperations/strategy/NullCoalescingOperatorCorrectnessStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/binaryOperations/strategy/NullCoalescingOperatorCorrectnessStrategy.java
@@ -7,7 +7,10 @@ import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.BinaryExpression;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.jetbrains.php.lang.psi.elements.UnaryExpression;
+import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiResolveUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.Types;
 import org.jetbrains.annotations.NotNull;
 
 /*
@@ -36,7 +39,13 @@ final public class NullCoalescingOperatorCorrectnessStrategy {
                     }
                 }
             } if (left instanceof FunctionReference) {
-                holder.registerProblem(left, String.format(messagePatternCall, left.getText()));
+                final PhpType resolved = OpenapiResolveUtil.resolveType((FunctionReference) left, holder.getProject());
+                if (resolved != null && !resolved.hasUnknown()) {
+                    final boolean isTarget = resolved.getTypes().stream().noneMatch(t -> Types.getType(t).equals(Types.strString));
+                    if (isTarget) {
+                        holder.registerProblem(left, String.format(messagePatternCall, left.getText()));
+                    }
+                }
             }
         }
         return result;
--- testData/fixtures/pitfalls/suspicious-binary-operations.null-coalescing.php
+++ testData/fixtures/pitfalls/suspicious-binary-operations.null-coalescing.php
@@ -1,11 +1,15 @@
 <?php
 
+function returns_string(): string { return ''; }
+
 $y = [
     <error descr="The operation results to '(int)$x', the right operator can be omitted.">(int)$x</error> ?? '...',
     (<error descr="The operation results to '(string)$x', the right operator can be omitted.">(string)$x</error>) ?? '...',
     (<error descr="The operation results to '!$x', the right operator can be omitted.">!$x</error>) ?? '...',
-    <error descr="Due to 'trim($x)' used as left operand, using '?:' instead of '??' would make more sense.">trim($x)</error> ?? '...',
+    <error descr="Due to 'call($x)' used as left operand, using '?:' instead of '??' would make more sense.">call($x)</error> ?? '...',
 
     $x ?? <error descr="The operation results to '(int)$x', the right operator can be omitted.">(int)$x</error> ?? '...',
-    $x ?? <error descr="Due to 'trim($x)' used as left operand, using '?:' instead of '??' would make more sense.">trim($x)</error> ?? '...',
+    $x ?? <error descr="Due to 'call($x)' used as left operand, using '?:' instead of '??' would make more sense.">call($x)</error> ?? '...',
+
+    returns_string() ?? ''
 ];

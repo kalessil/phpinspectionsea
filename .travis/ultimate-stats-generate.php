cf3f7417f526306df58efd2ea8f1527f9e9fecd4
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-19 09:22:00 +0200

    Ultimate: adding stats generation

--- .travis/ultimate-stats-generate.php
+++ .travis/ultimate-stats-generate.php
@@ -39,10 +39,17 @@
         $extendedDefinitions[$className] = $container;
     }
 
+    $statistics = [];
     foreach ($ultimateDefinitions as $className => $definition) {
         $extendedDefinition = isset($extendedDefinitions[$className]) ? $extendedDefinitions[$className] : [];
         if ((array)$definition != (array)$extendedDefinition) {
             $status = ($extendedDefinition === []) ? 'new' : 'enhanced';
-            echo $definition->groupName .': '. $className .'('. $status .')'. PHP_EOL;
+            ++$statistics[$definition->groupName][$status];
+            echo sprintf('%s: %s (%s)', $definition->groupName, $className, $status). PHP_EOL;
         }
+    }
+    foreach ($statistics as $group => $statistic) {
+        $new      = isset($statistic['new']) ? $statistic['new'] : 0;
+        $enhanced = isset($statistic['enhanced']) ? $statistic['enhanced'] : 0;
+        echo sprintf('%s: %s new, %s enhanced', $group, $new, $enhanced) .PHP_EOL;
     }

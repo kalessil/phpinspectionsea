30209fa086fcde7aa099059883ba8d2bf6650a03
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-28 10:50:29 +0200

    PregQuoteUsageInspector: new patterns

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/PregQuoteUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/PregQuoteUsageInspector.java
@@ -1,12 +1,12 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage;
 
-import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.inspections.PhpInspection;
+import com.jetbrains.php.lang.psi.elements.ConstantReference;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
-import com.kalessil.phpStorm.phpInspectionsEA.openApi.GenericPhpElementVisitor;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.FeaturedPhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.settings.StrictnessCategory;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,7 +20,8 @@ import org.jetbrains.annotations.NotNull;
  */
 
 public class PregQuoteUsageInspector extends PhpInspection {
-    private static final String message = "Please provide regex delimiter as the second argument for proper escaping.";
+    private static final String messageMissing   = "Please provide regex delimiter as the second argument for proper escaping.";
+    private static final String messageSeparator = "The separator value is platform-dependent, consider using '/' instead.";
 
     @NotNull
     public String getShortName() {
@@ -30,7 +31,7 @@ public class PregQuoteUsageInspector extends PhpInspection {
     @Override
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
-        return new GenericPhpElementVisitor() {
+        return new FeaturedPhpElementVisitor() {
             @Override
             public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
                 if (this.shouldSkipAnalysis(reference, StrictnessCategory.STRICTNESS_CATEGORY_PROBABLE_BUGS)) { return; }
@@ -39,7 +40,12 @@ public class PregQuoteUsageInspector extends PhpInspection {
                 if (functionName != null && functionName.equals("preg_quote")) {
                     final PsiElement[] arguments = reference.getParameters();
                     if (arguments.length == 1) {
-                        holder.registerProblem(reference, message, ProblemHighlightType.GENERIC_ERROR);
+                        holder.registerProblem(reference, messageMissing);
+                    } else if (arguments.length == 2) {
+                        final PsiElement escaped = arguments[1];
+                        if (escaped instanceof ConstantReference && escaped.getText().equals("DIRECTORY_SEPARATOR")) {
+                            holder.registerProblem(escaped, messageSeparator);
+                        }
                     }
                 }
             }
--- testData/fixtures/pitfalls/preg-quote.php
+++ testData/fixtures/pitfalls/preg-quote.php
@@ -1,9 +1,11 @@
 <?php
 
-    echo <error descr="Please provide regex delimiter as the second argument for proper escaping.">preg_quote</error> ('');
-    echo '#'.<error descr="Please provide regex delimiter as the second argument for proper escaping.">preg_quote</error> ('');
-    echo '/'.<error descr="Please provide regex delimiter as the second argument for proper escaping.">preg_quote</error> ('').'/';
-    echo "/".<error descr="Please provide regex delimiter as the second argument for proper escaping.">preg_quote</error> ('')."/";
+    echo <error descr="Please provide regex delimiter as the second argument for proper escaping.">preg_quote('')</error>;
+    echo '#'.<error descr="Please provide regex delimiter as the second argument for proper escaping.">preg_quote('')</error>;
+    echo '/'.<error descr="Please provide regex delimiter as the second argument for proper escaping.">preg_quote('')</error>.'/';
+    echo "/".<error descr="Please provide regex delimiter as the second argument for proper escaping.">preg_quote('')</error>."/";
+
+    echo preg_quote('', <error descr="The separator value is platform-dependent, consider using '/' instead.">DIRECTORY_SEPARATOR</error>);
 
     /* false -positives */
     echo preg_quote('', '');

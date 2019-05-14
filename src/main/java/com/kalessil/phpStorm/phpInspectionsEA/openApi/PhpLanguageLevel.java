cae2455942bf654184e716d6effda6dca607d94a
Vladimir Reznichenko <kalessil@gmail.com>
2019-05-14 10:53:17 +0200

    Enabled forcing language level

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/openApi/PhpLanguageLevel.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/openApi/PhpLanguageLevel.java
@@ -1,6 +1,9 @@
 package com.kalessil.phpStorm.phpInspectionsEA.openApi;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
 
 public enum PhpLanguageLevel {
     PHP530("5.3.0"),
@@ -16,6 +19,9 @@ public enum PhpLanguageLevel {
     @NotNull
     private final String version;
 
+    @Nullable
+    static private PhpLanguageLevel current;
+
     PhpLanguageLevel(@NotNull String version) {
         this.version = version;
     }
@@ -25,12 +31,17 @@ public enum PhpLanguageLevel {
         return this.version;
     }
 
+    static public void force(@Nullable PhpLanguageLevel level) {
+        current = level;
+    }
+
     static public PhpLanguageLevel fromString(@NotNull String version) {
-        for (final PhpLanguageLevel level : values()) {
-            if (level.getVersion().equals(version)) {
-                return level;
-            }
+        if (current == null) {
+            return Arrays.stream(values())
+                    .filter(level -> level.getVersion().equals(version))
+                    .findFirst()
+                    .orElse(PHP740);
         }
-        return PHP740;
+        return current;
     }
 }

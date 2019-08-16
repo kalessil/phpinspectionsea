4b219a7e2d0b273951d7ce97aa3af382d8b0da1f
Vladimir Reznichenko <kalessil@gmail.com>
2019-08-16 11:09:52 +0200

    [#1352] ArgumentEqualsDefaultValueInspector: resolved a false-positive (array_keys)

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeStyle/ArgumentEqualsDefaultValueInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/codeStyle/ArgumentEqualsDefaultValueInspector.java
@@ -45,6 +45,7 @@ public class ArgumentEqualsDefaultValueInspector extends BasePhpInspection {
         /* in exceptions die to conflict with strict types inspection, which requires argument specification */
         specialFunctions.add("array_search");
         specialFunctions.add("in_array");
+        specialFunctions.add("array_keys");
         specialFunctions.add("json_decode");
         specialFunctions.add("is_subclass_of");
         specialFunctions.add("is_a");

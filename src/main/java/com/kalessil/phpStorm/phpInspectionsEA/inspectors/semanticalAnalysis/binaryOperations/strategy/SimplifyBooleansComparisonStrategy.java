6bafb00e00ae908ef4c05be6dc108327a862672a
Vladimir Reznichenko <kalessil@gmail.com>
2019-08-21 14:40:08 +0200

    SuspiciousBinaryOperationInspector: WIP, simplification strategy

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/binaryOperations/strategy/SimplifyBooleansComparisonStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/binaryOperations/strategy/SimplifyBooleansComparisonStrategy.java
@@ -3,9 +3,15 @@ package com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.bin
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.util.Pair;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.BinaryExpression;
+import com.jetbrains.php.lang.psi.elements.PhpEmpty;
+import com.jetbrains.php.lang.psi.elements.PhpIsset;
+import com.jetbrains.php.lang.psi.elements.UnaryExpression;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.PhpLanguageUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,11 +46,9 @@ final public class SimplifyBooleansComparisonStrategy {
                         final Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> rightParts = extract((BinaryExpression) right);
                         if (rightParts != null) {
                             /* ensure same amount of arguments with and without inversion */
-                            final int checkSum = Stream.of(leftParts.first.second, leftParts.second.second, rightParts.first.second, rightParts.second.second)
-                                    .map(isInverted -> isInverted ? -1 : 1)
-                                    .reduce(0, Integer::sum);
+                            final int checkSum = Stream.of(leftParts.first.second, leftParts.second.second, rightParts.first.second, rightParts.second.second).mapToInt(isInverted -> isInverted ? -1 : 1).sum();
                             if (checkSum == 0) {
-
+                                holder.registerProblem(expression, "Check it!message");
                             }
                         }
                     }
@@ -80,28 +84,71 @@ final public class SimplifyBooleansComparisonStrategy {
 
     @Nullable
     private static PsiElement extractFalsyValue(@Nullable PsiElement expression) {
-        /* NOTE: ExpressionSemanticUtil.isUsedAsLogicalOperand is not applicable due to very specific context
-            - ! ..., but not !empty(), !isset()
-            - ... === false
-            - ... == false
-            - ... !== true
-            - ... != true
-            - yoda notation
-        */
+        /* NOTE: ExpressionSemanticUtil.isUsedAsLogicalOperand is not applicable due to very specific context */
+        expression = ExpressionSemanticUtil.getExpressionTroughParenthesis(expression);
+        if (expression != null) {
+            if (expression instanceof BinaryExpression) {
+                final BinaryExpression binary = (BinaryExpression) expression;
+                final IElementType operation  = binary.getOperationType();
+                if (operation == PhpTokenTypes.opIDENTICAL || operation == PhpTokenTypes.opEQUAL) {
+                    final PsiElement left  = ExpressionSemanticUtil.getExpressionTroughParenthesis(binary.getLeftOperand());
+                    final PsiElement right = ExpressionSemanticUtil.getExpressionTroughParenthesis(binary.getRightOperand());
+                    if (left != null && right != null && Stream.of(left, right).anyMatch(PhpLanguageUtil::isFalse)) {
+                        return PhpLanguageUtil.isFalse(right) ? left : right;
+                    }
+                } else if (operation == PhpTokenTypes.opNOT_IDENTICAL || operation == PhpTokenTypes.opNOT_EQUAL) {
+                    final PsiElement left  = ExpressionSemanticUtil.getExpressionTroughParenthesis(binary.getLeftOperand());
+                    final PsiElement right = ExpressionSemanticUtil.getExpressionTroughParenthesis(binary.getRightOperand());
+                    if (left != null && right != null && Stream.of(left, right).anyMatch(PhpLanguageUtil::isTrue)) {
+                        return PhpLanguageUtil.isTrue(right) ? left : right;
+                    }
+                }
+            } else if (expression instanceof UnaryExpression) {
+                final UnaryExpression unary = (UnaryExpression) expression;
+                if (OpenapiTypesUtil.is(unary.getOperation(), PhpTokenTypes.opNOT)) {
+                    final PsiElement argument = ExpressionSemanticUtil.getExpressionTroughParenthesis(unary.getValue());
+                    if (!(argument instanceof PhpEmpty) && !(argument instanceof PhpIsset)) {
+                        return argument;
+                    }
+                }
+            }
+        }
         return null;
     }
 
     @Nullable
     private static PsiElement extractTruthyValue(@Nullable PsiElement expression) {
-        /* NOTE: ExpressionSemanticUtil.isUsedAsLogicalOperand is not applicable due to very specific context
-            - ..., but not isset(), empty()
-            - ... === true
-            - ... == true
-            - ... !== false
-            - ... != false
-            - (cast) ...
-            - yoda notation
-        */
+        /* NOTE: ExpressionSemanticUtil.isUsedAsLogicalOperand is not applicable due to very specific context */
+        expression = ExpressionSemanticUtil.getExpressionTroughParenthesis(expression);
+        if (expression != null) {
+            if (expression instanceof BinaryExpression) {
+                final BinaryExpression binary = (BinaryExpression) expression;
+                final IElementType operation  = binary.getOperationType();
+                if (operation == PhpTokenTypes.opIDENTICAL || operation == PhpTokenTypes.opEQUAL) {
+                    final PsiElement left  = ExpressionSemanticUtil.getExpressionTroughParenthesis(binary.getLeftOperand());
+                    final PsiElement right = ExpressionSemanticUtil.getExpressionTroughParenthesis(binary.getRightOperand());
+                    if (left != null && right != null && Stream.of(left, right).anyMatch(PhpLanguageUtil::isTrue)) {
+                        return PhpLanguageUtil.isTrue(right) ? left : right;
+                    }
+                } else if (operation == PhpTokenTypes.opNOT_IDENTICAL || operation == PhpTokenTypes.opNOT_EQUAL) {
+                    final PsiElement left  = ExpressionSemanticUtil.getExpressionTroughParenthesis(binary.getLeftOperand());
+                    final PsiElement right = ExpressionSemanticUtil.getExpressionTroughParenthesis(binary.getRightOperand());
+                    if (left != null && right != null && Stream.of(left, right).anyMatch(PhpLanguageUtil::isFalse)) {
+                        return PhpLanguageUtil.isFalse(right) ? left : right;
+                    }
+                }
+            } else if (expression instanceof UnaryExpression) {
+                final UnaryExpression unary = (UnaryExpression) expression;
+                final PsiElement operation  = unary.getOperation();
+                if (operation != null && PhpTokenTypes.CAST_OPERATORS.contains(operation.getNode().getElementType())) {
+                    return ExpressionSemanticUtil.getExpressionTroughParenthesis(unary.getValue());
+                }
+            } else {
+                if (!(expression instanceof PhpEmpty) && !(expression instanceof PhpIsset)) {
+                    return expression;
+                }
+            }
+        }
         return null;
     }
 }

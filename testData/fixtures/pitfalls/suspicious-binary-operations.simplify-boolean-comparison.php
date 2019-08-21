3b046cf4c17bce32927ff8ea811e0ea41675d544
Vladimir Reznichenko <kalessil@gmail.com>
2019-08-21 17:02:02 +0200

    SuspiciousBinaryOperationInspector: WIP, simplification strategy

--- testData/fixtures/pitfalls/suspicious-binary-operations.simplify-boolean-comparison.php
+++ testData/fixtures/pitfalls/suspicious-binary-operations.simplify-boolean-comparison.php
@@ -6,4 +6,8 @@ function cases_holder() {
 
     if (($x === true && $y) || ($x === false && !$y)) {}
     if (($x !== false && $y) || ($x !== true && !$y)) {}
+
+    /* false-positives: empty()-handling */
+    if ((!empty($x) && $y) || ($x && !$y)) {}
+    if (($x && $y) || (empty($x) && !$y)) {}
 }

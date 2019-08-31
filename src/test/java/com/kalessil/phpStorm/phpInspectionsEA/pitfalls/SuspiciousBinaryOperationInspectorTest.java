a80f6830a4f2f5b5e8751e8f1d7e407c153d668c
Vladimir Reznichenko <kalessil@gmail.com>
2019-08-31 23:25:17 +0200

    SuspiciousBinaryOperationInspector: re-grouped test fixtures

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/pitfalls/SuspiciousBinaryOperationInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/pitfalls/SuspiciousBinaryOperationInspectorTest.java
@@ -8,67 +8,67 @@ import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.bina
 final public class SuspiciousBinaryOperationInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.php");
         myFixture.testHighlighting(true, false, true);
 
         myFixture.getAllQuickFixes().forEach(fix -> myFixture.launchAction(fix));
         myFixture.setTestDataPath(".");
-        myFixture.checkResultByFile("testData/fixtures/pitfalls/suspicious-binary-operations.fixed.php");
+        myFixture.checkResultByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.fixed.php");
     }
     public void testIfFindsPossiblyTyposPatterns() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.possibly-typos.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.possibly-typos.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsNullCoalescingPatterns() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.null-coalescing.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.null-coalescing.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsMistypedOperations() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.mistyped.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.mistyped.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsCountChecks() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.count.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.count.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsIsNumericChecks() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.is_numeric.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.is_numeric.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsVersionChecks() {
         PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP560);
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.version.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.version.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsInvalidArrayOperationPatterns() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.array-concatenation.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.array-concatenation.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsComparingNonIntersectingFunctionResultsPatterns() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.non-intersecting-types.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.non-intersecting-types.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsMultipleFalsyValuesCheckPatterns() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.multiple-falsy-values.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.multiple-falsy-values.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsMultipleValuesCheckPatterns() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.multiple-values.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.multiple-values.php");
         myFixture.testHighlighting(true, false, true);
     }
     public void testIfFindsSimplifyBooleansComparisonPatterns() {
         myFixture.enableInspections(new SuspiciousBinaryOperationInspector());
-        myFixture.configureByFile("testData/fixtures/pitfalls/suspicious-binary-operations.simplify-boolean-comparison.php");
+        myFixture.configureByFile("testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.simplify-boolean-comparison.php");
         myFixture.testHighlighting(true, false, true);
     }
 }

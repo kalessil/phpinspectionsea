52d78c6192d7dac3de6dbe59542b28657054f5f4
Vladimir Reznichenko <kalessil@gmail.com>
2019-09-02 13:23:10 +0200

    SuspiciousBinaryOperationInspector: fixed a broken build

--- testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.null-coalescing.php
+++ testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.null-coalescing.php
@@ -1,15 +1,16 @@
 <?php
 
 function returns_string(): string { return ''; }
+function returns_object(): ?object { return null; }
 
 $y = [
     <error descr="The operation results to '(int)$x', the right operand can be omitted.">(int)$x</error> ?? '...',
     (<error descr="The operation results to '(string)$x', the right operand can be omitted.">(string)$x</error>) ?? '...',
     (<error descr="The operation results to '!$x', the right operand can be omitted.">!$x</error>) ?? '...',
-    <error descr="Due to 'call($x)' used as left operand, using '?:' instead of '??' would make more sense.">call($x)</error> ?? '...',
+    <error descr="Due to 'returns_object($x)' used as left operand, using '?:' instead of '??' would make more sense.">returns_object($x)</error> ?? '...',
 
     $x ?? <error descr="The operation results to '(int)$x', the right operand can be omitted.">(int)$x</error> ?? '...',
-    $x ?? <error descr="Due to 'call($x)' used as left operand, using '?:' instead of '??' would make more sense.">call($x)</error> ?? '...',
+    $x ?? <error descr="Due to 'returns_object($x)' used as left operand, using '?:' instead of '??' would make more sense.">returns_object($x)</error> ?? '...',
 
     returns_string() ?? ''
 ];

db1d65e1bc0899f5b081e5260aaf6bc5b24fb817
Vladimir Reznichenko <kalessil@gmail.com>
2019-09-02 10:18:33 +0200

    [#1400] SubStrUsedAsStrPosInspector: code grooming

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/strings/SubStrUsedAsStrPosInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/strings/SubStrUsedAsStrPosInspector.java
@@ -104,74 +104,69 @@ public class SubStrUsedAsStrPosInspector extends BasePhpInspection {
             @Override
             public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
                 final String functionName = reference.getName();
-                if (functionName == null || !substringFunctions.contains(functionName)) {
-                    return;
-                }
-                final PsiElement[] arguments = reference.getParameters();
-                if (arguments.length != 3 && arguments.length != 4) {
-                    return;
-                }
+                if (functionName != null && substringFunctions.contains(functionName)) {
+                    final PsiElement[] arguments = reference.getParameters();
+                    if (arguments.length == 3 || arguments.length == 4) {
+                        /* checking 2nd and 3rd arguments is not needed/simplified:
+                         *   - 2nd re-used as it is (should be a positive number!)
+                         *   - 3rd is not important, as we'll rely on parent comparison operand instead
+                         */
+                        final String index = arguments[1].getText();
+                        if (OpenapiTypesUtil.isNumber(arguments[1]) && index.equals("0")) {
+                            /* prepare variables, so we could properly process polymorphic pattern */
+                            PsiElement highLevelCall    = reference;
+                            PsiElement parentExpression = reference.getParent();
+                            if (parentExpression instanceof ParameterList) {
+                                parentExpression = parentExpression.getParent();
+                            }
 
-                /* checking 2nd and 3rd arguments is not needed/simplified:
-                 *   - 2nd re-used as it is (should be a positive number!)
-                 *   - 3rd is not important, as we'll rely on parent comparison operand instead
-                 */
-                final String index = arguments[1].getText();
-                if (!OpenapiTypesUtil.isNumber(arguments[1]) || !index.equals("0")) {
-                    return;
-                }
+                            /* if the call wrapped with case manipulation, propose to use stripos */
+                            boolean caseManipulated = false;
+                            if (OpenapiTypesUtil.isFunctionReference(parentExpression)) {
+                                final FunctionReference parentCall = (FunctionReference) parentExpression;
+                                final PsiElement[] parentArguments = parentCall.getParameters();
+                                final String parentName            = parentCall.getName();
+                                if (parentName != null && parentArguments.length == 1 && outerFunctions.contains(parentName)) {
+                                    caseManipulated  = true;
+                                    highLevelCall    = parentExpression;
+                                    parentExpression = parentExpression.getParent();
+                                }
+                            }
 
-                /* prepare variables, so we could properly process polymorphic pattern */
-                PsiElement highLevelCall    = reference;
-                PsiElement parentExpression = reference.getParent();
-                if (parentExpression instanceof ParameterList) {
-                    parentExpression = parentExpression.getParent();
-                }
+                            /* check parent expression, to ensure pattern matched */
+                            if (parentExpression instanceof BinaryExpression) {
+                                final BinaryExpression parent = (BinaryExpression) parentExpression;
+                                if (OpenapiTypesUtil.tsCOMPARE_EQUALITY_OPS.contains(parent.getOperationType())) {
+                                    final PsiElement secondOperand = OpenapiElementsUtil.getSecondOperand(parent, highLevelCall);
+                                    final PsiElement operationNode = parent.getOperation();
+                                    if (secondOperand != null && operationNode != null) {
+                                        final String operator      = operationNode.getText();
+                                        final boolean isMbFunction = functionName.equals("mb_substr");
+                                        final boolean hasEncoding  = isMbFunction && arguments.length == 4;
 
-                /* if the call wrapped with case manipulation, propose to use stripos */
-                boolean caseManipulated = false;
-                if (OpenapiTypesUtil.isFunctionReference(parentExpression)) {
-                    final FunctionReference parentCall = (FunctionReference) parentExpression;
-                    final PsiElement[] parentArguments = parentCall.getParameters();
-                    final String parentName            = parentCall.getName();
-                    if (parentName != null && parentArguments.length == 1 && outerFunctions.contains(parentName)) {
-                        caseManipulated  = true;
-                        highLevelCall    = parentExpression;
-                        parentExpression = parentExpression.getParent();
-                    }
-                }
-
-                /* check parent expression, to ensure pattern matched */
-                if (parentExpression instanceof BinaryExpression) {
-                    final BinaryExpression parent = (BinaryExpression) parentExpression;
-                    if (OpenapiTypesUtil.tsCOMPARE_EQUALITY_OPS.contains(parent.getOperationType())) {
-                        final PsiElement secondOperand = OpenapiElementsUtil.getSecondOperand(parent, highLevelCall);
-                        final PsiElement operationNode = parent.getOperation();
-                        if (secondOperand != null && operationNode != null) {
-                            final String operator      = operationNode.getText();
-                            final boolean isMbFunction = functionName.equals("mb_substr");
-                            final boolean hasEncoding  = isMbFunction && arguments.length == 4;
-
-                            final String call          = String.format(
-                                    "%s%s(%s, %s%s)",
-                                    reference.getImmediateNamespaceName(),
-                                    (isMbFunction ? "mb_" : "") + (caseManipulated ? "stripos" : "strpos"),
-                                    arguments[0].getText(),
-                                    secondOperand.getText(),
-                                    hasEncoding ? (", " + arguments[3].getText()) : ""
-                            );
-                            final boolean isRegular    = ComparisonStyle.isRegular();
-                            final String replacement   = String.format(
-                                    "%s %s %s",
-                                    isRegular ? call : index,
-                                    operator.length() == 2 ? (operator + '=') : operator,
-                                    isRegular ? index : call
-                            );
-                            holder.registerProblem(
-                                    parentExpression,
-                                    String.format(messagePattern, replacement),
-                                    new UseStringSearchFix(replacement)
-                            );
+                                        final String call          = String.format(
+                                                "%s%s(%s, %s%s)",
+                                                reference.getImmediateNamespaceName(),
+                                                (isMbFunction ? "mb_" : "") + (caseManipulated ? "stripos" : "strpos"),
+                                                arguments[0].getText(),
+                                                secondOperand.getText(),
+                                                hasEncoding ? (", " + arguments[3].getText()) : ""
+                                        );
+                                        final boolean isRegular    = ComparisonStyle.isRegular();
+                                        final String replacement   = String.format(
+                                                "%s %s %s",
+                                                isRegular ? call : index,
+                                                operator.length() == 2 ? (operator + '=') : operator,
+                                                isRegular ? index : call
+                                        );
+                                        holder.registerProblem(
+                                                parentExpression,
+                                                String.format(messagePattern, replacement),
+                                                new UseStringSearchFix(replacement)
+                                        );
+                                    }
+                                }
+                            }
                         }
                     }
                 }

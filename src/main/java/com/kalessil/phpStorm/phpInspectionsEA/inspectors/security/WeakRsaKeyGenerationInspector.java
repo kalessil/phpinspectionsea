a73756767896e1381e195d2170baaf26593b99f9
Vladimir Reznichenko <kalessil@gmail.com>
2019-09-06 22:06:32 +0200

    WeakRsaKeyGenerationInspector: prototyping

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/security/WeakRsaKeyGenerationInspector.java
@@ -0,0 +1,94 @@
+package com.kalessil.phpStorm.phpInspectionsEA.inspectors.security;
+
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.psi.elements.*;
+import com.kalessil.phpStorm.phpInspectionsEA.openApi.FeaturedPhpElementVisitor;
+import com.kalessil.phpStorm.phpInspectionsEA.settings.StrictnessCategory;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiTypesUtil;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.PossibleValuesDiscoveryUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+public class WeakRsaKeyGenerationInspector extends LocalInspectionTool {
+    private static final String messageAllDefaults          = "...";
+    private static final String messageLengthIsDefault      = "...";
+    private static final String messageLengthIsNoSufficient = "...";
+
+    private static final Map<String, Integer> targetFunctions = new HashMap<>();
+    static {
+        targetFunctions.put("openssl_pkey_new", 1);
+        targetFunctions.put("openssl_csr_new",  3);
+    }
+
+    @Override
+    @NotNull
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+        return new FeaturedPhpElementVisitor() {
+            @Override
+            public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
+                if (this.shouldSkipAnalysis(reference, StrictnessCategory.STRICTNESS_CATEGORY_SECURITY)) { return; }
+
+                final String functionName = reference.getName();
+                if (functionName != null && targetFunctions.containsKey(functionName)) {
+                    final PsiElement[] arguments    = reference.getParameters();
+                    final int configurationPosition = targetFunctions.get(functionName);
+                    if (arguments.length >= configurationPosition) {
+                        for (final PsiElement variant : PossibleValuesDiscoveryUtil.discover(arguments[configurationPosition - 1])) {
+                            if (variant instanceof ArrayCreationExpression) {
+                                /* extract settings */
+                                PhpPsiElement keyLength = null;
+                                PhpPsiElement keyType   = null;
+                                for (final ArrayHashElement pair : ((ArrayCreationExpression) variant).getHashElements()) {
+                                    final PsiElement setting = pair.getKey();
+                                    if (setting instanceof StringLiteralExpression) {
+                                        final String settingName = ((StringLiteralExpression) setting).getContents();
+                                        if (settingName.equals("private_key_bits")) {
+                                            keyLength = pair.getValue();
+                                        } else if (settingName.equals("private_key_type")) {
+                                            keyType = pair.getValue();
+                                        }
+                                    }
+                                }
+                                /* understand settings */
+                                final boolean isTargetType = keyType == null || "OPENSSL_KEYTYPE_RSA".equals(keyType.getName());
+                                if (isTargetType) {
+                                    if (keyLength == null) {
+                                        holder.registerProblem(reference, messageLengthIsDefault);
+                                    } else {
+                                        if (OpenapiTypesUtil.isNumber(keyLength)) {
+                                            boolean isTarget;
+                                            try {
+                                                isTarget = Integer.parseInt(keyLength.getText()) <= 2048;
+                                            } catch (final NumberFormatException wrongFormat) {
+                                                isTarget = false;
+                                            }
+                                            if (isTarget) {
+                                                holder.registerProblem(reference, messageLengthIsNoSufficient);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        holder.registerProblem(reference, messageAllDefaults);
+                    }
+                }
+            }
+        };
+    }
+}

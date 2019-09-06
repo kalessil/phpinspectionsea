23804772f7463bf3570887e230eee5ccce801be5
Vladimir Reznichenko <kalessil@gmail.com>
2019-09-06 08:01:54 +0200

    GetTypeMissUseInspector: code grooming

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/GetTypeMissUseInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/GetTypeMissUseInspector.java
@@ -1,21 +1,15 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.SmartPointerManager;
-import com.intellij.psi.SmartPsiElementPointer;
 import com.intellij.psi.tree.IElementType;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
-import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
 import com.jetbrains.php.lang.psi.elements.BinaryExpression;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
-import com.jetbrains.php.lang.psi.elements.UnaryExpression;
+import com.kalessil.phpStorm.phpInspectionsEA.fixers.UseSuggestedReplacementFixer;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
@@ -36,8 +30,8 @@ import java.util.Map;
  */
 
 public class GetTypeMissUseInspector extends BasePhpInspection {
-    private static final String messagePattern     = "'%i%%f%(%p%)' construction is more compact and easier to read.";
-    private static final String messageInvalidType = "'%t%' is not a value returned by 'gettype(...)'.";
+    private static final String messageUseFunctionPattern = "'%s' construction is more compact and easier to read.";
+    private static final String messageInvalidPattern     = "'%s' is not a value returned by 'gettype(...)'.";
 
     @NotNull
     @Override
@@ -61,7 +55,7 @@ public class GetTypeMissUseInspector extends BasePhpInspection {
         mapping.put("object",   "is_object");
         mapping.put("resource", "is_resource");
         mapping.put("NULL",     "is_null");
-        // "unknown type" will not be processed: no is_* analog
+        // "unknown type" will not be processed: there is no is_* analog in API
     }
 
     @Override
@@ -71,70 +65,39 @@ public class GetTypeMissUseInspector extends BasePhpInspection {
             @Override
             public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
                 final String functionName = reference.getName();
-                if (functionName == null || !functionName.equals("gettype")) {
-                    return;
-                }
-                final PsiElement[] arguments = reference.getParameters();
-                if (arguments.length != 1) {
-                    return;
-                }
-
-                /* check context: expected to be binary expression */
-                final PsiElement parent = reference.getParent();
-                if (!(parent instanceof BinaryExpression)) {
-                    return;
-                }
-
-                /* ensure valid operations being analyzed */
-                final BinaryExpression expression = (BinaryExpression) parent;
-                final IElementType operator       = expression.getOperationType();
-                if (!OpenapiTypesUtil.tsCOMPARE_EQUALITY_OPS.contains(operator)) {
-                    return;
-                }
-
-                /* ensure the 2nd operator is string literal and we are taking care about it */
-                final PsiElement type                     = OpenapiElementsUtil.getSecondOperand(expression, reference);
-                final StringLiteralExpression typeLiteral = ExpressionSemanticUtil.resolveAsStringLiteral(type);
-                if (typeLiteral == null) {
-                    return;
-                }
-                final String typeString = typeLiteral.getContents();
-                if (!mapping.containsKey(typeString)) {
-                    /* edge case: compared string is wrong xD - bug */
-                    if (!typeString.equals("unknown type") && !typeString.equals("resource (closed)")) {
-                        final String message = messageInvalidType.replace("%t%", typeString);
-                        holder.registerProblem(type, message, ProblemHighlightType.GENERIC_ERROR);
+                if (functionName != null && functionName.equals("gettype")) {
+                    final PsiElement[] arguments = reference.getParameters();
+                    if (arguments.length == 1) {
+                        final PsiElement parent = reference.getParent();
+                        if (parent instanceof BinaryExpression) {
+                            final BinaryExpression expression = (BinaryExpression) parent;
+                            final IElementType operator       = expression.getOperationType();
+                            if (OpenapiTypesUtil.tsCOMPARE_EQUALITY_OPS.contains(operator)) {
+                                final PsiElement candidate          = OpenapiElementsUtil.getSecondOperand(expression, reference);
+                                final StringLiteralExpression value = ExpressionSemanticUtil.resolveAsStringLiteral(candidate);
+                                if (value != null) {
+                                    final String type = value.getContents();
+                                    if (!mapping.containsKey(type)) {
+                                        /* edge case: compared string is wrong xD - bug */
+                                        if (!type.equals("unknown type") && !type.equals("resource (closed)")) {
+                                            holder.registerProblem(value, String.format(messageInvalidPattern, type), ProblemHighlightType.GENERIC_ERROR);
+                                        }
+                                    } else {
+                                        final boolean isInverted = operator == PhpTokenTypes.opNOT_EQUAL || operator == PhpTokenTypes.opNOT_IDENTICAL;
+                                        final String replacement = String.format("%s%s(%s)", isInverted ? "!" : "", mapping.get(type), arguments[0].getText());
+                                        holder.registerProblem(parent, String.format(messageUseFunctionPattern, replacement), new UseSuggestedFunctionFix(replacement));
+                                    }
+                                }
+                            }
+                        }
                     }
-
-                    return;
                 }
-
-                /* now we can report */
-                final String suggestedName = mapping.get(typeString);
-                final boolean isInverted   = PhpTokenTypes.opNOT_EQUAL == operator || PhpTokenTypes.opNOT_IDENTICAL == operator;
-                final String message = messagePattern
-                        .replace("%p%", arguments[0].getText())
-                        .replace("%f%", suggestedName)
-                        .replace("%i%", isInverted ? "!" : "");
-                holder.registerProblem(parent, message, new TheLocalFix(holder.getProject(), suggestedName, arguments[0], isInverted));
             }
         };
     }
 
-    private static final class TheLocalFix implements LocalQuickFix {
+    private static final class UseSuggestedFunctionFix extends UseSuggestedReplacementFixer {
         private static final String title = "Use suggested is_*(...) function instead";
-
-        final private String suggestedName;
-        final private boolean isInverted;
-        final private SmartPsiElementPointer<PsiElement> param;
-
-        TheLocalFix(@NotNull Project project, @NotNull String suggestedName, @NotNull PsiElement param, boolean isInverted) {
-            super();
-
-            this.suggestedName = suggestedName;
-            this.param         = SmartPointerManager.getInstance(project).createSmartPsiElementPointer(param);
-            this.isInverted    = isInverted;
-        }
 
         @NotNull
         @Override
@@ -142,32 +105,8 @@ public class GetTypeMissUseInspector extends BasePhpInspection {
             return title;
         }
 
-        @NotNull
-        @Override
-        public String getFamilyName() {
-            return title;
-        }
-
-        @Override
-        public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-            final PsiElement expression = descriptor.getPsiElement();
-            final PsiElement param      = this.param.getElement();
-            if (param != null && expression instanceof BinaryExpression && !project.isDisposed()) {
-                final String pattern =
-                        (isInverted ? "!" : "") +
-                        (suggestedName + "(%p%)".replace("%p%", param.getText()));
-
-                final PsiElement replacement;
-                if (isInverted) {
-                    replacement = PhpPsiElementFactory.createFromText(project, UnaryExpression.class, pattern);
-                } else {
-                    replacement = PhpPsiElementFactory.createFromText(project, FunctionReference.class, pattern);
-                }
-
-                if (replacement != null) {
-                    expression.replace(replacement);
-                }
-            }
+        UseSuggestedFunctionFix(@NotNull String expression) {
+            super(expression);
         }
     }
 }

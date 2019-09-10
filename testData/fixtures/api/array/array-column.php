2caec49881635766acc601416dd33d32bea722e0
Vladimir Reznichenko <kalessil@gmail.com>
2019-09-10 08:37:59 +0200

    ArrayColumnCanBeUsedInspector: new test cases

--- testData/fixtures/api/array/array-column.php
+++ testData/fixtures/api/array/array-column.php
@@ -5,5 +5,9 @@ function cases_holder($index)
     return [
         <warning descr="'array_column([], 'id')' would fit more here (it also faster, but loses original keys).">array_map(function ($array) { return $array['id']; }, [])</warning>,
         <warning descr="'array_column([], $index)' would fit more here (it also faster, but loses original keys).">array_map(function ($array) use ($index) { return $array[$index]; }, [])</warning>,
+
+        <warning descr="'array_column([], 'property')' would fit more here (it also faster, but loses original keys).">array_map(function ($object) { return $object->property; }, [])</warning>,
+        <warning descr="'array_column([], $property)' would fit more here (it also faster, but loses original keys).">array_map(function ($object) use ($index) { return $object->$property; }, [])</warning>,
+        <warning descr="'array_column([], $property)' would fit more here (it also faster, but loses original keys).">array_map(function ($object) use ($index) { return $object->{$property}; }, [])</warning>,
     ];
 }

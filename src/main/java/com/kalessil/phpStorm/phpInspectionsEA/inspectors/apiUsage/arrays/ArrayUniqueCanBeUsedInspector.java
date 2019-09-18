588e075f7b2818ae67aaf04568cf97be7b1da77a
Vladimir Reznichenko <kalessil@gmail.com>
2019-09-18 16:12:56 +0200

    Minor code grooming

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/ArrayUniqueCanBeUsedInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/arrays/ArrayUniqueCanBeUsedInspector.java
@@ -41,7 +41,7 @@ public class ArrayUniqueCanBeUsedInspector extends BasePhpInspection {
         return new BasePhpElementVisitor() {
             @Override
             public void visitPhpFunctionCall(@NotNull FunctionReference reference) {
-                if (PhpLanguageLevel.get(holder.getProject()).atLeast(PhpLanguageLevel.PHP710)) {
+                if (PhpLanguageLevel.get(holder.getProject()).atLeast(PhpLanguageLevel.PHP720)) {
                     final String functionName = reference.getName();
                     if (functionName != null && functionName.equals("array_count_values")) {
                         final PsiElement[] arguments = reference.getParameters();
--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/CompactCanBeUsedInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/CompactCanBeUsedInspector.java
@@ -15,6 +15,15 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 public class CompactCanBeUsedInspector extends BasePhpInspection {
     private static final String messagePattern = "'%s' can be used instead (improves maintainability).";
 
@@ -61,10 +70,9 @@ public class CompactCanBeUsedInspector extends BasePhpInspection {
 
                 if (variables.size() > 1) {
                     final String replacement = String.format("compact(%s)", String.join(", ", variables));
-                    final String message     = String.format(messagePattern, replacement);
-                    holder.registerProblem(expression.getFirstChild(), message, new UseCompactFix(replacement));
-                    variables.clear();
+                    holder.registerProblem(expression.getFirstChild(), String.format(messagePattern, replacement), new UseCompactFix(replacement));
                 }
+                variables.clear();
             }
         };
     }

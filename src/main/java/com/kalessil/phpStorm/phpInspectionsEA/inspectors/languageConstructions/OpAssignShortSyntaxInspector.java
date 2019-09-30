476d91b755b09f42db86e4b4cda3b155c06094af
Vladimir Reznichenko <kalessil@gmail.com>
2019-09-30 09:37:39 +0200

    Code grooming: optimized imports

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/OpAssignShortSyntaxInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/OpAssignShortSyntaxInspector.java
@@ -4,7 +4,6 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.tree.IElementType;
-import com.intellij.spi.parsing.SPIElementTypes;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.AssignmentExpression;
 import com.jetbrains.php.lang.psi.elements.BinaryExpression;

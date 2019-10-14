d0b86eca95aeaeb2134febbdabae25f4a8879e6f
Vladimir Reznichenko <kalessil@gmail.com>
2019-10-14 21:21:41 +0200

    [#1425] WIP, fixed broken tests

--- testData/fixtures/api/array-unique.php
+++ testData/fixtures/api/array-unique.php
@@ -1,4 +1,4 @@
 <?php
 
-    $x = <weak_warning descr="'array_values(array_unique([]))' would be more readable here (array_unique(...) was optimized in PHP 7.2-beta3+).">array_keys(array_count_values([]))</weak_warning>;
-    $y = <weak_warning descr="'count(array_unique([]))' would be more readable here (array_unique(...) was optimized in PHP 7.2-beta3+).">count(array_count_values([]))</weak_warning>;
+    $x = <weak_warning descr="[EA] 'array_values(array_unique([]))' would be more readable here (array_unique(...) was optimized in PHP 7.2-beta3+).">array_keys(array_count_values([]))</weak_warning>;
+    $y = <weak_warning descr="[EA] 'count(array_unique([]))' would be more readable here (array_unique(...) was optimized in PHP 7.2-beta3+).">count(array_count_values([]))</weak_warning>;

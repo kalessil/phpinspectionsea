118e9c7c80cfc952b364249579a0e1d048d904da
Vladimir Reznichenko <kalessil@gmail.com>
2019-10-21 11:52:30 +0200

    [#1438] CallableParameterUseCaseInTypeContextInspection: proper language level in tests

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/types/CallableParameterUseCaseInTypeContextInspectionTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/types/CallableParameterUseCaseInTypeContextInspectionTest.java
@@ -1,10 +1,13 @@
 package com.kalessil.phpStorm.phpInspectionsEA.types;
 
+import com.jetbrains.php.config.PhpLanguageLevel;
+import com.jetbrains.php.config.PhpProjectConfigurationFacade;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.semanticalAnalysis.CallableParameterUseCaseInTypeContextInspection;
 
 final public class CallableParameterUseCaseInTypeContextInspectionTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
+        PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP700);
         myFixture.enableInspections(new CallableParameterUseCaseInTypeContextInspection());
         myFixture.configureByFile("testData/fixtures/types/parameter-types-checks.php");
         myFixture.testHighlighting(true, false, true);

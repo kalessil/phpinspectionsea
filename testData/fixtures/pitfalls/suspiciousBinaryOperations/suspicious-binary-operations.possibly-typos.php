3ae8742631590ddb87c9ebe10b1b95ac9d307566
Vladimir Reznichenko <kalessil@gmail.com>
2019-10-29 09:53:20 +0100

    SuspiciousBinaryOperationInspector: fixed failing UTs

--- testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.possibly-typos.php
+++ testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.possibly-typos.php
@@ -2,13 +2,13 @@
 
 
 /* a typo: comparison instead of assignment */
-$a <error descr="[EA] It seems that '=' should be here.">==</error> $b;
+$a <error descr="[EA] It was probably intended to use '=' here.">==</error> $b;
 
 /* a typo: greater or equal instead of has element definition */
-$a = ['x' <error descr="[EA] It seems that '=>' should be here.">>=</error> 'y'];
+$a = ['x' <error descr="[EA] It was probably intended to use '=>' here.">>=</error> 'y'];
 $a = [$x >= 'y']; // <- left operand is not a string, hence not reported
 
 /* a typo: greater instead of method reference */
 function collision() {}
 class Clazz { public function collision() {} }
-$a = (new Clazz())<error descr="[EA] It seems that '->' should be here.">></error>collision();
+$a = (new Clazz())<error descr="[EA] It was probably intended to use '->' here.">></error>collision();

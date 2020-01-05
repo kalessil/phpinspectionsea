e0feef02b800b515660aa7bfe5624a58477597cf
Vladimir Reznichenko <kalessil@gmail.com>
2020-01-05 21:00:56 +0100

    Optimized imports

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/options/OptionsComponent.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/options/OptionsComponent.java
@@ -13,8 +13,6 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;

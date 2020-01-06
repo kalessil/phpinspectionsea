c3299aa15554237b87aaa143e861338c898700f0
Vladimir Reznichenko <kalessil@gmail.com>
2020-01-06 09:37:31 +0100

    [#1458] BacktickOperatorUsageInspector: code review

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/BacktickOperatorUsageInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/BacktickOperatorUsageInspector.java
@@ -4,7 +4,6 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.inspections.PhpInspection;
 import com.jetbrains.php.lang.psi.elements.PhpShellCommandExpression;
-import com.jetbrains.php.util.PhpStringUtil;
 import com.kalessil.phpStorm.phpInspectionsEA.fixers.UseSuggestedReplacementFixer;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.GenericPhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.settings.StrictnessCategory;

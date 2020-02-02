04ee74713bc20cd4a7fc18505d0a2c3c675c80be
Vladimir Reznichenko <kalessil@gmail.com>
2020-02-02 06:58:30 +0100

    Merged master into ultimate

--- testData/fixtures/api/json-encoding-errors-handling.fixed.php
+++ testData/fixtures/api/json-encoding-errors-handling.fixed.php
@@ -1,6 +1,8 @@
 <?php
 
 function cases_holder($encoded, $options) {
+    $local = JSON_THROW_ON_ERROR;
+
     return [
         json_decode($encoded, false, 512, JSON_THROW_ON_ERROR),
         json_decode($encoded, false, 512, JSON_THROW_ON_ERROR),
@@ -12,6 +14,8 @@ function cases_holder($encoded, $options) {
         json_encode($encoded, JSON_THROW_ON_ERROR | $options, $depth),
 
         json_decode($encoded, false, 512, $options | JSON_THROW_ON_ERROR),
+        json_decode($encoded, false, 512, $local),
+
         json_encode($encoded, $options | JSON_THROW_ON_ERROR),
     ];
 }

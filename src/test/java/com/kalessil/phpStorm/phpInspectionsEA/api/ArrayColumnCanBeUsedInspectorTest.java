0fb27fc6db5ad218e32ed5fcc087532d7aca4262
Vladimir Reznichenko <kalessil@gmail.com>
2020-02-23 19:14:03 +0100

    [#1508] ArrayColumnCanBeUsedInspector: bumped language level in tests

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/ArrayColumnCanBeUsedInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/api/ArrayColumnCanBeUsedInspectorTest.java
@@ -1,10 +1,13 @@
 package com.kalessil.phpStorm.phpInspectionsEA.api;
 
+import com.jetbrains.php.config.PhpLanguageLevel;
+import com.jetbrains.php.config.PhpProjectConfigurationFacade;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.apiUsage.arrays.ArrayColumnCanBeUsedInspector;
 
 final public class ArrayColumnCanBeUsedInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
+        PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP700);
         myFixture.enableInspections(new ArrayColumnCanBeUsedInspector());
         myFixture.configureByFile("testData/fixtures/api/array/array-column.php");
         myFixture.testHighlighting(true, false, true);

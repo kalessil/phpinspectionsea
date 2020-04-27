11ddebdda87e1cb1ffedd9c860053704cf9c151a
Vladimir Reznichenko <kalessil@gmail.com>
2020-04-27 16:07:24 +0200

    CI: 2020.1 compatibility fixes

--- testData/fixtures/lang/variable-functions-php54.php
+++ testData/fixtures/lang/variable-functions-php54.php
@@ -13,8 +13,8 @@
     <weak_warning descr="[EA] 'forward_static_call($callable, 1, 2, 3)' would make possible to perform better code analysis here.">forward_static_call_array ($callable, [1, 2, 3])</weak_warning>;
     <weak_warning descr="[EA] 'forward_static_call($callable, 1, 2, 3)' would make possible to perform better code analysis here.">forward_static_call_array ($callable, [0 => 1, 'index' => 2, 3])</weak_warning>;
 
-    <weak_warning descr="[EA] 'Namespace\Clazz::method($a)' would make more sense here (it also faster).">call_user_func('Namespace\\Clazz::method', $a)</weak_warning>;
-    <weak_warning descr="[EA] 'Namespace\Clazz::method($a)' would make more sense here (it also faster).">forward_static_call('Namespace\\Clazz::method', $a)</weak_warning>;
+    <weak_warning descr="[EA] 'Namespazz\Clazz::method($a)' would make more sense here (it also faster).">call_user_func('Namespazz\\Clazz::method', $a)</weak_warning>;
+    <weak_warning descr="[EA] 'Namespazz\Clazz::method($a)' would make more sense here (it also faster).">forward_static_call('Namespazz\\Clazz::method', $a)</weak_warning>;
 
     /* QF correctness cases */
     <weak_warning descr="[EA] '$callable(...$arguments)' would make more sense here (it also faster).">call_user_func($callable, ...$arguments)</weak_warning>;

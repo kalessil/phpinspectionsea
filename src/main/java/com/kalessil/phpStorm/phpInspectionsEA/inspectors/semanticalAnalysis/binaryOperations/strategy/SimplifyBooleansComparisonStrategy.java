b6063af13e363ced00a0edc58926496e7a6f642e
Vladimir Reznichenko <kalessil@gmail.com>
2020-05-19 20:50:55 +0200

    SuspiciousBinaryOperationInspector: new patterns

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/binaryOperations/strategy/SimplifyBooleansComparisonStrategy.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/binaryOperations/strategy/SimplifyBooleansComparisonStrategy.java
@@ -24,7 +24,8 @@ import java.util.stream.Stream;
  */
 
 final public class SimplifyBooleansComparisonStrategy {
-    private static final String messagePattern = "'(%s) || (%s)' is the same as '(bool) %s %s (bool) %s', please review the conditions.";
+    private static final String messagePatternOr  = "'(%s) || (%s)' is the same as '(bool) %s %s (bool) %s', please review the conditions.";
+    private static final String messagePatternAnd = "'(%s) && (%s)' is the same as '(bool) %s !== (bool) %s', please review the conditions.";
 
     public static boolean apply(@NotNull BinaryExpression expression, @NotNull ProblemsHolder holder) {
         boolean result              = false;
@@ -37,20 +38,20 @@ final public class SimplifyBooleansComparisonStrategy {
                 if (fragments.size() > 1) {
                     final Map<BinaryExpression, Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>>> details = new HashMap<>();
                     for (final BinaryExpression fragment : fragments) {
-                        final Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> current = details.computeIfAbsent(fragment, SimplifyBooleansComparisonStrategy::extract);
+                        final Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> current = details.computeIfAbsent(fragment, (f) -> extract(f, PhpTokenTypes.opAND));
                         if (current != null) {
                             boolean reachedStartingPoint = false;
                             for (final BinaryExpression match : fragments) {
                                 reachedStartingPoint = reachedStartingPoint || match == fragment;
                                 if (reachedStartingPoint && match != fragment) {
-                                    final Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> next = details.computeIfAbsent(match, SimplifyBooleansComparisonStrategy::extract);
+                                    final Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> next = details.computeIfAbsent(match, (m) -> extract(m, PhpTokenTypes.opAND));
                                     if (next != null) {
                                         final boolean compare = Stream.of(current.first.second, current.second.second, next.first.second, next.second.second).mapToInt(isInverted -> isInverted ? -1 : 1).sum() == 0;
-                                        if (compare && isCoveredBy(current.first, next) && isCoveredBy(current.second, next)) {
+                                        if (compare && isCoveredByOr(current.first, next) && isCoveredByOr(current.second, next)) {
                                             holder.registerProblem(
                                                     match,
                                                     String.format(
-                                                            MessagesPresentationUtil.prefixWithEa(messagePattern),
+                                                            MessagesPresentationUtil.prefixWithEa(messagePatternOr),
                                                             fragment.getText(),
                                                             match.getText(),
                                                             current.first.first.getText(),
@@ -69,34 +70,86 @@ final public class SimplifyBooleansComparisonStrategy {
                 }
                 fragments.clear();
             }
+        } else if (operator == PhpTokenTypes.opAND) {
+            final PsiElement parent  = expression.getParent();
+            final PsiElement context = parent instanceof ParenthesizedExpression ? parent.getParent() : parent;
+            if (!(context instanceof BinaryExpression) || ((BinaryExpression) context).getOperationType() != operator) {
+                final List<BinaryExpression> fragments = extractFragments(expression, operator);
+                if (fragments.size() > 1) {
+                    final Map<BinaryExpression, Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>>> details = new HashMap<>();
+                    for (final BinaryExpression fragment : fragments) {
+                        final Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> current = details.computeIfAbsent(fragment, (f) -> extract(f, PhpTokenTypes.opOR));
+                        if (current != null) {
+                            boolean reachedStartingPoint = false;
+                            for (final BinaryExpression match : fragments) {
+                                reachedStartingPoint = reachedStartingPoint || match == fragment;
+                                if (reachedStartingPoint && match != fragment) {
+                                    final Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> next = details.computeIfAbsent(match, (m) -> {
+                                        final Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> probe = extract(m, PhpTokenTypes.opNOT_IDENTICAL);
+                                        return probe == null ? extract(m, PhpTokenTypes.opNOT_EQUAL) : probe;
+                                    });
+                                    if (next != null) {
+                                        final boolean compare = Stream.of(current.first.second, current.second.second, next.first.second, next.second.second).mapToInt(isInverted -> isInverted ? 1 : 0).sum() == 1;
+                                        if (compare && isCoveredByAnd(current.first, next, false) && isCoveredByAnd(current.second, next, true)) {
+                                            holder.registerProblem(
+                                                    match,
+                                                    String.format(
+                                                            MessagesPresentationUtil.prefixWithEa(messagePatternAnd),
+                                                            fragment.getText(),
+                                                            match.getText(),
+                                                            current.first.first.getText(),
+                                                            current.second.first.getText()
+                                                    )
+                                            );
+                                            result = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                fragments.clear();
+            }
         }
         return result;
     }
 
-    private static boolean isCoveredBy(@NotNull Pair<PsiElement, Boolean> what, Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> byWhat) {
+    private static boolean isCoveredByOr(@NotNull Pair<PsiElement, Boolean> what, Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> byWhat) {
         return (what.second != byWhat.first.second && OpenapiEquivalenceUtil.areEqual(what.first, byWhat.first.first)) ||
                (what.second != byWhat.second.second && OpenapiEquivalenceUtil.areEqual(what.first, byWhat.second.first));
     }
 
+    private static boolean isCoveredByAnd(@NotNull Pair<PsiElement, Boolean> what, Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> byWhat, boolean expectedInversion) {
+        return (! what.second && byWhat.first.second == expectedInversion && OpenapiEquivalenceUtil.areEqual(what.first, byWhat.first.first)) ||
+                (! what.second && byWhat.second.second == expectedInversion && OpenapiEquivalenceUtil.areEqual(what.first, byWhat.second.first));
+    }
+
     @Nullable
-    private static Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> extract(@NotNull BinaryExpression source) {
+    private static Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> extract(@NotNull BinaryExpression source, @NotNull IElementType targetOperation) {
         Pair<Pair<PsiElement, Boolean>, Pair<PsiElement, Boolean>> result = null;
-        if (source.getOperationType() == PhpTokenTypes.opAND) {
+        final IElementType operation                                      = source.getOperationType();
+        if (operation == targetOperation) {
             final PsiElement left  = source.getLeftOperand();
             final PsiElement right = source.getRightOperand();
             if (left != null && right != null) {
-                final List<Pair<PsiElement, Boolean>> parts = Stream.of(left, right)
-                        .map(part -> {
-                            final PsiElement falsyProbe = extractFalsyValue(part);
-                            final PsiElement value      = falsyProbe != null ? falsyProbe : extractTruthyValue(part);
-                            return value == null ? null : new Pair<>(value, falsyProbe != null);
-                        })
-                        .filter(Objects::nonNull)
-                        .collect(Collectors.toList());
-                if (parts.size() == 2) {
-                    result = new Pair<>(parts.get(0), parts.get(1));
+                if (operation == PhpTokenTypes.opNOT_IDENTICAL || operation == PhpTokenTypes.opNOT_EQUAL) {
+                    /* pretend we are dealing with <left> === !<right> here */
+                    result = new Pair<>(new Pair<>(left, false), new Pair<>(right, true));
+                } else {
+                    final List<Pair<PsiElement, Boolean>> parts = Stream.of(left, right)
+                            .map(part -> {
+                                final PsiElement falsyProbe = extractFalsyValue(part);
+                                final PsiElement value      = falsyProbe != null ? falsyProbe : extractTruthyValue(part);
+                                return value == null ? null : new Pair<>(value, falsyProbe != null);
+                            })
+                            .filter(Objects::nonNull)
+                            .collect(Collectors.toList());
+                    if (parts.size() == 2) {
+                        result = new Pair<>(parts.get(0), parts.get(1));
+                    }
+                    parts.clear();
                 }
-                parts.clear();
             }
         }
         return result;
--- testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.simplify-boolean-comparison.php
+++ testData/fixtures/pitfalls/suspiciousBinaryOperations/suspicious-binary-operations.simplify-boolean-comparison.php
@@ -8,6 +8,11 @@ function cases_holder($x, $y, $z, $t) {
     if (($x === true && $y) || (<error descr="[EA] '($x === true && $y) || ($x === false && !$y)' is the same as '(bool) $x === (bool) $y', please review the conditions.">$x === false && !$y</error>)) {}
     if (($x !== false && $y) || (<error descr="[EA] '($x !== false && $y) || ($x !== true && !$y)' is the same as '(bool) $x === (bool) $y', please review the conditions.">$x !== true && !$y</error>)) {}
 
+    if (($x || $y) && (<error descr="[EA] '($x || $y) && ($x != $y)' is the same as '(bool) $x !== (bool) $y', please review the conditions.">$x != $y</error>)) {}
+    if (($x || $y) && (<error descr="[EA] '($x || $y) && ($x !== $y)' is the same as '(bool) $x !== (bool) $y', please review the conditions.">$x !== $y</error>)) {}
+    if (($x === true || $y) && (<error descr="[EA] '($x === true || $y) && ($x !== $y)' is the same as '(bool) $x !== (bool) $y', please review the conditions.">$x !== $y</error>)) {}
+    if (($x || $y === true) && (<error descr="[EA] '($x || $y === true) && ($x !== $y)' is the same as '(bool) $x !== (bool) $y', please review the conditions.">$x !== $y</error>)) {}
+
     /* false-positives: empty()-handling */
     if ((!empty($x) && $y) || ($x && !$y)) {}
     if (($x && $y) || (empty($x) && !$y)) {}

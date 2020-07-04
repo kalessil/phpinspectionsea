b42022423c062470a569710cbbb763ee170a5a6e
Vladimir Reznichenko <kalessil@gmail.com>
2020-07-04 14:57:20 +0200

    [#1571] UnknownInspectionInspector: resolved false-positives (multiple cases)

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/codeStyle/UnknownInspectionInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/codeStyle/UnknownInspectionInspectorTest.java
@@ -1,12 +1,11 @@
 package com.kalessil.phpStorm.phpInspectionsEA.codeStyle;
 
-import com.jetbrains.php.lang.inspections.probablyBug.missingParentCall.PhpMissingParentCallCommonInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.phpDoc.UnknownInspectionInspector;
 
 final public class UnknownInspectionInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
-        myFixture.enableInspections(new UnknownInspectionInspector(), new PhpMissingParentCallCommonInspection());
+        myFixture.enableInspections(new UnknownInspectionInspector());
         myFixture.configureByFile("testData/fixtures/codeStyle/unknown-inspection-suppression.php");
         myFixture.testHighlighting(true, false, true);
     }

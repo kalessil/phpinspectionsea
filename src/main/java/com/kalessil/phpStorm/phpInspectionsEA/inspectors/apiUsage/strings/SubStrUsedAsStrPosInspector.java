7ca69c968aefd803fceb5a02179e003415d816dd
Vladimir Reznichenko <kalessil@gmail.com>
2020-07-06 19:08:00 +0200

    [#1575] SubStrUsedAsStrPosInspector: dropped $string[0] === '?' pattern

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/strings/SubStrUsedAsStrPosInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/strings/SubStrUsedAsStrPosInspector.java
@@ -62,43 +62,7 @@ public class SubStrUsedAsStrPosInspector extends BasePhpInspection {
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new BasePhpElementVisitor() {
-            @Override
-            public void visitPhpArrayAccessExpression(@NotNull ArrayAccessExpression expression) {
-                final PsiElement parent = expression.getParent();
-                if (parent instanceof BinaryExpression) {
-                    final BinaryExpression binary = (BinaryExpression) parent;
-                    final IElementType operator   = binary.getOperationType();
-                    if (operator == PhpTokenTypes.opIDENTICAL || operator == PhpTokenTypes.opEQUAL) {
-                        final PsiElement container = expression.getValue();
-                        if (container instanceof PhpTypedElement) {
-                            final PsiElement literal = OpenapiElementsUtil.getSecondOperand(binary, expression);
-                            if (literal instanceof StringLiteralExpression) {
-                                final StringLiteralExpression fragment = (StringLiteralExpression) literal;
-                                if (fragment.getContents().length() == 1) {
-                                    final ArrayIndex index  = expression.getIndex();
-                                    final PsiElement offset = index == null ? null : index.getValue();
-                                    if (offset != null && offset.getText().equals("0")) {
-                                        final PhpType containerType = OpenapiResolveUtil.resolveType((PhpTypedElement) container, holder.getProject());
-                                        if (containerType != null) {
-                                            /* false-positives: container should be a string */
-                                            final boolean isString = containerType.filterUnknown().getTypes().stream().map(Types::getType).anyMatch(t -> t.equals(Types.strString));
-                                            if (isString) {
-                                                // TODO: respect Yoda/Classic style as per plugin configuration, immediate NS
-                                                final String replacement = String.format("strpos(%s, %s) === 0", container.getText(), literal.getText());
-                                                holder.registerProblem(
-                                                        parent,
-                                                        String.format(MessagesPresentationUtil.prefixWithEa(messagePattern), replacement),
-                                                        new UseStringSearchFix(replacement)
-                                                );
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            /* dropped pattern: '$string[0] === '?'' -> 'substr(...) === 0' */
 
             @Override
             public void visitPhpFunctionCall(@NotNull FunctionReference reference) {

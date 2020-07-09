b18f387ee88f395f106d34fbf818e002f7d8d43d
Vladimir Reznichenko <kalessil@gmail.com>
2020-07-09 15:43:10 +0200

    MissUsingForeachInspector: new test cases

--- testData/fixtures/foreach/misused-foreach.php
+++ testData/fixtures/foreach/misused-foreach.php
@@ -1,41 +1,65 @@
 <?php
 
-    <warning descr="[EA] Perhaps can be replaced with 'implode(...)' call (reduces cognitive load).">foreach</warning> ([] as $index => $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'implode(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
         $accumulator .= $value;
     }
-
-    <warning descr="[EA] Perhaps can be replaced with 'implode(...)' call (reduces cognitive load).">foreach</warning> ([] as $index => $value) {
-        $accumulator .= '...' . $value;
+    <warning descr="[EA] Perhaps can be replaced with 'implode(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
+        $accumulator .= $array[$index];
     }
 
-    <warning descr="[EA] Perhaps can be replaced with 'array_flip(...)' call (reduces cognitive load).">foreach</warning> ([] as $index => $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'implode(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
+        $accumulator .= '...' . $value;
+    }
+    <warning descr="[EA] Perhaps can be replaced with 'implode(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
+        $accumulator .= '...' . $array[$index];
+    }
+
+    <warning descr="[EA] Perhaps can be replaced with 'array_flip(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
         $array[$value] = $index;
     }
 
-    <warning descr="[EA] Perhaps can be replaced with 'array_sum(...)' call (reduces cognitive load).">foreach</warning> ([] as $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'array_sum(...)' call (reduces cognitive load).">foreach</warning> ($array as $value) {
+        $accumulator += $value;
+    }
+    <warning descr="[EA] Perhaps can be replaced with 'array_sum(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
         $accumulator += $value;
     }
 
-    <warning descr="[EA] Perhaps can be replaced with 'array_product(...)' call (reduces cognitive load).">foreach</warning> ([] as $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'array_product(...)' call (reduces cognitive load).">foreach</warning> ($array as $value) {
         $accumulator *= $value;
     }
+    <warning descr="[EA] Perhaps can be replaced with 'array_product(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
+        $accumulator *= $array[$index];
+    }
 
-    <warning descr="[EA] Perhaps can be replaced with 'array_map(...)' call (reduces cognitive load).">foreach</warning> ([] as $index => $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'array_map(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
         $accumulator[$index] = trim($value);
     }
+    <warning descr="[EA] Perhaps can be replaced with 'array_map(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
+        $accumulator[$index] = trim($array[$index]);
+    }
 
-    <warning descr="[EA] Perhaps can be replaced with 'array_column(...)' call (reduces cognitive load).">foreach</warning> ([] as $index => $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'array_column(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
         $accumulator []= $value['...'];
     }
-    <warning descr="[EA] Perhaps can be replaced with 'array_column(...)' call (reduces cognitive load).">foreach</warning> ([] as $index => $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'array_column(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
+        $accumulator []= $array[$index]['...'];
+    }
+    <warning descr="[EA] Perhaps can be replaced with 'array_column(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
         $accumulator []= $value->property;
     }
-    foreach ([] as $index => $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'array_column(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
+        $accumulator []= $array[$index]->property;
+    }
+    foreach ($array as $index => $value) {
         $accumulator[$index] = $value['...'];
     }
 
-    <warning descr="[EA] Perhaps can be replaced with 'array_map(...)' call (reduces cognitive load).">foreach</warning> ([] as $index => $value) {
+    <warning descr="[EA] Perhaps can be replaced with 'array_map(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
         $accumulator[$index] = (int) $value;
+    }
+    <warning descr="[EA] Perhaps can be replaced with 'array_map(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {
+        $accumulator[$index] = (int) $array[$index];
     }
 
     <warning descr="[EA] Perhaps can be replaced with 'array_filter(...)' call (reduces cognitive load).">foreach</warning> ($array as $index => $value) {

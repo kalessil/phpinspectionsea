0b30bce0acdc4660a40830e187ff329675baebf6
Vladimir Reznichenko <kalessil@gmail.com>
2020-07-10 13:22:29 +0200

    ClassConstantCanBeUsedInspector: bug-fixed test fixtures

--- testData/fixtures/lang/classConstant/configuration-class-reference.fixed.php
+++ testData/fixtures/lang/classConstant/configuration-class-reference.fixed.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use PhpInspections\EAExtended\Clazz;
 
+class LocalClass {}
+
 return [
     Clazz::class,
     Clazz::class,
-    class_alias('...', '\PhpInspections\EAExtended\Clazz'),
+    class_alias('...', '\LocalClass'),
 ];

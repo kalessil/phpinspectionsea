0bbbe936f6a7e664ce20d6ca8a4e77edd4277cd3
Vladimir Reznichenko <kalessil@gmail.com>
2020-07-13 21:25:04 +0200

    NullCoalescingOperatorCanBeUsedInspector: new patterns

--- testData/fixtures/lang/null-coalescing-operator.ifs.php
+++ testData/fixtures/lang/null-coalescing-operator.ifs.php
@@ -1,6 +1,6 @@
 <?php
 
-function cases_holder() {
+function cases_holder_isset_implicit() {
     $container = 'default';
     <weak_warning descr="[EA] '$container = $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
         $container = $value;
@@ -39,16 +39,8 @@ function cases_holder() {
     }
 
     /* false-positives: value or container mismatches */
-    if (isset($value)) {
-        $container = trim($value);
-    } else {
-        $container = 'default';
-    }
-    if (isset($value)) {
-        $value = $value;
-    } else {
-        $container = 'default';
-    }
+    if (isset($value)) { $container = trim($value); } else { $container = 'default'; }
+    if (isset($value)) { $value = $value; } else { $container = 'default'; }
 
     <weak_warning descr="[EA] 'return $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
         return $value;
@@ -57,25 +49,106 @@ function cases_holder() {
     }
 
     /* false-positives: value mismatches */
-    if (isset($value)) {
-        return trim($value);
-    } else {
-        return 'default';
-    }
+    if (isset($value)) { return trim($value); } else { return 'default'; }
 
     <weak_warning descr="[EA] 'return $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
         return $value;
     }
     return 'default';
 
-    /* false-positives: value mismatches */
-    if (isset($value)) {
-        return trim($value);
+    /* false-positives: value mismatches, multi-assignments (refactoring changes semantics) */
+    if (isset($value)) { return trim($value); } return 'default';
+    $one = $two = 'default'; if (isset($value)) { $one = $value; }
+}
+
+function cases_holder_isset_non_implicit() {
+    return [
+        function ($value) {
+            <weak_warning descr="[EA] 'return $value ?? null' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
+                return $value;
+            }
+        },
+        function ($value) {
+            <weak_warning descr="[EA] 'return $value ?? null' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
+                return $value;
+            }
+            return;
+        },
+    ];
+}
+
+function cases_holder_identity() {
+    $container = 'default';
+    <weak_warning descr="[EA] '$container = $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> ($value !== null) {
+        $container = $value;
+    }
+    $container = 'default';
+    <weak_warning descr="[EA] '$container = $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> (!($value === null)) {
+        $container = $value;
+    }
+
+    /* false-positive: inverted logic */
+    $container = 'default'; if ($value === null) { $container = $value; }
+
+    <weak_warning descr="[EA] '$container = $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> ($value !== null) {
+        $container = $value;
+    } else {
+        $container = 'default';
+    }
+
+    /* false-positive: mismatched value */
+    if ($value !== null) { $container = trim($value); } else { $container = 'default'; }
+
+    <weak_warning descr="[EA] 'return $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> ($value !== null) {
+        return $value;
+    } else {
+        return 'default';
+    }
+
+    /* false-positive: mismatched value */
+    if ($value !== null) { return trim($value); } else { return 'default'; }
+
+    <weak_warning descr="[EA] 'return $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> ($value !== null) {
+        return $value;
     }
     return 'default';
 
-    $one = $two = 'default';
-    <weak_warning descr="[EA] '$one = $value ?? 'default'' can be used instead (reduces cognitive load).">if</weak_warning> (isset($value)) {
-        $one = $value;
+    /* false-positive: mismatched value */
+    if ($value !== null) { return trim($value); } return 'default';
+}
+
+function cases_holder_array_key_exists() {
+    $container = null;
+    <weak_warning descr="[EA] '$container = $array[$key] ?? null' can be used instead (reduces cognitive load).">if</weak_warning> (array_key_exists($key, $array)) {
+        $container = $array[$key];
     }
+
+    /* false-positive: no-null alternative */
+    $container = 'default'; if (array_key_exists($key, $array)) { $container = $array[$key]; }
+
+    <weak_warning descr="[EA] '$container = $array[$key] ?? null' can be used instead (reduces cognitive load).">if</weak_warning> (array_key_exists($key, $array)) {
+        $container = $array[$key];
+    } else {
+        $container = null;
+    }
+
+    /* false-positive: mismatched value */
+    if (array_key_exists($key, $array)) { $container = trim($array[$key]); } else { $container = null; }
+
+    <weak_warning descr="[EA] 'return $array[$key] ?? null' can be used instead (reduces cognitive load).">if</weak_warning> (array_key_exists($key, $array)) {
+        return $array[$key];
+    } else {
+        return null;
+    }
+
+    /* false-positive: mismatched value */
+    if (array_key_exists($key, $array)) { return trim($array[$key]); } else { return null; }
+
+    <weak_warning descr="[EA] 'return $array[$key] ?? null' can be used instead (reduces cognitive load).">if</weak_warning> (array_key_exists($key, $array)) {
+        return $array[$key];
+    }
+    return null;
+
+    /* false-positive: mismatched value */
+    if (array_key_exists($key, $array)) { return trim($array[$key]); } return null;
 }
--- testData/fixtures/lang/null-coalescing-operator.php
+++ testData/fixtures/lang/null-coalescing-operator.php
@@ -18,6 +18,14 @@
     echo <weak_warning descr="[EA] 'stdClass::$test ?? 'test'' can be used instead (reduces cognitive load).">isset(stdClass::$test) ? stdClass::$test : 'test'</weak_warning>;
     echo <weak_warning descr="[EA] '$classname::$test ?? 'test'' can be used instead (reduces cognitive load).">isset($classname::$test) ? $classname::$test : 'test'</weak_warning>;
 
+    /* pattern: not-empty ? property-reference : alternative */
+    echo <weak_warning descr="[EA] '$object->property ?? null' can be used instead (reduces cognitive load).">$object ? $object->property : null</weak_warning>;
+    echo <weak_warning descr="[EA] '$array[$index]->property ?? null' can be used instead (reduces cognitive load).">$array[$index] ? $array[$index]->property : null</weak_warning>;
+    echo <weak_warning descr="[EA] '$object->property->property ?? null' can be used instead (reduces cognitive load).">$object->property ? $object->property->property : null</weak_warning>;
+    echo <weak_warning descr="[EA] '$object->property ?? null' can be used instead (reduces cognitive load).">!empty($object) ? $object->property : null</weak_warning>;
+    echo <weak_warning descr="[EA] '$array[$index]->property ?? null' can be used instead (reduces cognitive load).">!empty($array[$index]) ? $array[$index]->property : null</weak_warning>;
+    echo <weak_warning descr="[EA] '$object->property->property ?? null' can be used instead (reduces cognitive load).">!empty($object->property) ? $object->property->property : null</weak_warning>;
+
     /* false-positives */
     echo array_key_exists(0, $x) ? $x[0] : 'default';
     echo isset($x[0], $s[1]) ? $x[0] : 'default';

bf9b546a52a4fb84219912671c524ff488670668
Vladimir Reznichenko <kalessil@gmail.com>
2020-08-03 16:01:53 +0200

    [#1597] UnknownInspectionInspector: new test cases

--- testData/fixtures/codeStyle/unknown-inspection-suppression.php
+++ testData/fixtures/codeStyle/unknown-inspection-suppression.php
@@ -7,5 +7,7 @@
     /** <weak_warning descr="[EA] Unknown inspection: UsingInclusionReturnValueInspectXXX.">@noinspection</weak_warning> UsingInclusionReturnValueInspectXXX - some comments */
 
     /* false-positives */
-    /*  @noinspection UnknownInspectionInspection PhpMissingParentCallCommonInspection */
-    /** @noinspection UnknownInspectionInspection PhpMissingParentCallCommonInspection */
+    /*  @noinspection UnknownInspectionInspection */
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /** @noinspection PhpUnused */
+    /** @noinspection SqlResolve */

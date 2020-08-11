f012c53e5108099b5c0db8280cefd3d8a214dff7
Vladimir Reznichenko <kalessil@gmail.com>
2020-08-11 09:35:24 +0200

    ElvisOperatorCanBeUsedInspector: re-worke reporting

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/ElvisOperatorCanBeUsedInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/languageConstructions/ElvisOperatorCanBeUsedInspector.java
@@ -1,13 +1,10 @@
 package com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiWhiteSpace;
 import com.jetbrains.php.lang.psi.elements.TernaryExpression;
+import com.kalessil.phpStorm.phpInspectionsEA.fixers.UseSuggestedReplacementFixer;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
 import com.kalessil.phpStorm.phpInspectionsEA.utils.ExpressionSemanticUtil;
@@ -25,7 +22,7 @@ import org.jetbrains.annotations.NotNull;
  */
 
 public class ElvisOperatorCanBeUsedInspector extends BasePhpInspection {
-    private static final String message = "'... ?: ...' should be used instead.";
+    private static final String messagePattern = "It's possible to use '%s' here (shorter notation).";
 
     @NotNull
     @Override
@@ -51,10 +48,15 @@ public class ElvisOperatorCanBeUsedInspector extends BasePhpInspection {
                     if (condition != null && trueVariant != null) {
                         final PsiElement falseVariant = ExpressionSemanticUtil.getExpressionTroughParenthesis(expression.getFalseVariant());
                         if (falseVariant != null && OpenapiEquivalenceUtil.areEqual(condition, trueVariant)) {
+                            final String replacement = String.format(
+                                    "%s ?: %s",
+                                    trueVariant.getText(),
+                                    falseVariant.getText()
+                            );
                             holder.registerProblem(
-                                    expression.getTrueVariant(),
-                                    MessagesPresentationUtil.prefixWithEa(message),
-                                    new UseElvisOperatorFix()
+                                    expression,
+                                    MessagesPresentationUtil.prefixWithEa(String.format(messagePattern, replacement)),
+                                    new UseElvisOperatorFix(replacement)
                             );
                         }
                     }
@@ -63,8 +65,8 @@ public class ElvisOperatorCanBeUsedInspector extends BasePhpInspection {
         };
     }
 
-    private static final class UseElvisOperatorFix implements LocalQuickFix {
-        private static final String title = "Use ?: instead";
+    private static final class UseElvisOperatorFix extends UseSuggestedReplacementFixer {
+        private static final String title = "Use '... ?: ...' instead";
 
         @NotNull
         @Override
@@ -72,28 +74,8 @@ public class ElvisOperatorCanBeUsedInspector extends BasePhpInspection {
             return MessagesPresentationUtil.prefixWithEa(title);
         }
 
-        @NotNull
-        @Override
-        public String getFamilyName() {
-            return getName();
-        }
-
-        @Override
-        public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-            final PsiElement target = descriptor.getPsiElement();
-            if (target != null && !project.isDisposed()) {
-                /* cleanup spaces around */
-                PsiElement before = target.getPrevSibling();
-                if (before instanceof PsiWhiteSpace) {
-                    before.delete();
-                }
-                PsiElement after = target.getNextSibling();
-                if (after instanceof PsiWhiteSpace) {
-                    after.delete();
-                }
-                /* drop true expression */
-                target.delete();
-            }
+        UseElvisOperatorFix(@NotNull String expression) {
+            super(expression);
         }
     }
 }

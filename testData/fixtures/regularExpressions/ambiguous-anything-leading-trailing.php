f6604fc3e1d61fb7156a8afb753121437868e115
EA Inspections Team <ea.inspections.team@gmail.com>
2021-06-08 19:51:55 +0200

    NotOptimalRegularExpressionsInspector: fixed failing tests

--- testData/fixtures/regularExpressions/ambiguous-anything-leading-trailing.php
+++ testData/fixtures/regularExpressions/ambiguous-anything-leading-trailing.php
@@ -1,8 +1,8 @@
 <?php
 
-    preg_match(<weak_warning descr="[EA] Leading .* can be probably removed.">'/.*-report/'</weak_warning>, '');
+    preg_match(<weak_warning descr="[EA] Leading .* can probably be removed.">'/.*-report/'</weak_warning>, '');
     preg_match('/.*-report/', '', $m);
-    preg_match(<weak_warning descr="[EA] Trailing .* can be probably removed.">'/report-.*/'</weak_warning>, '');
+    preg_match(<weak_warning descr="[EA] Trailing .* can probably be removed.">'/report-.*/'</weak_warning>, '');
     preg_replace('/report-.*/', '', '');
     preg_match('/report-.*/', '', $m);
     preg_replace('/report-.*/', '', '');

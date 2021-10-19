be0cc131c46f91f4da7f8d81d6360c0a1753d1ac
Claas Augner <github@caugner.de>
2021-10-19 18:08:06 +0200

    fix(phpunit): prefer assert{File,Directory}DoesNotExist
    See: https://github.com/sebastianbergmann/phpunit/issues/4077

--- testData/fixtures/phpUnit/assert-resource-exists.php
+++ testData/fixtures/phpUnit/assert-resource-exists.php
@@ -6,25 +6,25 @@ class TestAssertResourceExists
     {
         <weak_warning descr="[EA] 'assertFileExists(...)' would fit more here.">$this->assertTrue(file_exists('...'))</weak_warning>;
         <weak_warning descr="[EA] 'assertFileExists(...)' would fit more here.">$this->assertNotFalse(file_exists('...'))</weak_warning>;
-        <weak_warning descr="[EA] 'assertFileNotExists(...)' would fit more here.">$this->assertFalse(file_exists('...'))</weak_warning>;
-        <weak_warning descr="[EA] 'assertFileNotExists(...)' would fit more here.">$this->assertNotTrue(file_exists('...'))</weak_warning>;
+        <weak_warning descr="[EA] 'assertFileDoesNotExist(...)' would fit more here.">$this->assertFalse(file_exists('...'))</weak_warning>;
+        <weak_warning descr="[EA] 'assertFileDoesNotExist(...)' would fit more here.">$this->assertNotTrue(file_exists('...'))</weak_warning>;
 
         <weak_warning descr="[EA] 'assertDirectoryExists(...)' would fit more here.">$this->assertTrue(is_dir('...'))</weak_warning>;
         <weak_warning descr="[EA] 'assertDirectoryExists(...)' would fit more here.">$this->assertNotFalse(is_dir('...'))</weak_warning>;
-        <weak_warning descr="[EA] 'assertDirectoryNotExists(...)' would fit more here.">$this->assertFalse(is_dir('...'))</weak_warning>;
-        <weak_warning descr="[EA] 'assertDirectoryNotExists(...)' would fit more here.">$this->assertNotTrue(is_dir('...'))</weak_warning>;
+        <weak_warning descr="[EA] 'assertDirectoryDoesNotExist(...)' would fit more here.">$this->assertFalse(is_dir('...'))</weak_warning>;
+        <weak_warning descr="[EA] 'assertDirectoryDoesNotExist(...)' would fit more here.">$this->assertNotTrue(is_dir('...'))</weak_warning>;
     }
 
     public function testWithMessages()
     {
         <weak_warning descr="[EA] 'assertFileExists(...)' would fit more here.">$this->assertTrue(file_exists('...'), '')</weak_warning>;
         <weak_warning descr="[EA] 'assertFileExists(...)' would fit more here.">$this->assertNotFalse(file_exists('...'), '')</weak_warning>;
-        <weak_warning descr="[EA] 'assertFileNotExists(...)' would fit more here.">$this->assertFalse(file_exists('...'), '')</weak_warning>;
-        <weak_warning descr="[EA] 'assertFileNotExists(...)' would fit more here.">$this->assertNotTrue(file_exists('...'), '')</weak_warning>;
+        <weak_warning descr="[EA] 'assertFileDoesNotExist(...)' would fit more here.">$this->assertFalse(file_exists('...'), '')</weak_warning>;
+        <weak_warning descr="[EA] 'assertFileDoesNotExist(...)' would fit more here.">$this->assertNotTrue(file_exists('...'), '')</weak_warning>;
 
         <weak_warning descr="[EA] 'assertDirectoryExists(...)' would fit more here.">$this->assertTrue(is_dir('...'), '')</weak_warning>;
         <weak_warning descr="[EA] 'assertDirectoryExists(...)' would fit more here.">$this->assertNotFalse(is_dir('...'), '')</weak_warning>;
-        <weak_warning descr="[EA] 'assertDirectoryNotExists(...)' would fit more here.">$this->assertFalse(is_dir('...'), '')</weak_warning>;
-        <weak_warning descr="[EA] 'assertDirectoryNotExists(...)' would fit more here.">$this->assertNotTrue(is_dir('...'), '')</weak_warning>;
+        <weak_warning descr="[EA] 'assertDirectoryDoesNotExist(...)' would fit more here.">$this->assertFalse(is_dir('...'), '')</weak_warning>;
+        <weak_warning descr="[EA] 'assertDirectoryDoesNotExist(...)' would fit more here.">$this->assertNotTrue(is_dir('...'), '')</weak_warning>;
     }
 }

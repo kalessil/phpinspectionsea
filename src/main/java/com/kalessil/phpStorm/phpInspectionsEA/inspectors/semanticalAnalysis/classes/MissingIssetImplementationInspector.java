c3cf834cb67ec4e50ccc5966eecc3253ca65073a
EA Inspections Team <ea.inspections.team@gmail.com>
2021-11-20 20:52:49 +0100

    MissingIssetImplementationInspector: fixed failing tests

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/classes/MissingIssetImplementationInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/semanticalAnalysis/classes/MissingIssetImplementationInspector.java
@@ -4,18 +4,15 @@ import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpElementVisitor;
 import com.kalessil.phpStorm.phpInspectionsEA.openApi.BasePhpInspection;
-import com.kalessil.phpStorm.phpInspectionsEA.utils.MessagesPresentationUtil;
-import com.kalessil.phpStorm.phpInspectionsEA.utils.OpenapiResolveUtil;
-import com.kalessil.phpStorm.phpInspectionsEA.utils.Types;
+import com.kalessil.phpStorm.phpInspectionsEA.utils.*;
 import org.jetbrains.annotations.NotNull;
-import org.mozilla.javascript.ast.AstNode;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -79,6 +76,7 @@ public class MissingIssetImplementationInspector extends BasePhpInspection {
                         if (
                             (nameNode == null || nameNode instanceof Variable) ||
                             (parameterName == null || parameterName.isEmpty()) ||
+                            ! OpenapiTypesUtil.is(OpenapiPsiSearchUtil.findResolutionOperator(reference), PhpTokenTypes.ARROW) ||
                             OpenapiResolveUtil.resolveReference(reference) != null
                         ) {
                             continue;
@@ -98,13 +96,13 @@ public class MissingIssetImplementationInspector extends BasePhpInspection {
                         }
                         for (final String type : resolvedTypes) {
                             /* false-positives: SimpleXMLElement, stdClass */
-                            if (type.startsWith("\\") && !magicClasses.contains(type)) {
+                            if (type.startsWith("\\") && ! magicClasses.contains(type)) {
                                 final Collection<PhpClass> classes = projectIndex.getClassesByFQN(type);
                                 final PhpClass clazz               = classes.isEmpty() ? null : classes.iterator().next();
                                 /* resolved class FQN might differ from what type states */
-                                if (clazz != null && !magicClasses.contains(clazz.getFQN())) {
+                                if (clazz != null && ! magicClasses.contains(clazz.getFQN())) {
                                     final boolean hasField = OpenapiResolveUtil.resolveField(clazz, parameterName) != null;
-                                    if (!hasField && OpenapiResolveUtil.resolveMethod(clazz, "__isset") == null) {
+                                    if (! hasField && OpenapiResolveUtil.resolveMethod(clazz, "__isset") == null) {
                                         holder.registerProblem(
                                                 parameter,
                                                 MessagesPresentationUtil.prefixWithEa(messagePattern.replace("%c%", type)),

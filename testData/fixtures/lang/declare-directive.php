9c45da334a274869e2494eab27b167eedfe4dddf
EA Inspections Team <ea.inspections.team@gmail.com>
2021-11-21 18:21:48 +0100

    DeclareDirectiveCorrectnessInspector: dropped (Declare directive correctness)

--- testData/fixtures/lang/declare-directive.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    declare( strict_types = 1 );
-    declare( ticks=1 );
-    declare(encoding='ISO-8859-1');
-
-    <error descr="[EA] Unknown directive 'unknown'.">declare(unknown=1);</error>

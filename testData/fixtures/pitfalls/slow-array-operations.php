32ff91a14e31d4ccf915abeb0bf2e5b5af20c792
EA Inspections Team <ea.inspections.team@gmail.com>
2021-11-21 13:30:00 +0100

    Fixes some failing tests in PS 2020+

--- testData/fixtures/pitfalls/slow-array-operations.php
+++ testData/fixtures/pitfalls/slow-array-operations.php
@@ -6,9 +6,12 @@ function cases_holder(){
         $result = <error descr="[EA] 'array_merge_recursive(...)' is used in a loop and is a resources greedy construction.">array_merge_recursive($result, $collection)</error>;
         $result = <error descr="[EA] 'array_replace(...)' is used in a loop and is a resources greedy construction.">array_replace($result, $collection)</error>;
         $result = <error descr="[EA] 'array_replace_recursive(...)' is used in a loop and is a resources greedy construction.">array_replace_recursive($result, $collection)</error>;
+    }
+}
 
-        /* false-positives: multiple targets patching */
-        $result['...'] = array_merge($result['...'], $collection);
+function false_positives_holder() {
+    foreach ([] as $index => $collection) {
+        $result[$index]['breakdown'] = array_merge($result[$index]['breakdown'], $collection);
     }
 
     $result = array_merge($result, $collection);

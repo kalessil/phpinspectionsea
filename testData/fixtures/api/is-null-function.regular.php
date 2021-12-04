0d071e731c6b586c549d8a2b7cc875d66a4008f2
EA Inspections Team <ea.inspections.team@gmail.com>
2021-12-04 20:35:57 +0100

    IsNullFunctionUsageInspector: set "Weak warning" as default level

--- testData/fixtures/api/is-null-function.regular.php
+++ testData/fixtures/api/is-null-function.regular.php
@@ -1,23 +1,23 @@
 <?php
 
-    echo true  && <warning descr="[EA] '$x === null' construction should be used instead.">is_null($x)</warning>;
-    echo false || <warning descr="[EA] '$x === null' construction should be used instead.">is_null($x)</warning>;
+    echo true  && <weak_warning descr="[EA] '$x === null' construction should be used instead.">is_null($x)</weak_warning>;
+    echo false || <weak_warning descr="[EA] '$x === null' construction should be used instead.">is_null($x)</weak_warning>;
 
-    echo <warning descr="[EA] '$x === null' construction should be used instead.">is_null($x)</warning>;
-    echo <warning descr="[EA] '$x === null' construction should be used instead.">true  == is_null($x)</warning>;
-    echo <warning descr="[EA] '$x === null' construction should be used instead.">false != is_null($x)</warning>;
-    echo <warning descr="[EA] '$x === null' construction should be used instead.">is_null($x) === true</warning>;
-    echo <warning descr="[EA] '$x === null' construction should be used instead.">is_null($x) !== false</warning>;
+    echo <weak_warning descr="[EA] '$x === null' construction should be used instead.">is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] '$x === null' construction should be used instead.">true  == is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] '$x === null' construction should be used instead.">false != is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] '$x === null' construction should be used instead.">is_null($x) === true</weak_warning>;
+    echo <weak_warning descr="[EA] '$x === null' construction should be used instead.">is_null($x) !== false</weak_warning>;
 
-    echo <warning descr="[EA] '$x !== null' construction should be used instead.">!is_null($x)</warning>;
-    echo <warning descr="[EA] '$x !== null' construction should be used instead.">false == is_null($x)</warning>;
-    echo <warning descr="[EA] '$x !== null' construction should be used instead.">true  != is_null($x)</warning>;
-    echo <warning descr="[EA] '$x !== null' construction should be used instead.">is_null($x) === false</warning>;
-    echo <warning descr="[EA] '$x !== null' construction should be used instead.">is_null($x) !== true</warning>;
+    echo <weak_warning descr="[EA] '$x !== null' construction should be used instead.">!is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] '$x !== null' construction should be used instead.">false == is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] '$x !== null' construction should be used instead.">true  != is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] '$x !== null' construction should be used instead.">is_null($x) === false</weak_warning>;
+    echo <weak_warning descr="[EA] '$x !== null' construction should be used instead.">is_null($x) !== true</weak_warning>;
 
-    echo <warning descr="[EA] '($x = null) !== null' construction should be used instead.">is_null($x = null) !== true</warning>;
-    echo <warning descr="[EA] '($x ?: null) !== null' construction should be used instead.">!is_null($x ?: null)</warning>;
-    echo <warning descr="[EA] '($x ?? null) !== null' construction should be used instead.">!is_null($x ?? null)</warning>;
-    echo <warning descr="[EA] '($x ? null : null) !== null' construction should be used instead.">!is_null($x ? null : null)</warning>;
+    echo <weak_warning descr="[EA] '($x = null) !== null' construction should be used instead.">is_null($x = null) !== true</weak_warning>;
+    echo <weak_warning descr="[EA] '($x ?: null) !== null' construction should be used instead.">!is_null($x ?: null)</weak_warning>;
+    echo <weak_warning descr="[EA] '($x ?? null) !== null' construction should be used instead.">!is_null($x ?? null)</weak_warning>;
+    echo <weak_warning descr="[EA] '($x ? null : null) !== null' construction should be used instead.">!is_null($x ? null : null)</weak_warning>;
 
     echo is_null();
--- testData/fixtures/api/is-null-function.yoda.php
+++ testData/fixtures/api/is-null-function.yoda.php
@@ -1,20 +1,20 @@
 <?php
 
-    echo true  && <warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x)</warning>;
-    echo false || <warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x)</warning>;
+    echo true  && <weak_warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x)</weak_warning>;
+    echo false || <weak_warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x)</weak_warning>;
 
-    echo <warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x)</warning>;
-    echo <warning descr="[EA] 'null === $x' construction should be used instead.">true  == is_null($x)</warning>;
-    echo <warning descr="[EA] 'null === $x' construction should be used instead.">false != is_null($x)</warning>;
-    echo <warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x) === true</warning>;
-    echo <warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x) !== false</warning>;
+    echo <weak_warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] 'null === $x' construction should be used instead.">true  == is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] 'null === $x' construction should be used instead.">false != is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x) === true</weak_warning>;
+    echo <weak_warning descr="[EA] 'null === $x' construction should be used instead.">is_null($x) !== false</weak_warning>;
 
-    echo <warning descr="[EA] 'null !== $x' construction should be used instead.">!is_null($x)</warning>;
-    echo <warning descr="[EA] 'null !== $x' construction should be used instead.">false == is_null($x)</warning>;
-    echo <warning descr="[EA] 'null !== $x' construction should be used instead.">true  != is_null($x)</warning>;
-    echo <warning descr="[EA] 'null !== $x' construction should be used instead.">is_null($x) === false</warning>;
-    echo <warning descr="[EA] 'null !== $x' construction should be used instead.">is_null($x) !== true</warning>;
+    echo <weak_warning descr="[EA] 'null !== $x' construction should be used instead.">!is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] 'null !== $x' construction should be used instead.">false == is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] 'null !== $x' construction should be used instead.">true  != is_null($x)</weak_warning>;
+    echo <weak_warning descr="[EA] 'null !== $x' construction should be used instead.">is_null($x) === false</weak_warning>;
+    echo <weak_warning descr="[EA] 'null !== $x' construction should be used instead.">is_null($x) !== true</weak_warning>;
 
-    echo <warning descr="[EA] 'null !== ($x = null)' construction should be used instead.">is_null($x = null) !== true</warning>;
+    echo <weak_warning descr="[EA] 'null !== ($x = null)' construction should be used instead.">is_null($x = null) !== true</weak_warning>;
 
     echo is_null();

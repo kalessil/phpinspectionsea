ad825e67b2ebc5f59dcff3c6da55d80910c10466
l0gic <l0gic@ya.ru>
2024-04-29 20:15:23 +0500

    Buggregator functions as forgotten debug output

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/debug/ForgottenDebugOutputInspector.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/inspectors/apiUsage/debug/ForgottenDebugOutputInspector.java
@@ -115,6 +115,9 @@ public class ForgottenDebugOutputInspector extends BasePhpInspection {
         list.add("\\Illuminate\\Support\\Debug\\Dumper::dump");
         list.add("dd");
         list.add("dump");
+        /* Buggregator related functions */
+        list.add("\\Buggregator\\Trap\\Client\\TrapHandle::fromArray");
+        list.add("trap");
         /* Core-php debug functions */
         list.add("debug_print_backtrace");
         list.add("debug_zval_dump");

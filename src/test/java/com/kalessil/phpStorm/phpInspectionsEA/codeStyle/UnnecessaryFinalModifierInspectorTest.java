6def701257ac812012dc5d08aab9ed523c3f5b44
EA Inspections Team <ea.inspections.team@gmail.com>
2025-10-07 09:59:14 +0200

    [#1970] UnnecessaryFinalModifierInspector: new test cases (property hooks)

--- src/test/java/com/kalessil/phpStorm/phpInspectionsEA/codeStyle/UnnecessaryFinalModifierInspectorTest.java
+++ src/test/java/com/kalessil/phpStorm/phpInspectionsEA/codeStyle/UnnecessaryFinalModifierInspectorTest.java
@@ -1,7 +1,10 @@
 package com.kalessil.phpStorm.phpInspectionsEA.codeStyle;
 
+import com.jetbrains.php.config.PhpLanguageLevel;
+import com.jetbrains.php.config.PhpProjectConfigurationFacade;
 import com.kalessil.phpStorm.phpInspectionsEA.PhpCodeInsightFixtureTestCase;
 import com.kalessil.phpStorm.phpInspectionsEA.inspectors.codeStyle.UnnecessaryFinalModifierInspector;
+import com.kalessil.phpStorm.phpInspectionsEA.inspectors.languageConstructions.ReturnTypeCanBeDeclaredInspector;
 
 final public class UnnecessaryFinalModifierInspectorTest extends PhpCodeInsightFixtureTestCase {
     public void testIfFindsAllPatterns() {
@@ -12,5 +15,12 @@ final public class UnnecessaryFinalModifierInspectorTest extends PhpCodeInsightF
         myFixture.getAllQuickFixes().forEach(fix -> myFixture.launchAction(fix));
         myFixture.setTestDataPath(".");
         myFixture.checkResultByFile("testData/fixtures/codeStyle/unnecessary-final-modifier.fixed.php");
+    }
+
+    public void testPropertyHooks() {
+        PhpProjectConfigurationFacade.getInstance(myFixture.getProject()).setLanguageLevel(PhpLanguageLevel.PHP840);
+        myFixture.enableInspections(new ReturnTypeCanBeDeclaredInspector());
+        myFixture.configureByFile("testData/fixtures/codeStyle/unnecessary-final-modifier.property-hooks.php");
+        myFixture.testHighlighting(true, false, true);
     }
 }
---
+++ testData/fixtures/codeStyle/unnecessary-final-modifier.property-hooks.php
@@ -0,0 +1,8 @@
+<?php
+
+class ClassWithPropertyHooks {
+    public bool $property {
+        get => true;
+        set => true;
+    }
+}
